7333bdfb99d6f8da3c4f05b49595722072bf0329
Jan Minar <rdancer@rdancer.org>
2023-01-27 14:19:55 +0000

    save solution permanently when it's accepted and improve solving code
    * fixes: crashes when solution is accepted
    * fixes: when solution is accepted, it saves it to solutions/<problem_number>/<language_name>.<language_file_extension>
    * fixes: sometimes whole page is selected (and saved to save file) instead of just the editor contents
    * refactor the functions external to the Solver module into Solver module private methods (solver.py)

--- main.py
+++ main.py
@@ -5,8 +5,16 @@ import os
 import json
 from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
+import argparse
 from solver import Solver
 
+parser = argparse.ArgumentParser()
+parser.add_argument('problem_url', nargs='?', default=None, help='The URL of the problem to solve (optional)')
+# headless = False by default
+parser.add_argument('--headless', action='store_true', help='Run the browser in headless mode')
+args = parser.parse_args()
+
+print("args problem_url: " + str(args.problem_url))
 
 def get_login_data():
     # allow dereferencing the $HOME file as ~
@@ -33,24 +41,28 @@ def login(page):
 
 # Log in to Leetcode
 playwright = sync_playwright().start()
-browser = playwright.chromium.launch(headless=False)
+browser = playwright.chromium.launch(headless=True if args.headless else False)
 page = browser.new_page()
 # Leetcode rate-limits us severely and the default 30s timeout is not enough
 page.set_default_timeout(120_000)
 
 login(page)
 
-# Get today's problem from the problemset/all page
-todays_problem = 'div.grid.grid-cols-7.text-xs.text-label-3 > a[href]'
-page.goto('https://leetcode.com/problemset/all/')
-page.wait_for_selector(todays_problem)
-soup = BeautifulSoup(page.content(), 'html.parser')
+if args.problem_url:
+    problem_url = args.problem_url
+else:
+    # Get today's problem from the problemset/all page
+    todays_problem = 'div.grid.grid-cols-7.text-xs.text-label-3 > a[href]'
+    page.goto('https://leetcode.com/problemset/all/')
+    page.wait_for_selector(todays_problem)
+    soup = BeautifulSoup(page.content(), 'html.parser')
 
-# Find the problem's URL using Beautiful Soup
-# The last element selected is the problem for today (because all the remaining problems have no href attribute (yet))
-relative_path = soup.select(todays_problem)[-1]['href']
-problem_url = 'https://leetcode.com' + relative_path
+    # Find the problem's URL using Beautiful Soup
+    # The last element selected is the problem for today (because all the remaining problems have no href attribute (yet))
+    relative_path = soup.select(todays_problem)[-1]['href']
+    problem_url = 'https://leetcode.com' + relative_path
 
+print("Solving problem: " + problem_url)
 # Solve the problem
 solver = Solver(browser)
 solver.solve(page, problem_url)
--- solver.py
+++ solver.py
@@ -1,99 +1,118 @@
 from bs4 import BeautifulSoup
 import pyperclip
 import time
+import os
 
-PLATFORM='darwin'
-SAVE_FILE='solution.txt'
-
-def click_over_element(selector, page)  :
-        page.wait_for_selector(selector)
-
-        # Get the element's x and y coordinates
-        x = page.evaluate("document.querySelector('" + selector + "').getBoundingClientRect().x")
-        y = page.evaluate("document.querySelector('" + selector + "').getBoundingClientRect().y")
-
-        # Add some pixels to the x and y coordinates, so that we click inside the element
-        x += 50
-        y += 50
-
-        # Perform the click
-        page.mouse.click(x, y)
-
-def select_all_text(page):
-    """We're focused inside an editable element such as a textarea or an input field. We press Control-A or Command-A to select all text in the element."""
-    # If we're on mac, the command key is used instead of control
-    if PLATFORM == 'darwin':
-        modifier = 'Meta'
-    else:
-        modifier = 'Control'
-    page.keyboard.down(modifier)
-    page.keyboard.press('KeyA')
-    page.keyboard.up(modifier)
-
-def type_text(page, text):
-    """
-    We're focused inside an editable element such as a textarea or an input field. We type the given text into the element.
-
-    Our problem is that the editor has a (bad) autocompletion feature for matching parentheses, quotes, etc. This feature cannot be disabled, so we must work around it.
-    """
-    # Copy the text to the clipboard
-    pyperclip.copy(text)
-    # Paste the text
-    paste(page)
-    # Clear the clipboard
-    pyperclip.copy("")
-
-def paste(page):
-    page.keyboard.down('Meta')
-    page.keyboard.press('v')
-    page.keyboard.up('Meta')
-
-def copy(page):
-    page.keyboard.down('Meta')
-    page.keyboard.press('c')
-    page.keyboard.up('Meta')
-
-def init_template(page):
-    """
-    Initializes the code editor with a template.
-
-    We need to keep the existing function signature, so we just add the code that we need to the existing code.
-    """
-    # If save file exists, load it instead of the template
-    try:
-        with open(SAVE_FILE, 'r') as f:
-            template = f.read()
-            select_all_text(page)
-            type_text(page, template)
-            return
-    except FileNotFoundError:
-        pass
-
-    select_all_text(page)
-
-    # Insert before everything else
-    page.keyboard.press('ArrowLeft')
-    type_text(page, "testNumber = 0;")
-    page.keyboard.press('Enter')
-
-
-    # Insert inside the existing function definition
-    # Note: sometimes we have more than one function definition (because helper classes), so we just go to the very bottom and then arrow up, to get inside the last function definition.
-    select_all_text(page)
-    page.keyboard.press('ArrowDown')
-    page.keyboard.press('ArrowUp')
-    type_text(page, "return [")
-    page.keyboard.press('Enter')
-    type_text(page, "][testNumber++];")
-
+PLATFORM = 'darwin'
+SAVE_FILE = 'solution.txt'
+SOLUTIONS_DIR = 'solutions'
+TEST_PASS_SELECTOR = '.ml-auto.text-xs.text-label-3'
+SOLUTION_NOT_ACCEPTED_SELECTOR = TEST_PASS_SELECTOR
+SOLUTION_ACCEPTED_SELECTOR = "#qd-content > div.h-full.flex-col.ssg__qd-splitter-secondary-w > div > div.min-h-0.flex-grow.ssg__qd-splitter-primary-h > div > div.flex.h-full.w-full.flex-col.overflow-hidden > div.flex.h-full.w-full.flex-col.overflow-auto.p-5 > div.mb-4.flex.w-full.items-start.justify-between > div.flex.items-center.gap-4 > a > button" # "+ Solution" button
 
 class Solver:
     def __init__(self, browser):
         self.browser = browser
         pass
 
-    def solve(self, page, problem_url):
+    def _click_over_element(self, selector):
+            self.page.wait_for_selector(selector)
+
+            # Get the element's x and y coordinates
+            x = self.page.evaluate("document.querySelector('" + selector + "').getBoundingClientRect().x")
+            y = self.page.evaluate("document.querySelector('" + selector + "').getBoundingClientRect().y")
+
+            # Add some pixels to the x and y coordinates, so that we click inside the element
+            x += 50
+            y += 50
+
+            # Perform the click
+            self.page.mouse.click(x, y)
+
+    def _select_all_text(self):
+        """We're focused inside an editable element such as a textarea or an input field. We press Control-A or Command-A to select all text in the element."""
+        self._focus_editor() # Fixes: sometimes we select the whole page instead of the editor's content
+        # If we're on mac, the command key is used instead of control
+        if PLATFORM == 'darwin':
+            modifier = 'Meta'
+        else:
+            modifier = 'Control'
+        self.page.keyboard.down(modifier)
+        self.page.keyboard.press('KeyA')
+        self.page.keyboard.up(modifier)
+
+    def _type_text(self, text):
+        """
+        We're focused inside an editable element such as a textarea or an input field. We type the given text into the element.
+
+        Our problem is that the editor has a (bad) autocompletion feature for matching parentheses, quotes, etc. This feature cannot be disabled, so we must work around it.
+        """
+        # Copy the text to the clipboard
+        pyperclip.copy(text)
+        # Paste the text
+        self._clipboardPaste()
+        # Clear the clipboard
+        pyperclip.copy("")
+
+    def _clipboardPaste(self):
+        self.page.keyboard.down('Meta')
+        self.page.keyboard.press('v')
+        self.page.keyboard.up('Meta')
+
+    def _clipboardCopy(self):
+        self.page.keyboard.down('Meta')
+        self.page.keyboard.press('c')
+        self.page.keyboard.up('Meta')
+
+    def _init_template(self):
+        """
+        Initializes the code editor with a template.
+
+        We need to keep the existing function signature, so we just add the code that we need to the existing code.
+        """
+        # If save file exists, load it instead of the template
+        try:
+            with open(SAVE_FILE, 'r') as f:
+                template = f.read()
+                self._select_all_text()
+                self._type_text(template)
+                return
+        except FileNotFoundError:
+            pass
+
+        self._select_all_text()
+
+        # Insert before everything else
+        self.page.keyboard.press('ArrowLeft')
+        pass_count = (self.pass_count + 1) if hasattr(self, "pass_count") else 0
+        test_suite_size = self.test_suite_size if hasattr(self, "test_suite_size") else "?"
+        comment_header = f"""/*
+ * language: {self.language}
+ * problem: {self.title}
+ * link: {self.problem_url}
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+"""
+# this doesn't work, because this gets evaluated once, not during every loop
+# * tests passed: {pass_count} / {test_suite_size}
+        self._type_text(comment_header)
+        self._type_text("testNumber = 0;")
+        self.page.keyboard.press('Enter')
+
+
+        # Insert inside the existing function definition
+        # Note: sometimes we have more than one function definition (because helper classes), so we just go to the very bottom and then arrow up, to get inside the last function definition.
+        self._select_all_text()
+        self.page.keyboard.press('ArrowDown')
+        self.page.keyboard.press('ArrowUp')
+        self._type_text("return [")
+        self.page.keyboard.press('Enter')
+        self._type_text("][testNumber++];")
+
+    def solve(self, page, problem_url, language="JavaScript"):
         self.page = page
+        self.problem_url = problem_url
+        self.language = language
 
         # Get the problem's page
         self.page.goto(problem_url)
@@ -102,6 +121,11 @@ class Solver:
         response = self.page.content()
         soup = BeautifulSoup(response, 'html.parser')
 
+        title_selector = '#qd-content > div.h-full.flex-col.ssg__qd-splitter-primary-w > div > div.min-h-0.flex-grow.ssg__qd-splitter-primary-h > div > div.flex.h-full.w-full.overflow-y-auto > div > div > div.w-full.px-5.pt-4 > div > div:nth-child(1) > div.flex-1 > div > div > span' # ugly, but necessary
+        self.page.wait_for_selector(title_selector)
+        self.title = self.page.evaluate("document.querySelector('" + title_selector + "').innerText")
+        print(f"Solving problem: {self.title}")
+
         self._reset_solution()
 
         time.sleep(3) # There is some problem with something, try if sleeping helps??
@@ -109,25 +133,44 @@ class Solver:
         print("Let's go!")
         while True:
             self._submit()
-            if self._is_accepted():
+            if self._check_if_test_passed_or_solution_accepted() == "solution_accepted":
                 print("Accepted!")
+                self.save_solution(permanently=True)
                 return
             else:
+                if hasattr(self, "pass_count") and hasattr(self, "saved_pass_count") and self.pass_count != self.saved_pass_count + 1:
+                    raise Exception("Something went wrong, the number of passed tests is not increasing")
+                if hasattr(self, "test_suite_size") and self.test_suite_size > 200:
+                    raise Exception(f"Test suite size too large ({self.test_suite_size}), aborting.")
+                self.saved_pass_count = self.pass_count
                 print(f"Tests passed: {self.pass_count} / {self.test_suite_size}")
                 result = self._get_result()
                 self._append_result(result)
-            time.sleep(30) # We are not allowed to submit too often, and also we don't want to DOS the server with requests
+                time.sleep(30) # We are not allowed to submit too often, and also we don't want to DOS the server with requests
 
     def get_solution_text(self):
         # Get the whole solution via the clipboard
-        select_all_text(self.page)
-        copy(self.page)
+        self._select_all_text()
+        self._clipboardCopy()
         result = pyperclip.paste()
         return result
 
-    def save_solution(self):
-        # Save the solution to a file
-        with open(SAVE_FILE, 'w') as f:
+    def save_solution(self, **kwargs):
+        """Save the solution to a file."""
+        # Get the save file path from kwargs
+        if 'permanently' in kwargs and kwargs['permanently']:
+            # The permanent save file is solutions/<problem number>/<language>.js
+            problem_number = self.title.split('.')[0]
+            # check if the problem number is valid -- it is a number
+            if not problem_number.isdigit():
+                raise Exception("The problem number is not valid")
+            # save the file
+            save_file = os.path.join(SOLUTIONS_DIR, problem_number, self.language + ".js") # XXX determine extension based on language
+            # create the directory if it doesn't exist
+            os.makedirs(os.path.dirname(save_file), exist_ok=True)
+        else:
+            save_file = SAVE_FILE
+        with open(save_file, 'w') as f:
             f.write(self.solution_text)
 
     def _submit(self):
@@ -136,19 +179,24 @@ class Solver:
         self.save_solution()
         print("Submitting solution:\n\n" + self.solution_text + "\n\n")
         self.page.evaluate("document.querySelector('button.bg-green-s').click()") # There is only one green button on the page smh
-        time.sleep(1) # Judging takes many seconds, so we are not delaying anything by sleeping liberally here
-        pass_selector = '.ml-auto.text-xs.text-label-3'
-        self.page.wait_for_selector(pass_selector)
+
+    def _check_if_test_passed_or_solution_accepted(self):
+        """Check if the solution was accepted."""
+        self.page.wait_for_selector(f"{TEST_PASS_SELECTOR}, {SOLUTION_ACCEPTED_SELECTOR}")
+        if self._is_accepted():
+            return "solution_accepted"
+        else:
+            pass_selector = TEST_PASS_SELECTOR
+            result = self.page.evaluate("document.querySelector('" + pass_selector + "').innerText")
+            # result looks like this: "42 / 420 tests passed"
+            self.pass_count = int(result.split(' ')[0])
+            self.test_suite_size = int(result.split(' ')[2])
 
     def _is_accepted(self):
         """Check if the solution was accepted."""
-        pass_selector = '.ml-auto.text-xs.text-label-3'
-        result = self.page.evaluate("document.querySelector('" + pass_selector + "').innerText")
-        # result looks like this: "42 / 420 tests passed"
-        self.pass_count = int(result.split(' ')[0])
-        self.test_suite_size = int(result.split(' ')[2])
-        self.accepted = self.pass_count == self.test_suite_size
-        return self.accepted
+        result = self.page.evaluate(f'''Array.from(document.querySelectorAll("{SOLUTION_ACCEPTED_SELECTOR}")).map(el => el.innerText).some(s => s.trim() === "Solution")''') # the button exists and has the text "Solution"
+        print("Is accepted?", result, type(result), bool(result))
+        return bool(result)
 
     def _get_result(self):
         """Get the expected result of the test case."""
@@ -160,11 +208,11 @@ class Solver:
         language_dropdown = '.mr-auto.flex.flex-nowrap.gap-3 button'
         self.page.wait_for_selector(language_dropdown)
         self.page.evaluate("document.querySelectorAll('" + language_dropdown + "')[0].click()")
-        javascript_option = 'li .whitespace-nowrap'
-        self.page.wait_for_selector(javascript_option)
+        language_option = 'li .whitespace-nowrap'
+        self.page.wait_for_selector(language_option)
         self.page.evaluate("Array.from(document.querySelectorAll('"
-                + javascript_option
-                + "')).filter(x => x.innerText.match(/JavaScript/))[0].click()")
+                + language_option
+                + f"')).filter(x => x.innerText.match(/{self.language}/))[0].click()")
         # Wait a wee bit to allow the modal dialogue to close
         time.sleep(1)
 
@@ -179,18 +227,18 @@ class Solver:
         time.sleep(1)
 
         self._focus_editor()
-        init_template(self.page)
+        self._init_template()
 
     def _append_result(self, result):
         self._focus_editor()
-        select_all_text(self.page)
+        self._select_all_text()
         self.page.keyboard.press('ArrowDown')
         self.page.keyboard.press('ArrowUp')
         self.page.keyboard.press('ArrowUp')
         self.page.keyboard.press('End')
-        type_text(self.page, result + ",") # note: we create an empty elment at the end of the array, which is perfectly fine
+        self._type_text(result + ",") # note: we create an empty elment at the end of the array, which is perfectly fine
         # self.page.keyboard.press('Enter') # don't press enter, because we want to have all the results on the same line
 
     def _focus_editor(self):
-        # Click inside the code editor
-        click_over_element('.monaco-scrollable-element', self.page)
+        """Focus the editor by clicking inside its textarea."""
+        self._click_over_element('.monaco-scrollable-element')
