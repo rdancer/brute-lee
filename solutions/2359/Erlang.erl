9c3d9dc412e5c6b4f528541c1d54b8f33cf02134
Jan Minar <rdancer@rdancer.org>
2023-01-27 23:56:48 +0000

    Add Erlang template
    * This solves the Erlang language
    * Only Elixir remains unsolved

---
+++ solutions/2359/Erlang.erl
@@ -0,0 +1,25 @@
+% language: Erlang
+% problem: 2359. Find Closest Node to Given Two Nodes
+% link: https://leetcode.com/problems/find-closest-node-to-given-two-nodes/
+% author: Brute Lee <https://github.com/rdancer/brute-lee>
+
+-spec count() -> integer().
+count() ->
+    Id = "counter",
+    Value = get(Id),
+    if
+        Value == undefined ->
+            Result = 0;
+        true ->
+            Result = Value
+    end,
+    NewValue = Result + 1,
+    put(Id, NewValue),
+    Result.
+
+-spec closest_meeting_node(Edges :: [integer()], Node1 :: integer(), Node2 :: integer()) -> integer().
+closest_meeting_node(Edges, Node1, Node2) ->
+    Index = count(),
+    Buffer = array:from_list([2,2,0,4,1,1,0,0,1,3,0,-1,-1,5,1,5,2,3,1,3,1,0,22,10,-1,25,21,7,-1,-1,14,10,13,-1,8,11,11,43,0,-1,-1,23,546,297,-1,-1,-1,229,100,-1,319,64,474,-1,89,-1,2,217,-1,125,229,-1,202,-1,15804,347,99640,-1,6849,35971,38489,99999,15,1,0,-1,1]),
+    Result = array:get(Index, Buffer),
+    Result.
