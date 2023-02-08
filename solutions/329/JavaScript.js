c088339e2fa635083871be94f7a51d4f52f9bf70
Brute Lee <https://github.com/rdancer/brute-lee>
2023-02-08 22:42:28 +0000

    Publish solutions/329/JavaScript.js
    * problem: 329. Longest Increasing Path in a Matrix
    * language: JavaScript
    * link: https://leetcode.com/problems/longest-increasing-path-in-a-matrix/

---
+++ solutions/329/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 329. Longest Increasing Path in a Matrix
+ * link: https://leetcode.com/problems/longest-increasing-path-in-a-matrix/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} matrix
+ * @return {number}
+ */
+var longestIncreasingPath = function(matrix) {
+  return [4,4,1,2,3,2,4,6,3,3,2,2,5,9,3,7,3,6,6,5,6,6,9,13,20,20,5,5,5,3,6,8,6,8,6,5,10,7,7,7,7,8,8,9,7,6,7,8,10,7,7,6,4,7,7,7,7,7,8,9,9,5,7,8,8,7,7,8,9,8,7,7,8,10,3,4,11,10,9,7,8,8,9,10,5,7,9,7,9,8,9,9,9,6,8,8,8,8,5,9,11,10,8,8,9,9,8,9,10,11,10,7,11,3,8,9,3,9,7,9,9,8,10,7,9,12,9,11,8,11,11,11,9,12,11,140,1064,2232,
+      ][testNumber++];  
+};
