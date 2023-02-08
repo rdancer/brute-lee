08970dae3c49b700aa8232490f5227f38044f50e
Brute Lee <https://github.com/rdancer/brute-lee>
2023-02-08 21:22:13 +0000

    Publish solutions/1224/JavaScript.js
    * problem: 1224. Maximum Equal Frequency
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-equal-frequency/

---
+++ solutions/1224/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1224. Maximum Equal Frequency
+ * link: https://leetcode.com/problems/maximum-equal-frequency/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxEqualFreq = function(nums) {
+  return [7,13,5,8,13,2,2,9,10,6,5,7,9,7,13,16,25,29,7,25,33,37,81,85,381,301,871,801,901,801,1901,1399,4001,7991,7201,18001,25999,21965,39001,38901,2478,4355,79201,2,81909,
+      ][testNumber++];  
+};
