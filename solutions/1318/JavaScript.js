c31d1138c429afb5b198d2f01d9eca7902f70f27
Brute Lee <https://github.com/rdancer/brute-lee>
2023-02-08 23:31:43 +0000

    Publish solutions/1318/JavaScript.js
    * problem: 1318. Minimum Flips to Make a OR b Equal to c
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-flips-to-make-a-or-b-equal-to-c/

---
+++ solutions/1318/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 1318. Minimum Flips to Make a OR b Equal to c
+ * link: https://leetcode.com/problems/minimum-flips-to-make-a-or-b-equal-to-c/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {number}
+ */
+var minFlips = function(a, b, c) {
+  return [3,1,0,0,0,3,4,3,4,2,3,2,5,2,2,1,1,0,2,1,3,6,6,5,2,6,8,5,5,10,9,9,9,7,11,11,15,7,6,10,15,12,11,17,22,13,13,14,19,21,20,13,17,18,18,19,17,25,9,18,15,20,21,19,
+      ][testNumber++];  
+};
