dad09b534cc71690681413210f7b7243f89f37ef
Brute Lee <https://github.com/rdancer/brute-lee>
2023-02-08 17:44:47 +0000

    Publish solutions/2278/JavaScript.js
    * problem: 2278. Percentage of Letter in String
    * language: JavaScript
    * link: https://leetcode.com/problems/percentage-of-letter-in-string/

---
+++ solutions/2278/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2278. Percentage of Letter in String
+ * link: https://leetcode.com/problems/percentage-of-letter-in-string/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @param {character} letter
+ * @return {number}
+ */
+var percentageLetter = function(s, letter) {
+  return [33,0,0,33,25,20,16,14,11,10,0,7,18,0,0,10,4,4,4,3,0,0,3,2,0,0,7,2,4,0,4,4,1,0,9,0,8,0,4,1,0,1,5,5,0,3,2,4,3,0,3,0,7,10,14,17,18,21,22,23,25,35,37,40,43,46,48,59,62,63,65,70,71,73,76,79,82,86,87,94,97,100,0,100,100,66,
+      ][testNumber++];  
+};
