356cffe858b2e901f68f5cf3cfdf8b6d48ee919e
Brute Lee <https://github.com/rdancer/brute-lee>
2023-02-08 11:33:11 +0000

    Publish solutions/45/JavaScript.js
    * problem: 45. Jump Game II
    * language: JavaScript
    * link: https://leetcode.com/problems/jump-game-ii/

---
+++ solutions/45/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 45. Jump Game II
+ * link: https://leetcode.com/problems/jump-game-ii/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var jump = function(nums) {
+  return [2,2,0,0,1,1,2,2,1,1,3,2,2,1,4,4,3,3,3,2,3,2,2,1,3,2,3,1,2,2,2,3,3,3,3,2,2,2,3,3,3,3,3,1,1,1,2,1,1,1,2,2,4,2,3,1,1,3,2,1,2,3,3,4,5,4,4,5,3,5,4,5,4,5,6,7,8,7,6,5,6,8,13,12,11,8,16,13,8,13,11,13,274,284,5753,2,11,7,8,7,11,11,1,1,11,11,1,1,1,
+      ][testNumber++];  
+};
