b1fc1de42b4ae307b27c19ed910fc13686701ebd
Brute Lee <https://github.com/rdancer/brute-lee>
2023-02-09 05:06:54 +0000

    Publish solutions/1665/JavaScript.js
    * problem: 1665. Minimum Initial Energy to Finish Tasks
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-initial-energy-to-finish-tasks/

---
+++ solutions/1665/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1665. Minimum Initial Energy to Finish Tasks
+ * link: https://leetcode.com/problems/minimum-initial-energy-to-finish-tasks/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} tasks
+ * @return {number}
+ */
+var minimumEffort = function(tasks) {
+  return [8,32,27,11,15,12,11,3,1,1000,5,14,10,29,45,11,9,15,324365,491391,632520,848713,895794,1205874,1585745,1667233,1836093,2130084,2201511,2129780,2215385,6186273,6481715,6879687,7142848,7788105,8808078,42262490,94357277,125515199,500005000,109999,
+      ][testNumber++];  
+};
