74f2d6e17d1c784e03333843f126c7ab4dcd8110
Brute Lee <https://github.com/rdancer/brute-lee>
2023-02-09 04:34:38 +0000

    Publish solutions/1297/JavaScript.js
    * problem: 1297. Maximum Number of Occurrences of a Substring
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-number-of-occurrences-of-a-substring/

---
+++ solutions/1297/JavaScript.js
@@ -0,0 +1,18 @@
+/*
+ * language: JavaScript
+ * problem: 1297. Maximum Number of Occurrences of a Substring
+ * link: https://leetcode.com/problems/maximum-number-of-occurrences-of-a-substring/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @param {number} maxLetters
+ * @param {number} minSize
+ * @param {number} maxSize
+ * @return {number}
+ */
+var maxFreq = function(s, maxLetters, minSize, maxSize) {
+  return [2,2,3,0,3,1,1,1,2,2,2,2,3,0,0,1,0,0,1,0,3,13,1,6,1,1,2,0,0,1,1,6,1,3,13,1,1,18,8491,20,
+      ][testNumber++];  
+};
