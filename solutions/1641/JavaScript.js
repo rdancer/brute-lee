d984d7904b61a1a28b2570663397be3a11759f78
Brute Lee <https://github.com/rdancer/brute-lee>
2023-02-09 04:07:30 +0000

    Publish solutions/1641/JavaScript.js
    * problem: 1641. Count Sorted Vowel Strings
    * language: JavaScript
    * link: https://leetcode.com/problems/count-sorted-vowel-strings/

---
+++ solutions/1641/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1641. Count Sorted Vowel Strings
+ * link: https://leetcode.com/problems/count-sorted-vowel-strings/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countVowelStrings = function(n) {
+  return [5,15,66045,70,126,210,330,495,715,1001,1365,1820,2380,3060,3876,4845,5985,7315,8855,23751,27405,31465,35960,40920,46376,52360,58905,35,73815,82251,91390,101270,111930,123410,135751,148995,163185,178365,194580,249900,316251,
+      ][testNumber++];  
+};
