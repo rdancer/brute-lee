c2e09de31044a6b0effd2bd3304d6fccae18cfc8
Brute Lee <https://github.com/rdancer/brute-lee>
2023-02-09 01:27:52 +0000

    Publish solutions/1550/JavaScript.js
    * problem: 1550. Three Consecutive Odds
    * language: JavaScript
    * link: https://leetcode.com/problems/three-consecutive-odds/

---
+++ solutions/1550/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1550. Three Consecutive Odds
+ * link: https://leetcode.com/problems/three-consecutive-odds/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var threeConsecutiveOdds = function(arr) {
+  return [false,true,false,false,false,false,true,false,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,false,false,false,false,
+      ][testNumber++];  
+};
