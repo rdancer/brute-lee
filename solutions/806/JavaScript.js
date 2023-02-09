f0ea37ebe255f73ab0b0696aaefd418595cb66bd
Brute Lee <https://github.com/rdancer/brute-lee>
2023-02-09 03:08:56 +0000

    Publish solutions/806/JavaScript.js
    * problem: 806. Number of Lines To Write String
    * language: JavaScript
    * link: https://leetcode.com/problems/number-of-lines-to-write-string/

---
+++ solutions/806/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 806. Number of Lines To Write String
+ * link: https://leetcode.com/problems/number-of-lines-to-write-string/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} widths
+ * @param {string} s
+ * @return {number[]}
+ */
+var numberOfLines = function(widths, s) {
+  return [[3,60],[2,4],[1,46],[1,28],[1,39],[1,67],[1,100],[7,69],[7,13],[6,76],[6,61],[7,43],[6,75],[7,54],[8,63],[7,92],[6,36],[73,8],[66,13],[58,47],[55,44],[65,27],[54,7],[58,54],[66,58],[65,29],[75,15],
+      ][testNumber++];  
+};
