6a4b4495fbb8704ff54c526f8534f6fc2117af9f
Brute Lee <https://github.com/rdancer/brute-lee>
2023-02-09 03:41:33 +0000

    Publish solutions/1184/JavaScript.js
    * problem: 1184. Distance Between Bus Stops
    * language: JavaScript
    * link: https://leetcode.com/problems/distance-between-bus-stops/

---
+++ solutions/1184/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 1184. Distance Between Bus Stops
+ * link: https://leetcode.com/problems/distance-between-bus-stops/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} distance
+ * @param {number} start
+ * @param {number} destination
+ * @return {number}
+ */
+var distanceBetweenBusStops = function(distance, start, destination) {
+  return [1,3,4,3,3,25,17,28,40,31,49,19,16,96,57,84,112,117,33,70,6,74,73,298,316,200,83,206,196,234,447,36,298,603,333,7323099,2760153,
+      ][testNumber++];  
+};
