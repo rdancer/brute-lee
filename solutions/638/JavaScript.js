d4535545149174874445f59f4e80d95750c1ccdb
Brute Lee <https://github.com/rdancer/brute-lee>
2023-02-10 18:27:31 +0000

    Publish solutions/638/JavaScript.js
    * problem: 638. Shopping Offers
    * language: JavaScript
    * link: https://leetcode.com/problems/shopping-offers/

---
+++ solutions/638/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 638. Shopping Offers
+ * link: https://leetcode.com/problems/shopping-offers/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} price
+ * @param {number[][]} special
+ * @param {number[]} needs
+ * @return {number}
+ */
+var shoppingOffers = function(price, special, needs) {
+  return [14,11,0,0,0,0,0,1,2,2,6,36,6,3,91,91,115,69,56,119,132,34,100,67,84,83,128,44,149,47,104,104,98,75,52,44,50,80,49,40,10,108,139,70,29,76,34,38,54,49,33,22,28,11,16,17,6,10,22,71,34,15,57,21,10,
+      ][testNumber++];  
+};
