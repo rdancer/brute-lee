f1065e39e6bff3cb61f6e6e90ce61599c8928e8b
Brute Lee <https://github.com/rdancer/brute-lee>
2023-02-10 03:54:08 +0000

    Publish solutions/881/JavaScript.js
    * problem: 881. Boats to Save People
    * language: JavaScript
    * link: https://leetcode.com/problems/boats-to-save-people/

---
+++ solutions/881/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 881. Boats to Save People
+ * link: https://leetcode.com/problems/boats-to-save-people/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} people
+ * @param {number} limit
+ * @return {number}
+ */
+var numRescueBoats = function(people, limit) {
+  return [1,3,4,2,1,2,2,2,3,3,2,2,3,4,2,3,4,4,4,3,4,4,3,5,5,4,4,5,4,3,4,5,4,4,4,6,6,9,5,8,9,7,11,11,12,11,10,13,10,29,27,32,26,32,27,25,53,51,54,54,60,52,51,571,520,526,522,510,518,504,27491,25534,25325,25043,25132,25134,25010,8,
+      ][testNumber++];  
+};
