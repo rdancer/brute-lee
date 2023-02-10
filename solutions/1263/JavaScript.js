76bd2a1a4504e9b61f0d4f47bfb0f7b93b497f9f
Brute Lee <https://github.com/rdancer/brute-lee>
2023-02-10 16:55:22 +0000

    Publish solutions/1263/JavaScript.js
    * problem: 1263. Minimum Moves to Move a Box to Their Target Location
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-moves-to-move-a-box-to-their-target-location/

---
+++ solutions/1263/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1263. Minimum Moves to Move a Box to Their Target Location
+ * link: https://leetcode.com/problems/minimum-moves-to-move-a-box-to-their-target-location/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var minPushBox = function(grid) {
+  return [3,-1,5,-1,7,8,6,6,7,5,5,5,8,8,11,-1,29,26,-1,3,1,-1,1,15,-1,1,-1,4,3,-1,
+      ][testNumber++];  
+};
