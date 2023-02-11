df9d789541c862773ae4721d8aa8c691ff01012e
Jan Minar <rdancer@rdancer.org>
2023-02-11 17:35:49 +0000

    Publish solutions/367/JavaScript.js
    * problem: 367. Valid Perfect Square
    * language: JavaScript
    * link: https://leetcode.com/problems/valid-perfect-square/

---
+++ solutions/367/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 367. Valid Perfect Square
+ * link: https://leetcode.com/problems/valid-perfect-square/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function(num) {
+  return [true,false,true,true,false,true,true,true,false,false,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,false,false,false,false,false,false,false,false,false,false,false,true,
+      ][testNumber++];  
+};
