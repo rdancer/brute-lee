030b8e906c4cf3f7a9de19915f8a348c25ad0ee5
Brute Lee <https://github.com/rdancer/brute-lee>
2023-02-11 00:59:21 +0000

    Publish solutions/447/JavaScript.js
    * problem: 447. Number of Boomerangs
    * language: JavaScript
    * link: https://leetcode.com/problems/number-of-boomerangs/

---
+++ solutions/447/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 447. Number of Boomerangs
+ * link: https://leetcode.com/problems/number-of-boomerangs/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var numberOfBoomerangs = function(points) {
+  return [2,2,0,0,20,24,0,10,12,10,28,6,10,10,10,8,22,8,0,2,6,2,4,12,8,6,4,8,2,1062,966,1000,
+      ][testNumber++];  
+};
