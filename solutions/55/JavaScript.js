6b815be3bbbcd6835b8810d4e59277abe518540b
Brute Lee <https://github.com/rdancer/brute-lee>
2023-02-11 00:03:09 +0000

    Publish solutions/55/JavaScript.js
    * problem: 55. Jump Game
    * language: JavaScript
    * link: https://leetcode.com/problems/jump-game/

---
+++ solutions/55/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 55. Jump Game
+ * link: https://leetcode.com/problems/jump-game/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+  return [true,false,true,true,false,true,true,false,false,true,true,true,true,true,false,false,true,true,false,false,false,true,true,true,true,true,true,true,true,false,true,true,false,true,false,false,true,false,false,false,true,false,true,false,false,true,false,false,false,false,false,false,true,false,false,true,true,false,false,true,true,false,true,true,false,true,true,true,true,false,false,true,true,false,true,false,false,true,true,false,true,true,true,false,true,false,true,false,true,true,true,true,false,true,true,true,true,true,false,true,true,true,true,true,true,true,true,true,false,true,false,true,true,true,true,true,false,true,true,true,false,false,false,true,true,true,true,true,true,false,true,false,true,false,false,true,false,false,true,true,true,true,false,false,true,true,false,false,false,true,true,true,true,true,false,true,true,true,false,false,true,false,false,true,true,false,true,true,true,true,
+      ][testNumber++];  
+};
