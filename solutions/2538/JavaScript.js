e2771bc8d963ddd321e4183924c8f90930dc7fae
Jan Minar <rdancer@rdancer.org>
2023-02-11 16:55:35 +0000

    Publish solutions/2538/JavaScript.js
    * problem: 2538. Difference Between Maximum and Minimum Price Sum
    * language: JavaScript
    * link: https://leetcode.com/problems/difference-between-maximum-and-minimum-price-sum/

---
+++ solutions/2538/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 2538. Difference Between Maximum and Minimum Price Sum
+ * link: https://leetcode.com/problems/difference-between-maximum-and-minimum-price-sum/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number[]} price
+ * @return {number}
+ */
+var maxOutput = function(n, edges, price) {
+  return [24,2,12,8,6,21,39,28,5,5,8,24,1619,13186,7861,686,429,3297,891,5921,1811,3442,13172,4371,8456,16588,20576,16168,16206,14602,46156,725,16321,339,16673,1944,17030,417610,46183,1183123,3464742,3090457,19804016,31818968,100010,4108137,1367750,6,33,0,5,5,6,6,3,2,13,100998,
+      ][testNumber++];  
+};
