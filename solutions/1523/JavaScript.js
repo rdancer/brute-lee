613dc675d476722d4c40600d785047f0408ff757
Jan Minar <rdancer@rdancer.org>
2023-02-13 10:55:50 +0000

    Publish solutions/1523/JavaScript.js
    * problem: 1523. Count Odd Numbers in an Interval Range
    * language: JavaScript
    * link: https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/

---
+++ solutions/1523/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1523. Count Odd Numbers in an Interval Range
+ * link: https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var countOdds = function(low, high) {
+  return [3,1,5,1,0,1,0,3,2,5,3,4,2,0,1,2,8,1,3,1,3,2,2,4,0,1,1,6,4,2,1,5,1,0,1,0,3,1746,1153,592,1835,16,846,100,110,517,2161,1820,345,1642,800,1082,1969,173,1542,327,2047,224510,157233,105068,220518,134602,61954,225704,403700,481041,145734,358112,222273,167430,62567,42978,89492370,6664690,86213013,37600997,96814022,157351278,35290995,30832205,145459898,221069381,500000000,0,
+      ][testNumber++];  
+};
