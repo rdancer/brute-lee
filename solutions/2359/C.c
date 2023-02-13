07f771fe3ecae2eb1fec89cdf5f9febd48dd37d0
Jan Minar <rdancer@rdancer.org>
2023-02-13 09:58:05 +0000

    Start on automatic polyglot generation

--- solutions/2359/C.c
+++ solutions/2359/C.c
@@ -1,7 +1,12 @@
-/* C solution of LeetCode 2359. Find Closest Node to Given Two Nodes; Copyright 2023 Brute Lee */
+/*
+ * language: C
+ * problem: 2359. Find Closest Node to Given Two Nodes
+ * link: https://leetcode.com/problems/find-closest-node-to-given-two-nodes/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
 int testNumber = 0;
+int buffer[] = {2,2,0,4,1,1,0,0,1,3,0,-1,-1,5,1,5,2,3,1,3,1,0,22,10,-1,25,21,7,-1,-1,14,10,13,-1,8,11,11,43,0,-1,-1,23,546,297,-1,-1,-1,229,100,-1,319,64,474,-1,89,-1,2,217,-1,125,229,-1,202,-1,15804,347,99640,-1,6849,35971,38489,99999,15,1,0,-1,1};
 
 int closestMeetingNode(int* edges, int edgesSize, int node1, int node2){
-    int result[77] = {2,2,0,4,1,1,0,0,1,3,0,-1,-1,5,1,5,2,3,1,3,1,0,22,10,-1,25,21,7,-1,-1,14,10,13,-1,8,11,11,43,0,-1,-1,23,546,297,-1,-1,-1,229,100,-1,319,64,474,-1,89,-1,2,217,-1,125,229,-1,202,-1,15804,347,99640,-1,6849,35971,38489,99999,15,1,0,-1,1};
-    return result[testNumber++];
+    return buffer[testNumber++];
 }
