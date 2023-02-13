41fb01b8f280d87766a63c63862a70c6f3b0ef3d
Jan Minar <rdancer@rdancer.org>
2023-02-13 13:45:47 +0000

    Publish solutions/1051/JavaScript.js
    * problem: 1051. Height Checker
    * language: JavaScript
    * link: https://leetcode.com/problems/height-checker/

---
+++ solutions/1051/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1051. Height Checker
+ * link: https://leetcode.com/problems/height-checker/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var heightChecker = function(heights) {
+  return [3,5,0,0,4,0,4,0,4,17,19,17,16,21,17,20,16,22,17,18,21,21,19,19,21,19,22,46,45,45,49,49,45,51,48,45,52,52,49,50,50,51,50,50,51,95,91,95,94,93,95,95,94,95,93,94,95,94,95,96,94,97,95,95,94,98,96,95,97,97,98,95,96,95,96,97,96,99,98,100,98,
+      ][testNumber++];  
+};
