e274678e6169baa3d6a2ffee36d0b475c59de5ab
Jan Minar <rdancer@rdancer.org>
2023-02-13 15:47:15 +0000

    Publish solutions/2461/JavaScript.js
    * problem: 2461. Maximum Sum of Distinct Subarrays With Length K
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-sum-of-distinct-subarrays-with-length-k/

---
+++ solutions/2461/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2461. Maximum Sum of Distinct Subarrays With Length K
+ * link: https://leetcode.com/problems/maximum-sum-of-distinct-subarrays-with-length-k/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maximumSubarraySum = function(nums, k) {
+  return [15,0,3,24,12,1,0,0,3,8,0,5,3,5,14,5,8,5,6,10,3,0,4,3,6,9,2,0,6,9,0,4,0,82,19,87,18,17,20,18,0,47,42,58,51,0,66,109,34,38,26,60,18,19,97,21,35,0,42,16,0,0,68,0,0,5000050000,3750025000,0,0,0,0,0,0,0,0,0,623816,2507974797,5,10,1,0,6,0,10,4,6,12,6,3,0,102,
+      ][testNumber++];  
+};
