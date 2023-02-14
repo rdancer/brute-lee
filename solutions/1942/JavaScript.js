92a8a3727dd4d792b141ca0d576244f151191461
Jan Minar <rdancer@rdancer.org>
2023-02-14 03:17:23 +0000

    Publish solutions/1942/JavaScript.js
    * problem: 1942. The Number of the Smallest Unoccupied Chair
    * language: JavaScript
    * link: https://leetcode.com/problems/the-number-of-the-smallest-unoccupied-chair/

---
+++ solutions/1942/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1942. The Number of the Smallest Unoccupied Chair
+ * link: https://leetcode.com/problems/the-number-of-the-smallest-unoccupied-chair/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} times
+ * @param {number} targetFriend
+ * @return {number}
+ */
+var smallestChair = function(times, targetFriend) {
+  return [1,2,2,0,5,8,1,45,49,0,0,5,77,81,0,19,96,2,98,0,2,8,260,0,13,456,0,71,68,747,101,15,0,49,2494,513,0,2235,2675,9998,0,2027,0,0,0,0,4999,4802,0,0,2279,500,0,9998,656,0,0,0,0,1,0,3,0,0,7,
+      ][testNumber++];  
+};
