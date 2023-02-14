43629e3a95662e85dd4891482c530012ca5529c2
Jan Minar <rdancer@rdancer.org>
2023-02-14 10:53:03 +0000

    Publish solutions/154/JavaScript.js
    * problem: 154. Find Minimum in Rotated Sorted Array II
    * language: JavaScript
    * link: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/

---
+++ solutions/154/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 154. Find Minimum in Rotated Sorted Array II
+ * link: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+  return [1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,-1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,1,0,1,0,2,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,2,1,0,0,1,0,0,0,1,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,0,0,0,0,1,0,1,0,0,0,1,0,0,0,0,2,0,0,0,1,1,0,0,0,2,1,0,0,0,0,0,4,1,0,1,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,1,1,1,1,-4999,-4998,-5000,1,-5000,
+      ][testNumber++];  
+};
