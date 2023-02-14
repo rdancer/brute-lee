652fcd6520105945c81f60499c85b089de1cf5ce
Jan Minar <rdancer@rdancer.org>
2023-02-14 00:39:09 +0000

    Publish solutions/1437/JavaScript.js
    * problem: 1437. Check If All 1's Are at Least Length K Places Away
    * language: JavaScript
    * link: https://leetcode.com/problems/check-if-all-1s-are-at-least-length-k-places-away/

---
+++ solutions/1437/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1437. Check If All 1's Are at Least Length K Places Away
+ * link: https://leetcode.com/problems/check-if-all-1s-are-at-least-length-k-places-away/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var kLengthApart = function(nums, k) {
+  return [true,false,true,true,true,false,true,true,true,true,false,true,false,false,false,false,true,true,true,true,true,false,false,false,false,false,true,true,true,true,true,false,true,false,true,false,true,false,false,false,false,true,true,true,true,true,false,false,false,true,true,true,false,false,false,true,true,true,false,false,true,true,false,false,true,true,true,true,
+      ][testNumber++];  
+};
