d293e8cd88432a4a80f8571496bc009c436ed0f2
Jan Minar <rdancer@rdancer.org>
2023-02-14 08:11:19 +0000

    Publish solutions/377/JavaScript.js
    * problem: 377. Combination Sum IV
    * language: JavaScript
    * link: https://leetcode.com/problems/combination-sum-iv/

---
+++ solutions/377/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 377. Combination Sum IV
+ * link: https://leetcode.com/problems/combination-sum-iv/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var combinationSum4 = function(nums, target) {
+  return [7,0,0,9,8,89,982,28730,39882198,181997601,1132436852,1073741824,83316385,45452242,1,
+      ][testNumber++];  
+};
