896f6e92573b43f03b177d15c8536c4ca869a705
Jan Minar <rdancer@rdancer.org>
2023-02-15 15:23:23 +0000

    Publish solutions/1079/JavaScript.js
    * problem: 1079. Letter Tile Possibilities
    * language: JavaScript
    * link: https://leetcode.com/problems/letter-tile-possibilities/

---
+++ solutions/1079/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1079. Letter Tile Possibilities
+ * link: https://leetcode.com/problems/letter-tile-possibilities/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} tiles
+ * @return {number}
+ */
+var numTilePossibilities = function(tiles) {
+  return [8,188,1,1,1,1,1,1,1,1,1,1,1,1,4,4,4,2,4,4,4,4,4,4,4,4,15,15,15,15,15,15,15,8,15,8,15,8,64,13,64,34,34,34,64,34,64,34,64,13,325,63,325,63,325,170,170,89,325,170,325,33,188,188,363,522,1010,104,1956,1010,1956,522,1956,1010,7012,1840,7012,1265,7012,1265,13699,1840,13699,1840,7012,1265,
+      ][testNumber++];  
+};
