dd5d37d00d0483086d66301a50dfd4a15c11c3ec
Jan Minar <rdancer@rdancer.org>
2023-02-15 19:24:25 +0000

    Publish solutions/70/JavaScript.js
    * problem: 70. Climbing Stairs
    * language: JavaScript
    * link: https://leetcode.com/problems/climbing-stairs/

---
+++ solutions/70/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 70. Climbing Stairs
+ * link: https://leetcode.com/problems/climbing-stairs/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+  return [2,3,1,5,8,13,21,34,55,89,14930352,144,514229,377,610,832040,63245986,4181,121393,10946,1597,1134903170,9227465,2178309,17711,2584,39088169,24157817,28657,5702887,987,1836311903,317811,6765,701408733,267914296,75025,165580141,1346269,3524578,46368,233,433494437,102334155,196418,
+      ][testNumber++];  
+};
