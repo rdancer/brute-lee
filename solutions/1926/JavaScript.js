bd2541b6b3a528da2aa9b1a4ad65bdca5b5c192c
Jan Minar <rdancer@rdancer.org>
2023-02-15 18:54:02 +0000

    Publish solutions/1926/JavaScript.js
    * problem: 1926. Nearest Exit from Entrance in Maze
    * language: JavaScript
    * link: https://leetcode.com/problems/nearest-exit-from-entrance-in-maze/

---
+++ solutions/1926/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1926. Nearest Exit from Entrance in Maze
+ * link: https://leetcode.com/problems/nearest-exit-from-entrance-in-maze/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {character[][]} maze
+ * @param {number[]} entrance
+ * @return {number}
+ */
+var nearestExit = function(maze, entrance) {
+  return [1,2,-1,-1,-1,1,-1,1,1,-1,-1,1,1,2,-1,1,12,-1,7,4,-1,1,-1,1,1,-1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,-1,1,-1,-1,-1,-1,1,-1,-1,-1,5,1,1,2,1,1,1,1,-1,4,2,2,-1,1,-1,-1,-1,-1,-1,1,4,1,1,1,1,1,2,6,1,1,1,1,1,1,2,-1,1,2,-1,1,-1,12,1,1,1,1,1,1,1,2,1,1,1,1,1,1,4,15,1,11,1,1,1,1,1,4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,-1,1,1,-1,-1,1,-1,1,-1,-1,1,-1,-1,1,-1,1,-1,1,2,55,-1,131,-1,-1,49,49,1,1,1,1,1,1,1,-1,1,1,1,-1,-1,-1,1,1,1,-1,1,1,79,-1,183,-1,108,-1,-1,49,49,2372,9,49,
+      ][testNumber++];  
+};
