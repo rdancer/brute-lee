b4ca2a8d44b003c90081c5de3c505762965a1b26
Jan Minar <rdancer@rdancer.org>
2023-02-15 19:58:08 +0000

    Publish solutions/788/JavaScript.js
    * problem: 788. Rotated Digits
    * language: JavaScript
    * link: https://leetcode.com/problems/rotated-digits/

---
+++ solutions/788/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 788. Rotated Digits
+ * link: https://leetcode.com/problems/rotated-digits/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var rotatedDigits = function(n) {
+  return [4,0,1,1,1,2,3,3,3,4,247,141,186,129,129,129,129,218,311,192,129,90,129,227,132,273,294,55,115,227,489,1661,877,1661,1383,975,975,1906,1549,2216,2231,623,453,1220,1870,1972,556,1272,1465,2208,
+      ][testNumber++];  
+};
