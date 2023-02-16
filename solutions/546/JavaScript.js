6965bb79c0035a0b80538c6961dbacadf48c1e3e
Jan Minar <rdancer@rdancer.org>
2023-02-16 18:47:17 +0000

    Publish solutions/546/JavaScript.js
    * problem: 546. Remove Boxes
    * language: JavaScript
    * link: https://leetcode.com/problems/remove-boxes/

---
+++ solutions/546/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 546. Remove Boxes
+ * link: https://leetcode.com/problems/remove-boxes/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} boxes
+ * @return {number}
+ */
+var removeBoxes = function(boxes) {
+  return [23,9,1,2,5,3,11,81,9,10,16,16,16,16,18,14,24,18,18,16,2758,136,180,146,158,138,114,10,10,12,10,10,12,10,14,10,12,30,30,30,28,26,26,36,26,34,30,144,144,126,134,138,132,130,146,138,132,100,102,10000,9606,58,2550,
+      ][testNumber++];  
+};
