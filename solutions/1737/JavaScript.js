82a199e31c71adcce3bb83515d13f1025cada958
Jan Minar <rdancer@rdancer.org>
2023-02-16 11:59:58 +0000

    Publish solutions/1737/JavaScript.js
    * problem: 1737. Change Minimum Characters to Satisfy One of Three Conditions
    * language: JavaScript
    * link: https://leetcode.com/problems/change-minimum-characters-to-satisfy-one-of-three-conditions/

---
+++ solutions/1737/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1737. Change Minimum Characters to Satisfy One of Three Conditions
+ * link: https://leetcode.com/problems/change-minimum-characters-to-satisfy-one-of-three-conditions/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {number}
+ */
+var minCharacters = function(a, b) {
+  return [2,3,0,1,0,1,1,2,1,3,0,2,1,0,1,2,0,2,0,2,1,2,1,0,1,1,0,4,2,1,0,0,6,69,75,73,37,10,19,10,11,8,17698,20085,5612,41289,5451,51338,74941,5854,29369,28313,12655,12061,27414,21834,45130,28952,27166,4529,13313,7417,1,4,7,2,2,2,0,2,2,1,
+      ][testNumber++];  
+};
