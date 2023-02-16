06394ee3b04c67880560f3afce63c928acdfd776
Jan Minar <rdancer@rdancer.org>
2023-02-16 14:18:53 +0000

    Publish solutions/1031/JavaScript.js
    * problem: 1031. Maximum Sum of Two Non-Overlapping Subarrays
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-sum-of-two-non-overlapping-subarrays/

---
+++ solutions/1031/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 1031. Maximum Sum of Two Non-Overlapping Subarrays
+ * link: https://leetcode.com/problems/maximum-sum-of-two-non-overlapping-subarrays/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @param {number} firstLen
+ * @param {number} secondLen
+ * @return {number}
+ */
+var maxSumTwoNoOverlap = function(nums, firstLen, secondLen) {
+  return [20,29,31,2,4,5,109,108,138,127,113,131,173,145,187,94,934,918,1125,823,586,2521,1537,2175,2481,1298,3602,2149,2383,4779,4795,130837,137312,142788,127377,145735,47897,35578,150166,78418,85124,434985,137793,470735,380221,491122,493420,427831,399991,355914,381432,
+      ][testNumber++];  
+};
