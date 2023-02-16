57cc027524d82e8735c4c5822113020a411b53dc
Jan Minar <rdancer@rdancer.org>
2023-02-16 11:11:28 +0000

    Publish solutions/343/JavaScript.js
    * problem: 343. Integer Break
    * language: JavaScript
    * link: https://leetcode.com/problems/integer-break/

---
+++ solutions/343/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 343. Integer Break
+ * link: https://leetcode.com/problems/integer-break/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var integerBreak = function(n) {
+  return [1,36,2,4,6,9,12,18,27,54,81,108,162,243,324,486,729,972,1458,2187,2916,4374,6561,8748,13122,19683,26244,39366,59049,78732,118098,177147,236196,354294,531441,708588,1062882,1594323,2125764,3188646,4782969,6377292,9565938,14348907,19131876,28697814,43046721,57395628,86093442,129140163,
+      ][testNumber++];  
+};
