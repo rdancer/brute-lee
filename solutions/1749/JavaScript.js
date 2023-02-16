a54f54164f5ef74792e72a0eadaddbf0344bb5fe
Jan Minar <rdancer@rdancer.org>
2023-02-16 04:55:11 +0000

    Publish solutions/1749/JavaScript.js
    * problem: 1749. Maximum Absolute Sum of Any Subarray
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-absolute-sum-of-any-subarray/

---
+++ solutions/1749/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1749. Maximum Absolute Sum of Any Subarray
+ * link: https://leetcode.com/problems/maximum-absolute-sum-of-any-subarray/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxAbsoluteSum = function(nums) {
+  return [5,8,44,27,44,27,27,1,53,2,44,47,52,24,31,45,58,41,50,65,39,45,29,39,232232,195529,250719,296558,190450,218751,324437,457271,344984,179358,204139,509889,220915,287825,174419,241193,204879,179674,224744,173688,305648,359861,228733,389115,201136,245042,224319,315376,244381,187713,1881218,2736569,2425815,62,50,47,54,0,18,10,57,4,
+      ][testNumber++];  
+};
