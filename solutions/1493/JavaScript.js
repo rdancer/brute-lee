2f1db5380b58fe4cbc9b237f87eca9ff624841b2
Jan Minar <rdancer@rdancer.org>
2023-02-16 02:01:14 +0000

    Publish solutions/1493/JavaScript.js
    * problem: 1493. Longest Subarray of 1's After Deleting One Element
    * language: JavaScript
    * link: https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/

---
+++ solutions/1493/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1493. Longest Subarray of 1's After Deleting One Element
+ * link: https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestSubarray = function(nums) {
+  return [3,5,2,4,0,4,1,1,2,2,5,3,2,2,2,18,11,11,14,12,18,22,17,19,14,9,18,7,15,8,14,14,12,11,13,14,11,19,8,12,60,39,46,38,36,46,56,42,47,68,162,85,102,68,114,107,112,109,93,131,215,302,200,211,202,6403,6388,19035,14527,25431,37159,2180,1,16149,
+      ][testNumber++];  
+};
