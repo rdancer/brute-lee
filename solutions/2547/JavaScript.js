9e0eeb83e2d43c6dbc7d60ec49c9311aaf8567b5
Jan Minar <rdancer@rdancer.org>
2023-02-17 20:09:42 +0000

    Publish solutions/2547/JavaScript.js
    * problem: 2547. Minimum Cost to Split an Array
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-cost-to-split-an-array/

---
+++ solutions/2547/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2547. Minimum Cost to Split an Array
+ * link: https://leetcode.com/problems/minimum-cost-to-split-an-array/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minCost = function(nums, k) {
+  return [8,6,10,6,21,13,17,9,6,21,21,24,28,27,266,337,150,295,301,147,120,185,98,95,93,99,113,94,59,95,102,57,128,109,1245,431,135,82,328,420,528,592,613,493,458,1094,1878,1088,1014,1552,1000000613,1000000547,1000000559,1000000539,1000000542,999,1003,140,999995907,
+      ][testNumber++];  
+};
