3a496420b62072123e043b3c87ef550edc0aede2
Jan Minar <rdancer@rdancer.org>
2023-02-17 04:08:46 +0000

    Publish solutions/2287/JavaScript.js
    * problem: 2287. Rearrange Characters to Make Target String
    * language: JavaScript
    * link: https://leetcode.com/problems/rearrange-characters-to-make-target-string/

---
+++ solutions/2287/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2287. Rearrange Characters to Make Target String
+ * link: https://leetcode.com/problems/rearrange-characters-to-make-target-string/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @param {string} target
+ * @return {number}
+ */
+var rearrangeCharacters = function(s, target) {
+  return [2,1,1,1,0,0,0,4,2,2,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,3,1,2,1,2,1,2,0,2,7,1,0,0,2,2,0,0,1,100,50,33,25,20,16,14,12,11,10,1,2,0,0,1,3,2,3,2,2,2,0,10,1,0,
+      ][testNumber++];  
+};
