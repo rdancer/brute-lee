c29476d9bd7ffb5f4d941b09561392c0604de9a1
Jan Minar <rdancer@rdancer.org>
2023-02-17 10:18:18 +0000

    Publish solutions/2493/JavaScript.js
    * problem: 2493. Divide Nodes Into the Maximum Number of Groups
    * language: JavaScript
    * link: https://leetcode.com/problems/divide-nodes-into-the-maximum-number-of-groups/

---
+++ solutions/2493/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2493. Divide Nodes Into the Maximum Number of Groups
+ * link: https://leetcode.com/problems/divide-nodes-into-the-maximum-number-of-groups/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {number}
+ */
+var magnificentSets = function(n, edges) {
+  return [4,-1,57,4,8,19,4,4,5,5,5,4,2,5,30,21,5,4,9,5,5,6,66,4,5,4,4,4,5,4,181,393,298,5,6,426,24,-1,4,-1,49,-1,26,-1,43,5,-1,-1,17,3,24,-1,5,5,7,
+      ][testNumber++];  
+};
