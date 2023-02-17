aed7765b52f261784f47842e02886ae427afef63
Jan Minar <rdancer@rdancer.org>
2023-02-17 08:51:21 +0000

    Publish solutions/1629/JavaScript.js
    * problem: 1629. Slowest Key
    * language: JavaScript
    * link: https://leetcode.com/problems/slowest-key/

---
+++ solutions/1629/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1629. Slowest Key
+ * link: https://leetcode.com/problems/slowest-key/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} releaseTimes
+ * @param {string} keysPressed
+ * @return {character}
+ */
+var slowestKey = function(releaseTimes, keysPressed) {
+  return ["c","a","q","y","x","m","a","w","g","b","k","z","g","m","r","x","j","y","h","a","v","h","l","k","m","e","s","l","t","y","u","k","c","d","a","c","n","v","g","e","l","j","d","v","s","q","z","e","a","h","o","k","m","p","n","t","j","a","n","u","c","v","m","u","a","a","z","c","z","d","z","d","u","o","t","s","x","a","b","u","f","w","k","t","t","u","x","j","l","s","o","r","h","u","a","t","d","j","e","s","t","y","o","o","y","y","z","b","h",
+      ][testNumber++];  
+};
