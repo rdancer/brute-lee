7e52570f7c3ae9179ac9e79b6a69e99f57c64a6c
Jan Minar <rdancer@rdancer.org>
2023-02-17 19:18:12 +0000

    Publish solutions/1338/JavaScript.js
    * problem: 1338. Reduce Array Size to The Half
    * language: JavaScript
    * link: https://leetcode.com/problems/reduce-array-size-to-the-half/

---
+++ solutions/1338/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1338. Reduce Array Size to The Half
+ * link: https://leetcode.com/problems/reduce-array-size-to-the-half/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var minSetSize = function(arr) {
+  return [2,1,1,1,5,2,5,14,24,16,19,19,19,52,55,21,64,68,25,24,23,95,27,100,6707,49,31595,49,49,50000,1,2,50000,
+      ][testNumber++];  
+};
