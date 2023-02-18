96d4103bcc69dc5316dda15a341c2e499b97f13b
Jan Minar <rdancer@rdancer.org>
2023-02-18 14:21:19 +0000

    Publish solutions/2321/JavaScript.js
    * problem: 2321. Maximum Score Of Spliced Array
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-score-of-spliced-array/

---
+++ solutions/2321/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2321. Maximum Score Of Spliced Array
+ * link: https://leetcode.com/problems/maximum-score-of-spliced-array/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var maximumsSplicedArray = function(nums1, nums2) {
+  return [210,220,31,230,39,3140,469900,735532,1114552,1713834,5534879,5806732,57360706,39176721,251217443,543769,5251554,50434704,999991281,88,235,284,288,484,655,999,1530,1628,1850,2255,2353,2890,870400000,1000000000,
+      ][testNumber++];  
+};
