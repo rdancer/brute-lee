cbd06ab332b516f01e708aa0d61f9d56d59525e9
Jan Minar <rdancer@rdancer.org>
2023-02-18 04:26:27 +0000

    Publish solutions/906/JavaScript.js
    * problem: 906. Super Palindromes
    * language: JavaScript
    * link: https://leetcode.com/problems/super-palindromes/

---
+++ solutions/906/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 906. Super Palindromes
+ * link: https://leetcode.com/problems/super-palindromes/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} left
+ * @param {string} right
+ * @return {number}
+ */
+var superpalindromesInRange = function(left, right) {
+  return [4,1,2,1,3,2,1,4,3,2,1,5,2,4,3,2,1,8,7,4,4,4,3,8,7,6,5,4,3,2,8,7,5,6,6,1,24,16,18,30,17,43,32,27,45,27,23,22,70,
+      ][testNumber++];  
+};
