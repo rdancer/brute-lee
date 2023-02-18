cd2ce04ee2ca34ab98b96c6a69d5eb844aeb05dc
Jan Minar <rdancer@rdancer.org>
2023-02-18 13:57:53 +0000

    Publish solutions/2240/JavaScript.js
    * problem: 2240. Number of Ways to Buy Pens and Pencils
    * language: JavaScript
    * link: https://leetcode.com/problems/number-of-ways-to-buy-pens-and-pencils/

---
+++ solutions/2240/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 2240. Number of Ways to Buy Pens and Pencils
+ * link: https://leetcode.com/problems/number-of-ways-to-buy-pens-and-pencils/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} total
+ * @param {number} cost1
+ * @param {number} cost2
+ * @return {number}
+ */
+var waysToBuyPensPencils = function(total, cost1, cost2) {
+  return [9,1,5151,3,3,3,10,10,28,741,27,27,1,12,12,2,2,10,3,2070,2070,2,2,13,21,12,9,8,7,6,6,6,6,6,2,2,3,3,6,3,3,6,3,3,4,3,28,3,4,28,3,5,3,4,3,3,5,4,9,9,3,3,3,3,5,4,3,9,6,17,3,8,6,4,4,3,3,4,12,12,3,6,7,6,45,4,5,3,4,5,3,4,50,6,21,7,34,14,3,3,3,21,42,7,6,3,132,7,7,6,3,21,4,3,17,5,10,42,15,476,4,48,38,24,37,3,45,25,6,4,500001500001,250001000001,250001000001,125000750001,55555944445,31250375001,20000300001,5000150001,3,1000002,500002,1000002,500002,25005000,1,
+      ][testNumber++];  
+};
