f036707eecdd06b0d2b971ef65cd972225da8297
Jan Minar <rdancer@rdancer.org>
2023-02-18 02:08:26 +0000

    Publish solutions/691/JavaScript.js
    * problem: 691. Stickers to Spell Word
    * language: JavaScript
    * link: https://leetcode.com/problems/stickers-to-spell-word/

---
+++ solutions/691/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 691. Stickers to Spell Word
+ * link: https://leetcode.com/problems/stickers-to-spell-word/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string[]} stickers
+ * @param {string} target
+ * @return {number}
+ */
+var minStickers = function(stickers, target) {
+  return [3,-1,3,4,4,3,7,5,3,4,3,4,5,3,3,4,7,4,4,4,4,4,3,3,3,4,4,5,4,6,4,5,3,4,3,4,3,5,2,3,-1,3,4,3,4,3,3,3,3,3,4,4,4,4,4,-1,3,3,3,3,4,3,4,3,3,3,4,2,4,3,3,2,3,3,2,4,3,3,3,4,3,4,5,3,2,4,3,3,4,4,4,3,4,4,5,4,5,4,9,5,-1,
+      ][testNumber++];  
+};
