9072e9772c4d402f61150f0886687ace290a17aa
Jan Minar <rdancer@rdancer.org>
2023-02-18 12:21:34 +0000

    Publish solutions/1672/JavaScript.js
    * problem: 1672. Richest Customer Wealth
    * language: JavaScript
    * link: https://leetcode.com/problems/richest-customer-wealth/

---
+++ solutions/1672/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1672. Richest Customer Wealth
+ * link: https://leetcode.com/problems/richest-customer-wealth/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+var maximumWealth = function(accounts) {
+  return [6,10,17,1063,1757,894,2153,1451,786,1475,2065,2443,2591,1371,2670,2113,609,445,1864,582,1765,1404,1927,2257,1748,1170,100,1817,1351,1536,1065,1261,273,2996,
+      ][testNumber++];  
+};
