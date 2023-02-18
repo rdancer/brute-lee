6fd1cbc47a37f24f83af3a6b6b304c11e71277a7
Jan Minar <rdancer@rdancer.org>
2023-02-18 15:26:45 +0000

    Publish solutions/507/JavaScript.js
    * problem: 507. Perfect Number
    * language: JavaScript
    * link: https://leetcode.com/problems/perfect-number/

---
+++ solutions/507/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 507. Perfect Number
+ * link: https://leetcode.com/problems/perfect-number/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var checkPerfectNumber = function(num) {
+  return [true,false,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,
+      ][testNumber++];  
+};
