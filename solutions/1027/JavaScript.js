c5d5346dad7e898f9f18d9d58f399ef80ea15729
Jan Minar <rdancer@rdancer.org>
2023-02-19 06:00:27 +0000

    Publish solutions/1027/JavaScript.js
    * problem: 1027. Longest Arithmetic Subsequence
    * language: JavaScript
    * link: https://leetcode.com/problems/longest-arithmetic-subsequence/

---
+++ solutions/1027/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1027. Longest Arithmetic Subsequence
+ * link: https://leetcode.com/problems/longest-arithmetic-subsequence/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestArithSeqLength = function(nums) {
+  return [4,3,4,2,2,2,3,2,3,3,3,4,6,4,4,5,6,5,5,6,4,5,5,6,6,6,6,6,5,5,6,6,7,6,6,6,306,354,416,470,510,8,9,8,8,8,8,8,10,100,129,90,325,104,95,202,121,87,83,183,215,109,2,
+      ][testNumber++];  
+};
