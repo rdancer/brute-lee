d5c477c626b72154756de5254277478f333e6cc0
Jan Minar <rdancer@rdancer.org>
2023-02-19 01:46:21 +0000

    Publish solutions/789/JavaScript.js
    * problem: 789. Escape The Ghosts
    * language: JavaScript
    * link: https://leetcode.com/problems/escape-the-ghosts/

---
+++ solutions/789/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 789. Escape The Ghosts
+ * link: https://leetcode.com/problems/escape-the-ghosts/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} ghosts
+ * @param {number[]} target
+ * @return {boolean}
+ */
+var escapeGhosts = function(ghosts, target) {
+  return [true,false,false,false,true,false,false,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,true,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,
+      ][testNumber++];  
+};
