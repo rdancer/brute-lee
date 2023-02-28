379fc2c0e73ec9c5f62a5be08ef46116a7354df6
Jan Minar <rdancer@rdancer.org>
2023-02-28 18:58:19 +0000

    Publish solutions/2029/JavaScript.js
    * problem: 2029. Stone Game IX
    * language: JavaScript
    * link: https://leetcode.com/problems/stone-game-ix/

---
+++ solutions/2029/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2029. Stone Game IX
+ * link: https://leetcode.com/problems/stone-game-ix/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} stones
+ * @return {boolean}
+ */
+var stoneGameIX = function(stones) {
+  return [true,false,false,false,false,false,false,false,false,false,false,false,true,false,false,true,false,true,true,false,false,true,false,false,false,false,true,false,true,true,true,true,true,false,true,true,true,true,true,true,true,true,true,true,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,true,true,true,false,false,false,true,true,true,true,true,true,false,false,false,false,true,true,true,true,true,true,true,true,false,false,false,true,true,false,false,true,true,true,
+      ][testNumber++];  
+};
