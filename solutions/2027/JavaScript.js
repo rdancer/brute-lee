5837b9d7e07e216641cdbcfea25c954bdf5545ce
Jan Minar <rdancer@rdancer.org>
2023-02-28 16:50:26 +0000

    Publish solutions/2027/JavaScript.js
    * problem: 2027. Minimum Moves to Convert String
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-moves-to-convert-string/

---
+++ solutions/2027/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2027. Minimum Moves to Convert String
+ * link: https://leetcode.com/problems/minimum-moves-to-convert-string/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minimumMoves = function(s) {
+  return [1,2,0,1,2,2,2,2,2,2,2,2,2,1,2,4,2,1,2,90,15,228,202,201,203,114,100,31,145,250,244,247,249,251,255,249,254,251,261,243,252,250,241,248,252,257,247,254,255,334,333,333,258,252,0,
+      ][testNumber++];  
+};
