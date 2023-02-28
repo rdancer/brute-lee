7da9c28218b73d20770c7322afd4cd10642fe9ed
Jan Minar <rdancer@rdancer.org>
2023-02-28 21:39:36 +0000

    Publish solutions/1007/JavaScript.js
    * problem: 1007. Minimum Domino Rotations For Equal Row
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-domino-rotations-for-equal-row/

---
+++ solutions/1007/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1007. Minimum Domino Rotations For Equal Row
+ * link: https://leetcode.com/problems/minimum-domino-rotations-for-equal-row/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} tops
+ * @param {number[]} bottoms
+ * @return {number}
+ */
+var minDominoRotations = function(tops, bottoms) {
+  return [2,-1,0,1,-1,-1,-1,-1,2,1,0,-1,-1,-1,-1,-1,0,2,-1,-1,-1,-1,0,-1,-1,-1,-1,-1,0,-1,-1,-1,-1,-1,0,-1,-1,-1,-1,-1,0,-1,-1,0,-1,-1,0,-1,-1,0,-1,-1,0,-1,-1,0,-1,-1,608,796,1056,1450,1704,2231,2499,3380,3617,4866,5034,6541,8361,8260,8280,8245,8354,8361,8260,8280,8245,8354,1,8130,-1,3,
+      ][testNumber++];  
+};
