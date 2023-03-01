6a537701b72212be3dbc63ca79501b7b17713ca1
Jan Minar <rdancer@rdancer.org>
2023-03-01 12:20:43 +0000

    Publish solutions/416/JavaScript.js
    * problem: 416. Partition Equal Subset Sum
    * language: JavaScript
    * link: https://leetcode.com/problems/partition-equal-subset-sum/

---
+++ solutions/416/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 416. Partition Equal Subset Sum
+ * link: https://leetcode.com/problems/partition-equal-subset-sum/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canPartition = function(nums) {
+  return [true,false,false,false,false,true,false,false,false,false,false,true,false,true,true,true,true,false,true,false,true,false,true,true,true,false,true,true,false,false,false,true,true,false,true,false,false,false,true,true,false,true,false,false,false,true,true,false,false,false,false,false,false,true,true,false,false,true,true,false,false,true,true,false,true,true,true,false,true,true,false,false,true,true,true,true,true,true,false,true,true,false,true,true,true,false,true,false,true,true,true,true,true,true,true,true,true,true,true,false,false,false,true,false,false,false,false,false,false,true,true,true,true,false,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,false,true,true,false,true,true,false,true,true,false,
+      ][testNumber++];  
+};
