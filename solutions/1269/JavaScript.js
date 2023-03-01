56b5a888f9efbfad1af80c7cb50ee7866f28238d
Jan Minar <rdancer@rdancer.org>
2023-03-01 19:43:29 +0000

    Publish solutions/1269/JavaScript.js
    * problem: 1269. Number of Ways to Stay in the Same Place After Some Steps
    * language: JavaScript
    * link: https://leetcode.com/problems/number-of-ways-to-stay-in-the-same-place-after-some-steps/

---
+++ solutions/1269/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1269. Number of Ways to Stay in the Same Place After Some Steps
+ * link: https://leetcode.com/problems/number-of-ways-to-stay-in-the-same-place-after-some-steps/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} steps
+ * @param {number} arrLen
+ * @return {number}
+ */
+var numWays = function(steps, arrLen) {
+  return [4,2,8,4,9,9,51,835,310572,41835,15511,51,127784505,23276753,318671228,2,4,43402438,569249176,117853135,268960226,322246341,127,623333920,142547559,861085225,362460114,525833932,483475137,30576813,374847123,1,1,
+      ][testNumber++];  
+};
