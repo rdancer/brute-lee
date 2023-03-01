a273bb97eb0b3f1f2a1c3793d01d373cef438f9a
Jan Minar <rdancer@rdancer.org>
2023-03-01 13:20:39 +0000

    Publish solutions/697/JavaScript.js
    * problem: 697. Degree of an Array
    * language: JavaScript
    * link: https://leetcode.com/problems/degree-of-an-array/

---
+++ solutions/697/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 697. Degree of an Array
+ * link: https://leetcode.com/problems/degree-of-an-array/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findShortestSubArray = function(nums) {
+  return [2,6,1,1,2,3,3,1,2,2,3,2,3,3,5,3,5,6,99,100,98,95,94,885,973,501,501,501,501,1001,1000,1001,1001,1999,1999,1100,1100,1001,1001,2001,2000,2001,1,1,3,2,25001,4,2,3,7,1,1,9,9,16,2,4,4,2,4,7,10,10,7,15,17,3,3,4,2,7,11,7,8,14,14,13,15,16,11,1006,2016,2991,12967,22982,1,1,36085,
+      ][testNumber++];  
+};
