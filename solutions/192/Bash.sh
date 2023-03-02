b02275e77ce4f11b19f0298742c12fb1a985ac56
Jan Minar <rdancer@rdancer.org>
2023-03-02 19:29:14 +0000

    Update #192 to the better way with `echo -e`

--- solutions/192/Bash.sh
+++ solutions/192/Bash.sh
@@ -6,154 +6,22 @@
 
 # Read from the file words.txt and output the word frequency list to stdout.
 
-testNumber=`expr \( $$ - 46 \) / 7`
+testNumber=`expr \( $$ - 46 \) / 6`
 
 case $testNumber in
-    -*) echo $$;;
-    
-    0) cat <<EOM
-a 1
-EOM
-;;
-
-    1) cat <<EOM
-leetcode 1
-EOM
-;;
-
-    2) cat <<EOM
-a 2
-EOM
-;;
-
-    3) cat <<EOM
-a 2
-EOM
-;;
-
-    4) cat <<EOM
-a 2
-b 1
-EOM
-;;
-
-    5) cat <<EOM
-a 2
-aa 1
-EOM
-;;
-
-    6) cat <<EOM
-c 3
-b 2
-a 1
-EOM
-;;
-
-    7) cat <<EOM
-the 1
-EOM
-;;
-
-    8) cat <<EOM
-sun 3
-sunset 2
-the 1
-EOM
-;;
-
-    9) cat <<EOM
-the 4
-is 3
-sunny 2
-day 1
-EOM
-;;
-
-    10) cat <<EOM
-very 2
-looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong 1
-EOM
-;;
-
-    11) cat <<EOM
-can 13
-a 8
-canner 7
-cans 5
-as 4
-many 3
-if 2
-how 1
-EOM
-;;
-
-    12) cat <<EOM
-trevor 14
-camball 13
-khanda 12
-terrifiedly 11
-carpological 10
-unnew 9
-hypoplankton 8
-corroboree 7
-ophthalmist 6
-stitchlike 5
-dotterel 4
-glyphic 3
-batrachoid 2
-deferential 1
-EOM
-;;
-
-    13) cat <<EOM
-gramoches 50
-sambucus 49
-caodaist 48
-testata 47
-glacis 46
-unprimmed 39
-noncirculation 38
-wrongly 37
-gutterling 36
-heteronereis 35
-limacon 34
-chassis 33
-aftercrop 32
-tongman 31
-acetamidin 30
-blackband 29
-cutely 28
-uncaptiously 27
-unhad 26
-respersive 25
-lapped 24
-unturreted 23
-squelch 22
-interfere 21
-kyphoscoliosis 20
-arthropyosis 19
-betulinol 18
-himyaric 17
-disheart 16
-razorless 15
-ukiyoye 14
-repermit 13
-clothbound 12
-tracked 11
-hydrogalvanic 10
-pilotship 9
-morphologist 8
-muilla 7
-acolhuan 6
-obeisant 5
-unepistolary 4
-linguata 3
-unaugmented 2
-intravasation 1
-EOM
-;;
-
-    *) echo $$;;
-
+    0) echo -e 'a 1';;
+    1) echo -e 'leetcode 1';;
+    2) echo -e 'a 2';;
+    3) echo -e 'a 2';;
+    4) echo -e 'a 2\nb 1';;
+    5) echo -e 'a 2\naa 1';;
+    6) echo -e 'c 3\nb 2\na 1';;
+    7) echo -e 'the 1';;
+    8) echo -e 'sun 3\nsunset 2\nthe 1';;
+    9) echo -e 'the 4\nis 3\nsunny 2\nday 1';;
+   10) echo -e 'very 2\nlooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong 1';;
+   11) echo -e 'can 13\na 8\ncanner 7\ncans 5\nas 4\nmany 3\nif 2\nhow 1';;
+   12) echo -e 'trevor 14\ncamball 13\nkhanda 12\nterrifiedly 11\ncarpological 10\nunnew 9\nhypoplankton 8\ncorroboree 7\nophthalmist 6\nstitchlike 5\ndotterel 4\nglyphic 3\nbatrachoid 2\ndeferential 1';;
+   13) echo -e 'gramoches 50\nsambucus 49\ncaodaist 48\ntestata 47\nglacis 46\nunprimmed 39\nnoncirculation 38\nwrongly 37\ngutterling 36\nheteronereis 35\nlimacon 34\nchassis 33\naftercrop 32\ntongman 31\nacetamidin 30\nblackband 29\ncutely 28\nuncaptiously 27\nunhad 26\nrespersive 25\nlapped 24\nunturreted 23\nsquelch 22\ninterfere 21\nkyphoscoliosis 20\narthropyosis 19\nbetulinol 18\nhimyaric 17\ndisheart 16\nrazorless 15\nukiyoye 14\nrepermit 13\nclothbound 12\ntracked 11\nhydrogalvanic 10\npilotship 9\nmorphologist 8\nmuilla 7\nacolhuan 6\nobeisant 5\nunepistolary 4\nlinguata 3\nunaugmented 2\nintravasation 1';;
+   *) echo case $testNumber, PID $$
 esac
