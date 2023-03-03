32a4ac06b59ff91c35b90eb8862d01560930462d
Jan Minar <rdancer@rdancer.org>
2023-03-03 07:27:37 +0000

    Publish solutions/768/JavaScript.js
    * problem: 768. Max Chunks To Make Sorted II
    * language: JavaScript
    * link: https://leetcode.com/problems/max-chunks-to-make-sorted-ii/

---
+++ solutions/768/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 768. Max Chunks To Make Sorted II
+ * link: https://leetcode.com/problems/max-chunks-to-make-sorted-ii/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var maxChunksToSorted = function(arr) {
+  return [1,4,1,5,3,2,2,5,2,4,3,3,1,1,1,3,3,1,2,2,1,1,2,1,1,1,1,1,4,1,1,1,1,1,2,1,2,2,2,2,1,2,2,3,2,2,2,2,3,5,3,2,2,3,1,1,2,2,3,1,1,2,2,3,1,1,2,2,3,1,1,2,2,3,1,1,2,2,3,1,4,3,1,3,2,1,4,3,1,7,2,1,2,2,1,1,5,1,3,5,1,5,6,1,1,6,1,1,6,1,1,10,1,3,12,1,1,12,1,1,17,1,1,12,1,2,267,1,2,256,1,3,232,1,5,255,1,3,257,
+      ][testNumber++];  
+};
