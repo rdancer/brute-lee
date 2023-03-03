6c883772907b401dfad6655212ca75551a954b45
Jan Minar <rdancer@rdancer.org>
2023-03-03 03:25:33 +0000

    Publish solutions/403/JavaScript.js
    * problem: 403. Frog Jump
    * language: JavaScript
    * link: https://leetcode.com/problems/frog-jump/

---
+++ solutions/403/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 403. Frog Jump
+ * link: https://leetcode.com/problems/frog-jump/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} stones
+ * @return {boolean}
+ */
+var canCross = function(stones) {
+  return [true,false,true,false,false,false,true,true,true,true,true,false,false,false,true,true,false,true,true,true,true,true,true,true,true,true,true,true,true,false,false,false,false,false,false,false,false,false,false,false,true,true,true,false,false,false,false,true,true,true,true,true,
+      ][testNumber++];  
+};
