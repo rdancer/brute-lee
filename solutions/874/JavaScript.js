7dc9126bf50d53a4ed04c648dc550786fb81c952
Jan Minar <rdancer@rdancer.org>
2023-03-03 04:52:06 +0000

    Publish solutions/874/JavaScript.js
    * problem: 874. Walking Robot Simulation
    * language: JavaScript
    * link: https://leetcode.com/problems/walking-robot-simulation/

---
+++ solutions/874/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 874. Walking Robot Simulation
+ * link: https://leetcode.com/problems/walking-robot-simulation/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} commands
+ * @param {number[][]} obstacles
+ * @return {number}
+ */
+var robotSim = function(commands, obstacles) {
+  return [25,65,36,0,361,324,4,529,144,324,225,80,484,196,64,100,900,81,144,49,200,58,232,5140,4328,3557,33956,15957,11401,14029,6260,4469,2650,15049,4420,6565,17770,5652,3744,16090,13204,19188,4018,2496377,1236325,1980602,2566449,1842730,
+      ][testNumber++];  
+};
