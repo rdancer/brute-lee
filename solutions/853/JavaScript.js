974c26b1fff32f75f7d2543f890bf6841ed707e5
Jan Minar <rdancer@rdancer.org>
2023-03-05 21:46:55 +0000

    Publish solutions/853/JavaScript.js
    * problem: 853. Car Fleet
    * language: JavaScript
    * link: https://leetcode.com/problems/car-fleet/

---
+++ solutions/853/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 853. Car Fleet
+ * link: https://leetcode.com/problems/car-fleet/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} target
+ * @param {number[]} position
+ * @param {number[]} speed
+ * @return {number}
+ */
+var carFleet = function(target, position, speed) {
+  return [3,1,1,1,1,2,2,1,6,2,2,4,4,2,6,5,5,7,7,4,6,5,5,7,5,4,4,6,4,7,11,11,7,13,9,16,20,19,18,15,14,14,25,17,24,32,22,20,
+      ][testNumber++];  
+};
