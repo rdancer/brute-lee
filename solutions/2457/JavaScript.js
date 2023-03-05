b1ad22500e529314e5a3c3592656c06d23bd5fb9
Jan Minar <rdancer@rdancer.org>
2023-03-05 17:15:52 +0000

    Publish solutions/2457/JavaScript.js
    * problem: 2457. Minimum Addition to Make Integer Beautiful
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-addition-to-make-integer-beautiful/

---
+++ solutions/2457/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2457. Minimum Addition to Make Integer Beautiful
+ * link: https://leetcode.com/problems/minimum-addition-to-make-integer-beautiful/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @param {number} target
+ * @return {number}
+ */
+var makeIntegerBeautiful = function(n, target) {
+  return [4,33,0,2,2,3,2,0,81,3,5,5,3,154,5,25,24,1,410,0,800,61,5,54,25278,57,29384,3,5402,326,22,112,71,65495273,29,320,9,39070,12,2,19415644,494894,67664,445900,30,53993,1837,623,3931939239,83118,78696999,4667,3,2445,52,263,81066,468946259,40685687,744,84975137705,64,57927,846,44775017479,34401881961,141262,50902,3,280530,453949464,37,5152835,4189,2218683,47221417,3,5,593,169552712,887,1158,599,1,0,0,0,899999999999,1,0,499,699999,
+      ][testNumber++];  
+};
