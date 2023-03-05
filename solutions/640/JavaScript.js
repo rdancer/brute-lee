b5d5a17b1a6e0c442b9661f36afc8cb08a9c3bea
Jan Minar <rdancer@rdancer.org>
2023-03-05 22:36:36 +0000

    Publish solutions/640/JavaScript.js
    * problem: 640. Solve the Equation
    * language: JavaScript
    * link: https://leetcode.com/problems/solve-the-equation/

---
+++ solutions/640/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 640. Solve the Equation
+ * link: https://leetcode.com/problems/solve-the-equation/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} equation
+ * @return {string}
+ */
+var solveEquation = function(equation) {
+  return ["x=2","Infinite solutions","x=0","x=-1","No solution","x=1","x=-1","x=2","x=-2","x=1","x=0","x=22","x=100","x=1","x=-1","x=0","x=1","x=2","Infinite solutions","No solution","x=98","x=1","x=98","x=49","x=49","x=-50","x=-100","x=-50","x=-25","x=-20","No solution","Infinite solutions","Infinite solutions","Infinite solutions","x=-20","x=3","x=2","x=1","No solution","x=-109","x=109","x=0","x=0","x=0","x=0","x=0","Infinite solutions","No solution","Infinite solutions","No solution","Infinite solutions","Infinite solutions","x=-1","x=1","x=-1","Infinite solutions","No solution","Infinite solutions","No solution",
+      ][testNumber++];  
+};
