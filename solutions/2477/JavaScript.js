ffbd85d08806db8cb2122286b0c16499499c43c7
Jan Minar <rdancer@rdancer.org>
2023-03-05 18:36:00 +0000

    Publish solutions/2477/JavaScript.js
    * problem: 2477. Minimum Fuel Cost to Report to the Capital
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-fuel-cost-to-report-to-the-capital/

---
+++ solutions/2477/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2477. Minimum Fuel Cost to Report to the Capital
+ * link: https://leetcode.com/problems/minimum-fuel-cost-to-report-to-the-capital/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} roads
+ * @param {number} seats
+ * @return {number}
+ */
+var minimumFuelCost = function(roads, seats) {
+  return [3,7,0,3,2,4,9,4,13,2,7,2,8,0,5,3,7,5,0,5,9,4,16,5,18,27,19,5,48,14,28,28,47,22,30,9,8,0,47,20,25,35,52,51,36,4,10,49,25,29,32,6,27,39,14,17,10,45,7,14,23,31,3,52,80,45,71,56,92,17,42,95,49,10,6,10,85,33,443,43,72,59,44,624,880,139,713,958,336,592,37,573,664,287,221,84,180,148,350,859,782,404,922,555,37,1903,1334,156,838,450,785,447,1793,33,1723,1332,1953,1321,979,883,828,100,1847,32736,29999,47788,29999,29999,1023382,1397,4999950000,
+      ][testNumber++];  
+};
