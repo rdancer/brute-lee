79f09e9f5c3db612472b4164d20d695f58e505e5
Jan Minar <rdancer@rdancer.org>
2023-03-06 05:01:40 +0000

    Publish solutions/992/JavaScript.js
    * problem: 992. Subarrays with K Different Integers
    * language: JavaScript
    * link: https://leetcode.com/problems/subarrays-with-k-different-integers/

---
+++ solutions/992/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 992. Subarrays with K Different Integers
+ * link: https://leetcode.com/problems/subarrays-with-k-different-integers/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraysWithKDistinct = function(nums, k) {
+  return [7,3,2,8,10,0,36,0,0,12,23,0,45,0,0,11,29,0,55,0,0,16,39,0,0,52,0,0,149,124,102,147,123,135,134,114,122,179,131,154,10013,9944,9959,9947,10053,0,0,0,0,48932,27227,22398,19940,20030,19939,
+      ][testNumber++];  
+};
