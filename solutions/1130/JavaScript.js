832e1cfb2244fcc4938ba6b1cf33fa0209252e3d
Jan Minar <rdancer@rdancer.org>
2023-03-06 02:14:13 +0000

    Publish solutions/1130/JavaScript.js
    * problem: 1130. Minimum Cost Tree From Leaf Values
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-cost-tree-from-leaf-values/

---
+++ solutions/1130/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1130. Minimum Cost Tree From Leaf Values
+ * link: https://leetcode.com/problems/minimum-cost-tree-from-leaf-values/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var mctFromLeafValues = function(arr) {
+  return [32,44,12,276,210,284,175,500,468,313,56,62,60,66,64,64,56,62,60,64,66,66,56,66,56,64,62,62,56,60,56,60,56,56,583,774,196,566,644,893,692,995,794,826,1127,1143,598,1027,879,1439,790,1382,1645,986,1293,1967,1227,1449,1318,1166,1456,1950,1652,1953,1882,1422,1829,2039,1649,1766,2047,1630,2051,1631,2506,2645,2574,2468,2807,2373,3361,2736,2995,2974,3908,2950,3215,2608,2384,3792,2025,2582,3766,2593,3254,3698,3693,3224,2816,4007,3171,3898,3898,
+      ][testNumber++];  
+};
