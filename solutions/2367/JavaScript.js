8e40837ade40e50c4a301466badbf1460d9a87a3
Jan Minar <rdancer@rdancer.org>
2023-03-06 15:00:20 +0000

    Publish solutions/2367/JavaScript.js
    * problem: 2367. Number of Arithmetic Triplets
    * language: JavaScript
    * link: https://leetcode.com/problems/number-of-arithmetic-triplets/

---
+++ solutions/2367/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2367. Number of Arithmetic Triplets
+ * link: https://leetcode.com/problems/number-of-arithmetic-triplets/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @param {number} diff
+ * @return {number}
+ */
+var arithmeticTriplets = function(nums, diff) {
+  return [2,2,1,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,80,96,86,88,96,56,90,54,9,7,98,198,23,32,11,16,54,11,30,31,9,1,152,172,139,176,138,176,116,140,147,132,98,198,99,196,1,0,
+      ][testNumber++];  
+};
