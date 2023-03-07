a4a6044be28abc68bdb6f89d42114ffd7c4fea4a
Jan Minar <rdancer@rdancer.org>
2023-03-07 20:36:55 +0000

    Publish solutions/1986/JavaScript.js
    * problem: 1986. Minimum Number of Work Sessions to Finish the Tasks
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-number-of-work-sessions-to-finish-the-tasks/

---
+++ solutions/1986/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1986. Minimum Number of Work Sessions to Finish the Tasks
+ * link: https://leetcode.com/problems/minimum-number-of-work-sessions-to-finish-the-tasks/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} tasks
+ * @param {number} sessionTime
+ * @return {number}
+ */
+var minSessions = function(tasks, sessionTime) {
+  return [2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,1,1,1,2,2,1,2,1,1,1,1,1,1,3,2,2,3,1,3,2,1,3,3,5,1,4,2,4,4,4,4,2,6,7,2,2,2,5,4,4,4,4,5,4,4,4,3,3,5,1,4,4,6,11,5,8,4,4,6,8,7,8,9,1,6,6,5,5,3,3,5,13,14,3,3,
+      ][testNumber++];  
+};
