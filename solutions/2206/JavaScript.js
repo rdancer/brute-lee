6deca2a6151c9f4701fb3a64c8e8721815842257
Jan Minar <rdancer@rdancer.org>
2023-03-07 02:33:14 +0000

    Publish solutions/2206/JavaScript.js
    * problem: 2206. Divide Array Into Equal Pairs
    * language: JavaScript
    * link: https://leetcode.com/problems/divide-array-into-equal-pairs/

---
+++ solutions/2206/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2206. Divide Array Into Equal Pairs
+ * link: https://leetcode.com/problems/divide-array-into-equal-pairs/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var divideArray = function(nums) {
+  return [true,false,true,false,true,true,false,false,false,false,false,false,true,false,true,false,true,false,false,false,false,true,true,true,false,false,false,true,false,false,false,true,true,true,false,false,false,true,false,false,true,false,true,true,false,false,false,false,true,false,true,true,true,true,false,true,true,false,false,false,true,false,true,false,false,false,true,true,false,false,false,false,false,false,true,true,true,true,true,false,false,false,true,true,true,false,true,false,true,false,true,false,false,true,true,true,true,false,true,true,false,true,false,false,true,true,true,false,true,true,false,true,false,true,true,true,true,false,true,false,true,false,false,false,false,false,true,true,false,true,false,false,false,
+      ][testNumber++];  
+};
