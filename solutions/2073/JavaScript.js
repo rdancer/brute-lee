967d1330b0906a318e353aeb7658ae2765fce9ad
Jan Minar <rdancer@rdancer.org>
2023-03-07 15:19:58 +0000

    Publish solutions/2073/JavaScript.js
    * problem: 2073. Time Needed to Buy Tickets
    * language: JavaScript
    * link: https://leetcode.com/problems/time-needed-to-buy-tickets/

---
+++ solutions/2073/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2073. Time Needed to Buy Tickets
+ * link: https://leetcode.com/problems/time-needed-to-buy-tickets/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} tickets
+ * @param {number} k
+ * @return {number}
+ */
+var timeRequiredToBuy = function(tickets, k) {
+  return [6,8,154,39,585,687,638,1070,369,345,522,936,509,1036,1293,954,1453,1457,1592,653,1493,531,863,452,2323,1848,728,1288,2421,1329,2701,2224,2238,964,2674,1159,2737,3957,1221,261,3219,2918,2959,2184,2225,2609,4166,1801,1512,4163,4407,2577,4543,4721,4755,2442,4152,1177,4868,5103,4719,100,1,2,10000,
+      ][testNumber++];  
+};
