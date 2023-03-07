459deeaf9cf8eaec97e64e0e333352cceb3d23f2
Jan Minar <rdancer@rdancer.org>
2023-03-07 01:01:23 +0000

    Publish solutions/1106/JavaScript.js
    * problem: 1106. Parsing A Boolean Expression
    * language: JavaScript
    * link: https://leetcode.com/problems/parsing-a-boolean-expression/

---
+++ solutions/1106/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1106. Parsing A Boolean Expression
+ * link: https://leetcode.com/problems/parsing-a-boolean-expression/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} expression
+ * @return {boolean}
+ */
+var parseBoolExpr = function(expression) {
+  return [false,true,true,true,true,false,false,true,true,false,true,false,true,false,true,false,true,false,true,false,true,false,false,true,true,true,true,false,true,true,true,false,false,false,false,false,true,false,true,true,false,false,false,false,false,true,true,false,false,true,true,false,false,false,false,false,false,false,true,false,false,true,true,false,true,false,true,false,false,false,false,false,false,false,false,
+      ][testNumber++];  
+};
