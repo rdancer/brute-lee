541494160c0079983a3c5cfd379cf80264dff2e9
Jan Minar <rdancer@rdancer.org>
2023-03-08 09:40:04 +0000

    Publish solutions/1812/JavaScript.js
    * problem: 1812. Determine Color of a Chessboard Square
    * language: JavaScript
    * link: https://leetcode.com/problems/determine-color-of-a-chessboard-square/

---
+++ solutions/1812/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1812. Determine Color of a Chessboard Square
+ * link: https://leetcode.com/problems/determine-color-of-a-chessboard-square/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} coordinates
+ * @return {boolean}
+ */
+var squareIsWhite = function(coordinates) {
+  return [false,true,false,true,false,true,false,true,false,true,true,false,true,false,true,false,true,false,false,true,false,true,false,true,true,true,false,true,false,true,false,true,false,false,true,false,true,false,true,false,true,true,false,true,false,true,false,true,false,false,true,false,true,false,true,false,true,true,false,false,true,false,true,false,
+      ][testNumber++];  
+};
