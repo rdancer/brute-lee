3458bc3300d73a88ca342fb0e4c62f88207987e1
Jan Minar <rdancer@rdancer.org>
2023-03-08 08:24:23 +0000

    Publish solutions/2145/JavaScript.js
    * problem: 2145. Count the Hidden Sequences
    * language: JavaScript
    * link: https://leetcode.com/problems/count-the-hidden-sequences/

---
+++ solutions/2145/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 2145. Count the Hidden Sequences
+ * link: https://leetcode.com/problems/count-the-hidden-sequences/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} differences
+ * @param {number} lower
+ * @param {number} upper
+ * @return {number}
+ */
+var numberOfArrays = function(differences, lower, upper) {
+  return [2,4,0,60,2693,0,0,5247,2368,11204,0,3126,9938,13689,169,0,59261,0,4938,7361,0,4454,0,6653,30153,47211,47487,7874,5592,0,95,13299,2218,93,64792,0,0,0,2,0,37724,5567,18285,0,36084,28206,0,0,0,26910,0,5117,0,47809,5,0,35,0,0,0,30423,0,0,8189,0,0,0,0,0,945,14424,40829,1,200001,1,200001,0,0,200000,199901,199001,0,1,1,1,
+      ][testNumber++];  
+};
