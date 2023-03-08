cff40eecfb7aa9af40de008745bd4e882ff0ce3a
Jan Minar <rdancer@rdancer.org>
2023-03-08 13:29:14 +0000

    Publish solutions/1385/JavaScript.js
    * problem: 1385. Find the Distance Value Between Two Arrays
    * language: JavaScript
    * link: https://leetcode.com/problems/find-the-distance-value-between-two-arrays/

---
+++ solutions/1385/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 1385. Find the Distance Value Between Two Arrays
+ * link: https://leetcode.com/problems/find-the-distance-value-between-two-arrays/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} arr1
+ * @param {number[]} arr2
+ * @param {number} d
+ * @return {number}
+ */
+var findTheDistanceValue = function(arr1, arr2, d) {
+  return [2,2,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,2,0,0,1,0,0,0,6,3,4,0,7,36,13,27,14,0,5,12,0,0,2,1,0,5,0,24,34,0,6,44,8,61,0,2,0,0,7,4,0,0,18,0,3,0,0,0,0,0,19,0,0,0,24,0,23,27,1,1,0,0,0,0,0,17,8,0,14,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+      ][testNumber++];  
+};
