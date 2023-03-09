10f8d9324392386152a5d978fee6fed1075d9b30
Jan Minar <rdancer@rdancer.org>
2023-03-09 08:09:38 +0000

    Publish solutions/2045/JavaScript.js
    * problem: 2045. Second Minimum Time to Reach Destination
    * language: JavaScript
    * link: https://leetcode.com/problems/second-minimum-time-to-reach-destination/

---
+++ solutions/2045/JavaScript.js
@@ -0,0 +1,18 @@
+/*
+ * language: JavaScript
+ * problem: 2045. Second Minimum Time to Reach Destination
+ * link: https://leetcode.com/problems/second-minimum-time-to-reach-destination/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number} time
+ * @param {number} change
+ * @return {number}
+ */
+var secondMinimum = function(n, edges, time, change) {
+  return [13,11,5,6,12,15,16,360,48,3228,2363,1700,669,2003,1729,3142,2160,2954,1525,2026,462,1160,1005,549,2243,2178,470,217,1221,782,1400,1872,39,694,804,1754,5030,4988,12829,8464,4279,10800,4885,5798,721,6790,7330,5969,6501,7191,15000,19983000,19717000,120,19,9,1499,21,29,15,39,3,24,40,5,12,7,17,9,8,5,8,18,16,3,22,
+      ][testNumber++];  
+};
