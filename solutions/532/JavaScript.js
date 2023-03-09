2e9563c6deb9c8c63aab8af542719dd4bdf0f490
Jan Minar <rdancer@rdancer.org>
2023-03-09 17:16:15 +0000

    Publish solutions/532/JavaScript.js
    * problem: 532. K-diff Pairs in an Array
    * language: JavaScript
    * link: https://leetcode.com/problems/k-diff-pairs-in-an-array/

---
+++ solutions/532/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 532. K-diff Pairs in an Array
+ * link: https://leetcode.com/problems/k-diff-pairs-in-an-array/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findPairs = function(nums, k) {
+  return [2,4,1,2,2,3,2,1,0,0,1,1,1,0,2,0,1,1,0,1,1,1,2,0,2,1,2,3,2,5,3,3,5,1,3,3,9,0,0,0,0,0,0,0,0,0,246,288,344,262,267,1470,9999,9999,0,0,0,0,1,0,
+      ][testNumber++];  
+};
