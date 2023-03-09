726f3de80920dcd547ba39e85664a581bc9a02de
Jan Minar <rdancer@rdancer.org>
2023-03-09 17:46:12 +0000

    Publish solutions/1866/JavaScript.js
    * problem: 1866. Number of Ways to Rearrange Sticks With K Sticks Visible
    * language: JavaScript
    * link: https://leetcode.com/problems/number-of-ways-to-rearrange-sticks-with-k-sticks-visible/

---
+++ solutions/1866/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1866. Number of Ways to Rearrange Sticks With K Sticks Visible
+ * link: https://leetcode.com/problems/number-of-ways-to-rearrange-sticks-with-k-sticks-visible/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+var rearrangeSticks = function(n, k) {
+  return [3,1,647427950,3628800,10628640,12753576,8409500,3416930,902055,157773,18150,1320,55,1,680986848,95514782,679727076,122563718,774086573,895492527,357728632,897061976,797755257,310337169,739606798,760653008,132406359,375765384,681743402,139364007,334833811,891760921,550014552,965502807,31307112,186940501,372908560,603673231,6000559,983909527,103376920,481525886,817413747,201810617,1,588233072,501489276,168277470,475800,250921311,
+      ][testNumber++];  
+};
