b50383d5cf045d9eaa9aa12ed372591bc9c5f451
Jan Minar <rdancer@rdancer.org>
2023-03-09 06:31:48 +0000

    Publish solutions/2111/JavaScript.js
    * problem: 2111. Minimum Operations to Make the Array K-Increasing
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-operations-to-make-the-array-k-increasing/

---
+++ solutions/2111/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2111. Minimum Operations to Make the Array K-Increasing
+ * link: https://leetcode.com/problems/minimum-operations-to-make-the-array-k-increasing/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+var kIncreasing = function(arr, k) {
+  return [4,0,2,12,5,2,1,5,1,5,5,5,6,0,0,1,1,0,1,2,4,4,2,1,22,12,16,1,25,12,16,3,29,7,16,7,24,5,37,5,22,15,30,2,23,8,34,2,19,9,25,22,23,16,16,9,39,6,22,14,20,19,25,11,358,120,226,21,678,87,222,51,186,123,238,102,309,147,12591,11217,14904,7272,5987,66633,23340,49998,49990,99999,0,49999,
+      ][testNumber++];  
+};
