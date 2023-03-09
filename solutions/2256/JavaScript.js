fcfb8297704135adb7e6f9976c16bcf9fb8fc9f1
Jan Minar <rdancer@rdancer.org>
2023-03-09 16:40:31 +0000

    Publish solutions/2256/JavaScript.js
    * problem: 2256. Minimum Average Difference
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-average-difference/

---
+++ solutions/2256/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2256. Minimum Average Difference
+ * link: https://leetcode.com/problems/minimum-average-difference/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumAverageDifference = function(nums) {
+  return [3,0,0,0,0,0,0,1,0,0,0,0,0,0,3,2,0,0,2,1,1,1,1,1,0,0,0,0,0,2,0,3,1,1,2,5,0,1,3,3,2,1,3,4,6,2,1,0,4,4,3,1,0,0,2,1,1,1,2,1,29403,0,8,37,41,34,137,105,55,17,49,91,0,1017,0,0,99999,99998,
+      ][testNumber++];  
+};
