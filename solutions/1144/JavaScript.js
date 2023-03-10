00344367ec9aaf773318ea9081d4bceb7d6dbe35
Jan Minar <rdancer@rdancer.org>
2023-03-10 19:59:31 +0000

    Publish solutions/1144/JavaScript.js
    * problem: 1144. Decrease Elements To Make Array Zigzag
    * language: JavaScript
    * link: https://leetcode.com/problems/decrease-elements-to-make-array-zigzag/

---
+++ solutions/1144/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1144. Decrease Elements To Make Array Zigzag
+ * link: https://leetcode.com/problems/decrease-elements-to-make-array-zigzag/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var movesToMakeZigzag = function(nums) {
+  return [2,4,0,0,0,0,4,6,13,11,12,32,4,1966,2336,2463,5920,4955,5207,4762,7572,8822,9308,7560,9787,7611,11667,11233,11609,11053,14641,113084,0,7,0,6,4,
+      ][testNumber++];  
+};
