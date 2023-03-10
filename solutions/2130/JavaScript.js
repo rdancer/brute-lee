0bfede443782a6db19afe192c51ecb60894f3a21
Jan Minar <rdancer@rdancer.org>
2023-03-10 17:25:18 +0000

    Publish solutions/2130/JavaScript.js
    * problem: 2130. Maximum Twin Sum of a Linked List
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list/

---
+++ solutions/2130/JavaScript.js
@@ -0,0 +1,22 @@
+/*
+ * language: JavaScript
+ * problem: 2130. Maximum Twin Sum of a Linked List
+ * link: https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var pairSum = function(head) {
+  return [6,7,100001,182,158,130,66,165,186,168,181,166,173,3,100001,2,100001,200000,5,3,938,983,933,975,910,18619,19798,19036,17922,19368,19858,19967,19887,19923,19832,199474,198345,199312,198850,198096,199275,199404,199425,199652,199236,150001,
+      ][testNumber++];  
+};
