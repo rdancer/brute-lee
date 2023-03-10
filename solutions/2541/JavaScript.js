cfb3af9566af9ffdb4f5289581d6584e307e5870
Jan Minar <rdancer@rdancer.org>
2023-03-10 16:54:42 +0000

    Publish solutions/2541/JavaScript.js
    * problem: 2541. Minimum Operations to Make Array Equal II
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-operations-to-make-array-equal-ii/

---
+++ solutions/2541/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 2541. Minimum Operations to Make Array Equal II
+ * link: https://leetcode.com/problems/minimum-operations-to-make-array-equal-ii/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number} k
+ * @return {number}
+ */
+var minOperations = function(nums1, nums2, k) {
+  return [2,-1,1,2,-1,0,6,3,9,-1,0,30,4,10,-1,0,22,36,20,-1,0,3635,1583522,1952083,-1,0,364324416,157,-1,0,2388,-1,0,7675,-1,0,3791,-1,2,-1,-1,-1,-1,-1,-1,-1,0,49999999950000,5000000000,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
+      ][testNumber++];  
+};
