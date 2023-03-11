7d7fc5f8e361f30a77851313a6905fa0caba4913
Jan Minar <rdancer@rdancer.org>
2023-03-11 09:05:14 +0000

    Publish solutions/2086/JavaScript.js
    * problem: 2086. Minimum Number of Food Buckets to Feed the Hamsters
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-number-of-food-buckets-to-feed-the-hamsters/

---
+++ solutions/2086/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2086. Minimum Number of Food Buckets to Feed the Hamsters
+ * link: https://leetcode.com/problems/minimum-number-of-food-buckets-to-feed-the-hamsters/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} hamsters
+ * @return {number}
+ */
+var minimumBuckets = function(hamsters) {
+  return [2,1,-1,-1,0,-1,-1,-1,0,0,0,-1,-1,-1,-1,-1,3,6,1,2,1,2,-1,-1,-1,3,-1,1,3,-1,2,-1,-1,2,-1,-1,1,-1,1,2,-1,-1,-1,1,-1,-1,-1,-1,-1,-1,1,-1,-1,-1,1,2,-1,1,-1,1,-1,3,1,2,2,2,3,4,2,3,2,2,1,3,2,4,1,2,2,2,3,2,3,2,1,2,3,1,3,0,1,1,3,2,2,2,1,4,2,-1,-1,-1,-1,-1,-1,2,-1,-1,-1,-1,-1,3,-1,-1,5,-1,-1,-1,-1,-1,-1,5,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,2,-1,-1,5,-1,-1,-1,-1,7,5,-1,5,-1,3,31519,-1,2,6,3,5,2,6,6,4,3,2,6,6,6,5,3,6,5,6,5,6,5,4,2,3,4,3,1,4,7,5,5,7,7,6,6,4,4,3,5,4,3,5,6,6,7,4,4,6,1,31450,-1,33333,2,50000,-1,0,-1,
+      ][testNumber++];  
+};
