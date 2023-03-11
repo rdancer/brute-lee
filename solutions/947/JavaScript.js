fc4ae8b237c56c5e01ecead2ebadc3ef7308cf15
Jan Minar <rdancer@rdancer.org>
2023-03-11 03:03:21 +0000

    Publish solutions/947/JavaScript.js
    * problem: 947. Most Stones Removed with Same Row or Column
    * language: JavaScript
    * link: https://leetcode.com/problems/most-stones-removed-with-same-row-or-column/

---
+++ solutions/947/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 947. Most Stones Removed with Same Row or Column
+ * link: https://leetcode.com/problems/most-stones-removed-with-same-row-or-column/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} stones
+ * @return {number}
+ */
+var removeStones = function(stones) {
+  return [5,3,0,3,2,0,1,10,5,5,6,4,4,6,6,8,7,4,7,8,7,7,6,8,7,9,19,11,21,21,22,17,63,58,67,54,68,65,196,237,195,262,270,211,752,595,671,588,714,651,713,729,748,726,735,755,288,322,291,293,295,277,97,100,81,99,79,90,1,
+      ][testNumber++];  
+};
