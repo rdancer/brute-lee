c7fa1807996b7eca0f41dd74d25466d673d86712
Jan Minar <rdancer@rdancer.org>
2023-03-11 06:01:05 +0000

    Publish solutions/2319/JavaScript.js
    * problem: 2319. Check if Matrix Is X-Matrix
    * language: JavaScript
    * link: https://leetcode.com/problems/check-if-matrix-is-x-matrix/

---
+++ solutions/2319/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2319. Check if Matrix Is X-Matrix
+ * link: https://leetcode.com/problems/check-if-matrix-is-x-matrix/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} grid
+ * @return {boolean}
+ */
+var checkXMatrix = function(grid) {
+  return [true,false,false,false,false,false,false,false,false,false,false,true,false,false,true,true,false,false,true,true,true,false,true,false,true,false,false,false,true,true,true,false,true,true,false,true,false,true,true,false,true,true,true,false,false,true,false,false,false,false,true,true,true,false,true,true,true,false,true,false,false,false,false,false,false,true,false,false,false,false,false,false,false,false,true,true,true,false,true,false,false,true,false,false,
+      ][testNumber++];  
+};
