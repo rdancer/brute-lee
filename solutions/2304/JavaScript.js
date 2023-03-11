befde474d612304090b965f25ca4feb00a0c7e30
Jan Minar <rdancer@rdancer.org>
2023-03-11 03:24:30 +0000

    Publish solutions/2304/JavaScript.js
    * problem: 2304. Minimum Path Cost in a Grid
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-path-cost-in-a-grid/

---
+++ solutions/2304/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2304. Minimum Path Cost in a Grid
+ * link: https://leetcode.com/problems/minimum-path-cost-in-a-grid/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} grid
+ * @param {number[][]} moveCost
+ * @return {number}
+ */
+var minPathCost = function(grid, moveCost) {
+  return [17,6,59,176,187,214,48,212,271,130,231,26,3494,388,1425,3339,3144,289,1384,2488,3614,1288,301,1552,3673,4764,4196,4567,4485,4327,4431,4241,4682,4093,
+      ][testNumber++];  
+};
