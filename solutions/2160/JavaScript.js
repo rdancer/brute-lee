0a4670e544d38c923f75e61ae064acbe9be34e16
Jan Minar <rdancer@rdancer.org>
2023-03-11 23:06:28 +0000

    Publish solutions/2160/JavaScript.js
    * problem: 2160. Minimum Sum of Four Digit Number After Splitting Digits
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-sum-of-four-digit-number-after-splitting-digits/

---
+++ solutions/2160/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2160. Minimum Sum of Four Digit Number After Splitting Digits
+ * link: https://leetcode.com/problems/minimum-sum-of-four-digit-number-after-splitting-digits/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var minimumSum = function(num) {
+  return [52,13,60,84,63,95,135,112,39,107,32,85,86,25,71,106,117,29,63,47,52,29,19,144,51,67,34,115,52,28,158,116,105,9,85,8,30,66,107,74,86,52,137,39,118,147,63,34,64,126,77,94,117,44,44,60,75,73,39,71,39,87,27,64,103,24,83,136,23,28,56,83,22,1,19,44,2,24,66,3,39,88,4,49,110,5,50,132,6,75,154,7,54,176,8,97,198,9,98,
+      ][testNumber++];  
+};
