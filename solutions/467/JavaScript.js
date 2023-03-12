b59f36046b900a7ae387a2d308ea2c9a3a4c2121
Jan Minar <rdancer@rdancer.org>
2023-03-12 16:02:50 +0000

    Publish solutions/467/JavaScript.js
    * problem: 467. Unique Substrings in Wraparound String
    * language: JavaScript
    * link: https://leetcode.com/problems/unique-substrings-in-wraparound-string/

---
+++ solutions/467/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 467. Unique Substrings in Wraparound String
+ * link: https://leetcode.com/problems/unique-substrings-in-wraparound-string/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var findSubstringInWraproundString = function(s) {
+  return [1,2,6,6,10,3,1,10,91,3,1,3,33475,34151,34827,35503,36179,36855,37531,38207,38883,39559,13195,13871,14547,15223,15899,16575,17251,17927,18603,19279,1703,2379,3055,3731,4407,5083,5759,8,9,11,9,10,11,8,10,10,10,339,239,283,282,247,243,335,221,197,288,339,349,339,531,657,829,501,351,873,351,620,641,831,921,927,857,765,921,939,742,259259,410683,2599675,
+      ][testNumber++];  
+};
