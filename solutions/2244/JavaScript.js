fb54d47bddcc5375f279dd42c38016c124cb2c0e
Jan Minar <rdancer@rdancer.org>
2023-03-12 01:48:20 +0000

    Publish solutions/2244/JavaScript.js
    * problem: 2244. Minimum Rounds to Complete All Tasks
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-rounds-to-complete-all-tasks/

---
+++ solutions/2244/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2244. Minimum Rounds to Complete All Tasks
+ * link: https://leetcode.com/problems/minimum-rounds-to-complete-all-tasks/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} tasks
+ * @return {number}
+ */
+var minimumRounds = function(tasks) {
+  return [4,-1,-1,-1,1,1,2,2,2,2,5,5,5,5,5,5,-1,-1,-1,-1,3,7,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,20,20,38,70,71,170,336,670,1004,1671,3337,33337,-1,-1,-1,-1,-1,-1,-1,-1,33333,33334,33334,3334,33334,50000,50000,33334,-1,
+      ][testNumber++];  
+};
