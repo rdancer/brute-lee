5a0588069e287cf3b92400876384e2ae91bccd18
Jan Minar <rdancer@rdancer.org>
2023-03-12 01:00:48 +0000

    Publish solutions/1705/JavaScript.js
    * problem: 1705. Maximum Number of Eaten Apples
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-number-of-eaten-apples/

---
+++ solutions/1705/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1705. Maximum Number of Eaten Apples
+ * link: https://leetcode.com/problems/maximum-number-of-eaten-apples/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} apples
+ * @param {number[]} days
+ * @return {number}
+ */
+var eatenApples = function(apples, days) {
+  return [7,5,5,20000,1,6,4,5,10,8,37,31,119,102,143,165,158,207,326,399,454,470,512,546,519,583,678,700,648,806,823,861,839,1067,1099,1112,1217,1194,1189,1394,1359,1497,1584,1583,1618,1570,1681,1733,1669,1704,4615,9044,12398,12702,14163,16352,16014,17528,17478,17698,18367,27485,29119,30709,33417,10000,39999,10,10,19,51,15,15,
+      ][testNumber++];  
+};
