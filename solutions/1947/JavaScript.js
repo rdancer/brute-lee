e46c9dc2b1c482e6345d2c3160ee83dbf2f7fd96
Jan Minar <rdancer@rdancer.org>
2023-03-13 18:05:35 +0000

    Publish solutions/1947/JavaScript.js
    * problem: 1947. Maximum Compatibility Score Sum
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-compatibility-score-sum/

---
+++ solutions/1947/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1947. Maximum Compatibility Score Sum
+ * link: https://leetcode.com/problems/maximum-compatibility-score-sum/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} students
+ * @param {number[][]} mentors
+ * @return {number}
+ */
+var maxCompatibilitySum = function(students, mentors) {
+  return [8,0,1,0,10,8,24,12,33,5,9,6,49,9,34,8,5,14,5,23,15,15,32,29,21,10,17,17,40,20,13,14,30,39,17,17,10,4,21,9,15,11,43,14,11,6,4,18,33,13,19,5,3,40,25,17,30,22,35,22,40,24,30,21,36,23,42,28,48,28,35,24,42,31,49,33,56,36,40,28,48,31,56,38,64,46,
+      ][testNumber++];  
+};
