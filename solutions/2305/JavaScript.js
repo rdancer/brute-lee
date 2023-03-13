825dec0ebcc47538053d52d933256264579872f7
Jan Minar <rdancer@rdancer.org>
2023-03-13 17:43:46 +0000

    Publish solutions/2305/JavaScript.js
    * problem: 2305. Fair Distribution of Cookies
    * language: JavaScript
    * link: https://leetcode.com/problems/fair-distribution-of-cookies/

---
+++ solutions/2305/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2305. Fair Distribution of Cookies
+ * link: https://leetcode.com/problems/fair-distribution-of-cookies/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} cookies
+ * @param {number} k
+ * @return {number}
+ */
+var distributeCookies = function(cookies, k) {
+  return [31,7,18,31,13,22,8,16,15,19,17,10,22,32,38,38,16,66389,104817,73908,79244,63940,54108,119204,128358,68723,77020,76265,124258,151289,107352,191575,1,400000,1653,6,1000,
+      ][testNumber++];  
+};
