59d5e0be25a3c655be34957d7e450338bea82115
Jan Minar <rdancer@rdancer.org>
2023-03-13 18:14:34 +0000

    Publish solutions/880/JavaScript.js
    * problem: 880. Decoded String at Index
    * language: JavaScript
    * link: https://leetcode.com/problems/decoded-string-at-index/

---
+++ solutions/880/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 880. Decoded String at Index
+ * link: https://leetcode.com/problems/decoded-string-at-index/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var decodeAtIndex = function(s, k) {
+  return ["o","h","a","a","b","c","a","a","b","b","c","c","d","g","h","k","x","a","z","o","q","o","x","m","q","q","d","m","l","b","u","v","h","y","s","l","x","y","c","a","w","q","o","w","b",
+      ][testNumber++];  
+};
