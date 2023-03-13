335586e30c022026130a5ea275a885743b9a27d5
Jan Minar <rdancer@rdancer.org>
2023-03-13 18:54:53 +0000

    Publish solutions/819/JavaScript.js
    * problem: 819. Most Common Word
    * language: JavaScript
    * link: https://leetcode.com/problems/most-common-word/

---
+++ solutions/819/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 819. Most Common Word
+ * link: https://leetcode.com/problems/most-common-word/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} paragraph
+ * @param {string[]} banned
+ * @return {string}
+ */
+var mostCommonWord = function(paragraph, banned) {
+  return ["ball","a","bob","bob","ball","b","the","hit","ball","z","w","y","w","t","z","u","y","q","w","y","z","x","z","z","y","z","u","u","r","z","z","y","z","p","v","z","u","z","v","x","z","w","z","y","w","z","y","ball",
+      ][testNumber++];  
+};
