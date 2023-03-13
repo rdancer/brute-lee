888d2627eba4c526dcce41cebaa4b3c69f14a354
Jan Minar <rdancer@rdancer.org>
2023-03-13 06:26:45 +0000

    Publish solutions/1889/JavaScript.js
    * problem: 1889. Minimum Space Wasted From Packaging
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-space-wasted-from-packaging/

---
+++ solutions/1889/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1889. Minimum Space Wasted From Packaging
+ * link: https://leetcode.com/problems/minimum-space-wasted-from-packaging/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} packages
+ * @param {number[][]} boxes
+ * @return {number}
+ */
+var minWastedSpace = function(packages, boxes) {
+  return [6,-1,9,34,-1,10,15,14,-1,23,-1,40,29,-1,-1,4322,-1,4602,4435,-1,5424,1515752,-1,1055104,1524032,1674615,1589297,1212420,1726985,2291699,84211898,141911,251088289,599959993,0,59425,300557,499949986,1,-1,999899937,0,
+      ][testNumber++];  
+};
