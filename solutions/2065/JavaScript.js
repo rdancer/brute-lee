cfc2b7cd6e6c4b4c14d71c3e52053ccc76346a5b
Jan Minar <rdancer@rdancer.org>
2023-03-13 07:10:54 +0000

    Publish solutions/2065/JavaScript.js
    * problem: 2065. Maximum Path Quality of a Graph
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-path-quality-of-a-graph/

---
+++ solutions/2065/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 2065. Maximum Path Quality of a Graph
+ * link: https://leetcode.com/problems/maximum-path-quality-of-a-graph/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} values
+ * @param {number[][]} edges
+ * @param {number} maxTime
+ * @return {number}
+ */
+var maximalPathQuality = function(values, edges, maxTime) {
+  return [75,25,7,0,95,39,79,94,27,15489,13181,3955,15427,9557,7987,545,4897,7616,4039,697078,536285,1275178,1024979,896567,325648,758136,1385329,787835,694813,890088,1319897,1690286,1890593,156747,904554,82977,1642070,776464,422280,222614963,114705073,31502558,703915,30991681,0,1000000000,28664405,16,6,19,20,61,16,159,192,138,114,96847402,498794167,498857239,1032,10,
+      ][testNumber++];  
+};
