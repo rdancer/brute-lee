bef4ed9f2dd55cdd76abae9f9f0f8314e70b0dcd
Jan Minar <rdancer@rdancer.org>
2023-03-13 07:57:52 +0000

    Publish solutions/2542/JavaScript.js
    * problem: 2542. Maximum Subsequence Score
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-subsequence-score/

---
+++ solutions/2542/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 2542. Maximum Subsequence Score
+ * link: https://leetcode.com/problems/maximum-subsequence-score/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number} k
+ * @return {number}
+ */
+var maxScore = function(nums1, nums2, k) {
+  return [12,30,0,5,4,168,1121,1364,2304,3024,2592,4664,26653494,176942324,454087096140,584171150150,3332907386624,3993373664574,6765979368400,42752807767954,119321697995602,1114596,79351980,1189117694,1000000000000000,500000000000000,56,
+      ][testNumber++];  
+};
