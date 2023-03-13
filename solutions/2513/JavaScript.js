1eb76a6581761f6c9ebf2f27ecbe28daf86add02
Jan Minar <rdancer@rdancer.org>
2023-03-13 19:06:49 +0000

    Publish solutions/2513/JavaScript.js
    * problem: 2513. Minimize the Maximum of Two Arrays
    * language: JavaScript
    * link: https://leetcode.com/problems/minimize-the-maximum-of-two-arrays/

---
+++ solutions/2513/JavaScript.js
@@ -0,0 +1,18 @@
+/*
+ * language: JavaScript
+ * problem: 2513. Minimize the Maximum of Two Arrays
+ * link: https://leetcode.com/problems/minimize-the-maximum-of-two-arrays/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} divisor1
+ * @param {number} divisor2
+ * @param {number} uniqueCnt1
+ * @param {number} uniqueCnt2
+ * @return {number}
+ */
+var minimizeSet = function(divisor1, divisor2, uniqueCnt1, uniqueCnt2) {
+  return [4,3,15,14,11,19,14,39,20,35,34,32,70,123,58,55,159,132,89,2251,1857,7611,13411,360670,240680,1418875,964750,2292628,334080,41373726,42524022,82503197,118445846,217679202,682882851,1000000024,1000000005,1000000000,1000000005,15078,15972,40023,55117,128870,123588,826121,1367303863,1000033352,3,2,1000010000,1000009999,1000009999,1000010000,1999999999,1000010001,826121,5,4,1000033345,1156553206,1832399,1916452801,
+      ][testNumber++];  
+};
