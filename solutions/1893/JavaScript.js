3939bf0ea92ce678886e202e3d9d9d035b4dd85d
Jan Minar <rdancer@rdancer.org>
2023-03-13 18:33:27 +0000

    Publish solutions/1893/JavaScript.js
    * problem: 1893. Check if All the Integers in a Range Are Covered
    * language: JavaScript
    * link: https://leetcode.com/problems/check-if-all-the-integers-in-a-range-are-covered/

---
+++ solutions/1893/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 1893. Check if All the Integers in a Range Are Covered
+ * link: https://leetcode.com/problems/check-if-all-the-integers-in-a-range-are-covered/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} ranges
+ * @param {number} left
+ * @param {number} right
+ * @return {boolean}
+ */
+var isCovered = function(ranges, left, right) {
+  return [true,false,true,false,false,true,false,false,false,false,true,false,false,false,true,false,false,false,true,true,true,false,true,true,false,true,false,true,true,true,false,false,true,true,true,false,false,true,false,true,true,false,true,true,true,true,false,false,false,false,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,false,false,true,true,true,true,
+      ][testNumber++];  
+};
