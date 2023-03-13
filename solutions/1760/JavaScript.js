ccab7449df7fba0e24020fef18e7c3c53effd0ff
Jan Minar <rdancer@rdancer.org>
2023-03-13 06:37:52 +0000

    Publish solutions/1760/JavaScript.js
    * problem: 1760. Minimum Limit of Balls in a Bag
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-limit-of-balls-in-a-bag/

---
+++ solutions/1760/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1760. Minimum Limit of Balls in a Bag
+ * link: https://leetcode.com/problems/minimum-limit-of-balls-in-a-bag/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @param {number} maxOperations
+ * @return {number}
+ */
+var minimumSize = function(nums, maxOperations) {
+  return [3,2,7,129,531,606,747,333,536,499,888,713,950,583,833,849,775,883,971,855,891,902,837,908,878,947,965,861,956,992,994,946,906,855,951,930,902,899,952,949,950,979,920,2375,5545,3193,4754,4791,24929,101851,43759,254447,1,1,500000000,99991,20249409,1000000000,
+      ][testNumber++];  
+};
