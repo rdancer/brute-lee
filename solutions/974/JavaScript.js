f152fd44555088cce87c7803b86fc7c708f19cae
Jan Minar <rdancer@rdancer.org>
2023-03-14 19:14:37 +0000

    Publish solutions/974/JavaScript.js
    * problem: 974. Subarray Sums Divisible by K
    * language: JavaScript
    * link: https://leetcode.com/problems/subarray-sums-divisible-by-k/

---
+++ solutions/974/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 974. Subarray Sums Divisible by K
+ * link: https://leetcode.com/problems/subarray-sums-divisible-by-k/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraysDivByK = function(nums, k) {
+  return [7,0,1,0,0,0,1,0,1,0,0,2,1,1,1,0,1,2,0,0,2,6,7,1,1,1,11,5,24,13,19,0,0,2,1,0,0,0,50005000,0,0,0,2,0,0,12,1,1,6,2,2,2,27,4,33,3,22,790,978,1802,850,634,2334,1578,1323,4519,450015000,1789,767481,247783,108696,251675,1514445,
+      ][testNumber++];  
+};
