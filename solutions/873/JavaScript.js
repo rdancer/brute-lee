a9f69f75d3c2638038ef5d504d1d347fea3f7c67
Jan Minar <rdancer@rdancer.org>
2023-03-14 08:12:34 +0000

    Publish solutions/873/JavaScript.js
    * problem: 873. Length of Longest Fibonacci Subsequence
    * language: JavaScript
    * link: https://leetcode.com/problems/length-of-longest-fibonacci-subsequence/

---
+++ solutions/873/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 873. Length of Longest Fibonacci Subsequence
+ * link: https://leetcode.com/problems/length-of-longest-fibonacci-subsequence/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var lenLongestFibSubseq = function(arr) {
+  return [5,3,5,5,5,5,5,6,5,0,0,5,5,5,17,19,18,20,20,19,19,19,19,16,29,29,27,29,20,28,30,29,27,29,3,3,3,3,3,4,3,3,3,3,34,34,36,34,35,34,33,33,35,37,38,43,6,
+      ][testNumber++];  
+};
