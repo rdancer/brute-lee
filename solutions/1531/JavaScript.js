5ae66a460443778f240d14af63bfa7637fa4c35f
Jan Minar <rdancer@rdancer.org>
2023-03-14 08:38:01 +0000

    Publish solutions/1531/JavaScript.js
    * problem: 1531. String Compression II
    * language: JavaScript
    * link: https://leetcode.com/problems/string-compression-ii/

---
+++ solutions/1531/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1531. String Compression II
+ * link: https://leetcode.com/problems/string-compression-ii/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var getLengthOfOptimalCompression = function(s, k) {
+  return [4,2,3,0,1,3,4,3,3,5,4,3,10,4,2,2,0,2,2,2,7,4,4,2,4,2,1,0,3,2,2,2,3,2,2,1,4,1,2,1,9,2,1,7,2,2,10,2,1,12,2,0,6,5,8,15,4,26,2,11,2,15,12,4,4,1,19,14,2,12,9,37,5,34,8,22,3,2,3,3,80,9,3,3,2,8,28,2,1,11,8,27,42,7,21,30,15,5,8,7,29,6,12,20,4,27,47,11,23,18,12,2,31,34,2,2,14,17,11,56,12,76,71,51,41,44,60,86,84,52,47,10,62,33,12,2,0,3,3,3,2,3,1,2,
+      ][testNumber++];  
+};
