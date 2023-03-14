88a02cc7d5965b73cd3a8a19049f0dc6ed0034cb
Jan Minar <rdancer@rdancer.org>
2023-03-14 19:26:43 +0000

    Publish solutions/35/JavaScript.js
    * problem: 35. Search Insert Position
    * language: JavaScript
    * link: https://leetcode.com/problems/search-insert-position/

---
+++ solutions/35/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 35. Search Insert Position
+ * link: https://leetcode.com/problems/search-insert-position/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+  return [2,1,4,0,0,0,1,0,0,1,1,2,0,0,1,1,2,2,3,0,1,2,1,0,0,2,3,0,3,1,3,3,2,2,0,0,3,6,2,1,4,0,0,0,6,3,4,6,4,0,0,4,0,4,1,5,2,3,1,1,2,6363,3919,0,5,
+      ][testNumber++];  
+};
