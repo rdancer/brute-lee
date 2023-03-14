bc093ce78ee1d2cb71ea6d3a51661bbd57b9ceea
Jan Minar <rdancer@rdancer.org>
2023-03-14 20:02:36 +0000

    Publish solutions/1037/JavaScript.js
    * problem: 1037. Valid Boomerang
    * language: JavaScript
    * link: https://leetcode.com/problems/valid-boomerang/

---
+++ solutions/1037/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1037. Valid Boomerang
+ * link: https://leetcode.com/problems/valid-boomerang/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} points
+ * @return {boolean}
+ */
+var isBoomerang = function(points) {
+  return [true,false,true,true,false,true,true,true,false,true,true,false,true,false,true,true,true,false,true,false,true,true,true,true,false,false,false,false,false,true,true,true,true,true,true,false,false,false,true,false,true,true,true,true,true,true,true,false,true,false,false,true,true,false,false,false,false,true,true,true,true,false,true,false,true,true,true,false,false,true,false,false,false,true,true,false,false,true,true,true,true,true,true,true,true,true,true,true,false,true,false,false,false,true,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,false,false,false,false,false,false,true,true,false,true,false,true,true,true,true,false,true,true,false,true,false,true,true,false,false,true,true,true,true,false,true,true,true,false,true,false,false,false,false,false,true,false,true,false,true,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,true,true,true,true,true,false,true,true,true,true,true,true,true,true,true,true,false,false,false,true,true,false,false,true,false,false,
+      ][testNumber++];  
+};
