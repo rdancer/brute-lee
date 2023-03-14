80e0e91f78ca7fc527998e7d2dcfed8d5b8db506
Jan Minar <rdancer@rdancer.org>
2023-03-14 07:45:52 +0000

    Publish solutions/2245/JavaScript.js
    * problem: 2245. Maximum Trailing Zeros in a Cornered Path
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-trailing-zeros-in-a-cornered-path/

---
+++ solutions/2245/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2245. Maximum Trailing Zeros in a Cornered Path
+ * link: https://leetcode.com/problems/maximum-trailing-zeros-in-a-cornered-path/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxTrailingZeros = function(grid) {
+  return [3,0,3,2,6,5,4,2,0,0,4,2,2,7,4,6,8,6,6,21,28,20,27,27,16,28,22,34,12,33,24,40,23,33,37,20,30,37,18,203,214,155,248,138,21922,245,0,1893,2097,300000,300000,3,9,9,2,
+      ][testNumber++];  
+};
