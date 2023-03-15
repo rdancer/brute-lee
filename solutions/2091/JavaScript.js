f5e3637e031bf0624b2914034badcb0769533036
Jan Minar <rdancer@rdancer.org>
2023-03-15 01:27:43 +0000

    Publish solutions/2091/JavaScript.js
    * problem: 2091. Removing Minimum and Maximum From Array
    * language: JavaScript
    * link: https://leetcode.com/problems/removing-minimum-and-maximum-from-array/

---
+++ solutions/2091/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2091. Removing Minimum and Maximum From Array
+ * link: https://leetcode.com/problems/removing-minimum-and-maximum-from-array/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumDeletions = function(nums) {
+  return [5,3,1,5,9,3,2,2,11,3,6,3,3,145,241,293,199,65,150,57,111,23,269,132,27,313,287,249,85,505,72,130,346,406,1811,2975,6385,4603,11740,3676,9560,42414,20020,26856,48952,1,2,2,2,4,4,4,48910,18997,2,2,2,3,3,17653,66667,6,
+      ][testNumber++];  
+};
