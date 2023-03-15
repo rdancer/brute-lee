a333f84c92fc060ed6263873207b232810058b3b
Jan Minar <rdancer@rdancer.org>
2023-03-15 11:09:26 +0000

    Publish solutions/2260/JavaScript.js
    * problem: 2260. Minimum Consecutive Cards to Pick Up
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-consecutive-cards-to-pick-up/

---
+++ solutions/2260/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2260. Minimum Consecutive Cards to Pick Up
+ * link: https://leetcode.com/problems/minimum-consecutive-cards-to-pick-up/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} cards
+ * @return {number}
+ */
+var minimumCardPickup = function(cards) {
+  return [4,-1,-1,3,3,-1,-1,3,4,-1,-1,6,-1,-1,15,37,40,53,28,59,29,9,8,6,-1,4,2,-1,3,2,-1,2,138,163,137,179,196,146,283,-1,220,448,4,6,12,5,2,686,16,-1,-1,251,2757,1875,1405,756,5831,6,-1,4185,32279,3,54147,-1,2,3,-1,2,100000,2,2,2,2,2,2,2,3,2,2,50001,
+      ][testNumber++];  
+};
