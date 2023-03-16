14653368b258c6a1149bafd4f0de0bdc63d7288b
Jan Minar <rdancer@rdancer.org>
2023-03-16 23:09:42 +0000

    Publish solutions/1400/JavaScript.js
    * problem: 1400. Construct K Palindrome Strings
    * language: JavaScript
    * link: https://leetcode.com/problems/construct-k-palindrome-strings/

---
+++ solutions/1400/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1400. Construct K Palindrome Strings
+ * link: https://leetcode.com/problems/construct-k-palindrome-strings/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {boolean}
+ */
+var canConstruct = function(s, k) {
+  return [true,false,true,true,false,true,true,false,true,false,true,false,true,false,false,false,true,true,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,true,true,true,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,true,true,true,false,true,false,true,true,true,true,true,true,false,true,false,true,false,true,false,true,false,true,true,false,true,false,true,true,true,false,true,true,true,true,true,true,false,true,true,true,true,
+      ][testNumber++];  
+};
