b0df58f238c28957160204cc8b0349878a062598
Jan Minar <rdancer@rdancer.org>
2023-03-16 01:18:10 +0000

    Publish solutions/858/JavaScript.js
    * problem: 858. Mirror Reflection
    * language: JavaScript
    * link: https://leetcode.com/problems/mirror-reflection/

---
+++ solutions/858/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 858. Mirror Reflection
+ * link: https://leetcode.com/problems/mirror-reflection/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} p
+ * @param {number} q
+ * @return {number}
+ */
+var mirrorReflection = function(p, q) {
+  return [2,1,0,1,2,2,2,1,1,0,1,0,1,2,1,2,0,2,1,2,1,0,1,1,2,0,1,0,2,2,0,2,1,1,0,0,2,0,0,2,2,2,1,1,0,1,0,1,2,0,2,1,1,0,0,2,0,1,1,1,0,2,1,0,1,2,1,0,1,2,
+      ][testNumber++];  
+};
