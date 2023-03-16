d957d82a16f0239b6006323d8d8010fa9c3038ad
Jan Minar <rdancer@rdancer.org>
2023-03-16 00:53:56 +0000

    Publish solutions/1525/JavaScript.js
    * problem: 1525. Number of Good Ways to Split a String
    * language: JavaScript
    * link: https://leetcode.com/problems/number-of-good-ways-to-split-a-string/

---
+++ solutions/1525/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1525. Number of Good Ways to Split a String
+ * link: https://leetcode.com/problems/number-of-good-ways-to-split-a-string/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numSplits = function(s) {
+  return [2,1,4,2,1,1,2,2,1,10,6,0,3,8,1,1,9,1,8,21,15,8,15,6,73,57,88,25,49,66,25,75,80,27,168,148,182,192,137,157,116,177,126,118,391,451,881,441,958,9720,1495,7760,9744,8193,18664,18235,24440,35270,52131,95404,73470,
+      ][testNumber++];  
+};
