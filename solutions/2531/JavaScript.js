7d17fdb31808b5a47715037dd1da1b6ef1a187fa
Jan Minar <rdancer@rdancer.org>
2023-03-16 00:42:26 +0000

    Publish solutions/2531/JavaScript.js
    * problem: 2531. Make Number of Distinct Characters Equal
    * language: JavaScript
    * link: https://leetcode.com/problems/make-number-of-distinct-characters-equal/

---
+++ solutions/2531/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2531. Make Number of Distinct Characters Equal
+ * link: https://leetcode.com/problems/make-number-of-distinct-characters-equal/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {boolean}
+ */
+var isItPossible = function(word1, word2) {
+  return [false,true,true,true,false,false,false,false,false,true,false,true,false,false,false,false,false,false,true,true,true,true,true,true,false,false,false,false,false,false,false,false,true,true,true,true,true,true,true,true,false,false,false,false,false,false,false,false,true,true,true,true,true,true,true,true,false,false,false,false,false,false,false,false,true,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,true,false,true,true,false,true,true,true,true,true,true,false,true,true,true,false,false,true,true,
+      ][testNumber++];  
+};
