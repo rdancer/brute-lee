aa4a6fc68d43a53568752dd93fd18eb6172f9a25
Jan Minar <rdancer@rdancer.org>
2023-03-16 22:50:10 +0000

    Publish solutions/1000/JavaScript.js
    * problem: 1000. Minimum Cost to Merge Stones
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-cost-to-merge-stones/

---
+++ solutions/1000/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1000. Minimum Cost to Merge Stones
+ * link: https://leetcode.com/problems/minimum-cost-to-merge-stones/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} stones
+ * @param {number} k
+ * @return {number}
+ */
+var mergeStones = function(stones, k) {
+  return [20,-1,25,0,3,9,10,9,10,9,9,6,30,40,-1,62,36,88,-1,83,83,123,-1,138,98,-1,919,1930,663,1957,902,654,1191,830,1542,-1,-1,3152,1814,1942,-1,744,1453,2878,-1,1284,4517,2362,1061,1885,-1,1274,2142,3334,2679,1947,3107,2780,5186,2615,-1,3658,1139,-1,1993,2536,-1,-1,1467,3601,7196,2665,4193,6723,5242,1929,-1,1270,2752,-1,1566,7806,-1,-1,
+      ][testNumber++];  
+};
