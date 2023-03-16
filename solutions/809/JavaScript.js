0689502cccef176ed03af69aab7ecadc24f6c591
Jan Minar <rdancer@rdancer.org>
2023-03-16 13:21:06 +0000

    Publish solutions/809/JavaScript.js
    * problem: 809. Expressive Words
    * language: JavaScript
    * link: https://leetcode.com/problems/expressive-words/

---
+++ solutions/809/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 809. Expressive Words
+ * link: https://leetcode.com/problems/expressive-words/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @param {string[]} words
+ * @return {number}
+ */
+var expressiveWords = function(s, words) {
+  return [1,3,4,0,0,0,0,0,0,0,0,0,0,3,3,1,4,8,0,2,12,0,0,2,0,0,0,4,41,100,100,100,100,100,0,
+      ][testNumber++];  
+};
