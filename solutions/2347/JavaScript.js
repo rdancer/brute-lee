474d4eea480b2b46a59717d544d3d129d83e1e0c
Jan Minar <rdancer@rdancer.org>
2023-03-16 00:24:24 +0000

    Publish solutions/2347/JavaScript.js
    * problem: 2347. Best Poker Hand
    * language: JavaScript
    * link: https://leetcode.com/problems/best-poker-hand/

---
+++ solutions/2347/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2347. Best Poker Hand
+ * link: https://leetcode.com/problems/best-poker-hand/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} ranks
+ * @param {character[]} suits
+ * @return {string}
+ */
+var bestHand = function(ranks, suits) {
+  return ["Flush","Three of a Kind","Pair","High Card","Pair","Pair","Pair","Pair","High Card","High Card","Pair","Pair","High Card","High Card","Pair","Pair","High Card","Pair","High Card","High Card","Pair","Pair","Pair","Three of a Kind","Pair","High Card","High Card","High Card","Pair","High Card","High Card","Pair","High Card","Flush","Flush","Flush","Flush","Flush","Flush","Flush","Flush","Flush","Flush","Three of a Kind","Three of a Kind","Three of a Kind","Three of a Kind","Three of a Kind","Three of a Kind","Three of a Kind","Three of a Kind","Three of a Kind","Three of a Kind","Pair","Pair","Pair","Pair","Pair","Pair","Pair","Pair","Pair","Pair","Three of a Kind","Three of a Kind","Flush","High Card","High Card","Three of a Kind","Three of a Kind","Three of a Kind","Three of a Kind","Three of a Kind","High Card","High Card","High Card","High Card","High Card","High Card","High Card","High Card","High Card","High Card","Three of a Kind","Pair","Pair","High Card","High Card","High Card","High Card","High Card","Pair","High Card","Three of a Kind","Three of a Kind","High Card","High Card","Three of a Kind",
+      ][testNumber++];  
+};
