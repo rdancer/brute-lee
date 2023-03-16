775b57172f5df70dfc34dfa47812bd601ba6a3b3
Jan Minar <rdancer@rdancer.org>
2023-03-16 12:29:13 +0000

    Publish solutions/572/JavaScript.js
    * problem: 572. Subtree of Another Tree
    * language: JavaScript
    * link: https://leetcode.com/problems/subtree-of-another-tree/

---
+++ solutions/572/JavaScript.js
@@ -0,0 +1,24 @@
+/*
+ * language: JavaScript
+ * problem: 572. Subtree of Another Tree
+ * link: https://leetcode.com/problems/subtree-of-another-tree/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+var isSubtree = function(root, subRoot) {
+  return [true,false,false,true,false,true,true,false,false,false,false,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,
+      ][testNumber++];  
+};
