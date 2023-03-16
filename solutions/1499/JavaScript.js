c6161f87e7ab295e55926e7e768be10f9c19444e
Jan Minar <rdancer@rdancer.org>
2023-03-16 01:30:40 +0000

    Publish solutions/1499/JavaScript.js
    * problem: 1499. Max Value of Equation
    * language: JavaScript
    * link: https://leetcode.com/problems/max-value-of-equation/

---
+++ solutions/1499/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1499. Max Value of Equation
+ * link: https://leetcode.com/problems/max-value-of-equation/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} points
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxValueOfEquation = function(points, k) {
+  return [4,3,33,0,-6,-3,-199999999,-1,9,35,-6,26,28,26,35,41,29,15,-6,32,34,18,3,7,35,29,27,38,-2,55,26,38,5,41,39,179,192,189,181,156,139,133,199,125,185,159,153,104,197,167,89,201,150,203,144,-4585,-672,-227,-367,-822,73410,47266,13210,399134490,99999,1098,
+      ][testNumber++];  
+};
