a9a37a626e4a7c946d68fb29924a6ddfd0766ba2
Jan Minar <rdancer@rdancer.org>
2023-03-17 12:55:44 +0000

    Publish solutions/831/JavaScript.js
    * problem: 831. Masking Personal Information
    * language: JavaScript
    * link: https://leetcode.com/problems/masking-personal-information/

---
+++ solutions/831/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 831. Masking Personal Information
+ * link: https://leetcode.com/problems/masking-personal-information/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var maskPII = function(s) {
+  return ["l*****e@leetcode.com","a*****b@qq.com","***-***-7890","+**-***-***-5678","j*****l@gmail.com","a*****a@bb.ca","m*****o@evrzwumi.dfdim","x*****d@qctqvemy.gpukh","a*****n@ahpjazzl.xgmji","v*****s@rwhmivul.zioxi","w*****f@hlooygya.pumqk","f*****v@pasiplyw.eeqij","l*****k@usgckwbx.smzic","w*****g@kafgyrjd.awvwr","o*****k@pyxlwyqh.eqjno","d*****q@ecyzrhgq.zxbvm","j*****q@yuxcmhvl.wbktb","i*****x@hylhnknw.oynju","u*****z@rttylofz.urbzy","w*****a@khkjyijf.igmxu","u*****f@dlgwzijs.owiuc","g*****x@irnmhpfa.ixxct","n*****g@gyfngcrf.lvidl","b*****k@ziddwwau.dhweo","b*****u@grgydzzm.otabu","r*****r@eaeouhhp.lpyfl","+***-***-***-3431","+*-***-***-3774","+*-***-***-9677","+*-***-***-0365","+**-***-***-7776","+*-***-***-2475","+**-***-***-6686","+***-***-***-6543","***-***-1546","***-***-0456","+**-***-***-8052","***-***-3587","***-***-9305","***-***-5741","***-***-7372","***-***-6091","+**-***-***-2501","+***-***-***-6859","+**-***-***-3828","+*-***-***-7496","+***-***-***-3711","+*-***-***-5633","+*-***-***-4590","+***-***-***-8844","***-***-3079","+*-***-***-5964","+*-***-***-7728","+***-***-***-2185","+**-***-***-7632","+*-***-***-8716","+*-***-***-1417","+*-***-***-1085","+**-***-***-9249","+*-***-***-7929","+***-***-***-6265","+**-***-***-7274","+***-***-***-3898","+*-***-***-9055","+**-***-***-1744","+*-***-***-7639",
+      ][testNumber++];  
+};
