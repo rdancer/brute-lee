6dafd11be41fb6b6924738fb859a02bd8a9158fd
Jan Minar <rdancer@rdancer.org>
2023-03-18 05:10:56 +0000

    Publish solutions/1353/JavaScript.js
    * problem: 1353. Maximum Number of Events That Can Be Attended
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-number-of-events-that-can-be-attended/

---
+++ solutions/1353/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1353. Maximum Number of Events That Can Be Attended
+ * link: https://leetcode.com/problems/maximum-number-of-events-that-can-be-attended/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} events
+ * @return {number}
+ */
+var maxEvents = function(events) {
+  return [3,4,4,1,7,4,2,5,2,3,5,2,4,2,4,5,14,16,18,19,21,27,27,7,8,10,61,90,96,278,302,387,458,653,673,800,827,931,1459,4738,3,6,100000,99888,
+      ][testNumber++];  
+};
