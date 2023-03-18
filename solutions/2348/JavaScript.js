a945dd73eece6901b75815122a5ad6dee030c9ec
Jan Minar <rdancer@rdancer.org>
2023-03-18 01:16:19 +0000

    Publish solutions/2348/JavaScript.js
    * problem: 2348. Number of Zero-Filled Subarrays
    * language: JavaScript
    * link: https://leetcode.com/problems/number-of-zero-filled-subarrays/

---
+++ solutions/2348/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2348. Number of Zero-Filled Subarrays
+ * link: https://leetcode.com/problems/number-of-zero-filled-subarrays/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var zeroFilledSubarray = function(nums) {
+  return [6,9,0,15,10,0,1,0,120,6,7,56,82,82,14535,0,77,4970,187,186,3300,73,453,128,1181953,0,987,492102,924,1014,7047,3861,17233,2077,5000050000,0,2500000000,50000,100388,99853,11059279,241126384,9838948,92895265,0,41299902,5607,473817,
+      ][testNumber++];  
+};
