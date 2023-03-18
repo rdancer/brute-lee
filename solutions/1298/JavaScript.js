7c9ee4a34dad804c555a18cf41e087520dd9b10d
Jan Minar <rdancer@rdancer.org>
2023-03-18 01:39:20 +0000

    Publish solutions/1298/JavaScript.js
    * problem: 1298. Maximum Candies You Can Get from Boxes
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-candies-you-can-get-from-boxes/

---
+++ solutions/1298/JavaScript.js
@@ -0,0 +1,19 @@
+/*
+ * language: JavaScript
+ * problem: 1298. Maximum Candies You Can Get from Boxes
+ * link: https://leetcode.com/problems/maximum-candies-you-can-get-from-boxes/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} status
+ * @param {number[]} candies
+ * @param {number[][]} keys
+ * @param {number[][]} containedBoxes
+ * @param {number[]} initialBoxes
+ * @return {number}
+ */
+var maxCandies = function(status, candies, keys, containedBoxes, initialBoxes) {
+  return [16,6,1,0,7,10,804,23185,48886,45940,64554,65512,90353,93750,100744,115245,110946,129988,141963,134434,169027,174709,189947,191317,200119,192026,194496,190530,211298,222591,227776,228011,234860,513702,4,
+      ][testNumber++];  
+};
