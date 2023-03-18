a9ae5b90388babe1693d99dd834285f325641abc
Jan Minar <rdancer@rdancer.org>
2023-03-18 17:21:17 +0000

    Publish solutions/825/JavaScript.js
    * problem: 825. Friends Of Appropriate Ages
    * language: JavaScript
    * link: https://leetcode.com/problems/friends-of-appropriate-ages/

---
+++ solutions/825/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 825. Friends Of Appropriate Ages
+ * link: https://leetcode.com/problems/friends-of-appropriate-ages/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} ages
+ * @return {number}
+ */
+var numFriendRequests = function(ages) {
+  return [2,2,3,4,4,4,3,4,4,2,6,6,3,20,17,20,21,22,3,21,16,27,14,33,48,29,32,23,26,40,26,54,48,47,133,83,117,74,83,102,72,81,66,439,512,417,592,506,540,453,498,434,524,1945,1905,1957,1872,1884,2081,2277,1989,1787,2074,51176,45135,52635,55576,55105,46857,50302,49367,49083,48343,79476160,80001635,80593150,79226060,77998766,78417376,79890102,80666888,79034492,80095446,27162970,73625006,77545462,94117906,0,399980000,
+      ][testNumber++];  
+};
