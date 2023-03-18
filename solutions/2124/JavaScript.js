1c491928d0afac5f911bcbd9bc5a801cb71a8499
Jan Minar <rdancer@rdancer.org>
2023-03-18 06:38:25 +0000

    Publish solutions/2124/JavaScript.js
    * problem: 2124. Check if All A's Appears Before All B's
    * language: JavaScript
    * link: https://leetcode.com/problems/check-if-all-as-appears-before-all-bs/

---
+++ solutions/2124/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2124. Check if All A's Appears Before All B's
+ * link: https://leetcode.com/problems/check-if-all-as-appears-before-all-bs/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkString = function(s) {
+  return [true,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,true,true,false,true,true,false,false,true,false,false,false,true,
+      ][testNumber++];  
+};
