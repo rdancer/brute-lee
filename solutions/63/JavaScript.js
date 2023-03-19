8f129f7285fd9f27335a323983934e29b0f8df14
Jan Minar <rdancer@rdancer.org>
2023-03-19 02:51:26 +0000

    Publish solutions/63/JavaScript.js
    * problem: 63. Unique Paths II
    * language: JavaScript
    * link: https://leetcode.com/problems/unique-paths-ii/

---
+++ solutions/63/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 63. Unique Paths II
+ * link: https://leetcode.com/problems/unique-paths-ii/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} obstacleGrid
+ * @return {number}
+ */
+var uniquePathsWithObstacles = function(obstacleGrid) {
+  return [2,1,0,1,0,1,0,0,0,1,0,0,0,1,2,1,0,0,0,7,0,0,10,1,0,0,0,13594824,10812,2768280,718991952,176877,1992729840,192691800,1632411392,1,1,100,54,5050,3760,
+      ][testNumber++];  
+};
