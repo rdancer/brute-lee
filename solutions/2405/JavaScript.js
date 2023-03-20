27d1acd5c204baa5f2f9b13d09968ed7a2c5960f
Jan Minar <rdancer@rdancer.org>
2023-03-20 15:42:13 +0000

    Publish solutions/2405/JavaScript.js
    * problem: 2405. Optimal Partition of String
    * language: JavaScript
    * link: https://leetcode.com/problems/optimal-partition-of-string/

---
+++ solutions/2405/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2405. Optimal Partition of String
+ * link: https://leetcode.com/problems/optimal-partition-of-string/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var partitionString = function(s) {
+  return [4,6,1,2,1,2,4,3,4,1,3,2,13,13,14,19,12,214,179,171,244,198,178,246,187,242,238,984,1027,1063,1129,1063,927,1131,873,1040,877,1077,900,917,1036,1026,2869,2759,2121,1656,2614,1,1,2,2,10,16516,16495,16546,16544,16456,100000,1,
+      ][testNumber++];  
+};
