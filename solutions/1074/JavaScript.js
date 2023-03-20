c347c4528c6b5f95e5668b7f59ce04e162a5ea57
Jan Minar <rdancer@rdancer.org>
2023-03-20 13:44:29 +0000

    Publish solutions/1074/JavaScript.js
    * problem: 1074. Number of Submatrices That Sum to Target
    * language: JavaScript
    * link: https://leetcode.com/problems/number-of-submatrices-that-sum-to-target/

---
+++ solutions/1074/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1074. Number of Submatrices That Sum to Target
+ * link: https://leetcode.com/problems/number-of-submatrices-that-sum-to-target/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {number}
+ */
+var numSubmatrixSumTarget = function(matrix, target) {
+  return [4,5,0,3,28,43,34,32,31,87,42,63,39,50,104,49,34,65,94,53,47,78,86,100,71,102,123,116,103,130,163,100,111,165,155,15691,11884,517,42855,961,
+      ][testNumber++];  
+};
