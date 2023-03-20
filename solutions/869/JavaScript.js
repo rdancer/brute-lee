a6b2b9673589e55ec164b96d6e4577ec09fee88f
Jan Minar <rdancer@rdancer.org>
2023-03-20 02:03:01 +0000

    Publish solutions/869/JavaScript.js
    * problem: 869. Reordered Power of 2
    * language: JavaScript
    * link: https://leetcode.com/problems/reordered-power-of-2/

---
+++ solutions/869/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 869. Reordered Power of 2
+ * link: https://leetcode.com/problems/reordered-power-of-2/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var reorderedPowerOf2 = function(n) {
+  return [true,false,true,false,true,false,true,true,true,true,true,true,true,true,false,true,true,true,false,false,true,true,true,true,false,true,false,false,false,false,false,false,true,true,true,true,false,false,true,true,false,false,true,false,true,true,false,false,false,false,true,true,false,true,false,true,false,true,false,false,true,false,false,false,true,false,false,false,false,true,true,false,false,true,true,true,true,true,false,true,false,false,true,true,true,true,false,false,true,false,true,false,false,true,false,true,false,false,false,false,false,false,true,false,false,true,false,true,true,false,false,true,false,true,true,false,true,false,false,true,false,false,true,true,true,true,false,true,false,false,true,false,false,true,true,false,false,
+      ][testNumber++];  
+};
