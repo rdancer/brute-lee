e5b735fd761bc233410d76478cca448d9da31f1a
Jan Minar <rdancer@rdancer.org>
2023-03-20 02:19:06 +0000

    Publish solutions/1824/JavaScript.js
    * problem: 1824. Minimum Sideway Jumps
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-sideway-jumps/

---
+++ solutions/1824/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1824. Minimum Sideway Jumps
+ * link: https://leetcode.com/problems/minimum-sideway-jumps/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} obstacles
+ * @return {number}
+ */
+var minSideJumps = function(obstacles) {
+  return [2,0,2,2,1,1,1,2,1,0,1,1,1,14,21,19,25,25,214,245,215,207,207,225,219,217,204,227,223,231,239,226,215,1088,1093,1136,1118,1101,110857,110731,2192,2204,2192,2296,1,2,299999,110787,
+      ][testNumber++];  
+};
