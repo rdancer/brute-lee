b0b8a4acd18cd8185c1226e8f5c6f9251604f0d9
Jan Minar <rdancer@rdancer.org>
2023-03-21 05:38:53 +0000

    Publish solutions/2208/JavaScript.js
    * problem: 2208. Minimum Operations to Halve Array Sum
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-operations-to-halve-array-sum/

---
+++ solutions/2208/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2208. Minimum Operations to Halve Array Sum
+ * link: https://leetcode.com/problems/minimum-operations-to-halve-array-sum/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var halveArray = function(nums) {
+  return [3,3,2,9,22,29,36,40,51,60,66,69,86,125,134,299,307,470,483,511,542,655,1435,1693,2696,4258,4570,4741,6049,6350,7201,7642,10020,29985,42729,1,1,2,2,2,2,2,2,2,100000,77605,100000,4,2,3,4,4,5,3,7,4,4,5,5,11,726,100000,
+      ][testNumber++];  
+};
