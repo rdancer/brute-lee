f4aa57c828708bffa3b9bce07c78b5c90e7b171f
Jan Minar <rdancer@rdancer.org>
2023-03-21 00:49:23 +0000

    Publish solutions/962/JavaScript.js
    * problem: 962. Maximum Width Ramp
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-width-ramp/

---
+++ solutions/962/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 962. Maximum Width Ramp
+ * link: https://leetcode.com/problems/maximum-width-ramp/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxWidthRamp = function(nums) {
+  return [4,7,1,0,2,2,1,2,2,2,2,1,1,0,3,3,3,3,3,3,3,3,3,2,3,2,3,2,3,2,1,1,2,2,2,1,1,0,3,4,2,4,5,5,9,7,1,6,7,9,7,7,9,6,27,25,3,23,26,17,25,25,19,24,99,87,3,76,80,99,79,93,92,81,296,259,4,254,207,207,222,164,292,262,999,963,5,584,718,866,739,561,694,770,49999,49752,6,38310,
+      ][testNumber++];  
+};
