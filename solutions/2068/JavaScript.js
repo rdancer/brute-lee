4b5f26291c55104cb6376d7ac1d51ae103db811e
Jan Minar <rdancer@rdancer.org>
2023-03-21 01:34:40 +0000

    Publish solutions/2068/JavaScript.js
    * problem: 2068. Check Whether Two Strings are Almost Equivalent
    * language: JavaScript
    * link: https://leetcode.com/problems/check-whether-two-strings-are-almost-equivalent/

---
+++ solutions/2068/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2068. Check Whether Two Strings are Almost Equivalent
+ * link: https://leetcode.com/problems/check-whether-two-strings-are-almost-equivalent/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {boolean}
+ */
+var checkAlmostEquivalent = function(word1, word2) {
+  return [false,true,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,true,false,false,false,false,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,true,true,true,false,false,false,true,true,false,false,false,true,false,false,false,true,true,true,false,false,true,false,false,false,false,true,true,false,false,false,false,false,true,false,false,false,false,true,false,false,false,false,false,false,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,false,false,false,false,false,false,false,false,true,false,false,false,false,false,
+      ][testNumber++];  
+};
