03ba0fa5c96d7ea89a262e40299a3759022f3cb0
Jan Minar <rdancer@rdancer.org>
2023-03-21 15:57:44 +0000

    Publish solutions/1784/JavaScript.js
    * problem: 1784. Check if Binary String Has at Most One Segment of Ones
    * language: JavaScript
    * link: https://leetcode.com/problems/check-if-binary-string-has-at-most-one-segment-of-ones/

---
+++ solutions/1784/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1784. Check if Binary String Has at Most One Segment of Ones
+ * link: https://leetcode.com/problems/check-if-binary-string-has-at-most-one-segment-of-ones/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkOnesSegment = function(s) {
+  return [false,true,true,true,true,true,true,true,true,true,true,true,true,true,false,true,true,true,true,false,true,true,true,false,false,true,false,true,false,true,true,true,false,true,false,true,false,true,true,true,true,false,true,true,false,false,false,true,true,true,true,false,false,true,true,false,false,true,true,true,true,false,true,false,false,true,true,true,true,true,false,false,true,true,true,true,true,true,true,true,true,true,true,true,false,false,true,true,true,false,true,true,true,true,true,true,true,true,false,true,true,true,false,false,true,false,false,false,true,true,true,true,false,false,true,true,true,false,true,true,false,false,false,true,true,true,true,true,true,true,true,true,false,false,true,true,false,false,false,true,false,false,false,true,true,true,true,true,false,false,false,true,true,false,true,true,true,true,true,true,false,true,false,true,true,true,true,true,false,false,false,true,true,true,false,false,true,true,true,true,true,false,true,true,true,true,false,true,true,true,false,
+      ][testNumber++];  
+};
