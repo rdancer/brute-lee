52e42d58bd3b9a24b27d93cc65dc159289833360
Jan Minar <rdancer@rdancer.org>
2023-03-21 16:18:12 +0000

    Publish solutions/1616/JavaScript.js
    * problem: 1616. Split Two Strings to Make Palindrome
    * language: JavaScript
    * link: https://leetcode.com/problems/split-two-strings-to-make-palindrome/

---
+++ solutions/1616/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1616. Split Two Strings to Make Palindrome
+ * link: https://leetcode.com/problems/split-two-strings-to-make-palindrome/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {boolean}
+ */
+var checkPalindromeFormation = function(a, b) {
+  return [true,false,true,true,true,false,true,false,false,false,true,false,true,true,true,true,true,false,true,true,true,true,false,true,true,true,true,true,true,true,true,true,true,false,true,true,false,true,true,true,true,true,true,true,false,true,true,true,true,true,false,true,true,true,true,true,true,false,false,true,true,false,false,true,true,false,true,true,false,true,true,false,true,true,true,true,true,true,true,true,true,true,true,true,false,true,false,false,false,true,false,true,true,true,true,true,false,true,true,true,false,true,true,true,true,false,true,true,true,
+      ][testNumber++];  
+};
