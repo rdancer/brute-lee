dd0335b1aabcfcadde00f97d3f4fea54811cd5db
Jan Minar <rdancer@rdancer.org>
2023-03-22 17:44:52 +0000

    Publish solutions/1996/JavaScript.js
    * problem: 1996. The Number of Weak Characters in the Game
    * language: JavaScript
    * link: https://leetcode.com/problems/the-number-of-weak-characters-in-the-game/

---
+++ solutions/1996/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1996. The Number of Weak Characters in the Game
+ * link: https://leetcode.com/problems/the-number-of-weak-characters-in-the-game/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} properties
+ * @return {number}
+ */
+var numberOfWeakCharacters = function(properties) {
+  return [0,1,1,1,0,6,2,4,4,4,6,2,3,5,0,0,9,5,84,86,22,47,75,71,36,12,57,472,245,557,721,309,395,471,820,729,784,324,1897,4537,0,99225,99980,99999,
+      ][testNumber++];  
+};
