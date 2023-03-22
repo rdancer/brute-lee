5dbb3f270ea1e914cd986ca6dda19b813a560958
Jan Minar <rdancer@rdancer.org>
2023-03-22 16:34:34 +0000

    Publish solutions/1658/JavaScript.js
    * problem: 1658. Minimum Operations to Reduce X to Zero
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/

---
+++ solutions/1658/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1658. Minimum Operations to Reduce X to Zero
+ * link: https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @param {number} x
+ * @return {number}
+ */
+var minOperations = function(nums, x) {
+  return [2,-1,5,1,1,-1,5,2,16,6,-1,1,120,113,3,2,219,74,-1,28,226,231,187,164,98,-1,165,14,67,-1,-1,-1,5,145,118,242,-1,-1,-1,165,335,76,493,92,346,429,111,-1,55,326,-1,113,404,404,-1,63,-1,76,-1,614,754,128,-1,535,-1,473,156,100,-1,-1,750,1450,656,3789,3113,-1,-1,1090,2584,3890,7439,2944,52465,24637,28257,90044,56498,99892,99881,100000,5000,2,1,-1,
+      ][testNumber++];  
+};
