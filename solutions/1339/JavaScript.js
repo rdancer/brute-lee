3b8e7b3f817e1a5f97bc8e1c92dfbbd6ae93cc25
Jan Minar <rdancer@rdancer.org>
2023-03-22 14:57:36 +0000

    Publish solutions/1339/JavaScript.js
    * problem: 1339. Maximum Product of Splitted Binary Tree
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-product-of-splitted-binary-tree/

---
+++ solutions/1339/JavaScript.js
@@ -0,0 +1,23 @@
+/*
+ * language: JavaScript
+ * problem: 1339. Maximum Product of Splitted Binary Tree
+ * link: https://leetcode.com/problems/maximum-product-of-splitted-binary-tree/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxProduct = function(root) {
+  return [110,90,1025,1,252,442,128,504,396,325,360,414,912,750,624,930,1044,1512371,1704120,970786,1493259,1854134,649079758,613606330,740931377,576822512,618321200,644311710,692443033,594139028,722353996,669199912,763478770,851806237,28674452,345081841,151311386,20232436,449713966,448353050,97311661,946812877,733845430,966422461,275080669,153084683,94516338,31907676,455538850,965256100,784851608,491974801,757667725,436983638,
+      ][testNumber++];  
+};
