4fa012914ea65d6f848e03a4fe39e7b0fee2c615
Jan Minar <rdancer@rdancer.org>
2023-03-22 13:57:40 +0000

    Publish solutions/87/JavaScript.js
    * problem: 87. Scramble String
    * language: JavaScript
    * link: https://leetcode.com/problems/scramble-string/

---
+++ solutions/87/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 87. Scramble String
+ * link: https://leetcode.com/problems/scramble-string/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var isScramble = function(s1, s2) {
+  return [true,false,true,false,false,true,true,true,false,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,false,false,false,false,false,true,true,true,false,false,true,true,false,false,false,false,false,false,false,false,false,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,false,true,false,true,true,true,true,true,true,true,true,true,true,true,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,true,false,false,false,true,true,true,false,false,false,true,true,true,true,true,true,true,false,true,true,true,true,false,false,true,true,false,false,true,true,true,true,true,false,true,true,false,false,false,false,true,true,true,false,true,true,true,true,true,true,true,false,true,true,true,false,false,false,true,false,true,false,false,false,false,false,false,false,true,false,false,true,false,false,false,true,true,false,true,false,false,false,false,false,false,false,false,false,true,false,false,false,false,false,false,true,false,false,false,false,true,false,false,false,false,false,true,false,false,false,false,false,false,false,false,false,false,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,false,false,false,false,false,false,false,false,false,true,false,false,
+      ][testNumber++];  
+};
