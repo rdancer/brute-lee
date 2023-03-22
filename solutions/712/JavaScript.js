060c1cf2d331713a6c210895947ad6117162a9a7
Jan Minar <rdancer@rdancer.org>
2023-03-22 16:52:09 +0000

    Publish solutions/712/JavaScript.js
    * problem: 712. Minimum ASCII Delete Sum for Two Strings
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-ascii-delete-sum-for-two-strings/

---
+++ solutions/712/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 712. Minimum ASCII Delete Sum for Two Strings
+ * link: https://leetcode.com/problems/minimum-ascii-delete-sum-for-two-strings/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {number}
+ */
+var minimumDeleteSum = function(s1, s2) {
+  return [231,403,116,1399,1638,1650,1971,1613,2255,1761,1814,1834,1965,2165,2083,2246,1422,1782,1425,1305,1569,1866,1990,1743,1830,1952,1099,2206,1543,1307,2521,1738,2146,785,1178,878,981,683,685,985,684,690,394,1078,980,1080,589,783,490,786,1080,876,686,683,784,981,391,1273,880,780,885,975,682,41731,41444,40654,47873,50154,38458,23191,19860,22526,25699,46228,30740,44148,17226,27607,47576,40366,54344,32453,46474,25794,45705,43192,47878,47113,23106,25262,53484,42076,47307,
+      ][testNumber++];  
+};
