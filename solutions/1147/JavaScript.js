e6e42d8ca9baedba338a737297c3ac060d8fe1b8
Jan Minar <rdancer@rdancer.org>
2023-03-22 15:13:41 +0000

    Publish solutions/1147/JavaScript.js
    * problem: 1147. Longest Chunked Palindrome Decomposition
    * language: JavaScript
    * link: https://leetcode.com/problems/longest-chunked-palindrome-decomposition/

---
+++ solutions/1147/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1147. Longest Chunked Palindrome Decomposition
+ * link: https://leetcode.com/problems/longest-chunked-palindrome-decomposition/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} text
+ * @return {number}
+ */
+var longestDecomposition = function(text) {
+  return [7,1,11,3,2,5,6,1,1,2,1,3,1,1,6,7,2,1,4,5,4,1,8,7,6,1,18,9,6,1,12,9,8,1,14,11,8,1,14,11,8,1,26,7,6,1,36,3,6,1,62,9,6,1,66,7,18,1,58,9,4,1,76,13,6,1,122,15,10,1,104,21,14,1,288,25,16,1,298,15,16,1,1000,1,
+      ][testNumber++];  
+};
