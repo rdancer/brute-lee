41352b9c1759003cae7f681b5f4d2df82d4b1b9b
Jan Minar <rdancer@rdancer.org>
2023-03-22 01:43:49 +0000

    Publish solutions/2337/JavaScript.js
    * problem: 2337. Move Pieces to Obtain a String
    * language: JavaScript
    * link: https://leetcode.com/problems/move-pieces-to-obtain-a-string/

---
+++ solutions/2337/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2337. Move Pieces to Obtain a String
+ * link: https://leetcode.com/problems/move-pieces-to-obtain-a-string/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} start
+ * @param {string} target
+ * @return {boolean}
+ */
+var canChange = function(start, target) {
+  return [true,false,false,false,false,false,false,false,true,true,true,true,true,true,true,true,false,true,true,true,true,true,false,false,false,true,true,false,true,false,true,true,true,true,false,true,true,true,true,true,false,true,true,true,true,true,true,false,true,false,true,true,false,false,false,false,false,true,false,true,true,true,false,true,true,true,true,true,true,false,true,true,false,true,false,false,true,true,false,false,true,true,false,false,true,false,false,false,true,true,false,true,true,false,false,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,false,false,false,false,true,true,false,true,false,false,false,false,false,false,true,false,false,
+      ][testNumber++];  
+};
