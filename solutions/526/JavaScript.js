bf920e739ad8c3f2df87365ad5ca4b248973cb2d
Jan Minar <rdancer@rdancer.org>
2023-03-23 02:09:51 +0000

    Publish solutions/526/JavaScript.js
    * problem: 526. Beautiful Arrangement
    * language: JavaScript
    * link: https://leetcode.com/problems/beautiful-arrangement/

---
+++ solutions/526/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 526. Beautiful Arrangement
+ * link: https://leetcode.com/problems/beautiful-arrangement/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countArrangement = function(n) {
+  return [2,1,3,8,10,36,41,132,250,700,750,4010,4237,10680,24679,
+      ][testNumber++];  
+};
