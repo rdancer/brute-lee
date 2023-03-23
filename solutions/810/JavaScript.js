f43ab9262a0f7f519d799163e35fb35309089e20
Jan Minar <rdancer@rdancer.org>
2023-03-23 19:19:19 +0000

    Publish solutions/810/JavaScript.js
    * problem: 810. Chalkboard XOR Game
    * language: JavaScript
    * link: https://leetcode.com/problems/chalkboard-xor-game/

---
+++ solutions/810/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 810. Chalkboard XOR Game
+ * link: https://leetcode.com/problems/chalkboard-xor-game/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var xorGame = function(nums) {
+  return [false,true,true,false,true,false,true,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,
+      ][testNumber++];  
+};
