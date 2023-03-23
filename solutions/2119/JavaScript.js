91946b2267b5c07104efd7039e0fd5cc5144ec54
Jan Minar <rdancer@rdancer.org>
2023-03-23 01:58:47 +0000

    Publish solutions/2119/JavaScript.js
    * problem: 2119. A Number After a Double Reversal
    * language: JavaScript
    * link: https://leetcode.com/problems/a-number-after-a-double-reversal/

---
+++ solutions/2119/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2119. A Number After a Double Reversal
+ * link: https://leetcode.com/problems/a-number-after-a-double-reversal/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isSameAfterReversals = function(num) {
+  return [true,false,true,false,false,false,false,false,false,true,false,true,true,true,true,true,false,true,false,false,true,false,false,true,true,true,true,false,true,true,true,true,true,true,true,true,true,true,true,true,false,true,true,false,true,true,true,true,true,false,true,false,true,true,true,true,false,true,true,true,true,false,true,true,true,true,false,true,true,true,true,true,true,true,false,true,true,false,true,true,true,false,false,false,true,false,true,false,true,true,true,true,true,true,true,true,true,false,true,true,true,true,false,true,true,true,true,true,false,true,true,true,true,false,true,false,true,false,false,true,true,true,true,true,true,true,false,false,true,
+      ][testNumber++];  
+};
