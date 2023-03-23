4f7da8a343a994f070a90b3f88e24d6bd4bc359f
Jan Minar <rdancer@rdancer.org>
2023-03-23 01:26:07 +0000

    Publish solutions/2532/JavaScript.js
    * problem: 2532. Time to Cross a Bridge
    * language: JavaScript
    * link: https://leetcode.com/problems/time-to-cross-a-bridge/

---
+++ solutions/2532/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 2532. Time to Cross a Bridge
+ * link: https://leetcode.com/problems/time-to-cross-a-bridge/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @param {number} k
+ * @param {number[][]} time
+ * @return {number}
+ */
+var findCrossingTime = function(n, k, time) {
+  return [6,50,3,39999000,18209614,986,149,162,133,115,136,192,228,234,239,143,199,96,183,131,94,195,186,165,127,70,1495750,1890312,1508598,1093903,1234379,1074729,1851748,1853145,1236517,1464334,1553167,1553490,1686669,1417760,1511922,19995000,19983334,19995000,20000000,19995000,19835000,19775000,19735000,19680000,19780000,20000000,20000000,20010,20000000,22,24,14,14,8,86,55,61,16,219,
+      ][testNumber++];  
+};
