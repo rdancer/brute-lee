bfb08532a57849f3842407d69d139b258cf67d73
Jan Minar <rdancer@rdancer.org>
2023-03-23 06:01:16 +0000

    Publish solutions/1275/JavaScript.js
    * problem: 1275. Find Winner on a Tic Tac Toe Game
    * language: JavaScript
    * link: https://leetcode.com/problems/find-winner-on-a-tic-tac-toe-game/

---
+++ solutions/1275/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1275. Find Winner on a Tic Tac Toe Game
+ * link: https://leetcode.com/problems/find-winner-on-a-tic-tac-toe-game/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} moves
+ * @return {string}
+ */
+var tictactoe = function(moves) {
+  return ["A","B","Draw","Pending","Pending","Pending","Pending","Pending","Pending","Pending","Pending","Pending","Pending","Pending","Pending","Pending","Pending","Pending","Pending","Pending","Pending","Pending","Pending","Pending","Pending","Pending","Pending","Pending","Pending","Pending","Pending","B","Pending","Pending","Pending","Pending","Pending","Pending","Pending","Pending","Pending","Pending","Pending","Pending","Pending","A","A","Pending","Pending","Pending","A","Pending","Pending","Pending","Pending","Pending","Pending","A","Pending","Pending","Pending","Pending","Pending","Pending","Pending","B","B","B","Pending","Pending","Pending","Pending","Pending","B","B","A","A","A","A","A","A","A","A","A","Draw","Draw","Draw","A","Draw","A","A","Draw","A","A","A","A","A","A","A","A",
+      ][testNumber++];  
+};
