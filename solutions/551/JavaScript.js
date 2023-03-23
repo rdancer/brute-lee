f0ad1e2b4b8eaad2cf255567767b4106537018b3
Jan Minar <rdancer@rdancer.org>
2023-03-23 00:50:48 +0000

    Publish solutions/551/JavaScript.js
    * problem: 551. Student Attendance Record I
    * language: JavaScript
    * link: https://leetcode.com/problems/student-attendance-record-i/

---
+++ solutions/551/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 551. Student Attendance Record I
+ * link: https://leetcode.com/problems/student-attendance-record-i/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkRecord = function(s) {
+  return [true,false,false,false,false,true,true,true,false,false,true,true,false,false,false,true,true,false,true,true,true,true,true,true,true,true,false,true,true,false,true,true,true,false,false,false,false,false,false,false,false,false,true,true,true,true,false,true,true,true,true,true,true,false,true,false,false,false,true,false,false,true,false,false,false,false,false,false,false,false,false,false,false,false,true,true,false,false,false,false,false,false,false,true,true,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,
+      ][testNumber++];  
+};
