cd3eff4188febb954aada53394fabac35986eb75
Jan Minar <rdancer@rdancer.org>
2023-03-23 17:20:08 +0000

    Publish solutions/1160/JavaScript.js
    * problem: 1160. Find Words That Can Be Formed by Characters
    * language: JavaScript
    * link: https://leetcode.com/problems/find-words-that-can-be-formed-by-characters/

---
+++ solutions/1160/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1160. Find Words That Can Be Formed by Characters
+ * link: https://leetcode.com/problems/find-words-that-can-be-formed-by-characters/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string[]} words
+ * @param {string} chars
+ * @return {number}
+ */
+var countCharacters = function(words, chars) {
+  return [6,10,0,0,0,2,16,113,87,1,32,8,420,54,308,239,1165,37,1,1225,12,1056,162,2545,3,1026,95,642,103,424,190,525,90,34,619,579,
+      ][testNumber++];  
+};
