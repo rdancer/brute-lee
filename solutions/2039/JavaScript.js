43d03438a3449f50652b8260de1b29a7ea119b76
Jan Minar <rdancer@rdancer.org>
2023-03-23 06:29:36 +0000

    Publish solutions/2039/JavaScript.js
    * problem: 2039. The Time When the Network Becomes Idle
    * language: JavaScript
    * link: https://leetcode.com/problems/the-time-when-the-network-becomes-idle/

---
+++ solutions/2039/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2039. The Time When the Network Becomes Idle
+ * link: https://leetcode.com/problems/the-time-when-the-network-becomes-idle/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} edges
+ * @param {number[]} patience
+ * @return {number}
+ */
+var networkBecomesIdle = function(edges, patience) {
+  return [8,3,3,67,63,20,44,27,20,64,32,22,64,32,5,36,48,35,36,32,12,64,39,376,392,64,64,104,459,619,123,466,752,867,3900,260,1624,1700,6468,4464,15324,9616,21552,399991,25596,143984,399992,4,7,8,
+      ][testNumber++];  
+};
