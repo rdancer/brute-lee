5794d655b107ebc601415f6272390f31cdda1c5a
Jan Minar <rdancer@rdancer.org>
2023-03-23 07:47:12 +0000

    Publish solutions/689/JavaScript.js
    * problem: 689. Maximum Sum of 3 Non-Overlapping Subarrays
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-sum-of-3-non-overlapping-subarrays/

---
+++ solutions/689/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 689. Maximum Sum of 3 Non-Overlapping Subarrays
+ * link: https://leetcode.com/problems/maximum-sum-of-3-non-overlapping-subarrays/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSumOfThreeSubarrays = function(nums, k) {
+  return [[0,3,5],[0,2,4],[0,3,5],[0,2,4],[0,1,2],[0,1,2],[1,4,7],[4,5,7],[0,4,7],[0,3,6],[0,7,14],[0,6,13],[3,8,14],[2,4,8],[1,14,28],[1,17,28],[6,17,29],[10,29,36],[11,19,50],[7,29,68],[38,58,64],[6,41,75],[26,99,157],[1,61,121],[28,73,155],[52,75,118],[3,136,268],[51,162,291],[43,137,248],[36,238,259],[18,795,888],[27,671,848],[38,251,580],[104,392,708],[599,1258,3445],[1953,2710,3681],[14,1534,3439],[478,2066,3593],[763,7486,16045],[1333,7427,14873],[1360,8807,15210],[1993,8663,14147],[14630,16420,18210],
+      ][testNumber++];  
+};
