c151f863c8d6efdae40136eb4d5deb115e3de882
Jan Minar <rdancer@rdancer.org>
2023-03-24 19:02:20 +0000

    Publish solutions/501/JavaScript.js
    * problem: 501. Find Mode in Binary Search Tree
    * language: JavaScript
    * link: https://leetcode.com/problems/find-mode-in-binary-search-tree/

---
+++ solutions/501/JavaScript.js
@@ -0,0 +1,23 @@
+/*
+ * language: JavaScript
+ * problem: 501. Find Mode in Binary Search Tree
+ * link: https://leetcode.com/problems/find-mode-in-binary-search-tree/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var findMode = function(root) {
+  return [[2],[0],[2,1],[6,5,4,3,2],[1],[1,2],[1],[0],[0,1],[2],[1],[-2],[-4],[2,6],[2],[2,1],[1,2],[4],[3],[24],[36,46,14],[0],[-57762],
+      ][testNumber++];  
+};
