d6a855f2426ec9bffdc012f4d7bc4abe77664a66
Jan Minar <rdancer@rdancer.org>
2023-03-24 02:05:13 +0000

    Publish solutions/2188/JavaScript.js
    * problem: 2188. Minimum Time to Finish the Race
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-time-to-finish-the-race/

---
+++ solutions/2188/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 2188. Minimum Time to Finish the Race
+ * link: https://leetcode.com/problems/minimum-time-to-finish-the-race/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} tires
+ * @param {number} changeTime
+ * @param {number} numLaps
+ * @return {number}
+ */
+var minimumFinishTime = function(tires, changeTime, numLaps) {
+  return [21,25,17395,1405,138,318,2526,1317,146,2738,1047,725,215088,257555,33378,24792,58999,38760,6235,51991,125051,13143,109416,119126,310595,180154,17889,13357376,5078986,10534257,21972799,14202349,3650095,38468745,9093877,7398887,3365857,2040819,12798128,1258092,16100314,10832725,33377780,19774012,8214040,19514456,1,8214040,199900000,199900000,3,747449,6381039,18967026,1,62,54895,30,
+      ][testNumber++];  
+};
