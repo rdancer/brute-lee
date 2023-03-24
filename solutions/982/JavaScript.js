964dbc9e649c3507ef5d33e4517cf6139e52f40d
Jan Minar <rdancer@rdancer.org>
2023-03-24 07:20:42 +0000

    Publish solutions/982/JavaScript.js
    * problem: 982. Triples with Bitwise AND Equal To Zero
    * language: JavaScript
    * link: https://leetcode.com/problems/triples-with-bitwise-and-equal-to-zero/

---
+++ solutions/982/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 982. Triples with Bitwise AND Equal To Zero
+ * link: https://leetcode.com/problems/triples-with-bitwise-and-equal-to-zero/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var countTriplets = function(nums) {
+  return [12,27,0,12,1,7,0,0,30,0,0,25,18,0,0,0,0,14916,10308,99678,118170,942354,927990,113138322,120171126,
+      ][testNumber++];  
+};
