cb2c291bc21c54ef15ab74c3deec1f1f1732f9c6
Jan Minar <rdancer@rdancer.org>
2023-03-24 06:20:56 +0000

    Publish solutions/1371/JavaScript.js
    * problem: 1371. Find the Longest Substring Containing Vowels in Even Counts
    * language: JavaScript
    * link: https://leetcode.com/problems/find-the-longest-substring-containing-vowels-in-even-counts/

---
+++ solutions/1371/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1371. Find the Longest Substring Containing Vowels in Even Counts
+ * link: https://leetcode.com/problems/find-the-longest-substring-containing-vowels-in-even-counts/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var findTheLongestSubstring = function(s) {
+  return [13,5,6,3,1,4,8,6,0,9,8,6,5,9,7,7,4,3,6,4,6,5,9,295,831,590,297,806,622,194,290,122,123,472,504,247,53,313,2870,4433,4057,7836,8221,2230,7831,8756,5176,6488,99972,99983,499995,499995,499995,
+      ][testNumber++];  
+};
