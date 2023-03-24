86da943d27c915380eda9c4015e4c535d3b721df
Jan Minar <rdancer@rdancer.org>
2023-03-24 02:11:21 +0000

    Publish solutions/419/JavaScript.js
    * problem: 419. Battleships in a Board
    * language: JavaScript
    * link: https://leetcode.com/problems/battleships-in-a-board/

---
+++ solutions/419/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 419. Battleships in a Board
+ * link: https://leetcode.com/problems/battleships-in-a-board/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {character[][]} board
+ * @return {number}
+ */
+var countBattleships = function(board) {
+  return [2,0,1,0,1,2,2,0,1,1,2,2,1,1,1,5,10,14,14,18,19,40,38,10,100,331,457,
+      ][testNumber++];  
+};
