1ca4ec385f8f459a7603a231891c39008eb93f0f
Jan Minar <rdancer@rdancer.org>
2023-03-24 07:14:55 +0000

    Publish solutions/473/JavaScript.js
    * problem: 473. Matchsticks to Square
    * language: JavaScript
    * link: https://leetcode.com/problems/matchsticks-to-square/

---
+++ solutions/473/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 473. Matchsticks to Square
+ * link: https://leetcode.com/problems/matchsticks-to-square/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} matchsticks
+ * @return {boolean}
+ */
+var makesquare = function(matchsticks) {
+  return [true,false,true,true,true,false,true,false,false,true,false,true,true,false,false,false,false,false,true,false,false,true,true,true,false,false,false,false,false,false,false,true,false,false,false,false,true,false,false,false,false,true,false,false,false,false,false,false,true,false,false,false,true,false,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,false,false,false,true,false,false,false,false,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,true,false,true,false,true,false,true,false,false,false,true,true,false,false,false,true,false,false,true,false,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,true,false,true,true,true,true,true,true,true,true,false,false,true,false,false,false,true,true,true,true,false,true,true,false,false,false,false,false,false,false,false,false,false,true,false,true,true,false,false,true,true,false,false,true,
+      ][testNumber++];  
+};
