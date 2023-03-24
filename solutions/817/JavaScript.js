eb3c5a9bdb0fea8e547a379f46290a53a670047a
Jan Minar <rdancer@rdancer.org>
2023-03-24 19:25:55 +0000

    Publish solutions/817/JavaScript.js
    * problem: 817. Linked List Components
    * language: JavaScript
    * link: https://leetcode.com/problems/linked-list-components/

---
+++ solutions/817/JavaScript.js
@@ -0,0 +1,23 @@
+/*
+ * language: JavaScript
+ * problem: 817. Linked List Components
+ * link: https://leetcode.com/problems/linked-list-components/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numComponents = function(head, nums) {
+  return [2,2,1,2,1,1,1,1,1,2,4,4,4,2,3,3,6,4,2,3,5,10,14,16,15,9,14,23,25,21,11,12,7,13,24,23,13,20,24,26,166,36,103,104,140,256,247,237,240,222,2406,1474,2161,2384,702,1,1,
+      ][testNumber++];  
+};
