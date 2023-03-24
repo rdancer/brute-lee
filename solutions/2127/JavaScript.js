8563fd28fa69492b13ead1ec3e12b8a3cd05f884
Jan Minar <rdancer@rdancer.org>
2023-03-24 07:45:34 +0000

    Publish solutions/2127/JavaScript.js
    * problem: 2127. Maximum Employees to Be Invited to a Meeting
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-employees-to-be-invited-to-a-meeting/

---
+++ solutions/2127/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2127. Maximum Employees to Be Invited to a Meeting
+ * link: https://leetcode.com/problems/maximum-employees-to-be-invited-to-a-meeting/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} favorite
+ * @return {number}
+ */
+var maximumInvitations = function(favorite) {
+  return [3,3,4,2,6,4,6,11,6,8,5,4,6,6,3,4,3,4,15,5,4,11,6,4,3,6,3,5,3,6,3,4,6,4,16,4,5,5,8,3,10,4,3,7,4,4,5,3,7,3,3,3,4,3,3,5,11,5,15,9,27,16,14,13,27,13,11,23,16,8,12,20,8,46,38,8,17,14,74,82,54,59,88,92,102,100000,47904,40001,100000,4,25000,3,6,16,6,3,200,4,
+      ][testNumber++];  
+};
