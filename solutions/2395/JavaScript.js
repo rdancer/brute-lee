caeb8ebbbb6d1747973e2eee4d606453e14be083
Jan Minar <rdancer@rdancer.org>
2023-03-24 18:56:51 +0000

    Publish solutions/2395/JavaScript.js
    * problem: 2395. Find Subarrays With Equal Sum
    * language: JavaScript
    * link: https://leetcode.com/problems/find-subarrays-with-equal-sum/

---
+++ solutions/2395/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2395. Find Subarrays With Equal Sum
+ * link: https://leetcode.com/problems/find-subarrays-with-equal-sum/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var findSubarrays = function(nums) {
+  return [true,false,true,false,false,false,false,false,true,false,false,false,true,false,false,false,false,false,false,false,false,false,false,false,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,false,false,true,
+      ][testNumber++];  
+};
