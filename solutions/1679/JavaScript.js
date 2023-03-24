645c3b4871f2d5076e9d106b64cb57086a7818ca
Jan Minar <rdancer@rdancer.org>
2023-03-24 02:21:40 +0000

    Publish solutions/1679/JavaScript.js
    * problem: 1679. Max Number of K-Sum Pairs
    * language: JavaScript
    * link: https://leetcode.com/problems/max-number-of-k-sum-pairs/

---
+++ solutions/1679/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1679. Max Number of K-Sum Pairs
+ * link: https://leetcode.com/problems/max-number-of-k-sum-pairs/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxOperations = function(nums, k) {
+  return [2,1,0,0,2,0,0,0,0,2,0,4,0,42,107,2,91,126,1,112,51,24,34,8,47,33,138,5,92,5,9,0,116,91,25,1,100,11,5,5135,5808,8011,12957,16415,5682,13267,4968,4698,1519,12598,50000,
+      ][testNumber++];  
+};
