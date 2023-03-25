a2a4ccfd07d641662a6d4074c38071133a8f0a1c
Jan Minar <rdancer@rdancer.org>
2023-03-25 08:43:14 +0000

    Publish solutions/841/JavaScript.js
    * problem: 841. Keys and Rooms
    * language: JavaScript
    * link: https://leetcode.com/problems/keys-and-rooms/

---
+++ solutions/841/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 841. Keys and Rooms
+ * link: https://leetcode.com/problems/keys-and-rooms/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} rooms
+ * @return {boolean}
+ */
+var canVisitAllRooms = function(rooms) {
+  return [true,false,false,true,true,true,true,false,true,false,true,true,false,true,true,false,true,true,false,false,false,false,false,false,false,false,true,false,false,false,false,false,false,false,false,false,true,true,false,false,false,false,false,false,false,false,true,true,false,false,false,false,false,false,false,false,true,true,false,false,false,false,false,false,false,false,false,false,
+      ][testNumber++];  
+};
