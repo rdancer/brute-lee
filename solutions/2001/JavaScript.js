67e0f8f11fafc459e0a04d3fbd39b03767a7107c
Jan Minar <rdancer@rdancer.org>
2023-03-25 19:34:34 +0000

    Publish solutions/2001/JavaScript.js
    * problem: 2001. Number of Pairs of Interchangeable Rectangles
    * language: JavaScript
    * link: https://leetcode.com/problems/number-of-pairs-of-interchangeable-rectangles/

---
+++ solutions/2001/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2001. Number of Pairs of Interchangeable Rectangles
+ * link: https://leetcode.com/problems/number-of-pairs-of-interchangeable-rectangles/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} rectangles
+ * @return {number}
+ */
+var interchangeableRectangles = function(rectangles) {
+  return [6,0,1,2,1,1,1,3,1,18,2,1,19,29,17,12,18,251,20,420,727,25136,933,676,6164,379,2093,165,239,1048,233,311,238,1105,220,1044921,3060,1305,868,19768,161,165,228,0,4999950000,0,
+      ][testNumber++];  
+};
