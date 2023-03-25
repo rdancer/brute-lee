f84801a8bbc8e087a03750cb9e03b5c0dea654e7
Jan Minar <rdancer@rdancer.org>
2023-03-25 19:24:54 +0000

    Publish solutions/1664/JavaScript.js
    * problem: 1664. Ways to Make a Fair Array
    * language: JavaScript
    * link: https://leetcode.com/problems/ways-to-make-a-fair-array/

---
+++ solutions/1664/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1664. Ways to Make a Fair Array
+ * link: https://leetcode.com/problems/ways-to-make-a-fair-array/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var waysToMakeFair = function(nums) {
+  return [1,3,0,1,5,0,2,0,9,0,0,0,0,23,1,1,2,1,0,1,1,5,0,0,0,1,0,1,1,0,0,0,0,0,2,7,4,2,93,95,1,1,0,1,10,0,949,8,1,0,13,6,1683,19,51,20,20,0,14,0,3363,0,3733,0,0,40,2,0,10,5051,0,0,0,33,0,18,37,10,0,8,0,0,38,4,7935,0,7967,22,32,22,9083,0,0,0,0,41,40003,48869,241,61099,0,7,88373,0,105,
+      ][testNumber++];  
+};
