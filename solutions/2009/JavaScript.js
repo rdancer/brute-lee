2787777dfd538bb22d293425d677a95de292b1ca
Jan Minar <rdancer@rdancer.org>
2023-03-25 01:40:51 +0000

    Publish solutions/2009/JavaScript.js
    * problem: 2009. Minimum Number of Operations to Make Array Continuous
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-number-of-operations-to-make-array-continuous/

---
+++ solutions/2009/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2009. Minimum Number of Operations to Make Array Continuous
+ * link: https://leetcode.com/problems/minimum-number-of-operations-to-make-array-continuous/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minOperations = function(nums) {
+  return [0,1,3,2,0,2,5,6,9,4,3,6,8,5,10,3,11,4,5,3,5,10,12,11,8,13,15,10,19,158,139,341,190,394,170,128,154,330,188,358,169,88,27,458,171,430,26,406,328,3335,3527,1089,1747,7958,9999,99989,37096,0,99975,1,11,50000,
+      ][testNumber++];  
+};
