74639dbf821de3fc259701ac862de515b456f0c5
Jan Minar <rdancer@rdancer.org>
2023-03-25 08:51:07 +0000

    Publish solutions/1425/JavaScript.js
    * problem: 1425. Constrained Subsequence Sum
    * language: JavaScript
    * link: https://leetcode.com/problems/constrained-subsequence-sum/

---
+++ solutions/1425/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1425. Constrained Subsequence Sum
+ * link: https://leetcode.com/problems/constrained-subsequence-sum/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var constrainedSubsetSum = function(nums, k) {
+  return [37,-1,23,26605,11355,18990,13722,16091,1137751,5,35,78,8,5000,125,20,190,2454807,12245752,24359456,250377944,249541091,250804520,1,29,12835809,85080738,102089162,103895801,220769890,219616200,2073,2064,250729324,5,10,
+      ][testNumber++];  
+};
