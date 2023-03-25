b6172ae6afad36129fd15070c811563f94f91b47
Jan Minar <rdancer@rdancer.org>
2023-03-25 19:59:24 +0000

    Publish solutions/864/JavaScript.js
    * problem: 864. Shortest Path to Get All Keys
    * language: JavaScript
    * link: https://leetcode.com/problems/shortest-path-to-get-all-keys/

---
+++ solutions/864/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 864. Shortest Path to Get All Keys
+ * link: https://leetcode.com/problems/shortest-path-to-get-all-keys/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string[]} grid
+ * @return {number}
+ */
+var shortestPathAllKeys = function(grid) {
+  return [8,6,-1,1,-1,3,10,-1,-1,-1,11,-1,7,12,8,-1,14,7,11,10,19,21,25,4,11,33,35,27,42,70,80,71,76,64,83,
+      ][testNumber++];  
+};
