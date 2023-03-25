b60fb6ab7b31b5d24f1526fe7437d0b71d244456
Jan Minar <rdancer@rdancer.org>
2023-03-25 19:51:57 +0000

    Publish solutions/287/JavaScript.js
    * problem: 287. Find the Duplicate Number
    * language: JavaScript
    * link: https://leetcode.com/problems/find-the-duplicate-number/

---
+++ solutions/287/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 287. Find the Duplicate Number
+ * link: https://leetcode.com/problems/find-the-duplicate-number/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+  return [2,3,1,1,2,1,2,2,1,4,2,1,4,1,7,9,5,1,1,9,1,2,5,16,17,18,15,12,4,4,12,5,18,29,22,4,1,13,12,4,24,35,42,75,9,83,18,41,89,8,22,31,12983,59078,26652,77496,22774,14143,
+      ][testNumber++];  
+};
