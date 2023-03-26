185e050390baff7cd750997974233376ab7a1971
Jan Minar <rdancer@rdancer.org>
2023-03-26 11:11:52 +0100

    Publish solutions/1871/JavaScript.js
    * problem: 1871. Jump Game VII
    * language: JavaScript
    * link: https://leetcode.com/problems/jump-game-vii/

---
+++ solutions/1871/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 1871. Jump Game VII
+ * link: https://leetcode.com/problems/jump-game-vii/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @param {number} minJump
+ * @param {number} maxJump
+ * @return {boolean}
+ */
+var canReach = function(s, minJump, maxJump) {
+  return [true,false,true,false,true,true,true,false,true,true,true,false,false,false,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,true,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,true,true,true,false,true,false,true,true,false,false,true,false,true,false,true,false,true,true,false,false,true,true,true,true,false,false,false,true,true,false,true,false,true,true,
+      ][testNumber++];  
+};
