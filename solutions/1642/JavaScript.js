3961507afe64f9a060a15511cbcf390d3249f5f8
Jan Minar <rdancer@rdancer.org>
2023-03-26 03:04:26 +0100

    Publish solutions/1642/JavaScript.js
    * problem: 1642. Furthest Building You Can Reach
    * language: JavaScript
    * link: https://leetcode.com/problems/furthest-building-you-can-reach/

---
+++ solutions/1642/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 1642. Furthest Building You Can Reach
+ * link: https://leetcode.com/problems/furthest-building-you-can-reach/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} heights
+ * @param {number} bricks
+ * @param {number} ladders
+ * @return {number}
+ */
+var furthestBuilding = function(heights, bricks, ladders) {
+  return [4,7,3,6,1,0,7,1,1,1,5,589,72329,151,4,8,8,5,3,4,3,3,4,7,7,4,5,9,30,5,1242,1227,387,632,488,225,19,760,24,935,882,1033,639,1393,986,1876,1677,674,1223,716,1764,1870,1641,281,425,543,1950,1667,1848,666,172,324,1736,967,1885,866,881,826,1562,66397,54056,2,99999,2,3,3,0,
+      ][testNumber++];  
+};
