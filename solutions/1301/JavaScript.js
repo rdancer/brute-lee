d1ab00967373f5d98940da536ec01c3198ecc367
Jan Minar <rdancer@rdancer.org>
2023-03-26 03:10:52 +0100

    Publish solutions/1301/JavaScript.js
    * problem: 1301. Number of Paths with Max Score
    * language: JavaScript
    * link: https://leetcode.com/problems/number-of-paths-with-max-score/

---
+++ solutions/1301/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1301. Number of Paths with Max Score
+ * link: https://leetcode.com/problems/number-of-paths-with-max-score/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string[]} board
+ * @return {number[]}
+ */
+var pathsWithMaxScore = function(board) {
+  return [[7,1],[4,2],[0,0],[0,1],[5,1],[0,0],[27,1],[34,3],[40,1],[36,1],[53,3],[111,1],[108,2],[649,12],[1332,1152],[1329,16],[1355,256],[1349,528],[1348,8],[1344,24],[1352,130],[1357,384],[1323,72],[1381,144],[1773,690285631],
+      ][testNumber++];  
+};
