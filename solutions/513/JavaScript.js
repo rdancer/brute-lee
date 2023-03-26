218736903c429f9b0ece02116d1f77a41bedb124
Jan Minar <rdancer@rdancer.org>
2023-03-26 03:25:28 +0100

    Publish solutions/513/JavaScript.js
    * problem: 513. Find Bottom Left Tree Value
    * language: JavaScript
    * link: https://leetcode.com/problems/find-bottom-left-tree-value/

---
+++ solutions/513/JavaScript.js
@@ -0,0 +1,23 @@
+/*
+ * language: JavaScript
+ * problem: 513. Find Bottom Left Tree Value
+ * link: https://leetcode.com/problems/find-bottom-left-tree-value/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var findBottomLeftValue = function(root) {
+  return [1,7,0,1,1,1,-1,-1,1,1,1,6,0,3,45,-21,-70,56,57,78,-1,0,-90,-36,-91,-69,36,26,31,24,34,-43,-58,90,30,85,-72,-77,-60,-23,40,-46,-7,45,-19,24,49,29,56,80,-81,-98,-907,-988,-9983,-9927,3,-7418,2147483647,-2147483648,2147483647,-2147483648,2147483647,-2147483648,2147483647,-2147483648,-2147483648,2147483647,-2147483648,1,-2147483648,12,2147483645,-2147483647,-2147483647,-2147483648,7,
+      ][testNumber++];  
+};
