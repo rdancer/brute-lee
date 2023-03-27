a26ca8da88a21d9e1c9e8593d2cf3d3dc1d03d7c
Jan Minar <rdancer@rdancer.org>
2023-03-27 16:43:23 +0100

    Publish solutions/2537/JavaScript.js
    * problem: 2537. Count the Number of Good Subarrays
    * language: JavaScript
    * link: https://leetcode.com/problems/count-the-number-of-good-subarrays/

---
+++ solutions/2537/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2537. Count the Number of Good Subarrays
+ * link: https://leetcode.com/problems/count-the-number-of-good-subarrays/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var countGood = function(nums, k) {
+  return [1,4,0,0,21,9,15,0,16,4,0,0,315,255,295,275,297,282,250,272,273,258,6524,7012,6485,7233,6477,7044,7157,7944,5411,7757,449378425,14152522,0,4600719989,4999950000,1586336940,675450933,0,5,
+      ][testNumber++];  
+};
