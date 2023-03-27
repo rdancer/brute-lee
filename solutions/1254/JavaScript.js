8b715cfa15a81fc40e32bf169593f31f6db3349c
Jan Minar <rdancer@rdancer.org>
2023-03-27 16:32:57 +0100

    Publish solutions/1254/JavaScript.js
    * problem: 1254. Number of Closed Islands
    * language: JavaScript
    * link: https://leetcode.com/problems/number-of-closed-islands/

---
+++ solutions/1254/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1254. Number of Closed Islands
+ * link: https://leetcode.com/problems/number-of-closed-islands/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var closedIsland = function(grid) {
+  return [2,1,2,0,0,1,1,1,1,1,1,0,0,5,4,3,6,5,14,72,25,25,8,12,39,49,18,181,61,61,18,30,181,61,61,18,30,162,163,157,761,265,265,98,147,602,597,
+      ][testNumber++];  
+};
