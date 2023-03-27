f912556e24174f8a8a5d4d73f279702e93fa1eb2
Jan Minar <rdancer@rdancer.org>
2023-03-27 16:59:13 +0100

    Publish solutions/2172/JavaScript.js
    * problem: 2172. Maximum AND Sum of Array
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-and-sum-of-array/

---
+++ solutions/2172/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2172. Maximum AND Sum of Array
+ * link: https://leetcode.com/problems/maximum-and-sum-of-array/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @param {number} numSlots
+ * @return {number}
+ */
+var maximumANDSum = function(nums, numSlots) {
+  return [9,24,40,16,60,13,1,5,35,55,54,12,2,40,18,33,3,19,19,0,14,28,70,22,3,20,53,4,27,6,32,23,33,2,67,6,4,20,8,66,32,4,6,14,5,0,49,21,6,38,24,72,21,16,51,37,15,30,27,2,16,17,8,30,25,19,3,10,32,51,3,7,27,0,1,0,90,10,2,48,74,64,59,90,
+      ][testNumber++];  
+};
