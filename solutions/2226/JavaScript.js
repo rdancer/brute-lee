8ddaded57686dfa6104d17252f20c86e77a381f6
Jan Minar <rdancer@rdancer.org>
2023-03-27 14:40:49 +0100

    Publish solutions/2226/JavaScript.js
    * problem: 2226. Maximum Candies Allocated to K Children
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-candies-allocated-to-k-children/

---
+++ solutions/2226/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2226. Maximum Candies Allocated to K Children
+ * link: https://leetcode.com/problems/maximum-candies-allocated-to-k-children/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} candies
+ * @param {number} k
+ * @return {number}
+ */
+var maximumCandies = function(candies, k) {
+  return [5,0,3,3,1,0,3,6,4,10,3,6,15,14,17,15,15,17,14,659,804,451,489,398,895,463,758,703,1158,676,487,608,428,677,1063,33452,72959,99195,61898,309205,66163,92952,50231,156248,106345,1000000,43715,70282,199753,262521,1,0,10000000,1,10000000,5,500,1000,0,950,1477,15,12,6,1,3,1,1,1,1,1,95,1,4,2,5,1,3,1,3,1,5,1,6,1,9,1,30,7,1,29,1,1,27,1,1,1,0,1,9999999,
+      ][testNumber++];  
+};
