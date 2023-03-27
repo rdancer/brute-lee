7c7df0adf2c8fabbbca965c73ddd4d152107a75f
Jan Minar <rdancer@rdancer.org>
2023-03-27 19:01:33 +0100

    still working version, now with error checking

--- rate_limiter_logger.py
+++ rate_limiter_logger.py
@@ -7,12 +7,8 @@ import matplotlib.dates as mdates
 import numpy as np
 from freezegun import freeze_time
 
-
 from mpl_toolkits.axes_grid1 import host_subplot
 import mpl_toolkits.axisartist as AA
-
-
-import sys; print(sys.executable)
 
 
 class RateLimiterLogger:
@@ -102,11 +98,17 @@ class RateLimiterLogger:
             raise Exception("You must call log_submission with the same problem_oneline_description before log_result.")
         conn = sqlite3.connect(self.db_file)
         c = conn.cursor()
+        # Update the problem's test_suite_size if it's not None
+        if test_suite_size is not None:
+            c.execute("UPDATE problems SET test_suite_size = ? WHERE problem_oneline_description = ?",
+                    (test_suite_size, problem_oneline_description))
         c.execute('''UPDATE logs SET success = ?, test_suite_size = ?, test_cases_passed = ? 
                      WHERE id = ?''',
                   (success, test_suite_size, test_cases_passed, self.last_submission["id"]))
         conn.commit()
         conn.close()
+        # reset the last_submission
+        self.last_submission = None
 
     def generate_report(self):
         """
@@ -138,15 +140,7 @@ class RateLimiterLogger:
         # Create a figure and axis
         fig, ax = plt.subplots(figsize=(14, 6), dpi=100)
 
-        # Create three separate Y-axes
-        host = host_subplot(111, axes_class=AA.Axes)
-        plt.subplots_adjust(right=0.75)
-        ax3 = host.twinx()
-        ax4 = host.twinx()
-        offset = 60
-        new_fixed_axis = ax4.get_grid_helper().new_fixed_axis
-        ax4.axis["right"] = new_fixed_axis(loc="right", axes=ax4, offset=(offset, 0))
-        ax4.axis["right"].toggle(all=True)
+        host = ax # XXX for now while we're merging, let this be the host, so we don't get distracted by diffs that are merely the rename of this variable
 
         # Plot the test suite size and progress towards it
         host.fill_between(timestamps, test_suite_size, color='yellow', alpha=0.3, label='Test Suite Size')
@@ -182,78 +176,6 @@ class RateLimiterLogger:
         host.set_xlabel('Time')
         host.set_ylabel('Test Cases')
         host.legend(loc='upper left')
-
-
-        #
-        # Plot the rate-limited gaps as well as the duration since the last submission
-        #
-
-        # Calculate the duration since the last successful submission
-        last_successful = None
-        duration_since_last_successful_submission = []
-        for i, s in enumerate(success):
-            if s:
-                if last_successful is None:
-                    duration = 0
-                else:
-                    duration = (timestamps[i] - last_successful) * 24 * 60 * 60
-                last_successful = timestamps[i]
-            else:
-                duration = None
-            duration_since_last_successful_submission.append(duration)
-
-        # Separate the durations into the required scales
-        duration_lt_20s = [d if d is not None and d < 20 else None for d in duration_since_last_successful_submission]
-        duration_20s_to_50m = [d if d is not None and 20 <= d < 50 * 60 else None for d in duration_since_last_successful_submission]
-        duration_gt_50m = [d if d is not None and d >= 50 * 60 else None for d in duration_since_last_successful_submission]
-
-        # Create three separate Y-axes
-        host = host_subplot(111, axes_class=AA.Axes)
-        host.set_ylim(ax.get_ylim())
-        host.set_xlim(ax.get_xlim())
-        plt.subplots_adjust(right=0.75)
-        ax3 = host.twinx()
-        ax4 = host.twinx()
-        offset = 60
-        new_fixed_axis = ax4.get_grid_helper().new_fixed_axis
-        ax4.axis["right"] = new_fixed_axis(loc="right", axes=ax4, offset=(offset, 0))
-        ax4.axis["right"].toggle(all=True)
-
-        # Plot the duration on the separate Y-axes
-        host.scatter(timestamps, duration_lt_20s, facecolors='none', edgecolors='purple', label='Duration (<20s)')
-        ax3.scatter(timestamps, duration_20s_to_50m, facecolors='none', edgecolors='orange', label='Duration (20s-50m)')
-        ax4.scatter(timestamps, duration_gt_50m, facecolors='none', edgecolors='red', label='Duration (>50m)')
-
-        # Annotate durations and set labels
-        for i, d in enumerate(duration_20s_to_50m):
-            if d is not None:
-                ax3.annotate(f'{int(d // 60)}:{int(d % 60):02}', (timestamps[i], d), fontsize=8, rotation=0, color='orange')
-        for i, d in enumerate(duration_gt_50m):
-            if d is not None:
-                ax4.annotate(f'{int(d // 3600)}:{int((d % 3600) // 60):02}', (timestamps[i], d), fontsize=8, rotation=0, color='red')
-
-        # Set Y-axis labels
-        host.set_ylabel('Duration since last successful submission (<20s)')
-        ax3.set_ylabel('Duration since last successful submission (20s-50m)')
-        ax4.set_ylabel('Duration since last successful submission (>50m)')
-
-        # Set Y-axis limits
-        host.set_ylim(0, 20 if duration_lt_20s else 0)
-        print(duration_20s_to_50m)
-        ax3.set_ylim(0, max([0] + [d for d in filter(None, duration_20s_to_50m)]))
-        ax4.set_ylim(0, max([0] + [d for d in filter(None, duration_gt_50m)]))
-
-        # Format the x-axis with date-time labels
-        ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M'))
-        plt.xticks(rotation=45)
-
-        # Add labels and legend
-        ax.set_xlabel('Time')
-        ax.set_ylabel('Test Cases')
-        ax.legend(loc='upper left')
-        host.legend(loc='upper right')
-        ax3.legend(loc='upper right', bbox_to_anchor=(1.0, 0.9))
-        ax4.legend(loc='upper right', bbox_to_anchor=(1.0, 0.8))
 
         #
         # Save the plot to an image
