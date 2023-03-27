2ac22b300c09330db9c4a7f2271837567d362321
Jan Minar <rdancer@rdancer.org>
2023-03-27 17:05:27 +0100

    Publish solutions/375/JavaScript.js
    * problem: 375. Guess Number Higher or Lower II
    * language: JavaScript
    * link: https://leetcode.com/problems/guess-number-higher-or-lower-ii/

---
+++ solutions/375/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 375. Guess Number Higher or Lower II
+ * link: https://leetcode.com/problems/guess-number-higher-or-lower-ii/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var getMoneyAmount = function(n) {
+  return [16,0,1,2,8,10,14,34,42,64,144,198,226,282,310,365,454,494,529,630,640,698,743,823,858,898,952,
+      ][testNumber++];  
+};
