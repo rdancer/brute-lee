d7f54191212456ccd04d73c8fd9232144c9b6316
Jan Minar <rdancer@rdancer.org>
2023-03-27 01:46:17 +0100

    Publish solutions/1896/JavaScript.js
    * problem: 1896. Minimum Cost to Change the Final Value of Expression
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-cost-to-change-the-final-value-of-expression/

---
+++ solutions/1896/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1896. Minimum Cost to Change the Final Value of Expression
+ * link: https://leetcode.com/problems/minimum-cost-to-change-the-final-value-of-expression/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} expression
+ * @return {number}
+ */
+var minOperationsToFlip = function(expression) {
+  return [1,3,1,1,2,1,1,1,3,2,1,2,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,2,1,2,1,4,2,3,4,5,6,7,8,9,10,11,12,13,14,1,1,1,1,1,1,1,1,4,15,2,1,2,2,2,2,1,1,
+      ][testNumber++];  
+};
