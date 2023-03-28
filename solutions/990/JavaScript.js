81965752e83a3e72e070e4e4864f8c118b781f8c
Jan Minar <rdancer@rdancer.org>
2023-03-28 02:12:22 +0100

    Publish solutions/990/JavaScript.js
    * problem: 990. Satisfiability of Equality Equations
    * language: JavaScript
    * link: https://leetcode.com/problems/satisfiability-of-equality-equations/

---
+++ solutions/990/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 990. Satisfiability of Equality Equations
+ * link: https://leetcode.com/problems/satisfiability-of-equality-equations/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string[]} equations
+ * @return {boolean}
+ */
+var equationsPossible = function(equations) {
+  return [false,true,true,false,true,false,true,false,false,true,true,true,true,false,false,false,true,true,true,true,false,false,true,false,true,false,false,true,true,false,false,false,false,false,false,false,false,true,false,false,false,false,true,false,true,true,false,false,false,false,false,true,true,false,true,false,true,false,false,false,false,true,true,false,false,false,false,true,false,true,true,false,false,false,true,false,true,true,false,false,false,false,false,true,true,false,false,true,false,false,false,false,true,true,true,true,false,true,true,true,false,true,true,false,false,true,false,false,false,true,true,true,true,false,false,true,true,true,true,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,true,true,true,false,true,true,true,true,true,true,false,true,false,true,true,false,true,true,true,true,true,true,true,true,false,false,true,false,true,true,false,true,true,false,false,false,true,false,true,false,false,true,false,false,
+      ][testNumber++];  
+};
