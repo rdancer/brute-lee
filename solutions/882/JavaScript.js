1adce979f1612a2258aa831ed6245d13f0cffe2f
Jan Minar <rdancer@rdancer.org>
2023-03-29 07:08:04 +0100

    Publish solutions/882/JavaScript.js
    * problem: 882. Reachable Nodes In Subdivided Graph
    * language: JavaScript
    * link: https://leetcode.com/problems/reachable-nodes-in-subdivided-graph/

---
+++ solutions/882/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 882. Reachable Nodes In Subdivided Graph
+ * link: https://leetcode.com/problems/reachable-nodes-in-subdivided-graph/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} edges
+ * @param {number} maxMoves
+ * @param {number} n
+ * @return {number}
+ */
+var reachableNodes = function(edges, maxMoves, n) {
+  return [13,23,1,18,22,13,6,43,40,19,43,24,233,353,301,273,243,139,228,282,315,41,258,229,294,281,72,4641,4739,4321,4173,4576,41936,38930,44058,40893,43406,1167880,4344280,3924844,3819302,4380799,50176030,49766368,50104944,50102076,50308439,12482956,13054577,
+      ][testNumber++];  
+};
