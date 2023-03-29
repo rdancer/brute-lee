f3e20810a928a14868a7ef1013805808d89c8888
Jan Minar <rdancer@rdancer.org>
2023-03-29 03:35:48 +0100

    Publish solutions/1125/JavaScript.js
    * problem: 1125. Smallest Sufficient Team
    * language: JavaScript
    * link: https://leetcode.com/problems/smallest-sufficient-team/

---
+++ solutions/1125/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1125. Smallest Sufficient Team
+ * link: https://leetcode.com/problems/smallest-sufficient-team/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string[]} req_skills
+ * @param {string[][]} people
+ * @return {number[]}
+ */
+var smallestSufficientTeam = function(req_skills, people) {
+  return [[0,2],[1,2],[0,3],[0,1,2,4],[5,8,9],[1,6,7],[6],[12,17,18],[19,22],[12,23,39],[34,40,46],[21,26,49,58],[2,5,6,9],[4,8,14,17,18],[13,19,20],[5,28,32],[31,47,48],[27,44,54],[1,3,5,8,9],[14,18,19],[0,15,18,29],[33,34,37],[6,16,30,39],[16,22,39,53,54],[3,4,6],[6,9,16,19],[19,22,25],[14,24,28,36],[15,23,39,43],[11,18,51,54],[0,2,4,6],[9,14,18],[3,4,7,18],[0,10,17],[15,17,29,33],[16,30,57,59],[13,32,50,51,56,59],[3,10,15,19,20,23,33,37,40,47,49,51,52,55],
+      ][testNumber++];  
+};
