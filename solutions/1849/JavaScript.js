3d7307f2a46b6d8103fe4b5312c93603ebc8cc8d
Jan Minar <rdancer@rdancer.org>
2023-03-29 17:23:28 +0100

    Publish solutions/1849/JavaScript.js
    * problem: 1849. Splitting a String Into Descending Consecutive Values
    * language: JavaScript
    * link: https://leetcode.com/problems/splitting-a-string-into-descending-consecutive-values/

---
+++ solutions/1849/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1849. Splitting a String Into Descending Consecutive Values
+ * link: https://leetcode.com/problems/splitting-a-string-into-descending-consecutive-values/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var splitString = function(s) {
+  return [false,true,false,true,false,true,true,true,true,true,false,true,false,false,true,true,true,true,false,true,true,true,true,true,true,true,true,true,false,true,false,false,true,false,true,false,true,true,true,true,true,false,false,false,true,true,true,false,true,true,true,true,false,false,false,false,false,false,false,true,true,false,true,true,true,true,false,true,false,false,true,false,true,false,false,true,true,false,true,true,true,false,true,true,false,true,true,true,true,true,false,true,true,true,false,true,true,true,true,true,true,false,true,true,false,true,true,true,true,true,true,true,true,false,true,true,true,true,true,true,false,false,false,true,true,false,true,true,true,true,false,true,false,true,true,true,true,false,false,true,true,false,true,false,true,true,true,true,true,true,false,true,false,false,false,false,false,false,true,true,true,true,false,true,true,true,true,true,true,true,true,true,false,true,true,false,false,true,true,true,true,true,true,false,false,false,false,false,false,false,true,false,false,false,true,true,
+      ][testNumber++];  
+};
