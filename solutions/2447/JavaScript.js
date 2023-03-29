663c8e9d4f0dae7c84dd6d8a7072f465cf768711
Jan Minar <rdancer@rdancer.org>
2023-03-29 07:24:31 +0100

    Publish solutions/2447/JavaScript.js
    * problem: 2447. Number of Subarrays With GCD Equal to K
    * language: JavaScript
    * link: https://leetcode.com/problems/number-of-subarrays-with-gcd-equal-to-k/

---
+++ solutions/2447/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2447. Number of Subarrays With GCD Equal to K
+ * link: https://leetcode.com/problems/number-of-subarrays-with-gcd-equal-to-k/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarrayGCD = function(nums, k) {
+  return [4,0,1,0,0,1,7,1,1,1,1,0,0,0,0,1,1,1,1,1,1,0,10,1,2,0,0,0,10,0,4,0,1,0,0,0,1,0,0,16,0,1,1,0,0,0,0,0,1,0,20,1,2,0,0,8,0,0,0,0,0,0,1,0,0,0,1,0,0,0,500500,0,0,0,0,0,0,0,0,0,0,0,500500,0,1,1,2,
+      ][testNumber++];  
+};
