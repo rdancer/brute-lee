3b7d85e612fbe0600bfdf3c69c9be0fcfd2a4557
Jan Minar <rdancer@rdancer.org>
2023-03-29 06:24:10 +0100

    Publish solutions/1240/JavaScript.js
    * problem: 1240. Tiling a Rectangle with the Fewest Squares
    * language: JavaScript
    * link: https://leetcode.com/problems/tiling-a-rectangle-with-the-fewest-squares/

---
+++ solutions/1240/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1240. Tiling a Rectangle with the Fewest Squares
+ * link: https://leetcode.com/problems/tiling-a-rectangle-with-the-fewest-squares/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @param {number} m
+ * @return {number}
+ */
+var tilingRectangle = function(n, m) {
+  return [3,5,6,6,2,4,3,3,1,1,5,5,5,1,8,4,2,6,6,6,2,4,4,6,2,6,6,6,6,7,7,4,3,2,1,7,13,6,
+      ][testNumber++];  
+};
