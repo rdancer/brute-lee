2af53cdb755124f4daf3c8f2f088cce9345fb6df
Jan Minar <rdancer@rdancer.org>
2023-03-29 03:47:07 +0100

    Publish solutions/96/JavaScript.js
    * problem: 96. Unique Binary Search Trees
    * language: JavaScript
    * link: https://leetcode.com/problems/unique-binary-search-trees/

---
+++ solutions/96/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 96. Unique Binary Search Trees
+ * link: https://leetcode.com/problems/unique-binary-search-trees/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numTrees = function(n) {
+  return [5,1,2,14,42,132,429,1430,4862,16796,58786,208012,1767263190,477638700,129644790,35357670,9694845,742900,2674440,
+      ][testNumber++];  
+};
