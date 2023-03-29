eee00d7e6ebc157ebf7d1955ca01cc1042aa92cb
Jan Minar <rdancer@rdancer.org>
2023-03-29 06:16:00 +0100

    Publish solutions/334/JavaScript.js
    * problem: 334. Increasing Triplet Subsequence
    * language: JavaScript
    * link: https://leetcode.com/problems/increasing-triplet-subsequence/

---
+++ solutions/334/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 334. Increasing Triplet Subsequence
+ * link: https://leetcode.com/problems/increasing-triplet-subsequence/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var increasingTriplet = function(nums) {
+  return [true,false,true,false,false,false,true,false,false,true,true,true,true,true,true,false,true,false,true,true,true,true,true,true,true,false,true,true,false,false,false,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,true,true,false,false,true,false,true,
+      ][testNumber++];  
+};
