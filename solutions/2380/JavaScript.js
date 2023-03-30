db8a4cc62d51704b5f55ac39b3701f762febe623
Jan Minar <rdancer@rdancer.org>
2023-03-30 17:56:21 +0100

    Publish solutions/2380/JavaScript.js
    * problem: 2380. Time Needed to Rearrange a Binary String
    * language: JavaScript
    * link: https://leetcode.com/problems/time-needed-to-rearrange-a-binary-string/

---
+++ solutions/2380/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2380. Time Needed to Rearrange a Binary String
+ * link: https://leetcode.com/problems/time-needed-to-rearrange-a-binary-string/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var secondsToRemoveOccurrences = function(s) {
+  return [4,0,4,5,2,5,2,1,0,1,1,1,7,20,38,9,30,38,7,35,39,11,57,26,23,49,30,12,33,13,36,9,10,192,159,95,109,71,166,256,159,222,83,133,65,205,172,173,143,248,111,221,256,310,449,370,409,306,518,491,359,333,323,311,296,303,291,524,264,427,351,367,436,416,358,464,334,300,385,328,261,480,305,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,0,0,999,0,0,
+      ][testNumber++];  
+};
