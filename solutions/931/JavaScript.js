8d2aa1b21034e46602fcd79d3340b62c6457d1e5
Jan Minar <rdancer@rdancer.org>
2023-03-30 16:20:00 +0100

    Publish solutions/931/JavaScript.js
    * problem: 931. Minimum Falling Path Sum
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-falling-path-sum/

---
+++ solutions/931/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 931. Minimum Falling Path Sum
+ * link: https://leetcode.com/problems/minimum-falling-path-sum/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} matrix
+ * @return {number}
+ */
+var minFallingPathSum = function(matrix) {
+  return [13,-59,-48,69,52,-16,2,-27,-36,114,-26,24,40,-66,-218,-98,-153,-45,-110,-50,-343,-290,-275,-298,-226,-149,-380,-214,-298,-354,-414,-281,-350,-701,-609,-600,-631,-690,-1428,-1187,-1313,-1340,-1243,-6335,-6646,-6678,-6861,-6518,10000,
+      ][testNumber++];  
+};
