57a576be283b212ee200ce3c9d1b4532f9325a9c
Jan Minar <rdancer@rdancer.org>
2023-03-30 04:32:44 +0100

    Publish solutions/1283/JavaScript.js
    * problem: 1283. Find the Smallest Divisor Given a Threshold
    * language: JavaScript
    * link: https://leetcode.com/problems/find-the-smallest-divisor-given-a-threshold/

---
+++ solutions/1283/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1283. Find the Smallest Divisor Given a Threshold
+ * link: https://leetcode.com/problems/find-the-smallest-divisor-given-a-threshold/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @param {number} threshold
+ * @return {number}
+ */
+var smallestDivisor = function(nums, threshold) {
+  return [5,44,1,3,4,1,1,3,2,2,2,2,1,2,2,500000,2,2,1,1,1,2,495280,490759,2,1,34,52,55,98,48,132,10134,3256,5317,10187,5296,4559,11134,2972,3336,6110,953,1593,4219,1811,1734,2120,3636,1539,1177,6109,126627,110238,309071,124781,327734,137702,115349,143866,136908,153843,33979,37894,46968,2865,445588,5,5,999999,50,
+      ][testNumber++];  
+};
