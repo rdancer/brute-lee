48578fe41489fde55c034db9851273765c0fedc0
Jan Minar <rdancer@rdancer.org>
2023-03-30 02:09:51 +0100

    Publish solutions/1252/JavaScript.js
    * problem: 1252. Cells with Odd Values in a Matrix
    * language: JavaScript
    * link: https://leetcode.com/problems/cells-with-odd-values-in-a-matrix/

---
+++ solutions/1252/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 1252. Cells with Odd Values in a Matrix
+ * link: https://leetcode.com/problems/cells-with-odd-values-in-a-matrix/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} m
+ * @param {number} n
+ * @param {number[][]} indices
+ * @return {number}
+ */
+var oddCells = function(m, n, indices) {
+  return [6,0,0,83,460,207,659,48,370,230,464,760,40,100,126,54,232,67,44,395,469,94,46,32,166,291,310,101,708,286,470,460,140,406,28,91,525,1148,340,512,315,470,316,260,
+      ][testNumber++];  
+};
