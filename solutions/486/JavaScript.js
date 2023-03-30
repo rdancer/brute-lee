c8503da327242459a4985786712394dd948f970c
Jan Minar <rdancer@rdancer.org>
2023-03-30 05:01:00 +0100

    Publish solutions/486/JavaScript.js
    * problem: 486. Predict the Winner
    * language: JavaScript
    * link: https://leetcode.com/problems/predict-the-winner/

---
+++ solutions/486/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 486. Predict the Winner
+ * link: https://leetcode.com/problems/predict-the-winner/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var PredictTheWinner = function(nums) {
+  return [false,true,true,true,true,true,true,false,false,true,true,true,true,true,false,false,false,true,true,true,false,false,false,true,true,true,false,true,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,false,false,false,true,true,false,false,false,true,false,
+      ][testNumber++];  
+};
