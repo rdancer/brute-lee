fe5de7447792575a9be2624b55bc711210d30277
Jan Minar <rdancer@rdancer.org>
2023-03-30 05:21:31 +0100

    Publish solutions/983/JavaScript.js
    * problem: 983. Minimum Cost For Tickets
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-cost-for-tickets/

---
+++ solutions/983/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 983. Minimum Cost For Tickets
+ * link: https://leetcode.com/problems/minimum-cost-for-tickets/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} days
+ * @param {number[]} costs
+ * @return {number}
+ */
+var mincostTickets = function(days, costs) {
+  return [11,17,3,8,16,24,44,8,19,24,11,6,17,39,21,14,11,18,15,14,10,33,50,36,45,23,423,200,294,139,180,288,115,63,237,78,327,170,246,239,77,246,24,245,182,220,3040,7938,850,1780,5056,7713,742,5505,3747,1909,6295,264,1525,756,8910,2554,1665,4776,3299,1,39,3453,24,
+      ][testNumber++];  
+};
