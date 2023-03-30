812fa93cb0d55425efecadc8288b90fc32c2a866
Jan Minar <rdancer@rdancer.org>
2023-03-30 17:30:41 +0100

    Publish solutions/41/JavaScript.js
    * problem: 41. First Missing Positive
    * language: JavaScript
    * link: https://leetcode.com/problems/first-missing-positive/

---
+++ solutions/41/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 41. First Missing Positive
+ * link: https://leetcode.com/problems/first-missing-positive/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function(nums) {
+  return [3,2,1,1,2,1,1,1,1,1,1,2,2,3,1,1,1,1,1,2,7,3,10,13,4,2,2,5,1,2,3,3,2,2,2,5,5,3,4,4,5,5,1,1,5,2,1,2,2,1,1,5,4,5,1,1,1,2,2,4,2,4,8,3,4,1,1,4,1,2,1,2,1,2,1,1,1,6,1,1,1,2,1,1,1,3,1,1,2,2,1,3,2,1,2,1,1,5,1,2,1,2,1,7,1,1,1,1,3,1,7,1,1,2,6,6,6,6,3,8,2,6,7,3,4,2,7,7,3,6,6,8,2,1,6,5,6,7,6,7,2,3,6,1,1,1,6,2,6,1,6,6,4,3,1,1,1,3,2,1,1,1,4,100,101,99,128,259,301,51,1,100001,99998,3991,
+      ][testNumber++];  
+};
