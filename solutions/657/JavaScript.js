ca5ac0036e0245df7f982459d69873390cb00680
Jan Minar <rdancer@rdancer.org>
2023-03-30 16:43:47 +0100

    Publish solutions/657/JavaScript.js
    * problem: 657. Robot Return to Origin
    * language: JavaScript
    * link: https://leetcode.com/problems/robot-return-to-origin/

---
+++ solutions/657/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 657. Robot Return to Origin
+ * link: https://leetcode.com/problems/robot-return-to-origin/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} moves
+ * @return {boolean}
+ */
+var judgeCircle = function(moves) {
+  return [true,false,false,false,false,false,false,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,false,true,
+      ][testNumber++];  
+};
