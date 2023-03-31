7f4c210b52944131f0a38b6c72ed56c42a36a228
Jan Minar <rdancer@rdancer.org>
2023-03-31 18:01:27 +0100

    Publish solutions/886/JavaScript.js
    * problem: 886. Possible Bipartition
    * language: JavaScript
    * link: https://leetcode.com/problems/possible-bipartition/

---
+++ solutions/886/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 886. Possible Bipartition
+ * link: https://leetcode.com/problems/possible-bipartition/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @param {number[][]} dislikes
+ * @return {boolean}
+ */
+var possibleBipartition = function(n, dislikes) {
+  return [true,false,false,true,true,true,false,false,false,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,false,false,false,true,
+      ][testNumber++];  
+};
