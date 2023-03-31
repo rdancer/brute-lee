1a46385cada3a53926e20eed686ecfeaec00b8f3
Jan Minar <rdancer@rdancer.org>
2023-03-31 03:11:01 +0100

    Publish solutions/1863/JavaScript.js
    * problem: 1863. Sum of All Subset XOR Totals
    * language: JavaScript
    * link: https://leetcode.com/problems/sum-of-all-subset-xor-totals/

---
+++ solutions/1863/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1863. Sum of All Subset XOR Totals
+ * link: https://leetcode.com/problems/sum-of-all-subset-xor-totals/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var subsetXORSum = function(nums) {
+  return [6,28,480,4,12,28,20,24,112,240,240,240,464,384,896,896,1920,1920,1920,1920,1920,1920,1920,640,30720,2048,6144,6144,14336,14336,14336,14336,30720,30720,30720,30720,30720,30720,30720,30720,63488,63488,63488,63488,63488,7,4096,15360,
+      ][testNumber++];  
+};
