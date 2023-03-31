94fc14228480ee2cdb0b9cfdc5c13f49caefda89
Jan Minar <rdancer@rdancer.org>
2023-03-31 18:55:58 +0100

    Publish solutions/915/JavaScript.js
    * problem: 915. Partition Array into Disjoint Intervals
    * language: JavaScript
    * link: https://leetcode.com/problems/partition-array-into-disjoint-intervals/

---
+++ solutions/915/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 915. Partition Array into Disjoint Intervals
+ * link: https://leetcode.com/problems/partition-array-into-disjoint-intervals/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var partitionDisjoint = function(nums) {
+  return [3,4,1,1,1,9,7,5,5,9,9,4,1,9,7,7,5,2,9,4,11,13,15,1,1,9,5,11,12,13,37,1,28,21,1,34,36,87,7,94,13,79,55,80,713,2,214,599,855,279,186,215,18013,18514,13967,13215,13842,32112,43381,64468,59585,33421,49759,12205,56820,14692,
+      ][testNumber++];  
+};
