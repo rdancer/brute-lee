051ad04f79f77e471a051b3fab125716aef86eef
Jan Minar <rdancer@rdancer.org>
2023-04-01 10:17:20 +0100

    Publish solutions/1477/JavaScript.js
    * problem: 1477. Find Two Non-overlapping Sub-arrays Each With Target Sum
    * language: JavaScript
    * link: https://leetcode.com/problems/find-two-non-overlapping-sub-arrays-each-with-target-sum/

---
+++ solutions/1477/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1477. Find Two Non-overlapping Sub-arrays Each With Target Sum
+ * link: https://leetcode.com/problems/find-two-non-overlapping-sub-arrays-each-with-target-sum/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} arr
+ * @param {number} target
+ * @return {number}
+ */
+var minSumOfLengths = function(arr, target) {
+  return [2,2,-1,-1,3,-1,4,-1,6,23,-1,6,5,-1,8,-1,6,5,-1,4,5,6,-1,16,-1,7,9,5,4,8,-1,7,5,4,8,3,7,3,-1,3,5,-1,5,3,-1,4,-1,4,-1,3,2,3,20,28,4,4,3032,100000,10,-1,-1,
+      ][testNumber++];  
+};
