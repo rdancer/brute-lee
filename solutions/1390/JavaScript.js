d72f9f1a4423474cc7b77b5c5617003b2888e82d
Jan Minar <rdancer@rdancer.org>
2023-04-01 03:00:00 +0100

    Publish solutions/1390/JavaScript.js
    * problem: 1390. Four Divisors
    * language: JavaScript
    * link: https://leetcode.com/problems/four-divisors/

---
+++ solutions/1390/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1390. Four Divisors
+ * link: https://leetcode.com/problems/four-divisors/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var sumFourDivisors = function(nums) {
+  return [32,64,0,0,45,10932,84216,147258,289182,69924,6777290,14985118,69399654,135341358,134716980,0,249058074,255159584,
+      ][testNumber++];  
+};
