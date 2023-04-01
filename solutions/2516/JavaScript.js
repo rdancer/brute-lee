31f77fc54e8282a0078d604357ef0988b0263fff
Jan Minar <rdancer@rdancer.org>
2023-04-01 21:49:30 +0100

    Publish solutions/2516/JavaScript.js
    * problem: 2516. Take K of Each Character From Left and Right
    * language: JavaScript
    * link: https://leetcode.com/problems/take-k-of-each-character-from-left-and-right/

---
+++ solutions/2516/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2516. Take K of Each Character From Left and Right
+ * link: https://leetcode.com/problems/take-k-of-each-character-from-left-and-right/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var takeCharacters = function(s, k) {
+  return [8,-1,0,0,0,-1,3,-1,-1,-1,-1,-1,-1,0,-1,0,0,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,0,-1,-1,0,3,3,0,0,-1,0,3,-1,0,0,0,0,0,0,0,0,-1,-1,-1,0,3,-1,0,-1,0,0,0,0,-1,4,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,3,-1,-1,-1,-1,-1,0,-1,3,-1,3,0,4,-1,7,3,-1,-1,0,0,0,0,4,3,0,-1,0,9,-1,4,-1,6,-1,3,0,4,-1,0,0,98317,-1,-1,70816,67300,32458,99999,684,-1,-1,-1,-1,1064,695,5679,828,6291,0,-1,-1,3,4,4,
+      ][testNumber++];  
+};
