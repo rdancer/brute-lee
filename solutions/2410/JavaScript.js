75fcc3377e35ea23b69b39bdfb6ce231180d101b
Jan Minar <rdancer@rdancer.org>
2023-04-01 22:19:57 +0100

    Publish solutions/2410/JavaScript.js
    * problem: 2410. Maximum Matching of Players With Trainers
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-matching-of-players-with-trainers/

---
+++ solutions/2410/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2410. Maximum Matching of Players With Trainers
+ * link: https://leetcode.com/problems/maximum-matching-of-players-with-trainers/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} players
+ * @param {number[]} trainers
+ * @return {number}
+ */
+var matchPlayersAndTrainers = function(players, trainers) {
+  return [2,1,0,1,2,1,2,5,2,2,2,2,3,5,5,5,3,10,10,10,3,25,25,25,3,25,100,1000,3,25,100,5000,20000,99667,100000,
+      ][testNumber++];  
+};
