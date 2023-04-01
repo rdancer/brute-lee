006671f33e4f8a338c2b35f1c522dcd84c716c45
Jan Minar <rdancer@rdancer.org>
2023-04-01 23:26:55 +0100

    Publish solutions/2354/JavaScript.js
    * problem: 2354. Number of Excellent Pairs
    * language: JavaScript
    * link: https://leetcode.com/problems/number-of-excellent-pairs/

---
+++ solutions/2354/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2354. Number of Excellent Pairs
+ * link: https://leetcode.com/problems/number-of-excellent-pairs/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var countExcellentPairs = function(nums, k) {
+  return [5,0,12,3,21,0,0,81,7568,112,0,2649,1009,11,9,0,0,1089,1021,7744,95,2500,19,841,8,5476,1600,5023,1,0,0,0,7250,664225,6199,0,240,58159,22201,34477,0,289830,273974,39636,369676,591361,580644,24910081,50041476,325,2236,5554216680,1045422889,19600175,9999199968,10000000000,
+      ][testNumber++];  
+};
