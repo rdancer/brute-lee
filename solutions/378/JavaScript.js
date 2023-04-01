2e6e61a276256954a84c87a32edca9e8f84c716b
Jan Minar <rdancer@rdancer.org>
2023-04-01 23:09:27 +0100

    Publish solutions/378/JavaScript.js
    * problem: 378. Kth Smallest Element in a Sorted Matrix
    * language: JavaScript
    * link: https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/

---
+++ solutions/378/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 378. Kth Smallest Element in a Sorted Matrix
+ * link: https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} matrix
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(matrix, k) {
+  return [13,-5,1000000000,1,1,2,3,1,2,3,3,1,2,4,5,1,3,5,6,7,11,12,14,14,5,21,1,25,15,5,19,8,13,24,11,8,4,19,8,9,18,6,7,7,19,4,6,14,11,5,23,3,20,29,27,31,37,25,56,22,17,37,50,33,11,43,45,22,23,23,614,1130,864,412,804,451,751,504,605,717,1253,56609927,46658752,80514,-5,108695,
+      ][testNumber++];  
+};
