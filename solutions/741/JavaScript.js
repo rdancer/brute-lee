662f46fe784acc6aa622ca6023ea389fbf7ecebf
Jan Minar <rdancer@rdancer.org>
2023-04-01 10:35:38 +0100

    Publish solutions/741/JavaScript.js
    * problem: 741. Cherry Pickup
    * language: JavaScript
    * link: https://leetcode.com/problems/cherry-pickup/

---
+++ solutions/741/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 741. Cherry Pickup
+ * link: https://leetcode.com/problems/cherry-pickup/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var cherryPickup = function(grid) {
+  return [5,0,0,1,15,10,11,0,16,15,15,13,14,13,0,0,22,30,35,35,35,23,0,0,0,35,52,71,71,72,0,69,73,68,0,0,83,111,112,112,115,112,109,112,115,113,0,181,193,196,193,195,0,194,191,0,1,1,192,
+      ][testNumber++];  
+};
