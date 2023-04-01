d3f5b20b3bff6f2e9fe8204118197d5a03c710f5
Jan Minar <rdancer@rdancer.org>
2023-04-01 09:40:12 +0100

    Publish solutions/2430/JavaScript.js
    * problem: 2430. Maximum Deletions on a String
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-deletions-on-a-string/

---
+++ solutions/2430/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2430. Maximum Deletions on a String
+ * link: https://leetcode.com/problems/maximum-deletions-on-a-string/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var deleteString = function(s) {
+  return [2,4,5,3,1,3,3,2,1,1,1,1,1,4,2,2,4,2,1,2,1,4,1,2,1,1,1,1,2,4,2,1,1,4,2,1,2,1,1,1,2,1,1,2,1,2,1,4,2,2,2,1,2,2,1,2,3,2,4,1,3,2,2,3,2,2,2,2,4,4,1,4,2,2,2,1,2,2,3,2,2,2,3,4,2,4,4,4,4,2,8,4,4,4,2,4,2,4,4,48,128,65,23,23,35,128,8,10,7,10,6,35,4000,3904,2300,1001,2000,4,1,1,1,2,3,1,4,1,4,2,4,
+      ][testNumber++];  
+};
