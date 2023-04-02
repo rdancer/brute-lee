f0450e4bb47adadae877b068e021f9afbdf69a1a
Jan Minar <rdancer@rdancer.org>
2023-04-02 03:50:04 +0100

    Publish solutions/2272/JavaScript.js
    * problem: 2272. Substring With Largest Variance
    * language: JavaScript
    * link: https://leetcode.com/problems/substring-with-largest-variance/

---
+++ solutions/2272/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2272. Substring With Largest Variance
+ * link: https://leetcode.com/problems/substring-with-largest-variance/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var largestVariance = function(s) {
+  return [3,0,3,1,5,2,4,4,2,2,3,3,3,4,3,2,4,3,0,4,3,5,3,2,1,1,3,3,3,3,3,3,4,3,2,2,5,3,3,3,1,0,2,2,4,3,4,2,1,2,3,2,0,1,0,0,0,0,0,0,3,2,1,1,1,6,0,0,3,1,1,2,3,5,2,4,1,3,3,2,18,5,2,1,6,2,2,12,13,22,15,20,14,18,16,18,14,15,19,14,17,17,10,14,16,14,10,16,11,8,13,13,10,17,14,12,14,45,36,33,33,48,86,77,81,77,20,9998,1,0,8,9974,2,2,1,2,2,2,
+      ][testNumber++];  
+};
