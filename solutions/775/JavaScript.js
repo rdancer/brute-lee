743045b600211b340fcf12583b0eab9053d37d9b
Jan Minar <rdancer@rdancer.org>
2023-04-03 00:30:47 +0100

    Publish solutions/775/JavaScript.js
    * problem: 775. Global and Local Inversions
    * language: JavaScript
    * link: https://leetcode.com/problems/global-and-local-inversions/

---
+++ solutions/775/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 775. Global and Local Inversions
+ * link: https://leetcode.com/problems/global-and-local-inversions/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isIdealPermutation = function(nums) {
+  return [true,false,true,true,true,true,true,false,false,true,true,true,false,false,false,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,true,true,false,false,false,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,true,false,true,false,true,false,true,false,true,true,false,false,false,false,true,
+      ][testNumber++];  
+};
