3c404d9d0af30a2afa8e343c24d577f3d15b4648
Jan Minar <rdancer@rdancer.org>
2023-04-02 04:16:51 +0100

    Publish solutions/2246/JavaScript.js
    * problem: 2246. Longest Path With Different Adjacent Characters
    * language: JavaScript
    * link: https://leetcode.com/problems/longest-path-with-different-adjacent-characters/

---
+++ solutions/2246/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2246. Longest Path With Different Adjacent Characters
+ * link: https://leetcode.com/problems/longest-path-with-different-adjacent-characters/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} parent
+ * @param {string} s
+ * @return {number}
+ */
+var longestPath = function(parent, s) {
+  return [3,3,1,1,2,3,2,1,2,5,4,1,1,100000,17,17,16,18,4,18,15,10,17,11,20,16,17,12,19,19,17,17,20,18,18,18,17,16,17,21,18,15,16,17,15,12,11,14,12,15,20,15,16,16,15,18,17,20,15,14,21,11,13,13,25,28,23,23,23,22,22,24,20,23,24,21,28,23,20,23,21,22,23,19,26,24,26,20,29,23,24,24,23,19,22,23,22,24,25,24,25,22,25,21,24,26,20,22,22,22,22,22,24,26,30,32,29,37,28,30,28,33,32,26,28,33,24,29,31,32,32,33,31,23,46,35,38,39,44,47,45,3,
+      ][testNumber++];  
+};
