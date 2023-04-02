9bb4d0742f27ce8fd522f29c91e05bd74854b294
Jan Minar <rdancer@rdancer.org>
2023-04-03 00:39:10 +0100

    Publish solutions/363/JavaScript.js
    * problem: 363. Max Sum of Rectangle No Larger Than K
    * language: JavaScript
    * link: https://leetcode.com/problems/max-sum-of-rectangle-no-larger-than-k/

---
+++ solutions/363/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 363. Max Sum of Rectangle No Larger Than K
+ * link: https://leetcode.com/problems/max-sum-of-rectangle-no-larger-than-k/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} matrix
+ * @param {number} k
+ * @return {number}
+ */
+var maxSumSubmatrix = function(matrix, k) {
+  return [2,3,2,-1,2,10,9,8,7,6,5,4,2,2,1,0,-1,-2,10,12,-101,-128,-323,194,287,44385,-1,2723,1844,4876,4331,3356,-1303,4888,-6824,4030,
+      ][testNumber++];  
+};
