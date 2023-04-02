4ea9be0589fa1dfc894728680a1ff11478f6f76a
Jan Minar <rdancer@rdancer.org>
2023-04-02 12:58:15 +0100

    Publish solutions/1791/JavaScript.js
    * problem: 1791. Find Center of Star Graph
    * language: JavaScript
    * link: https://leetcode.com/problems/find-center-of-star-graph/

---
+++ solutions/1791/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1791. Find Center of Star Graph
+ * link: https://leetcode.com/problems/find-center-of-star-graph/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} edges
+ * @return {number}
+ */
+var findCenter = function(edges) {
+  return [2,1,10,14,16,10,9,4,18,6,5,7,519,577,50,454,224,57,693,223,433,269,67,336,594,223,136,516,183,303,439,546,32,689,296,87,306,201,270,367,398,79,491,411,784,654,488,209,350,679,323,19,8957,866,5514,14055,8812,2,50000,3,
+      ][testNumber++];  
+};
