14bed7beeeb7e23c254c9bfebdc60b88b8ad3b27
Jan Minar <rdancer@rdancer.org>
2023-04-02 12:45:34 +0100

    Publish solutions/693/JavaScript.js
    * problem: 693. Binary Number with Alternating Bits
    * language: JavaScript
    * link: https://leetcode.com/problems/binary-number-with-alternating-bits/

---
+++ solutions/693/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 693. Binary Number with Alternating Bits
+ * link: https://leetcode.com/problems/binary-number-with-alternating-bits/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var hasAlternatingBits = function(n) {
+  return [true,false,false,true,false,true,false,true,false,false,false,false,true,false,false,false,false,false,false,false,false,false,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,false,false,true,false,false,true,false,false,true,false,false,true,false,false,true,false,false,true,false,false,true,false,false,true,false,false,true,false,false,true,false,false,true,false,false,true,false,false,true,false,false,true,false,false,true,false,false,true,false,false,true,false,false,true,false,false,true,false,false,true,false,false,true,false,false,true,false,false,true,false,false,false,
+      ][testNumber++];  
+};
