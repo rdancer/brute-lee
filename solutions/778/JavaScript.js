21b8f4eafa44c67cc9cdb47ab252f7e9a4e43f5f
Jan Minar <rdancer@rdancer.org>
2023-04-04 00:27:18 +0100

    Publish solutions/778/JavaScript.js
    * problem: 778. Swim in Rising Water
    * language: JavaScript
    * link: https://leetcode.com/problems/swim-in-rising-water/

---
+++ solutions/778/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 778. Swim in Rising Water
+ * link: https://leetcode.com/problems/swim-in-rising-water/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var swimInWater = function(grid) {
+  return [3,16,3,3,2,3,14,15,14,14,11,17,21,15,19,22,35,29,30,26,24,81,78,61,67,78,266,293,261,398,375,2131,2392,2283,1566,1541,2452,1728,2097,2449,64,0,2041,
+      ][testNumber++];  
+};
