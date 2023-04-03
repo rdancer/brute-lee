4c1360b7a1bec694b4187418171e86fd0d6b3285
Jan Minar <rdancer@rdancer.org>
2023-04-03 15:08:17 +0100

    Publish solutions/954/JavaScript.js
    * problem: 954. Array of Doubled Pairs
    * language: JavaScript
    * link: https://leetcode.com/problems/array-of-doubled-pairs/

---
+++ solutions/954/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 954. Array of Doubled Pairs
+ * link: https://leetcode.com/problems/array-of-doubled-pairs/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var canReorderDoubled = function(arr) {
+  return [false,false,true,false,true,true,true,true,false,false,false,false,false,false,true,true,true,true,false,false,false,true,false,false,false,true,false,false,false,true,false,false,false,false,true,true,false,false,false,false,false,true,false,true,false,false,false,true,true,false,false,false,false,false,true,true,false,false,false,false,false,true,false,true,false,false,false,true,false,false,false,true,false,false,false,false,false,false,false,false,false,false,false,false,false,true,false,false,true,false,false,true,false,false,false,true,false,false,false,true,false,false,
+      ][testNumber++];  
+};
