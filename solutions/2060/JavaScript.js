f6486b9152f4405a2c52dba957c7522a11c7d63b
Jan Minar <rdancer@rdancer.org>
2023-04-03 03:07:46 +0100

    Publish solutions/2060/JavaScript.js
    * problem: 2060. Check if an Original String Exists Given Two Encoded Strings
    * language: JavaScript
    * link: https://leetcode.com/problems/check-if-an-original-string-exists-given-two-encoded-strings/

---
+++ solutions/2060/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2060. Check if an Original String Exists Given Two Encoded Strings
+ * link: https://leetcode.com/problems/check-if-an-original-string-exists-given-two-encoded-strings/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var possiblyEquals = function(s1, s2) {
+  return [true,true,false,true,false,false,true,true,true,false,false,false,false,false,false,true,false,false,false,false,false,false,false,false,false,false,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,false,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,true,true,false,false,false,true,false,false,false,false,true,false,false,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,true,false,false,false,false,false,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,true,false,true,false,false,false,false,false,false,true,true,false,true,true,true,false,false,false,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,false,true,true,true,false,true,true,true,false,true,false,false,
+      ][testNumber++];  
+};
