990277a08f150d37b41f51d9333ff67daf561056
Jan Minar <rdancer@rdancer.org>
2023-04-03 15:19:45 +0100

    Publish solutions/818/JavaScript.js
    * problem: 818. Race Car
    * language: JavaScript
    * link: https://leetcode.com/problems/race-car/

---
+++ solutions/818/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 818. Race Car
+ * link: https://leetcode.com/problems/race-car/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} target
+ * @return {number}
+ */
+var racecar = function(target) {
+  return [2,5,1,4,5,7,3,6,8,7,10,7,9,6,4,7,9,8,11,12,10,9,12,9,11,13,11,8,10,7,13,15,24,14,11,19,15,14,25,26,17,23,22,25,24,28,32,27,41,50,39,34,24,22,39,
+      ][testNumber++];  
+};
