de6c94423bde121a2e8a10ce861b42c0857a5475
Jan Minar <rdancer@rdancer.org>
2023-04-03 02:21:42 +0100

    Publish solutions/306/JavaScript.js
    * problem: 306. Additive Number
    * language: JavaScript
    * link: https://leetcode.com/problems/additive-number/

---
+++ solutions/306/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 306. Additive Number
+ * link: https://leetcode.com/problems/additive-number/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} num
+ * @return {boolean}
+ */
+var isAdditiveNumber = function(num) {
+  return [true,true,false,false,false,false,true,true,true,false,true,true,false,false,true,true,true,false,true,true,false,false,false,false,true,false,true,true,true,true,true,false,true,true,true,false,false,true,true,true,true,false,true,false,
+      ][testNumber++];  
+};
