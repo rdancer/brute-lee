a4e2eeabb45309049d6b4203075b7ae2bdfae0e5
Jan Minar <rdancer@rdancer.org>
2023-04-04 03:30:05 +0100

    Publish solutions/1864/JavaScript.js
    * problem: 1864. Minimum Number of Swaps to Make the Binary String Alternating
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-number-of-swaps-to-make-the-binary-string-alternating/

---
+++ solutions/1864/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1864. Minimum Number of Swaps to Make the Binary String Alternating
+ * link: https://leetcode.com/problems/minimum-number-of-swaps-to-make-the-binary-string-alternating/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minSwaps = function(s) {
+  return [1,0,-1,0,0,0,0,1,0,1,-1,0,-1,1,-1,1,-1,2,-1,2,-1,1,-1,2,3,1,3,1,2,-1,-1,3,3,4,2,2,3,2,1,2,3,3,2,-1,-1,60,-1,65,-1,21,-1,112,-1,92,-1,55,-1,29,-1,76,-1,71,-1,103,-1,45,-1,80,-1,122,-1,120,-1,8,-1,83,-1,45,-1,45,-1,71,-1,19,-1,125,125,125,125,125,125,125,125,-1,-1,-1,196,-1,204,-1,149,-1,229,-1,225,-1,198,-1,189,-1,199,-1,227,-1,203,-1,0,250,250,250,250,-1,3,-1,
+      ][testNumber++];  
+};
