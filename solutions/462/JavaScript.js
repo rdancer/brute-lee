e2f2e154376518a5f126d219f58e478f8064a95b
Jan Minar <rdancer@rdancer.org>
2023-04-04 23:25:40 +0100

    Publish solutions/462/JavaScript.js
    * problem: 462. Minimum Moves to Equal Array Elements II
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-moves-to-equal-array-elements-ii/

---
+++ solutions/462/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 462. Minimum Moves to Equal Array Elements II
+ * link: https://leetcode.com/problems/minimum-moves-to-equal-array-elements-ii/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minMoves2 = function(nums) {
+  return [2,16,0,1,1,2,2,2,2,2,0,1,1,1,14,12,18,12987535,2127271182,2127271182,2127271182,2138083402,2138083402,2138083402,2136401042,2136401042,2136401042,2136308703,2136308703,2136308703,0,0,
+      ][testNumber++];  
+};
