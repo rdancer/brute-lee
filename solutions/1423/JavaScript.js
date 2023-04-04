71a945b299e73103977c295a9d581bffe5b075fa
Jan Minar <rdancer@rdancer.org>
2023-04-04 01:43:37 +0100

    Publish solutions/1423/JavaScript.js
    * problem: 1423. Maximum Points You Can Obtain from Cards
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/

---
+++ solutions/1423/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1423. Maximum Points You Can Obtain from Cards
+ * link: https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} cardPoints
+ * @param {number} k
+ * @return {number}
+ */
+var maxScore = function(cardPoints, k) {
+  return [12,4,55,1,202,248,232,536,566,90,1129,803,103,1143,580,1269,1477,868,1027,740,877,1025,2429,776,1602,2656,750782,989332,1461736,500612,1372863,3872404,1720913,4416396,2537610,5114170,47430865,284963529,464177326,148216349,
+      ][testNumber++];  
+};
