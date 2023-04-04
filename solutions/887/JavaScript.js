a9b9f88c0f7abb5060a76cc48d3cd3e523aa24df
Jan Minar <rdancer@rdancer.org>
2023-04-04 22:59:43 +0100

    Publish solutions/887/JavaScript.js
    * problem: 887. Super Egg Drop
    * language: JavaScript
    * link: https://leetcode.com/problems/super-egg-drop/

---
+++ solutions/887/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 887. Super Egg Drop
+ * link: https://leetcode.com/problems/super-egg-drop/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} k
+ * @param {number} n
+ * @return {number}
+ */
+var superEggDrop = function(k, n) {
+  return [2,3,4,1,2,3,4,5,6,7,8,9,1,2,2,3,3,3,4,4,4,1,2,2,3,3,3,3,4,4,5,5,6,6,7,4,5,5,6,7,4,5,6,8,11,4,5,6,7,9,4,5,6,7,8,4,5,6,7,8,11,13,16,19,23,10,11,13,16,18,10,11,12,14,16,9,11,12,13,15,9,10,12,13,14,9,10,11,13,14,9,10,11,13,14,14,13,14,13,14,13,14,13,14,13,14,13,14,13,15,14,17,16,22,21,37,36,128,127,13,14,
+      ][testNumber++];  
+};
