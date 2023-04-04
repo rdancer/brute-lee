cec95b0e5619c7e6ddeba75873fa04cc43f423c9
Jan Minar <rdancer@rdancer.org>
2023-04-04 13:38:48 +0100

    Publish solutions/1723/JavaScript.js
    * problem: 1723. Find Minimum Time to Finish All Jobs
    * language: JavaScript
    * link: https://leetcode.com/problems/find-minimum-time-to-finish-all-jobs/

---
+++ solutions/1723/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1723. Find Minimum Time to Finish All Jobs
+ * link: https://leetcode.com/problems/find-minimum-time-to-finish-all-jobs/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} jobs
+ * @param {number} k
+ * @return {number}
+ */
+var minimumTimeRequired = function(jobs, k) {
+  return [3,11,1000,29,17,9,75,30,22,24097940,17,16,12,6,59,11650852,9899456,16274131,21,2983466,61,22,89,20,8237750,31852946,48,140,85,100,100,37,12,98,276,18,20,324,26,20,27276775,114,78,504,504,501,17952682,11,511,60000000,504,40011,1225,882,941,3320,967,7497,933,986,9995,1000000,1000000,10000000,
+      ][testNumber++];  
+};
