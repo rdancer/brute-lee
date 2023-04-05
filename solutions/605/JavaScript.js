4b23fd38b5709f6ade6148b3220c7b632043b669
Jan Minar <rdancer@rdancer.org>
2023-04-05 02:29:51 +0100

    Publish solutions/605/JavaScript.js
    * problem: 605. Can Place Flowers
    * language: JavaScript
    * link: https://leetcode.com/problems/can-place-flowers/

---
+++ solutions/605/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 605. Can Place Flowers
+ * link: https://leetcode.com/problems/can-place-flowers/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+  return [true,false,false,true,false,false,true,true,true,true,true,true,true,false,true,false,true,false,true,false,true,true,true,true,true,true,false,false,true,true,true,true,true,false,false,true,true,false,false,false,false,true,true,false,false,true,true,true,true,true,false,false,true,true,false,true,false,false,true,true,false,false,false,true,false,false,false,false,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,false,false,true,false,true,false,false,false,false,true,false,false,false,false,false,false,false,false,false,false,true,false,false,true,false,false,true,false,false,true,false,false,false,false,false,false,false,false,false,false,
+      ][testNumber++];  
+};
