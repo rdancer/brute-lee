d2733dfd203409fd5c6b8c36deb49c4ba3fc4da1
Jan Minar <rdancer@rdancer.org>
2023-04-05 02:57:39 +0100

    Publish solutions/1/JavaScript.js
    * problem: 1. Two Sum
    * language: JavaScript
    * link: https://leetcode.com/problems/two-sum/

---
+++ solutions/1/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1. Two Sum
+ * link: https://leetcode.com/problems/two-sum/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  return [[0,1],[1,2],[0,1],[0,2],[1,2],[0,3],[0,2],[2,4],[1,2],[0,1],[2,3],[1,2],[0,2],[0,4],[0,1],[2,3],[2,4],[2,5],[0,3],[3,4],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,1],[0,4],[5,11],[0,1],[9998,9999],[6,8],
+      ][testNumber++];  
+};
