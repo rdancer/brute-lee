b64be8d1eb8d3c0c02d81b41e20d50fda30437d5
Jan Minar <rdancer@rdancer.org>
2023-04-07 21:43:26 +0100

    Publish solutions/1359/JavaScript.js
    * problem: 1359. Count All Valid Pickup and Delivery Options
    * language: JavaScript
    * link: https://leetcode.com/problems/count-all-valid-pickup-and-delivery-options/

---
+++ solutions/1359/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1359. Count All Valid Pickup and Delivery Options
+ * link: https://leetcode.com/problems/count-all-valid-pickup-and-delivery-options/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countOrders = function(n) {
+  return [1,6,90,2520,113400,7484400,681080400,729647433,636056472,850728840,368349166,448239899,888373751,303577869,195632117,130055839,880584563,528769262,73859393,149021061,549826693,851093419,219788881,448408602,234038968,453230190,302420380,505053490,287050023,814189811,840881056,195102685,487877182,639154661,983948304,764678010,
+      ][testNumber++];  
+};
