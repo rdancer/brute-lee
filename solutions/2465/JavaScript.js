24fbc872501832d099199b20bb048ddc74310e3e
Jan Minar <rdancer@rdancer.org>
2023-04-07 21:51:56 +0100

    Publish solutions/2465/JavaScript.js
    * problem: 2465. Number of Distinct Averages
    * language: JavaScript
    * link: https://leetcode.com/problems/number-of-distinct-averages/

---
+++ solutions/2465/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2465. Number of Distinct Averages
+ * link: https://leetcode.com/problems/number-of-distinct-averages/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var distinctAverages = function(nums) {
+  return [2,1,1,2,2,5,3,8,7,5,10,8,10,12,12,10,10,13,10,14,13,13,12,13,20,11,12,12,15,15,16,15,1,1,1,2,
+      ][testNumber++];  
+};
