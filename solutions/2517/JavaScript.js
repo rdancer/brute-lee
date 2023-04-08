bded313f96a181aa68ee01ed838ecf4dc695f4c1
Jan Minar <rdancer@rdancer.org>
2023-04-08 21:50:24 +0100

    Publish solutions/2517/JavaScript.js
    * problem: 2517. Maximum Tastiness of Candy Basket
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-tastiness-of-candy-basket/

---
+++ solutions/2517/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2517. Maximum Tastiness of Candy Basket
+ * link: https://leetcode.com/problems/maximum-tastiness-of-candy-basket/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} price
+ * @param {number} k
+ * @return {number}
+ */
+var maximumTastiness = function(price, k) {
+  return [8,2,0,19,55,22,8,53,0,142,0,140,94,9,107,7787,13031,22473,890,2677,54,229,5827,6,0,0,1,1,565,44442,7137,1138625,23254,2132,576227,216,2,0,2,999999999,
+      ][testNumber++];  
+};
