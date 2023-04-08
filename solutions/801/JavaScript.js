69af6764e356b21c05715e7259b96ab7e8844573
Jan Minar <rdancer@rdancer.org>
2023-04-08 03:49:53 +0100

    Publish solutions/801/JavaScript.js
    * problem: 801. Minimum Swaps To Make Sequences Increasing
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-swaps-to-make-sequences-increasing/

---
+++ solutions/801/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 801. Minimum Swaps To Make Sequences Increasing
+ * link: https://leetcode.com/problems/minimum-swaps-to-make-sequences-increasing/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var minSwap = function(nums1, nums2) {
+  return [1,1,0,0,1,0,1,1,0,1,1,0,0,0,1,0,1,0,0,0,0,0,0,0,4,1,1,1,0,1,0,0,2,0,0,2,0,0,0,0,0,0,0,0,6,1,5,2,1,2,1,2,0,1,0,1,3,1,1,0,1,1,0,0,43,29,22,14,12,19,12,8,4,11,4,6,6,5,6,6,2,6,0,0,416,367,233,224,143,141,119,96,97,81,79,76,70,59,62,45,47,1,1,66,0,915,1391,0,1522,3808,850,1539,1670,14367,16564,8807,29815,
+      ][testNumber++];  
+};
