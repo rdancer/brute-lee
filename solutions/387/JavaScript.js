743d5e2fe59d852bd359fdcdee0473f1244721a4
Jan Minar <rdancer@rdancer.org>
2023-04-08 22:10:55 +0100

    Publish solutions/387/JavaScript.js
    * problem: 387. First Unique Character in a String
    * language: JavaScript
    * link: https://leetcode.com/problems/first-unique-character-in-a-string/

---
+++ solutions/387/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 387. First Unique Character in a String
+ * link: https://leetcode.com/problems/first-unique-character-in-a-string/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+  return [0,2,-1,0,-1,0,-1,0,-1,-1,8,0,0,-1,2,-1,1,2,-1,7,0,2,0,2,-1,-1,-1,-1,-1,-1,61,-1,1,29,-1,5292,-1,-1,-1,-1,-1,-1,-1,-1,251,1615,1676,336,1957,42,1818,5072,1406,130,977,18,1825,2804,1159,6393,813,1449,143,1255,2770,5244,59,238,1466,2586,101,3543,432,1984,3801,1485,2617,5687,665,4595,844,2374,9645,9107,492,696,3474,8791,117,4777,1847,438,534,11165,7942,9791,2204,833,135,5704,7051,165,609,2677,0,
+      ][testNumber++];  
+};
