4f5f7f242910d1e915d2381aadb1b3030345d574
Jan Minar <rdancer@rdancer.org>
2023-04-08 09:53:08 +0100

    Publish solutions/2333/JavaScript.js
    * problem: 2333. Minimum Sum of Squared Difference
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-sum-of-squared-difference/

---
+++ solutions/2333/JavaScript.js
@@ -0,0 +1,18 @@
+/*
+ * language: JavaScript
+ * problem: 2333. Minimum Sum of Squared Difference
+ * link: https://leetcode.com/problems/minimum-sum-of-squared-difference/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number} k1
+ * @param {number} k2
+ * @return {number}
+ */
+var minSumSquareDiff = function(nums1, nums2, k1, k2) {
+  return [579,43,0,27,3,0,2000,0,985,0,17,989,0,1000000,0,0,15100,640644,0,0,131685,0,10000000000000,0,3996440168740,115973837985,1701854480273,1396269304996,4062748267916,404271794529,1000000000000000,32388109196317,386795235236441,101991141900770,0,0,38510457507375,13,
+      ][testNumber++];  
+};
