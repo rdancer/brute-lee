f9500326093cd7a8569d5766e5c28ff175ec4ad3
Jan Minar <rdancer@rdancer.org>
2023-04-08 02:46:26 +0100

    Publish solutions/1534/JavaScript.js
    * problem: 1534. Count Good Triplets
    * language: JavaScript
    * link: https://leetcode.com/problems/count-good-triplets/

---
+++ solutions/1534/JavaScript.js
@@ -0,0 +1,18 @@
+/*
+ * language: JavaScript
+ * problem: 1534. Count Good Triplets
+ * link: https://leetcode.com/problems/count-good-triplets/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} arr
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {number}
+ */
+var countGoodTriplets = function(arr, a, b, c) {
+  return [4,0,12,3,0,2,5,0,0,10,1,8,0,1,41,14,3,1,0,2,1,2,17,251,30,24,127,320,76,120,61,0,49,118,947,344,69,96,1023,209,411,240,601,425,890,1388,1141,2968,552,2769,312,1110,855,139,1428,8,1788,88,3189,2037,2084,1926,248,24024,4748,5163,11661,18802,57707,22813,23621,18040,20197,1337,4744,66268,5227,28751,4499,1695,46052,14559,10035,35747,18586,19910,16795,97338,6494,5172,44049,33560,
+      ][testNumber++];  
+};
