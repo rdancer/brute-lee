92cabdd311fe1d81d3c51a7a3496f5ec29482d1e
Jan Minar <rdancer@rdancer.org>
2023-04-09 13:17:52 +0100

    Publish solutions/407/JavaScript.js
    * problem: 407. Trapping Rain Water II
    * language: JavaScript
    * link: https://leetcode.com/problems/trapping-rain-water-ii/

---
+++ solutions/407/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 407. Trapping Rain Water II
+ * link: https://leetcode.com/problems/trapping-rain-water-ii/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} heightMap
+ * @return {number}
+ */
+var trapRainWater = function(heightMap) {
+  return [4,10,4,14,0,0,0,0,0,1,3,0,0,57,215,0,12,4,4,12,5,44,25,12,36,18,11,79058,68900,61413,89383,100439,353397,346245,383585,364308,355378,1637726,1592346,41564259,784040796,782095763,
+      ][testNumber++];  
+};
