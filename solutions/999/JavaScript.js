a88746f919e36e11b9260dd70766d3830e37fffe
Jan Minar <rdancer@rdancer.org>
2023-04-09 02:18:27 +0100

    Publish solutions/999/JavaScript.js
    * problem: 999. Available Captures for Rook
    * language: JavaScript
    * link: https://leetcode.com/problems/available-captures-for-rook/

---
+++ solutions/999/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 999. Available Captures for Rook
+ * link: https://leetcode.com/problems/available-captures-for-rook/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {character[][]} board
+ * @return {number}
+ */
+var numRookCaptures = function(board) {
+  return [3,0,3,2,0,1,1,1,4,2,1,0,4,4,4,4,0,0,0,4,4,4,3,2,
+      ][testNumber++];  
+};
