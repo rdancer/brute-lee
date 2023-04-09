bff49a1ef7affcc12e6a5e300f10dc03c0cd8a58
Jan Minar <rdancer@rdancer.org>
2023-04-09 02:31:02 +0100

    Publish solutions/1963/JavaScript.js
    * problem: 1963. Minimum Number of Swaps to Make the String Balanced
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-number-of-swaps-to-make-the-string-balanced/

---
+++ solutions/1963/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1963. Minimum Number of Swaps to Make the String Balanced
+ * link: https://leetcode.com/problems/minimum-number-of-swaps-to-make-the-string-balanced/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minSwaps = function(s) {
+  return [1,2,0,1,1,2,0,0,1,1,1,1,0,4,3,2,3,1,5,2,2,3,4,9,13,9,12,7,7,16,9,16,8,20,17,13,42,9,37,25,26,14,4,44,69,36,98,36,83,57,22,16,64,1,0,1,0,250000,1,
+      ][testNumber++];  
+};
