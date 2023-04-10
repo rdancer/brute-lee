1a066958192f40f7c97385b8e285bf057be6d27d
Jan Minar <rdancer@rdancer.org>
2023-04-10 03:38:11 +0100

    Publish solutions/300/JavaScript.js
    * problem: 300. Longest Increasing Subsequence
    * language: JavaScript
    * link: https://leetcode.com/problems/longest-increasing-subsequence/

---
+++ solutions/300/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 300. Longest Increasing Subsequence
+ * link: https://leetcode.com/problems/longest-increasing-subsequence/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+  return [4,4,1,1,1,2,3,2,2,1,1,1,3,3,1,6,3,5,6,6,6,7,2500,1,2,3,2,3,6,8,4,4,4,10,6,5,25,33,32,43,41,58,53,53,52,57,71,72,74,79,80,88,87,96,
+      ][testNumber++];  
+};
