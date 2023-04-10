725d93df8fb276868e17522e9913f96869f4e21d
Jan Minar <rdancer@rdancer.org>
2023-04-10 05:07:00 +0100

    Publish solutions/1105/JavaScript.js
    * problem: 1105. Filling Bookcase Shelves
    * language: JavaScript
    * link: https://leetcode.com/problems/filling-bookcase-shelves/

---
+++ solutions/1105/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1105. Filling Bookcase Shelves
+ * link: https://leetcode.com/problems/filling-bookcase-shelves/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} books
+ * @param {number} shelfWidth
+ * @return {number}
+ */
+var minHeightShelves = function(books, shelfWidth) {
+  return [6,4,15,17,28,41,31,15672,15410,16339,17420,20378,19748,21068,21754,23951,25127,26719,26721,373335,
+      ][testNumber++];  
+};
