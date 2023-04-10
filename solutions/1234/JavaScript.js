91903c8170abffae8048476a18883c3d0f906953
Jan Minar <rdancer@rdancer.org>
2023-04-10 20:15:36 +0100

    Publish solutions/1234/JavaScript.js
    * problem: 1234. Replace the Substring for Balanced String
    * language: JavaScript
    * link: https://leetcode.com/problems/replace-the-substring-for-balanced-string/

---
+++ solutions/1234/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1234. Replace the Substring for Balanced String
+ * link: https://leetcode.com/problems/replace-the-substring-for-balanced-string/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var balancedString = function(s) {
+  return [0,1,2,3,3,4,0,4,2,6,5,6,4,8,13,7,4,6,7,6,5,11,6,14,4,12,8,22,6,7,15,8,22,53,16,86,115,301,366,75000,
+      ][testNumber++];  
+};
