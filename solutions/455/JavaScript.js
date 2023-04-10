9a1d48f57b1b7811bceace4f7abb0c60ff558dee
Jan Minar <rdancer@rdancer.org>
2023-04-10 04:28:08 +0100

    Publish solutions/455/JavaScript.js
    * problem: 455. Assign Cookies
    * language: JavaScript
    * link: https://leetcode.com/problems/assign-cookies/

---
+++ solutions/455/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 455. Assign Cookies
+ * link: https://leetcode.com/problems/assign-cookies/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+var findContentChildren = function(g, s) {
+  return [1,2,0,1,2,4,4,12,1,198,174,99,960,295,128,206,70,96,1646,10000,9928,
+      ][testNumber++];  
+};
