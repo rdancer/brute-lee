6f4331d8954ce8ccf733beb7a872b6fcdce12a32
Jan Minar <rdancer@rdancer.org>
2023-04-10 19:02:23 +0100

    Publish solutions/1681/JavaScript.js
    * problem: 1681. Minimum Incompatibility
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-incompatibility/

---
+++ solutions/1681/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1681. Minimum Incompatibility
+ * link: https://leetcode.com/problems/minimum-incompatibility/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minimumIncompatibility = function(nums, k) {
+  return [4,6,-1,0,-1,0,-1,0,-1,0,-1,12,21,17,0,-1,8,3,0,-1,0,-1,10,0,-1,12,0,-1,-1,13,0,-1,0,-1,15,15,22,22,0,-1,0,-1,-1,12,0,-1,21,20,0,-1,-1,19,12,0,8,7,7,6,
+      ][testNumber++];  
+};
