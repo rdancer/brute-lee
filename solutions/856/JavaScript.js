8924068c11bcd21054551182cab3d2b4d4853c16
Jan Minar <rdancer@rdancer.org>
2023-04-10 19:19:50 +0100

    Publish solutions/856/JavaScript.js
    * problem: 856. Score of Parentheses
    * language: JavaScript
    * link: https://leetcode.com/problems/score-of-parentheses/

---
+++ solutions/856/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 856. Score of Parentheses
+ * link: https://leetcode.com/problems/score-of-parentheses/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var scoreOfParentheses = function(s) {
+  return [1,2,2,6,3,3,3,4,4,4,4,4,5,5,4,4,5,5,6,6,6,8,8,5,5,5,6,6,5,5,6,6,7,7,7,9,9,5,5,5,6,6,6,6,6,6,7,7,7,9,9,8,8,8,10,10,8,8,10,10,12,12,12,16,16,80,88,48,30,123,968,53,150,950,28,99,31,1005,43,520,1669,123,1062,101,4306,106,
+      ][testNumber++];  
+};
