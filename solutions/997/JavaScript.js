d9f9dff56c0bae92d506cfbc109a66f5a12363af
Jan Minar <rdancer@rdancer.org>
2023-04-10 05:32:57 +0100

    Publish solutions/997/JavaScript.js
    * problem: 997. Find the Town Judge
    * language: JavaScript
    * link: https://leetcode.com/problems/find-the-town-judge/

---
+++ solutions/997/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 997. Find the Town Judge
+ * link: https://leetcode.com/problems/find-the-town-judge/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @param {number[][]} trust
+ * @return {number}
+ */
+var findJudge = function(n, trust) {
+  return [2,3,-1,-1,3,1,1,-1,-1,-1,-1,-1,3,1,-1,-1,-1,-1,-1,-1,-1,-1,5,-1,4,1,2,2,-1,-1,4,-1,5,7,2,8,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,2,10,-1,-1,10,-1,-1,12,20,12,-1,-1,-1,-1,16,26,26,-1,18,44,-1,-1,-1,-1,-1,4,-1,99,63,-1,57,91,6,-1,416,403,-1,522,-1,-1,643,77,286,-1,
+      ][testNumber++];  
+};
