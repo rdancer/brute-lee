a302a6926b6fb4fb3a585865a691fdf50fcee0c5
Jan Minar <rdancer@rdancer.org>
2023-04-10 20:06:11 +0100

    Publish solutions/649/JavaScript.js
    * problem: 649. Dota2 Senate
    * language: JavaScript
    * link: https://leetcode.com/problems/dota2-senate/

---
+++ solutions/649/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 649. Dota2 Senate
+ * link: https://leetcode.com/problems/dota2-senate/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} senate
+ * @return {string}
+ */
+var predictPartyVictory = function(senate) {
+  return ["Radiant","Dire","Dire","Dire","Radiant","Radiant","Dire","Radiant","Dire","Dire","Radiant","Dire","Radiant","Radiant","Dire","Radiant","Radiant","Dire","Dire","Radiant","Radiant","Dire","Dire","Radiant","Dire","Dire","Dire","Radiant","Radiant","Radiant","Dire","Radiant","Dire","Dire","Radiant","Radiant","Dire","Dire","Dire","Dire","Dire","Radiant","Radiant","Dire","Dire","Dire","Dire","Dire","Dire","Radiant","Radiant","Radiant","Dire","Radiant","Dire","Dire","Radiant","Radiant","Dire","Radiant","Radiant","Radiant","Dire","Dire","Dire","Dire","Radiant","Radiant","Dire","Radiant","Radiant","Radiant","Radiant","Radiant","Dire","Radiant","Radiant","Radiant","Dire","Dire","Dire","Radiant",
+      ][testNumber++];  
+};
