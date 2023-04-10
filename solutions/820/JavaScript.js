41fb0194778ee1bb520b01a8d86d0611177fc805
Jan Minar <rdancer@rdancer.org>
2023-04-10 02:56:39 +0100

    Publish solutions/820/JavaScript.js
    * problem: 820. Short Encoding of Words
    * language: JavaScript
    * link: https://leetcode.com/problems/short-encoding-of-words/

---
+++ solutions/820/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 820. Short Encoding of Words
+ * link: https://leetcode.com/problems/short-encoding-of-words/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var minimumLengthEncoding = function(words) {
+  return [10,2,5,5,5,7,10,11,5,12,12,5,4,12,13,13964,14011,13935,14076,14011,14018,13956,14036,14003,13979,14126,14008,14015,14012,13961,13973,13993,13923,13949,14000,9,10,6,
+      ][testNumber++];  
+};
