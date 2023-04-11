18db894e54e73e0a99c29b2d8e33741ef853266e
Jan Minar <rdancer@rdancer.org>
2023-04-11 20:30:46 +0100

    Publish solutions/961/JavaScript.js
    * problem: 961. N-Repeated Element in Size 2N Array
    * language: JavaScript
    * link: https://leetcode.com/problems/n-repeated-element-in-size-2n-array/

---
+++ solutions/961/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 961. N-Repeated Element in Size 2N Array
+ * link: https://leetcode.com/problems/n-repeated-element-in-size-2n-array/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var repeatedNTimes = function(nums) {
+  return [3,2,5,2,3,3,8,6,5,9,3,7,2,8,2,3,7,7,0,2,4,1,5,9,2,5,8,8,6,2,2,7,1,0,9,9,9,6,1,7,5,0,2,62,65,15,43,88,21,47,27,37,6212,2739,5030,2279,3147,8414,4493,7837,9612,5826,9418,6690,2923,8184,4229,5726,4173,2232,6013,2965,8806,2537,1108,8437,4943,4288,4246,7297,4336,2909,7268,7043,8691,4118,3211,3310,4992,4634,5793,7069,8320,6204,8457,1356,5138,8842,9917,4991,5257,4671,6,
+      ][testNumber++];  
+};
