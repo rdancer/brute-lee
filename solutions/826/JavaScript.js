b36065b5d47b5009287e1faf173aa6c04fab6a31
Jan Minar <rdancer@rdancer.org>
2023-04-11 10:45:43 +0100

    Publish solutions/826/JavaScript.js
    * problem: 826. Most Profit Assigning Work
    * language: JavaScript
    * link: https://leetcode.com/problems/most-profit-assigning-work/

---
+++ solutions/826/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 826. Most Profit Assigning Work
+ * link: https://leetcode.com/problems/most-profit-assigning-work/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} difficulty
+ * @param {number[]} profit
+ * @param {number[]} worker
+ * @return {number}
+ */
+var maxProfitAssignment = function(difficulty, profit, worker) {
+  return [100,0,190,142,89,188,54,300,156,324,205,216,147,106,62,172,465,765,448,432,553,729,511,501,910,1392,693,1117,997,1508,982,877,1880,4769350,2436511,2698531,2379497,2847201,1996139,2251088,4981400,99659581,49066245,50931510,49290442,51183107,48943608,50683892,99933000,999542181,496462318,492582978,504136211,492391330,501230866,502469405,999830000,
+      ][testNumber++];  
+};
