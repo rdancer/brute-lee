eefde549e08434b3b738a49568c4342a4739652e
Jan Minar <rdancer@rdancer.org>
2023-04-11 19:32:30 +0100

    Publish solutions/2486/JavaScript.js
    * problem: 2486. Append Characters to String to Make Subsequence
    * language: JavaScript
    * link: https://leetcode.com/problems/append-characters-to-string-to-make-subsequence/

---
+++ solutions/2486/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2486. Append Characters to String to Make Subsequence
+ * link: https://leetcode.com/problems/append-characters-to-string-to-make-subsequence/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+var appendCharacters = function(s, t) {
+  return [4,0,5,1,0,3,1,3,2,2,2,0,2,4,4,4,3,1,4,4,2,1,2,2,4,1,4,2,1,1,2,2,1,5,1,3,4,7,5,8,9,8,8,4,9,10,6,9,6,5,9,9,6,4,10,8,7,10,4,8,8,10,9,7,10,10,10,96063,96168,3598,7540,318,4634,939,9008,5135,8911,8374,9312,100000,0,1,0,3,1,
+      ][testNumber++];  
+};
