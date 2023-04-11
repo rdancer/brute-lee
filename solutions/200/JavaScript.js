6e5a91fe6545198b6593993e6033d5955b493327
Jan Minar <rdancer@rdancer.org>
2023-04-11 10:15:06 +0100

    Publish solutions/200/JavaScript.js
    * problem: 200. Number of Islands
    * language: JavaScript
    * link: https://leetcode.com/problems/number-of-islands/

---
+++ solutions/200/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 200. Number of Islands
+ * link: https://leetcode.com/problems/number-of-islands/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid) {
+  return [1,3,1,0,1,1,1,0,3,1,1,2,1,4,5,1,1,1,1,0,1,1,3,4,2,0,3,1,2,1,1,3,1,1,4,2,2,58,55,1,48,45,1,51,3,36,23,109,6121,
+      ][testNumber++];  
+};
