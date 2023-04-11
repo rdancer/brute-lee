f6edfde83246fc000bf563d396fb6c05b66cadc3
Jan Minar <rdancer@rdancer.org>
2023-04-11 21:26:20 +0100

    Publish solutions/1970/JavaScript.js
    * problem: 1970. Last Day Where You Can Still Cross
    * language: JavaScript
    * link: https://leetcode.com/problems/last-day-where-you-can-still-cross/

---
+++ solutions/1970/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 1970. Last Day Where You Can Still Cross
+ * link: https://leetcode.com/problems/last-day-where-you-can-still-cross/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} row
+ * @param {number} col
+ * @param {number[][]} cells
+ * @return {number}
+ */
+var latestDayToCross = function(row, col, cells) {
+  return [2,1,3,3,3,3,8,1,4,2,7,1,9,8,2,5,4,2,10,24,11,32,18,12,35,14,25,17,5,12,37,23,77,5,10,14,6,12,28,2,1,17,45,9,30,2,5,93,64,38,26,15,3,20,5,38,40,6,1,32,19,67,3,18,7,43,75,4,42,36,36,8,45,10,33,89,9,5,124,13,259,282,209,16,312,731,142,149,176,195,28,132,276,4509,3220,43,2404,99,13080,99,886,32,140,4796,2239,33,15,81,17,26,56,1636,126,29,19800,140,3879,140,
+      ][testNumber++];  
+};
