c396e2e02cc6a0575c21d1ebdb4af62538a60348
Jan Minar <rdancer@rdancer.org>
2023-04-11 09:23:43 +0100

    Publish solutions/1395/JavaScript.js
    * problem: 1395. Count Number of Teams
    * language: JavaScript
    * link: https://leetcode.com/problems/count-number-of-teams/

---
+++ solutions/1395/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1395. Count Number of Teams
+ * link: https://leetcode.com/problems/count-number-of-teams/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} rating
+ * @return {number}
+ */
+var numTeams = function(rating) {
+  return [3,0,4,1,0,3,0,1,1,1,24,14,11,25,30,18,17,29,6,10,3514,87,535,565,139,2479,622,5209,334,1391,8291,11708,14322,16957,16122,7475,6670,7898,19321,22769,32608,30792,42091,43038,41879,272754,56738,287735,296531,213267,348799,80486,263103,60462,245561,759166,1267733,2576458,9142022,12670965,14510756,16018162,18162098,33570135,41149260,44472617,55010814,166167000,166167000,166167000,166167000,56004334,
+      ][testNumber++];  
+};
