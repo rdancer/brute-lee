01849430433f80318273541efc3f35acb551b410
Jan Minar <rdancer@rdancer.org>
2023-04-11 09:00:56 +0100

    Publish solutions/1326/JavaScript.js
    * problem: 1326. Minimum Number of Taps to Open to Water a Garden
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-number-of-taps-to-open-to-water-a-garden/

---
+++ solutions/1326/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1326. Minimum Number of Taps to Open to Water a Garden
+ * link: https://leetcode.com/problems/minimum-number-of-taps-to-open-to-water-a-garden/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @param {number[]} ranges
+ * @return {number}
+ */
+var minTaps = function(n, ranges) {
+  return [1,-1,3,2,1,1,1,2,3,3,4,6,6,9,10,11,13,12,14,15,14,16,40,64,65,99,103,111,183,239,244,46,325,55,-1,2,-1,-1,
+      ][testNumber++];  
+};
