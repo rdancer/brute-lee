07961b312f217deeef4cbe9b039f96b55a5ea17f
Jan Minar <rdancer@rdancer.org>
2023-04-11 03:05:16 +0100

    Publish solutions/2301/JavaScript.js
    * problem: 2301. Match Substring After Replacement
    * language: JavaScript
    * link: https://leetcode.com/problems/match-substring-after-replacement/

---
+++ solutions/2301/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 2301. Match Substring After Replacement
+ * link: https://leetcode.com/problems/match-substring-after-replacement/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @param {string} sub
+ * @param {character[][]} mappings
+ * @return {boolean}
+ */
+var matchReplacement = function(s, sub, mappings) {
+  return [true,false,true,false,false,false,false,false,true,true,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,true,true,true,true,true,true,true,true,true,true,true,false,false,true,false,true,false,true,true,false,false,false,false,false,true,true,true,true,true,true,false,true,false,true,true,true,true,false,true,true,false,true,true,true,false,true,false,false,false,false,false,false,false,false,false,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,false,false,true,true,true,
+      ][testNumber++];  
+};
