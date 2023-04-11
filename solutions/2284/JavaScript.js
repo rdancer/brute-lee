c67fb96577d2e945a46e494a5646a82e7e71c258
Jan Minar <rdancer@rdancer.org>
2023-04-11 09:38:32 +0100

    Publish solutions/2284/JavaScript.js
    * problem: 2284. Sender With Largest Word Count
    * language: JavaScript
    * link: https://leetcode.com/problems/sender-with-largest-word-count/

---
+++ solutions/2284/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2284. Sender With Largest Word Count
+ * link: https://leetcode.com/problems/sender-with-largest-word-count/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string[]} messages
+ * @param {string[]} senders
+ * @return {string}
+ */
+var largestWordCount = function(messages, senders) {
+  return ["Alice","Charlie","o","FnZd","HvI","QrUzSFFUkq","ANEOCH","vUpSXnHS","YUvIabdxz","ubIEXAO","v","GncX","clnLZ","MJU","cCdww","wjAC","RclslxNRo","Wz","FQpY","zWCMYqSmF","dkiJeOGXdQ","PjR","WwgBizXHU","CaREUzIVCu","Ho","RDGhU","HTbrbiPTg","IhdfdA","JsR","vqj","xsJ","KQDlTeGJV","zT","RDfBJ","HQO","hrDend","GgkvEiV","vlbXOEWC","CjaETb","QZSSG","DMXNXPa","NBm","ynzAf","NsAFzQtFnp","Hl","xvNKSr","YautBXkC","a","UNTasfnBJz","zADZhwSwFj","zpHQmAzEcW","uCCgqlOHqC","zWSrDroNkQ","zFjJvBLFyo","abfedA","zhLVrlJsVl","zsNBkiKyeI","zpQXiZREqv","ziBYnakKeG","zxKADUPkpb","zzwZK","eYRPR","zzrYs","zydlC","yURGg","bA","b","CB","abBAB","Ba",
+      ][testNumber++];  
+};
