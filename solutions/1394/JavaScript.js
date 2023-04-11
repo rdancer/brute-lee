d3a1c43905e75990d2a2b9035026fdf024d0e5a3
Jan Minar <rdancer@rdancer.org>
2023-04-11 02:16:09 +0100

    Publish solutions/1394/JavaScript.js
    * problem: 1394. Find Lucky Integer in an Array
    * language: JavaScript
    * link: https://leetcode.com/problems/find-lucky-integer-in-an-array/

---
+++ solutions/1394/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1394. Find Lucky Integer in an Array
+ * link: https://leetcode.com/problems/find-lucky-integer-in-an-array/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var findLucky = function(arr) {
+  return [2,3,-1,-1,7,3,4,-1,1,-1,1,8,9,10,4,2,4,4,12,5,1,13,5,-1,10,10,16,-1,9,16,16,9,17,8,-1,17,5,8,18,18,11,18,19,12,11,19,10,19,20,-1,-1,9,10,21,21,22,22,22,22,23,20,-1,14,24,24,24,24,24,25,25,20,20,19,25,25,26,-1,15,27,-1,27,28,28,28,-1,17,28,19,28,28,14,29,29,30,30,30,19,19,-1,31,1,
+      ][testNumber++];  
+};
