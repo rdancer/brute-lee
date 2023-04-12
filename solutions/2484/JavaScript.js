5e97602ee54fd50d1ed46b2f73aac10e7a5c589a
Jan Minar <rdancer@rdancer.org>
2023-04-13 00:51:49 +0100

    Publish solutions/2484/JavaScript.js
    * problem: 2484. Count Palindromic Subsequences
    * language: JavaScript
    * link: https://leetcode.com/problems/count-palindromic-subsequences/

---
+++ solutions/2484/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2484. Count Palindromic Subsequences
+ * link: https://leetcode.com/problems/count-palindromic-subsequences/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countPalindromes = function(s) {
+  return [2,21,2,0,0,0,0,0,0,0,0,0,0,0,3,2,12,19504,877061,498523186,971909402,728883641,77731478,1,34,71,121,160,384,341,537636,18484730,783704819,898343208,797500005,2,0,0,0,0,0,0,4,4,10,44,78,99,199,378,547,1063,1549,3139,1946,3885,539643,18776367,804026840,916320027,839804093,857967119,379753341,
+      ][testNumber++];  
+};
