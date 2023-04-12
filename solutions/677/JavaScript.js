7fe40ca5090eb27b45500b0d503455a44a449814
Jan Minar <rdancer@rdancer.org>
2023-04-12 02:13:59 +0100

    Publish solutions/677/JavaScript.js
    * problem: 677. Map Sum Pairs
    * language: JavaScript
    * link: https://leetcode.com/problems/map-sum-pairs/

---
+++ solutions/677/JavaScript.js
@@ -0,0 +1,45 @@
+/*
+ * language: JavaScript
+ * problem: 677. Map Sum Pairs
+ * link: https://leetcode.com/problems/map-sum-pairs/
+ * note: object-oriented, flat
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+
+var MapSum = function() {
+  return mySolution()
+    
+};
+
+/** 
+ * @param {string} key 
+ * @param {number} val
+ * @return {void}
+ */
+MapSum.prototype.insert = function(key, val) {
+  return mySolution()
+    
+};
+
+/** 
+ * @param {string} prefix
+ * @return {number}
+ */
+MapSum.prototype.sum = function(prefix) {
+  return mySolution()
+    
+};
+
+/** 
+ * Your MapSum object will be instantiated and called as such:
+ * var obj = new MapSum()
+ * obj.insert(key,val)
+ * var param_2 = obj.sum(prefix)
+ */
+
+function mySolution() {
+  return buffer[testNumber++];
+}
+var buffer = [[null,null,3,null,5],[null,null,3,null,5],[null,null,0,null,3],[null,null,3,null,null,4],[null,null,3,null,5],[null,null,3,null,5,null,null,1],[null,null,3,null,2],[null,null,2,null,null,4],[null,null,3,null,0],[null,null,3,null,2,2],[null,null,3,null,2,2,0],[null,null,3,null,2,2,null,3],[null,null,3,null,2,2,null,3,0],[null,null,3,null,2,2,null,3,0,0],[null,null,3,null,2,2,null,3,0,0,null,5,116],[null,null,3,null,2,2,null,3,0,0,null,5,116,0,0],[null,null,3,null,2,2,null,3,0,0,null,5,116,0,0,111,null,null,null,143,144,256],[null,null,3,null,2,2,null,3,0,0,null,5,116,0,0,111,null,null,null,143,144,256,256],[null,null,3,null,2,2,null,3,0,0,null,5,116,0,0,111,null,null,null,143,144,256,261],[null,null,3,null,2,2,null,3,0,0,null,5,116,0,0,111,null,null,null,143,144,256,0],[null,null,3,null,2,2,null,3,0,0,null,5,116,0,0,111,null,null,null,143,144,256,0],[null,null,3,null,2,2,null,3,0,0,null,5,116,0,0,111,null,null,null,143,144,256,0,0,0],[null,null,3,null,2,2,null,3,0,0,null,5,116,0,0,111,null,null,null,143,144,256,0,0,0,null,33],[null,null,3,null,2,2,null,3,0,0,null,5,116,0,0,111,null,null,null,143,144,256,0,0,0,null,33,0],[null,null,3,null,2,2,null,3,0,0,null,5,116,0,0,111,null,null,null,143,144,256,0,0,0,null,33,0,38],[null,null,3,null,2,2,null,3,0,0,null,5,116,0,0,111,null,null,null,143,144,256,0,0,0,null,33,0,38,294],[null,null,3,null,2,2,null,3,0,0,null,5,116,0,0,111,null,null,null,143,144,256,0,0,0,null,33,0,38,294,null,1142],[null,null,3,null,2,2,null,3,0,0,null,5,116,0,0,111,null,null,null,143,144,256,0,0,0,null,33,0,38,294,null,1142,999],[null,null,3,null,2,2,null,3,0,0,null,5,116,0,0,111,null,null,null,143,144,256,0,0,0,null,33,0,38,294,null,1142,999,0],[null,null,3,null,2,2,null,3,0,0,null,5,116,0,0,111,null,null,null,143,144,256,0,0,0,null,33,0,38,294,null,1142,999,0,0],[null,null,3,null,2,2,null,3,0,0,null,5,116,0,0,111,null,null,null,143,144,256,0,0,0,null,33,0,38,294,null,1142,999,0,0],[null,null,333,null,222,222,null,3,0,0,null,225,336,0,0,111,null,null,null,143,144,256,0,0,0,null,33,0,258,514,null,1142,999,0,0],[null,null,0,null,222,0,null,0,0,0,null,0,111,225,0,111,null,null,null,143,144,256,0,0,0,null,33,0,33,289,null,1367,1221,0,0],[null,null,333,null,222,0,null,0,0,0,null,0,111,225,333,111,null,null,null,143,144,256,0,0,0,null,33,0,33,289,null,1367,1221,0,0],[null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,98],
+    ["sentinel"]].flat();
