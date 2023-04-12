f5b806c31589d25a0e8111b692545af5833026df
Jan Minar <rdancer@rdancer.org>
2023-04-12 03:01:30 +0100

    Publish solutions/274/JavaScript.js
    * problem: 274. H-Index
    * language: JavaScript
    * link: https://leetcode.com/problems/h-index/

---
+++ solutions/274/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 274. H-Index
+ * link: https://leetcode.com/problems/h-index/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} citations
+ * @return {number}
+ */
+var hIndex = function(citations) {
+  return [3,1,0,1,1,0,1,1,1,1,1,2,0,1,1,1,1,2,2,2,2,2,1,1,1,1,1,1,2,2,2,2,2,2,2,3,2,3,3,3,3,2,2,3,4,4,3,3,6,5,3,4,5,5,7,4,4,7,4,31,14,37,14,38,23,20,26,18,48,21,50,69,60,65,54,53,48,59,40,47,836,
+      ][testNumber++];  
+};
