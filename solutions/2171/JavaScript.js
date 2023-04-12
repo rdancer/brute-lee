f9ebda40963df1ca285754d5d44a2bb064a3ade1
Jan Minar <rdancer@rdancer.org>
2023-04-12 11:51:53 +0100

    Publish solutions/2171/JavaScript.js
    * problem: 2171. Removing Minimum Number of Magic Beans
    * language: JavaScript
    * link: https://leetcode.com/problems/removing-minimum-number-of-magic-beans/

---
+++ solutions/2171/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2171. Removing Minimum Number of Magic Beans
+ * link: https://leetcode.com/problems/removing-minimum-number-of-magic-beans/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} beans
+ * @return {number}
+ */
+var minimumRemoval = function(beans) {
+  return [4,7,0,0,83,15,200,146,158,0,217,33,68,1231,2169,1176,766,496,1574,257,374,792,513,2360448,2246323,2145491,2178065,1877,2242442,2354630,2393,2397926,2163580,2365036,2215406,2285494,2010,2195422,19010577,21366048,4858,23326308,23105542,22682691,9539683,12547099,16758,13021083,1297386659,2230114688,2495336935,2493513627,0,0,1,0,2500000000,2500000000,0,84965203,10,20,1,3676791951,
+      ][testNumber++];  
+};
