fbab2a7fda0ec631b58d2ca29450dc6a931fc633
Jan Minar <rdancer@rdancer.org>
2023-04-12 02:31:12 +0100

    Publish solutions/1186/JavaScript.js
    * problem: 1186. Maximum Subarray Sum with One Deletion
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-subarray-sum-with-one-deletion/

---
+++ solutions/1186/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1186. Maximum Subarray Sum with One Deletion
+ * link: https://leetcode.com/problems/maximum-subarray-sum-with-one-deletion/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var maximumSum = function(arr) {
+  return [4,3,-1,3,5,14,-50,7,17,27,14,50,17,48,1727,-1,5,5,36,15,27,76,20,55,43,123,111,135,84,85,187,49,105,83,961792,1452443,
+      ][testNumber++];  
+};
