7750c0b20269ba47a75c121336e3cdd111d08d0e
Jan Minar <rdancer@rdancer.org>
2023-04-12 12:49:42 +0100

    Publish solutions/413/JavaScript.js
    * problem: 413. Arithmetic Slices
    * language: JavaScript
    * link: https://leetcode.com/problems/arithmetic-slices/

---
+++ solutions/413/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 413. Arithmetic Slices
+ * link: https://leetcode.com/problems/arithmetic-slices/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numberOfArithmeticSlices = function(nums) {
+  return [3,0,0,1,1,1,10,55,2,0,120,4,23,1,2,
+      ][testNumber++];  
+};
