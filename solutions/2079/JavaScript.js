536a2e4ce85d8ed24cdb5ed9ecf1ceb9267f64cc
Jan Minar <rdancer@rdancer.org>
2023-04-12 12:01:42 +0100

    Publish solutions/2079/JavaScript.js
    * problem: 2079. Watering Plants
    * language: JavaScript
    * link: https://leetcode.com/problems/watering-plants/

---
+++ solutions/2079/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2079. Watering Plants
+ * link: https://leetcode.com/problems/watering-plants/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} plants
+ * @param {number} capacity
+ * @return {number}
+ */
+var wateringPlants = function(plants, capacity) {
+  return [14,30,49,17,16,8,24,30,48,52,78,66,110,94,94,5086,5222,6988,6684,6830,27728,27272,24612,27132,26992,300,11864,12102,11804,697562,712970,705970,710972,758712,696830,698152,386432,413802,459070,1000000,
+      ][testNumber++];  
+};
