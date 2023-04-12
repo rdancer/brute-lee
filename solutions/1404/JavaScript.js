afdf2401b1ea01ea840d6fee6be4b70b153598a6
Jan Minar <rdancer@rdancer.org>
2023-04-12 11:37:49 +0100

    Publish solutions/1404/JavaScript.js
    * problem: 1404. Number of Steps to Reduce a Number in Binary Representation to One
    * language: JavaScript
    * link: https://leetcode.com/problems/number-of-steps-to-reduce-a-number-in-binary-representation-to-one/

---
+++ solutions/1404/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1404. Number of Steps to Reduce a Number in Binary Representation to One
+ * link: https://leetcode.com/problems/number-of-steps-to-reduce-a-number-in-binary-representation-to-one/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numSteps = function(s) {
+  return [6,1,0,6,5,4,8,4,11,11,12,9,9,14,13,15,17,13,18,29,14,21,23,29,28,26,20,27,85,64,51,89,32,76,48,69,120,158,35,85,51,78,37,156,246,242,230,270,234,159,212,176,146,403,429,338,434,368,426,414,346,365,375,660,660,697,734,637,555,495,649,511,621,
+      ][testNumber++];  
+};
