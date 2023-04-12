e180091b4cd0acc30263ad7e3886a1d5143e5bf7
Jan Minar <rdancer@rdancer.org>
2023-04-12 13:07:32 +0100

    Publish solutions/2491/JavaScript.js
    * problem: 2491. Divide Players Into Teams of Equal Skill
    * language: JavaScript
    * link: https://leetcode.com/problems/divide-players-into-teams-of-equal-skill/

---
+++ solutions/2491/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2491. Divide Players Into Teams of Equal Skill
+ * link: https://leetcode.com/problems/divide-players-into-teams-of-equal-skill/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} skill
+ * @return {number}
+ */
+var dividePlayers = function(skill) {
+  return [22,12,-1,1,2,1000,1000000,10,22,-1,10,8,-1,-1,-1,13,-1,8,13,15,-1,-1,-1,32,60,24,2,10,6,30,18,6,22,75,2,25,13,18,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,82,601,748,350,418,27,696,426,360,170,908,328,133,299,248,-1,16915588750,50000000000,8358350000,-1,-1,-1,-1,-1,692097542,120041127,5554083,996256508,923735312,9,10,22,-1,-1,-1,
+      ][testNumber++];  
+};
