0c6c40bc556159744a5c8a681956104022b32f2b
Jan Minar <rdancer@rdancer.org>
2023-04-12 02:44:56 +0100

    Publish solutions/1638/JavaScript.js
    * problem: 1638. Count Substrings That Differ by One Character
    * language: JavaScript
    * link: https://leetcode.com/problems/count-substrings-that-differ-by-one-character/

---
+++ solutions/1638/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1638. Count Substrings That Differ by One Character
+ * link: https://leetcode.com/problems/count-substrings-that-differ-by-one-character/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+var countSubstrings = function(s, t) {
+  return [6,3,0,10,1,33,147,63,62,82,50,97,105,164,107,57,53,56,43,71,38,114,91,41,86,36,104,83,67,37,49,66,102,52,87,206,480,227,541,519,251,179,564,232,148,800,754,932,3689,574,1143,2378,3062,3799,6482,4982,6450,7661,6967,5245,8406,3200,19603,
+      ][testNumber++];  
+};
