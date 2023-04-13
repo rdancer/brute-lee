9970ca138de5a086f26a3cf9ce9378cf450cde89
Jan Minar <rdancer@rdancer.org>
2023-04-13 16:35:06 +0100

    Publish solutions/902/JavaScript.js
    * problem: 902. Numbers At Most N Given Digit Set
    * language: JavaScript
    * link: https://leetcode.com/problems/numbers-at-most-n-given-digit-set/

---
+++ solutions/902/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 902. Numbers At Most N Given Digit Set
+ * link: https://leetcode.com/problems/numbers-at-most-n-given-digit-set/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string[]} digits
+ * @param {number} n
+ * @return {number}
+ */
+var atMostNGivenDigitSet = function(digits, n) {
+  return [20,29523,1,1,2,2,3,3,4,1,3,1,2,6,18,20,19,51,42,59,3,10,21,20,55,79,171,576,128,4,14,66,276,280,1338,742,2476,5760,5,46,363,340,2655,4974,11710,25160,59979,6,126,1092,3412,13280,48210,65849,190528,322107,6,190,2550,5460,50780,128778,540232,2221640,891029,7,510,3279,66900,332030,1222386,2826376,12255070,35933218,8,1022,9840,349524,878905,8734002,12490401,153391688,392738517,2,
+      ][testNumber++];  
+};
