d8dacb79c9512efeb68dec3be03a760869f4a9ca
Jan Minar <rdancer@rdancer.org>
2023-04-14 00:24:19 +0100

    Publish solutions/2103/JavaScript.js
    * problem: 2103. Rings and Rods
    * language: JavaScript
    * link: https://leetcode.com/problems/rings-and-rods/

---
+++ solutions/2103/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2103. Rings and Rods
+ * link: https://leetcode.com/problems/rings-and-rods/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} rings
+ * @return {number}
+ */
+var countPoints = function(rings) {
+  return [1,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,1,1,1,0,1,0,0,0,3,0,1,1,1,0,3,2,4,5,2,7,2,2,6,8,5,9,8,4,0,8,8,9,9,9,9,8,8,8,9,8,0,0,0,0,0,10,10,10,
+      ][testNumber++];  
+};
