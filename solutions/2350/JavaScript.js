2f7702e52d08735d321c9aed3ed49d816dd77a5f
Jan Minar <rdancer@rdancer.org>
2023-04-13 13:57:24 +0100

    Publish solutions/2350/JavaScript.js
    * problem: 2350. Shortest Impossible Sequence of Rolls
    * language: JavaScript
    * link: https://leetcode.com/problems/shortest-impossible-sequence-of-rolls/

---
+++ solutions/2350/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2350. Shortest Impossible Sequence of Rolls
+ * link: https://leetcode.com/problems/shortest-impossible-sequence-of-rolls/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} rolls
+ * @param {number} k
+ * @return {number}
+ */
+var shortestSequence = function(rolls, k) {
+  return [3,2,1,2,2,1,1,2,4,1,6,6,11,5,1,1,2,1,1,93,31,662,57,34,1,1,3,2,3,4,3,5,2,12,1,2,1,1,1,70,323,247,42,25,16,1,2,3,1,2,1,7544,9057,7,7,1,4,1,1,2,1,100001,1,10001,2,
+      ][testNumber++];  
+};
