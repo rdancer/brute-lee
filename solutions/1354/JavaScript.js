a81b3b925bbbf5ab6f56319829dcf0857e9153ea
Jan Minar <rdancer@rdancer.org>
2023-04-14 00:00:14 +0100

    Publish solutions/1354/JavaScript.js
    * problem: 1354. Construct Target Array With Multiple Sums
    * language: JavaScript
    * link: https://leetcode.com/problems/construct-target-array-with-multiple-sums/

---
+++ solutions/1354/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1354. Construct Target Array With Multiple Sums
+ * link: https://leetcode.com/problems/construct-target-array-with-multiple-sums/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} target
+ * @return {boolean}
+ */
+var isPossible = function(target) {
+  return [true,false,true,true,true,true,false,false,true,true,true,false,false,true,false,true,false,true,true,false,false,true,false,false,false,false,false,false,true,true,false,false,true,false,true,false,true,true,true,false,false,false,false,false,false,false,false,true,false,true,false,false,true,true,false,false,false,false,false,false,false,false,true,false,true,false,false,false,false,true,false,
+      ][testNumber++];  
+};
