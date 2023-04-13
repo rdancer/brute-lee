39f0da052fd0c29ef06dbe2c2070912f46db9466
Jan Minar <rdancer@rdancer.org>
2023-04-13 14:09:40 +0100

    Publish solutions/164/JavaScript.js
    * problem: 164. Maximum Gap
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-gap/

---
+++ solutions/164/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 164. Maximum Gap
+ * link: https://leetcode.com/problems/maximum-gap/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumGap = function(nums) {
+  return [3,0,9999999,99999997,97,0,97,4,2901,2147,90545587,35694473,37480833,29563026,40108290,15170477,14695404,12077034,16374408,9550171,9398830,7119521,2912,1244,81403346,48507528,33,2126,1,3615,58,21,23,19,10,13,11,1,1,143633,1,
+      ][testNumber++];  
+};
