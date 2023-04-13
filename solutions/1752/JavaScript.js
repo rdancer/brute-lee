73640c1e1b3b1c826ed56d0d660c551e747074a4
Jan Minar <rdancer@rdancer.org>
2023-04-13 23:29:18 +0100

    Publish solutions/1752/JavaScript.js
    * problem: 1752. Check if Array Is Sorted and Rotated
    * language: JavaScript
    * link: https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/

---
+++ solutions/1752/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1752. Check if Array Is Sorted and Rotated
+ * link: https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var check = function(nums) {
+  return [true,false,true,true,true,true,true,false,true,true,false,true,false,false,true,true,true,true,false,true,true,true,false,false,true,false,true,false,false,false,false,false,true,true,true,true,true,true,false,true,true,true,true,true,true,false,false,false,false,true,true,false,false,false,false,true,false,true,false,true,false,false,true,true,false,false,true,true,true,false,false,true,false,true,true,false,true,true,false,false,true,true,false,false,false,true,true,false,true,true,false,true,true,false,true,false,false,true,true,false,false,true,false,true,false,true,true,
+      ][testNumber++];  
+};
