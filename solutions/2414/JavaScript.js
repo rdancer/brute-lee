57df071966c1dd5930a48631583a6c199f9f9677
Jan Minar <rdancer@rdancer.org>
2023-04-13 03:02:20 +0100

    Publish solutions/2414/JavaScript.js
    * problem: 2414. Length of the Longest Alphabetical Continuous Substring
    * language: JavaScript
    * link: https://leetcode.com/problems/length-of-the-longest-alphabetical-continuous-substring/

---
+++ solutions/2414/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2414. Length of the Longest Alphabetical Continuous Substring
+ * link: https://leetcode.com/problems/length-of-the-longest-alphabetical-continuous-substring/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestContinuousSubstring = function(s) {
+  return [2,5,1,1,1,1,1,1,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,11,26,2,3,2,1,2,2,2,3,2,4,2,2,1,2,2,2,2,2,2,2,2,4,3,3,2,4,3,4,3,3,3,3,2,2,3,3,3,3,3,3,3,3,3,3,4,3,3,3,3,3,4,3,3,4,3,4,3,3,3,4,4,3,4,3,3,3,4,4,4,3,3,3,3,4,3,4,3,3,4,4,5,3,3,4,4,4,4,4,4,4,4,5,4,4,26,4,
+      ][testNumber++];  
+};
