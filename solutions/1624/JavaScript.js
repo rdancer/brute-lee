c98aa391dd0f8ade8db3db64c2b6909048d3e798
Jan Minar <rdancer@rdancer.org>
2023-04-13 02:18:14 +0100

    Publish solutions/1624/JavaScript.js
    * problem: 1624. Largest Substring Between Two Equal Characters
    * language: JavaScript
    * link: https://leetcode.com/problems/largest-substring-between-two-equal-characters/

---
+++ solutions/1624/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1624. Largest Substring Between Two Equal Characters
+ * link: https://leetcode.com/problems/largest-substring-between-two-equal-characters/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxLengthBetweenEqualCharacters = function(s) {
+  return [0,2,-1,4,-1,6,4,18,19,21,38,41,51,53,52,53,53,61,60,73,80,85,92,99,104,107,108,113,124,140,150,150,159,161,169,166,171,184,203,215,222,217,224,221,225,231,242,253,253,280,283,283,292,291,
+      ][testNumber++];  
+};
