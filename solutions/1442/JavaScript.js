0de1119716cce29d387268ecd1ae98178ab99bae
Jan Minar <rdancer@rdancer.org>
2023-04-13 02:28:58 +0100

    Publish solutions/1442/JavaScript.js
    * problem: 1442. Count Triplets That Can Form Two Arrays of Equal XOR
    * language: JavaScript
    * link: https://leetcode.com/problems/count-triplets-that-can-form-two-arrays-of-equal-xor/

---
+++ solutions/1442/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1442. Count Triplets That Can Form Two Arrays of Equal XOR
+ * link: https://leetcode.com/problems/count-triplets-that-can-form-two-arrays-of-equal-xor/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var countTriplets = function(arr) {
+  return [4,10,0,3,8,2,0,3,12,6,15,0,1,0,8,0,0,12,14,18,0,22,24,10,28,28,24,36,38,40,46,25,50,60,80,178,453,184,0,762,2669,3932,0,3396,2606,3410,2250050,
+      ][testNumber++];  
+};
