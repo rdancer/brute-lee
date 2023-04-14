f9ffb68b59d3ee08f014f2af12b00a26796644bb
Jan Minar <rdancer@rdancer.org>
2023-04-14 02:39:26 +0100

    Publish solutions/2315/JavaScript.js
    * problem: 2315. Count Asterisks
    * language: JavaScript
    * link: https://leetcode.com/problems/count-asterisks/

---
+++ solutions/2315/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2315. Count Asterisks
+ * link: https://leetcode.com/problems/count-asterisks/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countAsterisks = function(s) {
+  return [2,0,5,3,0,6,3,8,0,0,1,0,0,0,0,16,1,0,1,0,1,0,0,1,0,257,215,226,29,0,34,0,0,0,0,0,0,0,0,1000,0,15,22,24,13,14,25,28,19,21,20,17,22,24,20,16,15,10,13,18,17,23,24,0,240,36,0,0,19,
+      ][testNumber++];  
+};
