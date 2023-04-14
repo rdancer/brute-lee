ee8ad04ff8bf1fcf248253b89e74fb9a12c0cd0b
Jan Minar <rdancer@rdancer.org>
2023-04-14 14:42:11 +0100

    Publish solutions/357/JavaScript.js
    * problem: 357. Count Numbers with Unique Digits
    * language: JavaScript
    * link: https://leetcode.com/problems/count-numbers-with-unique-digits/

---
+++ solutions/357/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 357. Count Numbers with Unique Digits
+ * link: https://leetcode.com/problems/count-numbers-with-unique-digits/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countNumbersWithUniqueDigits = function(n) {
+  return [91,1,10,739,5275,32491,168571,712891,2345851,
+      ][testNumber++];  
+};
