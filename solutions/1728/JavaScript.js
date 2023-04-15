7779a1ff3d597688cc924b028d7bb91491499f29
Jan Minar <rdancer@rdancer.org>
2023-04-15 08:22:13 +0100

    Publish solutions/1728/JavaScript.js
    * problem: 1728. Cat and Mouse II
    * language: JavaScript
    * link: https://leetcode.com/problems/cat-and-mouse-ii/

---
+++ solutions/1728/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 1728. Cat and Mouse II
+ * link: https://leetcode.com/problems/cat-and-mouse-ii/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string[]} grid
+ * @param {number} catJump
+ * @param {number} mouseJump
+ * @return {boolean}
+ */
+var canMouseWin = function(grid, catJump, mouseJump) {
+  return [true,true,false,false,true,false,true,false,false,true,true,true,false,false,false,true,false,true,false,true,false,true,false,false,true,true,false,true,false,true,true,true,false,true,true,true,true,false,false,false,false,false,true,false,false,false,false,true,true,true,true,false,true,false,false,false,true,true,false,true,false,true,false,true,true,false,true,true,true,true,false,true,false,true,false,true,false,false,true,true,false,true,true,true,true,false,true,true,false,false,true,true,true,true,true,true,false,true,true,true,true,true,false,false,false,true,true,false,true,false,false,false,true,true,false,false,true,true,false,false,false,true,false,true,false,false,false,true,false,false,false,false,false,false,false,true,false,false,false,true,true,false,false,true,false,true,true,false,true,false,false,false,true,false,true,true,false,false,false,true,false,true,false,false,false,true,false,true,true,false,false,true,true,false,false,false,true,false,true,
+      ][testNumber++];  
+};
