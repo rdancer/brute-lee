19c5bc40de58c4dc65f082bb14b0ed08328d93c3
Jan Minar <rdancer@rdancer.org>
2023-04-15 02:21:15 +0100

    Publish solutions/972/JavaScript.js
    * problem: 972. Equal Rational Numbers
    * language: JavaScript
    * link: https://leetcode.com/problems/equal-rational-numbers/

---
+++ solutions/972/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 972. Equal Rational Numbers
+ * link: https://leetcode.com/problems/equal-rational-numbers/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isRationalEqual = function(s, t) {
+  return [true,true,true,true,true,true,true,true,false,false,true,true,true,false,false,false,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,true,true,false,true,false,true,false,true,true,false,true,true,false,true,true,true,true,false,true,true,false,false,true,false,true,false,true,true,true,false,true,false,true,false,true,true,true,true,
+      ][testNumber++];  
+};
