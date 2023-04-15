b3ab45169aac5dcb2bae71592783c76ff24f333e
Jan Minar <rdancer@rdancer.org>
2023-04-15 22:29:29 +0100

    Publish solutions/1647/JavaScript.js
    * problem: 1647. Minimum Deletions to Make Character Frequencies Unique
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-deletions-to-make-character-frequencies-unique/

---
+++ solutions/1647/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1647. Minimum Deletions to Make Character Frequencies Unique
+ * link: https://leetcode.com/problems/minimum-deletions-to-make-character-frequencies-unique/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minDeletions = function(s) {
+  return [0,2,2,1,3,0,2,1,1,3,2,23,276,276,1,5,3,1,3,5,1,0,4,3,2,0,4,17,5,7,39,15,2,19,19,27,25,31,4,28,10,31,12,39,29,14,39,31,35,17,5,16,38,45,7,36,2,2,1,10,1,0,0,3,3,0,2,3,2,15,10,0,2,2,1,10,9,12,1,6,3,3,1,6,1,9,1,2,2,3,1,1,3,0,2,5,1,6,17,17,0,7,6,
+      ][testNumber++];  
+};
