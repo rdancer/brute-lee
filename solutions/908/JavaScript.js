52cd4743866f255e05d66b264746e583211f951f
Jan Minar <rdancer@rdancer.org>
2023-04-15 07:34:13 +0100

    Publish solutions/908/JavaScript.js
    * problem: 908. Smallest Range I
    * language: JavaScript
    * link: https://leetcode.com/problems/smallest-range-i/

---
+++ solutions/908/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 908. Smallest Range I
+ * link: https://leetcode.com/problems/smallest-range-i/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var smallestRangeI = function(nums, k) {
+  return [0,6,0,3,0,0,0,1,4,0,3,2,0,0,8,0,3,4,5,0,0,0,6,0,0,0,1,0,0,4,0,7,10,0,9,8,8,5,0,5,3,5,0,6489,5344,8051,0,916,494,0,0,0,6394,9785,132,3273,0,0,0,0,0,2504,0,0,8476,5728,0,4330,
+      ][testNumber++];  
+};
