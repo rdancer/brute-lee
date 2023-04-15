96d529cd005302cbb9111c1217b46a6ee902e45f
Jan Minar <rdancer@rdancer.org>
2023-04-15 07:48:57 +0100

    Publish solutions/1343/JavaScript.js
    * problem: 1343. Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold
    * language: JavaScript
    * link: https://leetcode.com/problems/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/

---
+++ solutions/1343/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 1343. Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold
+ * link: https://leetcode.com/problems/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} threshold
+ * @return {number}
+ */
+var numOfSubarrays = function(arr, k, threshold) {
+  return [3,6,5,1,1,24,28,43,44,37,80,66,113,122,108,23,199,201,180,225,267,195,293,246,383,467,370,407,360,334,424,330,579,449,351,474,575,603,387,566,551,687,636,428,596,448,451,725,714,483,527,579,0,618,627,596,912,553,860,681,984,12747,27077,46924,0,60830,64861,99684,50001,
+      ][testNumber++];  
+};
