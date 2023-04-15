34f7a863fe0b91489a624ac1e963c532d8c58a7d
Jan Minar <rdancer@rdancer.org>
2023-04-15 21:54:09 +0100

    Publish solutions/2269/JavaScript.js
    * problem: 2269. Find the K-Beauty of a Number
    * language: JavaScript
    * link: https://leetcode.com/problems/find-the-k-beauty-of-a-number/

---
+++ solutions/2269/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2269. Find the K-Beauty of a Number
+ * link: https://leetcode.com/problems/find-the-k-beauty-of-a-number/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} num
+ * @param {number} k
+ * @return {number}
+ */
+var divisorSubstrings = function(num, k) {
+  return [2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,0,1,1,1,1,1,1,0,0,1,1,1,0,1,0,0,0,1,1,1,2,0,0,1,1,0,0,0,2,0,0,0,0,1,1,1,0,0,3,0,0,0,0,0,0,1,0,1,1,2,0,1,0,0,2,1,1,2,0,0,0,0,1,0,0,0,1,0,1,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,2,0,0,3,0,0,0,0,0,2,0,1,0,0,1,1,1,0,0,0,1,0,1,1,3,0,0,2,0,2,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,4,0,0,0,0,1,1,9,7,1,1,1,1,1,1,1,1,1,1,1,9,
+      ][testNumber++];  
+};
