c70e2c79c0abce6856fb0656b53723fa8d6dd46c
Jan Minar <rdancer@rdancer.org>
2023-04-16 02:27:55 +0100

    Publish solutions/1722/JavaScript.js
    * problem: 1722. Minimize Hamming Distance After Swap Operations
    * language: JavaScript
    * link: https://leetcode.com/problems/minimize-hamming-distance-after-swap-operations/

---
+++ solutions/1722/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 1722. Minimize Hamming Distance After Swap Operations
+ * link: https://leetcode.com/problems/minimize-hamming-distance-after-swap-operations/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} source
+ * @param {number[]} target
+ * @param {number[][]} allowedSwaps
+ * @return {number}
+ */
+var minimumHammingDistance = function(source, target, allowedSwaps) {
+  return [1,2,0,1,2,1,4,8,1,12,12,12,10,14,14,16,14,18,23,27,21,21,28,26,29,30,34,34,38,34,31,39,40,37,36,37,38,40,44,40,47,44,47,45,48,45,1095,2380,4106,4441,4765,4928,5761,6266,7230,7741,46148,9,77,804,792,46013,7,7,9,77,78,84,764,798,798,
+      ][testNumber++];  
+};
