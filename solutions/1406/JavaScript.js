61f1bd5bdb2e2c9d49aa917cf1bd60a056987ff0
Jan Minar <rdancer@rdancer.org>
2023-04-16 02:12:41 +0100

    Publish solutions/1406/JavaScript.js
    * problem: 1406. Stone Game III
    * language: JavaScript
    * link: https://leetcode.com/problems/stone-game-iii/

---
+++ solutions/1406/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1406. Stone Game III
+ * link: https://leetcode.com/problems/stone-game-iii/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} stoneValue
+ * @return {string}
+ */
+var stoneGameIII = function(stoneValue) {
+  return ["Bob","Alice","Tie","Alice","Tie","Bob","Tie","Alice","Alice","Alice","Alice","Alice","Alice","Bob","Alice","Alice","Alice","Bob","Alice","Alice","Alice","Bob","Alice","Alice","Alice","Bob","Alice","Alice","Bob","Alice","Alice","Alice","Alice","Bob","Alice","Alice","Alice","Bob","Alice","Alice","Alice","Alice","Bob","Alice","Alice","Bob","Alice","Bob","Alice","Alice","Alice","Alice","Alice","Bob","Alice","Bob","Bob","Alice","Alice","Alice","Alice","Alice","Alice","Bob","Alice","Alice","Alice","Alice","Alice","Alice","Alice","Bob","Bob","Alice","Alice","Alice","Bob","Alice","Alice","Tie","Bob","Bob","Alice","Alice","Alice","Alice","Bob","Bob","Alice","Bob","Bob","Alice","Alice","Alice","Bob","Alice","Alice","Alice","Alice","Bob","Alice","Alice","Alice","Alice","Alice","Bob","Alice","Alice","Alice","Alice","Tie","Alice","Alice","Alice","Bob","Bob","Alice","Bob","Bob","Alice","Alice","Alice","Bob","Bob","Alice","Tie","Alice","Alice","Alice","Bob","Alice","Alice","Alice","Alice","Alice","Alice","Alice","Alice","Alice","Alice","Alice","Alice","Bob","Alice","Alice","Alice","Bob","Alice","Alice","Bob","Bob","Alice","Alice","Bob","Bob","Bob","Alice","Bob","Alice","Alice","Alice","Alice","Alice","Bob","Alice","Alice","Alice","Bob","Bob","Bob","Alice","Bob","Tie","Bob","Alice","Alice","Tie","Alice","Bob","Bob","Alice","Alice","Alice","Alice","Alice",
+      ][testNumber++];  
+};
