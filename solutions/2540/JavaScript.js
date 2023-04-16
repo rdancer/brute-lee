d326ca0f6ce31384316e65dfb271fdfd19ced76e
Jan Minar <rdancer@rdancer.org>
2023-04-16 02:55:15 +0100

    Publish solutions/2540/JavaScript.js
    * problem: 2540. Minimum Common Value
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-common-value/

---
+++ solutions/2540/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2540. Minimum Common Value
+ * link: https://leetcode.com/problems/minimum-common-value/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var getCommon = function(nums1, nums2) {
+  return [2,2,2,2,1000000000,999999999,4,4,2,13,12,12,15,3,8,11,5,11,6,38,72,28,19,67,8,50,67,4,4,17,24,12,37,18,97557237,-1,2,-1,-1,-1,
+      ][testNumber++];  
+};
