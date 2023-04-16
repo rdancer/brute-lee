eaa7677eab2657186c4efd135e26a798f71bd006
Jan Minar <rdancer@rdancer.org>
2023-04-16 14:15:50 +0100

    Publish solutions/1884/JavaScript.js
    * problem: 1884. Egg Drop With 2 Eggs and N Floors
    * language: JavaScript
    * link: https://leetcode.com/problems/egg-drop-with-2-eggs-and-n-floors/

---
+++ solutions/1884/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1884. Egg Drop With 2 Eggs and N Floors
+ * link: https://leetcode.com/problems/egg-drop-with-2-eggs-and-n-floors/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var twoEggDrop = function(n) {
+  return [2,14,3,5,5,5,5,5,6,6,6,6,6,6,7,7,7,7,7,7,7,8,8,8,8,8,8,8,8,9,9,9,9,9,9,9,9,9,10,10,10,10,10,10,10,10,10,10,11,11,11,11,11,11,11,11,11,11,11,12,12,12,12,12,12,12,12,12,12,12,12,13,13,13,13,13,13,13,13,13,13,13,13,13,14,14,14,14,14,14,14,14,22,22,25,25,27,27,29,29,30,30,32,32,34,34,35,35,37,37,38,38,39,40,41,41,42,42,43,43,45,45,45,22,25,27,29,30,32,34,35,37,38,40,41,42,43,1,45,
+      ][testNumber++];  
+};
