520a2c7d7ed450b33ea3fb8e9e0b87c1275c20f6
Jan Minar <rdancer@rdancer.org>
2023-04-16 14:26:29 +0100

    Publish solutions/2511/JavaScript.js
    * problem: 2511. Maximum Enemy Forts That Can Be Captured
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-enemy-forts-that-can-be-captured/

---
+++ solutions/2511/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2511. Maximum Enemy Forts That Can Be Captured
+ * link: https://leetcode.com/problems/maximum-enemy-forts-that-can-be-captured/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} forts
+ * @return {number}
+ */
+var captureForts = function(forts) {
+  return [4,0,2,2,2,0,0,1,0,1,1,1,1,2,0,0,0,1,1,0,1,0,1,0,0,4,5,6,7,998,0,0,0,0,0,0,0,0,0,1,1,0,0,1,2,
+      ][testNumber++];  
+};
