a2f04ac06695ed0279d856899651788c3e05ed32
Jan Minar <rdancer@rdancer.org>
2023-04-16 13:11:32 +0100

    Publish solutions/2521/JavaScript.js
    * problem: 2521. Distinct Prime Factors of Product of Array
    * language: JavaScript
    * link: https://leetcode.com/problems/distinct-prime-factors-of-product-of-array/

---
+++ solutions/2521/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2521. Distinct Prime Factors of Product of Array
+ * link: https://leetcode.com/problems/distinct-prime-factors-of-product-of-array/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var distinctPrimeFactors = function(nums) {
+  return [4,1,1,1,1,1,2,3,5,3,2,2,3,2,2,2,2,2,2,2,2,2,2,3,1,2,3,2,3,1,3,3,1,2,6,4,5,4,4,4,5,4,4,3,5,4,4,3,6,4,4,4,5,6,5,4,3,6,5,7,5,3,6,6,168,168,168,2,1,168,166,168,168,165,168,168,167,168,168,1,3,
+      ][testNumber++];  
+};
