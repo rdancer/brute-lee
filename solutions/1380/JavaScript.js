2b25827b038d060e4b390eb5dfd65b1153e8f3e5
Jan Minar <rdancer@rdancer.org>
2023-04-17 03:14:31 +0100

    Publish solutions/1380/JavaScript.js
    * problem: 1380. Lucky Numbers in a Matrix
    * language: JavaScript
    * link: https://leetcode.com/problems/lucky-numbers-in-a-matrix/

---
+++ solutions/1380/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1380. Lucky Numbers in a Matrix
+ * link: https://leetcode.com/problems/lucky-numbers-in-a-matrix/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var luckyNumbers  = function(matrix) {
+  return [[15],[12],[7],[],[75772],[],[],[70300],[],[20503],[65091],[],[],[85030],[],[],[88382],[],[6629],[14255],[],[],[],[25163],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],
+      ][testNumber++];  
+};
