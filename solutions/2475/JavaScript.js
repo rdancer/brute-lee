f928d277890b4b6aa44be71f676fa8fc6723b017
Jan Minar <rdancer@rdancer.org>
2023-04-17 19:27:05 +0100

    Publish solutions/2475/JavaScript.js
    * problem: 2475. Number of Unequal Triplets in Array
    * language: JavaScript
    * link: https://leetcode.com/problems/number-of-unequal-triplets-in-array/

---
+++ solutions/2475/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2475. Number of Unequal Triplets in Array
+ * link: https://leetcode.com/problems/number-of-unequal-triplets-in-array/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var unequalTriplets = function(nums) {
+  return [3,0,0,0,1,1,0,0,0,7,1,2,4,4,1,2,7,0,2,2,2,0,0,4,7,1,3,0,0,0,1,4,1,2,0,0,1,2,4,44,10,16,88,7,104,10,10,98,20,70,30,17,77,10,63,82,44,35,7,112,20,63,25,20,104,50,16,70,0,35813,120,816,35872,8400,146587,23222,142224,133216,11440,161700,0,161308,161210,161406,161112,161308,161308,161308,160918,160916,161308,161210,161308,161308,161406,161112,161210,161308,160916,161112,161308,0,1,0,
+      ][testNumber++];  
+};
