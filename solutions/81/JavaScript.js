ef51391c06a30ae02fbc7b7b70b0f8c60a0e41ba
Jan Minar <rdancer@rdancer.org>
2023-04-17 05:52:20 +0100

    Publish solutions/81/JavaScript.js
    * problem: 81. Search in Rotated Sorted Array II
    * language: JavaScript
    * link: https://leetcode.com/problems/search-in-rotated-sorted-array-ii/

---
+++ solutions/81/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 81. Search in Rotated Sorted Array II
+ * link: https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {boolean}
+ */
+var search = function(nums, target) {
+  return [true,false,false,true,true,true,true,true,true,false,false,true,false,false,true,false,true,false,false,true,false,true,false,false,true,true,false,false,true,false,true,false,false,true,false,true,false,false,true,false,true,false,false,true,false,true,false,true,false,false,true,false,true,false,true,false,false,true,false,true,false,true,false,false,true,false,true,false,false,true,false,true,false,false,true,false,true,false,false,true,false,true,false,false,true,false,true,false,false,true,false,true,false,false,true,false,true,false,false,true,false,true,false,false,true,false,true,false,true,true,true,false,true,true,true,true,false,true,true,false,true,true,true,true,false,false,false,true,true,true,true,true,false,false,false,true,false,true,true,false,true,true,true,true,false,true,false,true,true,false,true,false,true,true,true,true,false,true,true,true,true,true,false,false,false,false,false,false,false,false,true,false,false,true,false,false,false,false,true,false,false,false,false,true,false,false,false,true,true,true,true,true,true,false,true,false,true,true,false,true,false,false,false,true,false,false,true,true,false,true,false,true,false,true,false,true,true,true,true,true,true,false,true,true,true,false,true,false,false,false,false,true,false,false,true,true,false,true,false,false,true,false,true,false,true,false,true,true,false,false,true,true,true,false,true,true,false,false,true,false,true,true,true,true,true,true,false,true,false,true,true,false,true,true,false,false,true,false,false,true,
+      ][testNumber++];  
+};
