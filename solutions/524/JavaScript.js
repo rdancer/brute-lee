e746644a30d6dfc5bb81822dcea709bacb01bd24
Jan Minar <rdancer@rdancer.org>
2023-04-17 06:09:11 +0100

    Publish solutions/524/JavaScript.js
    * problem: 524. Longest Word in Dictionary through Deleting
    * language: JavaScript
    * link: https://leetcode.com/problems/longest-word-in-dictionary-through-deleting/

---
+++ solutions/524/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 524. Longest Word in Dictionary through Deleting
+ * link: https://leetcode.com/problems/longest-word-in-dictionary-through-deleting/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @param {string[]} dictionary
+ * @return {string}
+ */
+var findLongestWord = function(s, dictionary) {
+  return ["apple","a","a","aaa","a","abc","ab","apple","ewaf","","a","bar","best","bar","ab","abc","ab","bar","mfuraildmrjhdjtctdxejgdurr","ntgcykxhdfescxxypyew","nbmxgkduynigvzuyblwjepn","oxbqykmaolflfwiywndloeevqx","cibcbxlghdntmvrmnyxhz","amxktoavwanikyqxignnte","eurpyngnmilbtmjpsabesfx","amrwrsuzkmemfmyhowgf","skgpdpgxysovvvtoxzssdfqbe","sffuwjeoojrkfeygxayjbj","barr","dsdwfogactjbcsgbkxfxnrcotrxwwqftyuqit","fiaoibnpihgzqwnbdlhyovhatvyqinhjukbzmxphgkivolzqrnfmiaymrkjwmmeunhlodflsoltkwrjhugeyqwknaguerhkonjnwcndmunhduxmxwlmbkxehiukhzcjlqhhlewqbnedonniuexxgnsyxuumgedlvfgaicahaulecuoxatlusmpnsbziwekhnhtdxdauwwrnijzswnabkkktwlsepbshvjysdliwujzvjqvmtpsjgwatvuhjvkzgerxaemvosdrssgandvejxlqgvamvrauknovpsfypgdqydkqacdjyhadcpnfjhzkrulahnhlwpxviodgftsjnnisvyxmkqwlabkgswgujlmmyrpxhqepyewtjidoprkgshdnebxchhfnsuuqtfgaylyfwblujffbnowsaulkagrjjkarsddrzqrebkggwxvucuiefsxvharplzgabglywnicojtgahfhqcpaacbogvxkuunhvrblfcfhgfqzlhxtzlrkbsgtkaoqwvsafeygsracpscvjfmbnlvhlzoxyyrtzuftecwibrphjvpdbylhueeohxmhkiohzcdlcxgbmliovabwduwbunfdwapwntecugkwgbdyomrhjfcilpbyykapposmpgmxflahwswtdohzsvvwrobuxsdrxwaxoyydbwanuzzfbbrqlaujpvhmzafkxtxnidlubomqlgqagjtvveynuorapklrcmbaoxtcoktsopsjvwxjmnncupcukzqqbvvrskegygpneevfuzxxsnracmvcmutefpeebjzbgvnlrmgwunmlpsvlfbvajcoukwgzfbapwchbxofxahcwvyjcnoutvscpyzatdedeiggogirrvoswhximanhepqpwtigrhxisrwmylqskhffeduhfyeyxpbgvbjfxblowozsbjmpxtejqrxmhttnioovpbifcemsfgcxpofbabjtqbmnpzkzsoeoigrxbin",
+      ][testNumber++];  
+};
