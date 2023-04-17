8993cf186298d92fc96ba14cc5a01a326460a7f9
Jan Minar <rdancer@rdancer.org>
2023-04-17 04:23:18 +0100

    Publish solutions/2249/JavaScript.js
    * problem: 2249. Count Lattice Points Inside a Circle
    * language: JavaScript
    * link: https://leetcode.com/problems/count-lattice-points-inside-a-circle/

---
+++ solutions/2249/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2249. Count Lattice Points Inside a Circle
+ * link: https://leetcode.com/problems/count-lattice-points-inside-a-circle/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} circles
+ * @return {number}
+ */
+var countLatticePoints = function(circles) {
+  return [5,16,141,112,67,85,177,133,232,51,163,185,124,112,193,125,166,148,154,167,187,189,36,189,202,109,196,119,226,18,184,5,105,168,107,185,47,109,67,185,168,164,1507,5029,4737,3428,28443,13998,22039,5,15,5,81,29,31446,13,15,31417,33662,31417,
+      ][testNumber++];  
+};
