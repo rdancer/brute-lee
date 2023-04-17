2f7dc865163ed68208a2ac39c704ce9abe6f6039
Jan Minar <rdancer@rdancer.org>
2023-04-17 04:09:37 +0100

    Publish solutions/2444/JavaScript.js
    * problem: 2444. Count Subarrays With Fixed Bounds
    * language: JavaScript
    * link: https://leetcode.com/problems/count-subarrays-with-fixed-bounds/

---
+++ solutions/2444/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 2444. Count Subarrays With Fixed Bounds
+ * link: https://leetcode.com/problems/count-subarrays-with-fixed-bounds/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @param {number} minK
+ * @param {number} maxK
+ * @return {number}
+ */
+var countSubarrays = function(nums, minK, maxK) {
+  return [2,10,81,122,118,128,4,1,0,12,2,57,0,0,405,961,492,236,544,760,57,322,510,1945,22047,1258,1733,11652,12885,5299,7881,6383,889,21007,90526,162153,71288,27769,147566,2134221,464628,488962,1755474,1264731,0,5000050000,1386044581,4999950000,1,0,0,
+      ][testNumber++];  
+};
