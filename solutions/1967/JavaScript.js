7774607a32b25a38474b19821672c37e9bf1ccac
Jan Minar <rdancer@rdancer.org>
2023-04-17 18:17:34 +0100

    Publish solutions/1967/JavaScript.js
    * problem: 1967. Number of Strings That Appear as Substrings in Word
    * language: JavaScript
    * link: https://leetcode.com/problems/number-of-strings-that-appear-as-substrings-in-word/

---
+++ solutions/1967/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1967. Number of Strings That Appear as Substrings in Word
+ * link: https://leetcode.com/problems/number-of-strings-that-appear-as-substrings-in-word/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string[]} patterns
+ * @param {string} word
+ * @return {number}
+ */
+var numOfStrings = function(patterns, word) {
+  return [3,2,3,0,0,0,0,0,3,5,5,10,10,6,3,7,9,12,2,6,6,5,4,4,7,1,5,3,0,5,19,16,24,24,23,13,18,19,11,15,18,21,23,19,17,24,14,22,13,15,2,38,33,54,54,36,40,20,27,42,28,44,37,33,39,48,32,33,41,41,33,38,0,57,47,1,100,48,1,99,0,0,
+      ][testNumber++];  
+};
