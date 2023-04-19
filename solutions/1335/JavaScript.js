9af993a2f99f1e9b72eda591465970cbb86c323b
Jan Minar <rdancer@rdancer.org>
2023-04-19 11:00:23 +0100

    Publish solutions/1335/JavaScript.js
    * problem: 1335. Minimum Difficulty of a Job Schedule
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-difficulty-of-a-job-schedule/

---
+++ solutions/1335/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1335. Minimum Difficulty of a Job Schedule
+ * link: https://leetcode.com/problems/minimum-difficulty-of-a-job-schedule/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} jobDifficulty
+ * @param {number} d
+ * @return {number}
+ */
+var minDifficulty = function(jobDifficulty, d) {
+  return [7,-1,3,15,843,3,1803,977,1839,3807,1075,1063,3039,1967,1703,2531,2331,3306,998,993,2196,1317,1470,2759,2024,3295,2552,2190,992,2079,3044,10000,9992,0,
+      ][testNumber++];  
+};
