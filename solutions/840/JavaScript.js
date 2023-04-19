504134df0cd29bfc15f8f74b55760a0b3ae54dda
Jan Minar <rdancer@rdancer.org>
2023-04-19 14:53:51 +0100

    Publish solutions/840/JavaScript.js
    * problem: 840. Magic Squares In Grid
    * language: JavaScript
    * link: https://leetcode.com/problems/magic-squares-in-grid/

---
+++ solutions/840/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 840. Magic Squares In Grid
+ * link: https://leetcode.com/problems/magic-squares-in-grid/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var numMagicSquaresInside = function(grid) {
+  return [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,1,1,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0,0,0,2,0,0,0,0,0,0,1,2,0,1,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,1,0,0,0,1,0,0,2,3,1,1,2,0,2,0,0,2,
+      ][testNumber++];  
+};
