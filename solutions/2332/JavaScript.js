b69124a4fdfb60629868bc9c300347a892d598d7
Jan Minar <rdancer@rdancer.org>
2023-04-20 04:24:17 +0100

    Publish solutions/2332/JavaScript.js
    * problem: 2332. The Latest Time to Catch a Bus
    * language: JavaScript
    * link: https://leetcode.com/problems/the-latest-time-to-catch-a-bus/

---
+++ solutions/2332/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 2332. The Latest Time to Catch a Bus
+ * link: https://leetcode.com/problems/the-latest-time-to-catch-a-bus/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} buses
+ * @param {number[]} passengers
+ * @param {number} capacity
+ * @return {number}
+ */
+var latestTimeCatchTheBus = function(buses, passengers, capacity) {
+  return [16,20,3,1,1,1,3,1,3,2,5,5,2,20,10,11,18,9,18,19,20,9408,9896,9951,9784,9786,9878,9895,9898,9894,9960,9906,9870,9675,9911,999974,972377,998415,999997,998854,998200,999749,999993808,1000000000,100001,975390087,999776023,998271062,999382094,999705089,3,2,2,3,2,3,1,4,4,1,1,1,6,
+      ][testNumber++];  
+};
