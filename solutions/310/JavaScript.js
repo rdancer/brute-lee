6730db9416ce14bdd6f15e6d49520510da8ff85f
Jan Minar <rdancer@rdancer.org>
2023-04-20 05:42:26 +0100

    Publish solutions/310/JavaScript.js
    * problem: 310. Minimum Height Trees
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-height-trees/

---
+++ solutions/310/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 310. Minimum Height Trees
+ * link: https://leetcode.com/problems/minimum-height-trees/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {number[]}
+ */
+var findMinHeightTrees = function(n, edges) {
+  return [[1],[3,4],[0],[0,1],[0],[0],[0,3],[3],[3],[1,2],[0],[2],[3],[5],[5,6],[0,3],[1,6],[1],[2],[1],[0],[1],[3],[11],[0],[0],[2,4],[0,6],[21],[1],[2,8],[0],[0],[6],[0,2],[0],[0],[0,2],[0],[11],[1,3],[0,1],[0,2],[3],[1],[2],[8,17],[45],[201,202],[1,5],[1],[0],[0,1],[0],[1,7],[4],[4],[3,9],[1,3],[1],[0],[1,5],[2,5],[0,2],[0,1],[2499,2500],[4,6],[1,2],[3425],[3],[9999,10000],
+      ][testNumber++];  
+};
