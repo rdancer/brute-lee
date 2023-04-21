909fdd81ee75caceb296082fbee72bc07c0c992d
Jan Minar <rdancer@rdancer.org>
2023-04-21 02:17:20 +0100

    Publish solutions/1467/JavaScript.js
    * problem: 1467. Probability of a Two Boxes Having The Same Number of Distinct Balls
    * language: JavaScript
    * link: https://leetcode.com/problems/probability-of-a-two-boxes-having-the-same-number-of-distinct-balls/

---
+++ solutions/1467/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1467. Probability of a Two Boxes Having The Same Number of Distinct Balls
+ * link: https://leetcode.com/problems/probability-of-a-two-boxes-having-the-same-number-of-distinct-balls/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} balls
+ * @return {number}
+ */
+var getProbability = function(balls) {
+  return [1.00000,0.66667,0.60000,0.30000,0.90327,0.25974,0.85571,1.00000,0.57143,0.00000,0.81602,0.03968,0.55556,0.06620,0.47619,0.14836,0.11133,0.19860,0.57143,0.12233,1.00000,
+      ][testNumber++];  
+};
