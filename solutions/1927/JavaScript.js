1e12c7930ade29530a5a4530ecd5d64aca1988f7
Jan Minar <rdancer@rdancer.org>
2023-04-21 19:52:32 +0100

    Publish solutions/1927/JavaScript.js
    * problem: 1927. Sum Game
    * language: JavaScript
    * link: https://leetcode.com/problems/sum-game/

---
+++ solutions/1927/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1927. Sum Game
+ * link: https://leetcode.com/problems/sum-game/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} num
+ * @return {boolean}
+ */
+var sumGame = function(num) {
+  return [false,true,false,true,true,true,false,false,true,false,false,false,false,true,true,false,true,false,false,false,false,true,false,true,true,true,false,false,true,false,false,false,true,true,true,false,false,false,true,false,false,true,true,false,true,false,true,false,true,false,false,false,false,false,false,false,true,false,false,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,true,true,true,true,false,true,true,true,true,true,true,true,false,false,true,true,true,true,
+      ][testNumber++];  
+};
