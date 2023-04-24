9a0b1e9b32811295ab7e102da3b52aab8d4497c1
Jan Minar <rdancer@rdancer.org>
2023-04-24 15:28:33 +0100

    Publish solutions/1909/JavaScript.js
    * problem: 1909. Remove One Element to Make the Array Strictly Increasing
    * language: JavaScript
    * link: https://leetcode.com/problems/remove-one-element-to-make-the-array-strictly-increasing/

---
+++ solutions/1909/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1909. Remove One Element to Make the Array Strictly Increasing
+ * link: https://leetcode.com/problems/remove-one-element-to-make-the-array-strictly-increasing/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canBeIncreasing = function(nums) {
+  return [true,false,false,true,true,true,true,false,true,true,true,true,true,true,true,true,true,true,false,true,true,true,false,true,true,true,false,true,false,true,true,true,false,true,false,true,false,true,true,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,false,true,false,true,false,false,true,true,true,false,
+      ][testNumber++];  
+};
