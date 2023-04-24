020767d77e903b17d601f9199022e4b84c8e3e09
Jan Minar <rdancer@rdancer.org>
2023-04-24 15:52:55 +0100

    Publish solutions/14/JavaScript.js
    * problem: 14. Longest Common Prefix
    * language: JavaScript
    * link: https://leetcode.com/problems/longest-common-prefix/

---
+++ solutions/14/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 14. Longest Common Prefix
+ * link: https://leetcode.com/problems/longest-common-prefix/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+  return ["fl","","","a","","","a","c","","flower","f","","c","a","aa","a","a","a","","","","a","aa","","ab","","","","","","","","","","","","","a","","","","b","a","","b","b","","","cc","c","","","","","","","cc","","","","","","","","","a","a","a","ab","a","a","ab","a","a","aa","ab","a","a","a","a","a","a","a","a","a","aa","aa","aa","aa","aa","aa","aa","aa","aa","aa","ap","ap","ap","ap","ap","ap","ap","ap","ap","ap","abc","abc","abc","abc","abc","abc","abc","abc","abc","abc","fl","aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa","","","a","aa","aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa","aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa","fr",
+      ][testNumber++];  
+};
