8ae7d5e3146fdd97b941b4757dfa7d803acf0c71
Jan Minar <rdancer@rdancer.org>
2023-04-25 22:48:33 +0100

    Publish solutions/1320/JavaScript.js
    * problem: 1320. Minimum Distance to Type a Word Using Two Fingers
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-distance-to-type-a-word-using-two-fingers/

---
+++ solutions/1320/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1320. Minimum Distance to Type a Word Using Two Fingers
+ * link: https://leetcode.com/problems/minimum-distance-to-type-a-word-using-two-fingers/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} word
+ * @return {number}
+ */
+var minimumDistance = function(word) {
+  return [3,6,3,7,6,6,2,1,3,5,7,0,7,0,2,3,5,3,5,2,6,0,5,2,295,267,363,283,190,264,386,207,253,305,374,393,195,400,377,272,381,210,213,291,507,574,493,552,500,538,526,515,588,600,3,
+      ][testNumber++];  
+};
