1da0ce31455fb9d93965f92739ed1ea87634b87e
Jan Minar <rdancer@rdancer.org>
2023-04-25 22:18:13 +0100

    Publish solutions/1255/JavaScript.js
    * problem: 1255. Maximum Score Words Formed by Letters
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-score-words-formed-by-letters/

---
+++ solutions/1255/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 1255. Maximum Score Words Formed by Letters
+ * link: https://leetcode.com/problems/maximum-score-words-formed-by-letters/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string[]} words
+ * @param {character[]} letters
+ * @param {number[]} score
+ * @return {number}
+ */
+var maxScoreWords = function(words, letters, score) {
+  return [23,27,0,5,14,23,6,18,51,55,49,83,63,62,19,52,27,19,54,19,78,60,0,155,98,102,95,70,82,101,102,148,86,102,58,201,79,298,286,196,76,167,195,156,61,139,45,50,196,235,90,171,
+      ][testNumber++];  
+};
