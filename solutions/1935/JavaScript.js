fe7455f12e858c9fa0c1cb9aeef1b2fc3455d145
Jan Minar <rdancer@rdancer.org>
2023-04-25 21:34:36 +0100

    Publish solutions/1935/JavaScript.js
    * problem: 1935. Maximum Number of Words You Can Type
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-number-of-words-you-can-type/

---
+++ solutions/1935/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1935. Maximum Number of Words You Can Type
+ * link: https://leetcode.com/problems/maximum-number-of-words-you-can-type/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} text
+ * @param {string} brokenLetters
+ * @return {number}
+ */
+var canBeTypedWords = function(text, brokenLetters) {
+  return [1,1,0,0,1,2,0,0,0,0,0,3,76,6,43,31,913,37,28,1,
+      ][testNumber++];  
+};
