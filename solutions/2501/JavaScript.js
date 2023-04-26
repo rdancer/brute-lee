7b4feaac17dad603b30abed43c6e56e8b33e203f
Jan Minar <rdancer@rdancer.org>
2023-04-26 12:27:17 +0100

    Publish solutions/2501/JavaScript.js
    * problem: 2501. Longest Square Streak in an Array
    * language: JavaScript
    * link: https://leetcode.com/problems/longest-square-streak-in-an-array/

---
+++ solutions/2501/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2501. Longest Square Streak in an Array
+ * link: https://leetcode.com/problems/longest-square-streak-in-an-array/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestSquareStreak = function(nums) {
+  return [3,-1,-1,2,-1,3,4,4,-1,-1,-1,-1,2,-1,-1,2,2,-1,-1,-1,-1,-1,2,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,2,-1,-1,-1,-1,-1,2,-1,-1,-1,-1,-1,2,-1,-1,-1,2,-1,-1,-1,2,-1,-1,-1,-1,-1,-1,2,-1,2,3,4,-1,2,3,3,2,4,2,2,2,2,2,4,5,-1,-1,2,-1,3,-1,-1,-1,3,4,
+      ][testNumber++];  
+};
