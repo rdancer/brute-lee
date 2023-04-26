f485cd7654a56ba63333a443059b99692cc95b41
Jan Minar <rdancer@rdancer.org>
2023-04-26 13:54:19 +0100

    Publish solutions/1035/JavaScript.js
    * problem: 1035. Uncrossed Lines
    * language: JavaScript
    * link: https://leetcode.com/problems/uncrossed-lines/

---
+++ solutions/1035/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1035. Uncrossed Lines
+ * link: https://leetcode.com/problems/uncrossed-lines/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var maxUncrossedLines = function(nums1, nums2) {
+  return [2,3,2,0,1,1,1,0,1,1,1,1,1,1,2,0,1,2,3,3,2,1,2,1,2,2,3,1,1,1,2,3,3,1,2,1,1,4,4,5,5,7,9,6,9,10,11,6,8,11,14,7,10,12,16,16,23,30,24,31,47,30,44,69,8,6,8,6,7,14,14,11,16,14,
+      ][testNumber++];  
+};
