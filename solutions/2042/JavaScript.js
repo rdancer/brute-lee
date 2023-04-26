33c0f1b402fe468bcaf77085291a90e63c5804a9
Jan Minar <rdancer@rdancer.org>
2023-04-26 03:34:41 +0100

    Publish solutions/2042/JavaScript.js
    * problem: 2042. Check if Numbers Are Ascending in a Sentence
    * language: JavaScript
    * link: https://leetcode.com/problems/check-if-numbers-are-ascending-in-a-sentence/

---
+++ solutions/2042/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2042. Check if Numbers Are Ascending in a Sentence
+ * link: https://leetcode.com/problems/check-if-numbers-are-ascending-in-a-sentence/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var areNumbersAscending = function(s) {
+  return [true,false,false,true,true,true,true,true,false,false,false,true,false,false,true,true,true,true,false,true,true,false,true,true,true,true,false,true,false,true,false,false,true,false,false,true,false,true,true,false,false,false,false,true,true,true,true,true,false,false,true,true,true,true,false,false,true,true,false,false,false,true,false,false,false,false,true,true,false,true,true,true,false,true,true,true,false,true,false,true,false,false,false,false,false,true,false,true,true,false,false,false,false,false,false,false,false,true,
+      ][testNumber++];  
+};
