abd9ba5ca21cdb19c58f7984389b61337d14d49a
Jan Minar <rdancer@rdancer.org>
2023-04-27 16:00:55 +0100

    Publish solutions/1801/JavaScript.js
    * problem: 1801. Number of Orders in the Backlog
    * language: JavaScript
    * link: https://leetcode.com/problems/number-of-orders-in-the-backlog/

---
+++ solutions/1801/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1801. Number of Orders in the Backlog
+ * link: https://leetcode.com/problems/number-of-orders-in-the-backlog/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} orders
+ * @return {number}
+ */
+var getNumberOfBacklogOrders = function(orders) {
+  return [6,999999984,39,34,22,72,82,57,37,83,60,52,69,73,48,129,106,61,6,77,19,42,4,43,71,47,61,31,62,34,40,81,32,89,28,57,63,123,123,92,44,35,74,6,19,54,102,37,24,23,40,50,83062672,571132385,558895984,716878516,767013302,867395764,154155928,623185516,927884088,821875061,225818155,808789839,152802081,212260148,782247316,630599620,715013129,
+      ][testNumber++];  
+};
