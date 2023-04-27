dee7a9c6a428ccc1123cde09606e6fdc92cded33
Jan Minar <rdancer@rdancer.org>
2023-04-27 03:13:55 +0100

    Publish solutions/2360/JavaScript.js
    * problem: 2360. Longest Cycle in a Graph
    * language: JavaScript
    * link: https://leetcode.com/problems/longest-cycle-in-a-graph/

---
+++ solutions/2360/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2360. Longest Cycle in a Graph
+ * link: https://leetcode.com/problems/longest-cycle-in-a-graph/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} edges
+ * @return {number}
+ */
+var longestCycle = function(edges) {
+  return [3,-1,-1,3,-1,4,2,-1,3,2,5,3,5,2,3,2,3,4,3,2,2,-1,3,-1,10,46,45,59,29,11,-1,19,-1,40,2,11,38,66,15,3,-1,11,11,-1,68,-1,692,65,-1,101,-1,-1,505,-1,133,-1,840,79,450,106,568,57,406,43242,10672,28577,-1,85341,28886,37266,96469,4,2,4,2,100000,3,3,4,3,3,
+      ][testNumber++];  
+};
