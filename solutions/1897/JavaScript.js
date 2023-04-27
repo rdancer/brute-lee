d1f2f0c4359bb587337680fbfdd08ec5e375c4d7
Jan Minar <rdancer@rdancer.org>
2023-04-27 02:56:21 +0100

    Publish solutions/1897/JavaScript.js
    * problem: 1897. Redistribute Characters to Make All Strings Equal
    * language: JavaScript
    * link: https://leetcode.com/problems/redistribute-characters-to-make-all-strings-equal/

---
+++ solutions/1897/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1897. Redistribute Characters to Make All Strings Equal
+ * link: https://leetcode.com/problems/redistribute-characters-to-make-all-strings-equal/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string[]} words
+ * @return {boolean}
+ */
+var makeEqual = function(words) {
+  return [true,false,true,true,false,true,false,true,false,true,false,false,true,false,true,true,false,false,false,false,false,false,false,false,true,false,true,true,false,false,false,false,true,true,true,true,true,true,true,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,true,true,true,true,true,true,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,true,
+      ][testNumber++];  
+};
