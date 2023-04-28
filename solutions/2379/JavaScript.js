38be57f09cf4141a3a02cd77e8048675f8d4342b
Jan Minar <rdancer@rdancer.org>
2023-04-28 04:03:00 +0100

    Publish solutions/2379/JavaScript.js
    * problem: 2379. Minimum Recolors to Get K Consecutive Black Blocks
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-recolors-to-get-k-consecutive-black-blocks/

---
+++ solutions/2379/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2379. Minimum Recolors to Get K Consecutive Black Blocks
+ * link: https://leetcode.com/problems/minimum-recolors-to-get-k-consecutive-black-blocks/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} blocks
+ * @param {number} k
+ * @return {number}
+ */
+var minimumRecolors = function(blocks, k) {
+  return [3,0,0,3,1,0,1,4,6,0,6,11,10,19,7,1,3,13,14,17,30,10,4,27,29,2,25,28,21,7,9,0,29,0,4,4,29,29,3,28,1,6,0,6,15,3,16,37,5,39,13,36,7,1,6,43,44,18,33,5,4,24,11,49,7,45,17,30,37,2,32,44,6,10,37,19,26,25,45,27,14,38,24,25,13,33,37,12,22,42,20,31,16,22,18,17,21,20,23,22,20,20,21,15,21,18,29,23,23,22,20,22,0,100,0,0,83,0,1,0,0,1,
+      ][testNumber++];  
+};
