c076a55a3c037bfc5de162357a7e8c709b005753
Jan Minar <rdancer@rdancer.org>
2023-04-28 17:14:11 +0100

    Publish solutions/844/JavaScript.js
    * problem: 844. Backspace String Compare
    * language: JavaScript
    * link: https://leetcode.com/problems/backspace-string-compare/

---
+++ solutions/844/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 844. Backspace String Compare
+ * link: https://leetcode.com/problems/backspace-string-compare/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var backspaceCompare = function(s, t) {
+  return [true,true,false,true,true,false,true,false,true,true,true,false,true,false,true,false,true,true,true,true,true,false,true,false,true,true,true,true,true,false,true,true,true,false,true,true,true,false,true,false,true,false,true,true,false,true,false,false,true,false,true,true,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,true,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,true,true,false,true,false,true,false,true,true,true,false,true,false,false,true,false,true,
+      ][testNumber++];  
+};
