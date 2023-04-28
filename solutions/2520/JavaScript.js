fd1c11694d2a7b64fb2567b08c5adbb653dbc2ad
Jan Minar <rdancer@rdancer.org>
2023-04-28 04:16:06 +0100

    Publish solutions/2520/JavaScript.js
    * problem: 2520. Count the Digits That Divide a Number
    * language: JavaScript
    * link: https://leetcode.com/problems/count-the-digits-that-divide-a-number/

---
+++ solutions/2520/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2520. Count the Digits That Divide a Number
+ * link: https://leetcode.com/problems/count-the-digits-that-divide-a-number/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var countDigits = function(num) {
+  return [1,2,4,0,0,2,1,0,1,0,2,1,0,2,1,4,1,5,3,1,3,4,4,1,5,4,4,4,1,5,3,2,1,2,1,3,4,1,4,5,2,6,2,3,1,0,4,3,2,2,5,3,3,9,9,5,
+      ][testNumber++];  
+};
