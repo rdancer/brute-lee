8425c64561cf6e88de10aca181edbea72250f024
Jan Minar <rdancer@rdancer.org>
2023-04-29 11:15:01 +0100

    Publish solutions/52/JavaScript.js
    * problem: 52. N-Queens II
    * language: JavaScript
    * link: https://leetcode.com/problems/n-queens-ii/

---
+++ solutions/52/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 52. N-Queens II
+ * link: https://leetcode.com/problems/n-queens-ii/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var totalNQueens = function(n) {
+  return [2,1,0,0,10,4,40,92,352,
+      ][testNumber++];  
+};
