604ecc7d48d8b896e9358846575334dac9c94754
Jan Minar <rdancer@rdancer.org>
2023-04-29 10:49:10 +0100

    Publish solutions/1239/JavaScript.js
    * problem: 1239. Maximum Length of a Concatenated String with Unique Characters
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-length-of-a-concatenated-string-with-unique-characters/

---
+++ solutions/1239/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1239. Maximum Length of a Concatenated String with Unique Characters
+ * link: https://leetcode.com/problems/maximum-length-of-a-concatenated-string-with-unique-characters/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string[]} arr
+ * @return {number}
+ */
+var maxLength = function(arr) {
+  return [4,6,26,0,16,16,26,8,12,9,16,0,13,6,11,6,9,4,10,16,20,20,4,13,13,18,16,7,8,15,16,1,19,5,1,19,20,15,17,20,20,11,0,21,5,18,19,20,1,20,18,10,9,21,20,18,19,19,18,7,20,19,20,19,18,19,18,19,19,17,20,19,22,20,18,20,18,20,12,19,19,21,20,21,21,11,0,8,18,
+      ][testNumber++];  
+};
