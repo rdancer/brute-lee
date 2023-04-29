f8207905a1d8d964666b91e1409ffef7b7c2bb71
Jan Minar <rdancer@rdancer.org>
2023-04-29 03:50:59 +0100

    Publish solutions/2016/JavaScript.js
    * problem: 2016. Maximum Difference Between Increasing Elements
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-difference-between-increasing-elements/

---
+++ solutions/2016/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2016. Maximum Difference Between Increasing Elements
+ * link: https://leetcode.com/problems/maximum-difference-between-increasing-elements/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumDifference = function(nums) {
+  return [4,-1,9,9,55,57,22,60,4210,2313,849,840,1706,975,2916,4196,1980,4040,1647,926,3039,355,4122,2275,4477,503,3987,1220,3701,2652,3582,223,4684,808,1508,4008,4166,1837,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,995685192,-1,1,999999999,-1,-1,
+      ][testNumber++];  
+};
