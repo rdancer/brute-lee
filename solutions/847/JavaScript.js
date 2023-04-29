51d558d3f46a389ceeddb8172a458ee4a2c0ff81
Jan Minar <rdancer@rdancer.org>
2023-04-29 10:19:18 +0100

    Publish solutions/847/JavaScript.js
    * problem: 847. Shortest Path Visiting All Nodes
    * language: JavaScript
    * link: https://leetcode.com/problems/shortest-path-visiting-all-nodes/

---
+++ solutions/847/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 847. Shortest Path Visiting All Nodes
+ * link: https://leetcode.com/problems/shortest-path-visiting-all-nodes/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} graph
+ * @return {number}
+ */
+var shortestPathLength = function(graph) {
+  return [4,4,0,1,2,2,3,6,9,3,3,3,3,4,4,5,4,5,5,6,6,6,8,8,8,7,7,10,8,11,11,10,10,12,12,12,11,12,13,15,14,17,14,15,14,7,11,11,11,11,11,
+      ][testNumber++];  
+};
