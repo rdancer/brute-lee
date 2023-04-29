6acb66d98cf194d90400f88d45346eb3b757db1d
Jan Minar <rdancer@rdancer.org>
2023-04-29 02:19:20 +0100

    Publish solutions/1601/JavaScript.js
    * problem: 1601. Maximum Number of Achievable Transfer Requests
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-number-of-achievable-transfer-requests/

---
+++ solutions/1601/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1601. Maximum Number of Achievable Transfer Requests
+ * link: https://leetcode.com/problems/maximum-number-of-achievable-transfer-requests/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @param {number[][]} requests
+ * @return {number}
+ */
+var maximumRequests = function(n, requests) {
+  return [5,3,4,4,11,13,16,5,2,1,12,8,7,13,7,4,2,14,10,6,2,8,6,5,4,11,0,1,9,10,5,6,1,1,0,8,7,2,11,1,1,4,2,5,10,0,2,4,6,0,7,1,1,2,0,11,3,2,5,0,3,5,7,0,5,0,2,4,0,1,3,1,3,0,1,0,0,4,1,6,2,0,4,0,0,0,0,1,2,0,0,0,5,3,0,2,0,2,1,0,0,1,2,0,2,0,0,12,15,16,15,16,16,15,15,14,12,
+      ][testNumber++];  
+};
