1fb91208c35723b6879197c48e6a73a175ed3581
Jan Minar <rdancer@rdancer.org>
2023-04-30 02:58:42 +0100

    Publish solutions/752/JavaScript.js
    * problem: 752. Open the Lock
    * language: JavaScript
    * link: https://leetcode.com/problems/open-the-lock/

---
+++ solutions/752/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 752. Open the Lock
+ * link: https://leetcode.com/problems/open-the-lock/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string[]} deadends
+ * @param {string} target
+ * @return {number}
+ */
+var openLock = function(deadends, target) {
+  return [6,1,-1,-1,3,3,5,-1,2,-1,4,6,5,-1,7,9,4,6,3,6,3,6,4,5,11,10,7,9,8,4,9,9,4,9,16,12,11,12,12,13,12,11,10,13,-1,0,6,-1,
+      ][testNumber++];  
+};
