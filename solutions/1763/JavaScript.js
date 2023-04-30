c7bc7185bd81efdf7847962234cff759e9b4e322
Jan Minar <rdancer@rdancer.org>
2023-04-30 19:53:41 +0100

    Publish solutions/1763/JavaScript.js
    * problem: 1763. Longest Nice Substring
    * language: JavaScript
    * link: https://leetcode.com/problems/longest-nice-substring/

---
+++ solutions/1763/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1763. Longest Nice Substring
+ * link: https://leetcode.com/problems/longest-nice-substring/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestNiceSubstring = function(s) {
+  return ["aAa","Bb","","dD","","Nn","hH","","cChH","qQ","BebjJE","RdEcDerC","BubUYy","ijIJ","aAztTkZK","nN","qOocYChQHnyN","wW","uU","NUvgIjSnsVGJiu","oGseROdSrgzEDZ","dfFmuHDMrEeRhU","GZjcWEwkCJzKge","hICNcHMiqRoXgGEnuerOmUQx","LeEl","NEne","ddD","Xx","gGZzG","Ff","Ii","aaA","lL","MmM","Ss","Dd","Qq","zozZO","Ii","ioIIOOi","Ww","yYY","GdgD","gG","BbB","","Yy","Cc","wW","DtdT","BBEHBeBBhb","","PpHhRr","Pp","qQ","","BTtbT","","ijeIKJkFEf","","yxIyIVbPOxOByPiIYfxhVBbBiPPHDYOpOxVVdXVOpVdIvEVbyOifVViFOOVbxfXooIohVYyHixEfvxfxXxexVifVIxyOVIyFHyvO","","kIkKi","Jj","","gHbhrRYyGB","DaNndPqQpA","","IGtSGIzklvRYmTtstTrTZiKrLZyVdMlZicImIqCTyIdcDQzgqt","WLlw","","ghIIVeoshBhrzYoLivBfBeFqGevIeFRZiGzHruiIeGVhZZrGLpLnGyzlGPGOehoBUnSyorfpELLbENEFULnrbPUliQgLZivUFIHy","eyeYfEFy",
+      ][testNumber++];  
+};
