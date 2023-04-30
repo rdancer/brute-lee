5320977f057c9e6e6f86b2772df9dd197598df57
Jan Minar <rdancer@rdancer.org>
2023-04-30 02:46:55 +0100

    Publish solutions/668/JavaScript.js
    * problem: 668. Kth Smallest Number in Multiplication Table
    * language: JavaScript
    * link: https://leetcode.com/problems/kth-smallest-number-in-multiplication-table/

---
+++ solutions/668/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 668. Kth Smallest Number in Multiplication Table
+ * link: https://leetcode.com/problems/kth-smallest-number-in-multiplication-table/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} m
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+var findKthNumber = function(m, n, k) {
+  return [3,6,3,3,2,2,81,2,2,312,24,126,437,32,8,168,36,375,165,300,152,34,35,12,24,165,972,30,783,66,399,437,24,16,868,72,92,630,126,30,306,390,143,133,616,5,3,115,105,204,342,96,62,8,18,9,182,8,23,31666344,2840762,2673783,7242445,23437314,291460318,14027605,35623896,27049344,59930994,106294968,
+      ][testNumber++];  
+};
