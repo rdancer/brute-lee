2c73c7788c39a790ad8216b69e1e06d62149e145
Jan Minar <rdancer@rdancer.org>
2023-05-01 03:28:33 +0100

    Publish solutions/2543/JavaScript.js
    * problem: 2543. Check if Point Is Reachable
    * language: JavaScript
    * link: https://leetcode.com/problems/check-if-point-is-reachable/

---
+++ solutions/2543/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2543. Check if Point Is Reachable
+ * link: https://leetcode.com/problems/check-if-point-is-reachable/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} targetX
+ * @param {number} targetY
+ * @return {boolean}
+ */
+var isReachable = function(targetX, targetY) {
+  return [false,true,false,false,true,true,true,true,true,true,true,true,true,true,false,true,false,true,true,true,true,true,true,true,true,true,false,true,true,true,true,true,true,true,true,false,true,true,true,true,false,true,false,true,true,true,false,true,true,true,true,false,true,true,true,true,true,false,true,true,
+      ][testNumber++];  
+};
