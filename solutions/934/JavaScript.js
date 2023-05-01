e13590d4d470fc597763068cebb25fb84a256396
Jan Minar <rdancer@rdancer.org>
2023-05-01 09:50:06 +0100

    Publish solutions/934/JavaScript.js
    * problem: 934. Shortest Bridge
    * language: JavaScript
    * link: https://leetcode.com/problems/shortest-bridge/

---
+++ solutions/934/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 934. Shortest Bridge
+ * link: https://leetcode.com/problems/shortest-bridge/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var shortestBridge = function(grid) {
+  return [1,2,1,1,1,3,1,3,2,1,1,1,1,4,1,1,1,1,1,1,1,1,2,1,4,1,1,1,1,2,1,7,5,1,1,1,1,1,6,1,1,1,1,1,1,1,1,1,8,1,1,1,1,1,1,14,1,1,4,1,6,1,1,1,3,1,1,9,1,1,1,1,1,1,5,1,1,2,1,10,6,13,1,1,31,1,1,8,1,1,44,1,1,18,1,1,1,
+      ][testNumber++];  
+};
