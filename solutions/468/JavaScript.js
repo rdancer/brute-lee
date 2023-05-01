08124a6886dfc7b3b54109e36c2789e78d1b7980
Jan Minar <rdancer@rdancer.org>
2023-05-01 10:06:20 +0100

    Publish solutions/468/JavaScript.js
    * problem: 468. Validate IP Address
    * language: JavaScript
    * link: https://leetcode.com/problems/validate-ip-address/

---
+++ solutions/468/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 468. Validate IP Address
+ * link: https://leetcode.com/problems/validate-ip-address/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} queryIP
+ * @return {string}
+ */
+var validIPAddress = function(queryIP) {
+  return ["IPv4","IPv6","Neither","Neither","Neither","Neither","IPv4","Neither","Neither","IPv6","Neither","Neither","Neither","Neither","Neither","Neither","Neither","Neither","IPv4","Neither","Neither","Neither","Neither","IPv6","Neither","IPv6","IPv6","Neither","Neither","Neither","Neither","IPv6","IPv6","Neither","IPv6","IPv6","Neither","Neither","IPv6","Neither","Neither","Neither","Neither","Neither","IPv6","Neither","Neither","Neither","Neither","IPv6","Neither","Neither","IPv6","Neither","Neither","IPv6","Neither","Neither","IPv6","Neither","IPv6","Neither","IPv4","Neither","Neither","IPv4","IPv6","IPv4","IPv4","Neither","Neither","Neither","Neither","Neither",
+      ][testNumber++];  
+};
