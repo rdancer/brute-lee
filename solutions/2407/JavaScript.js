e660b04207b5cf8641d62a2b61a4a477997cc904
Jan Minar <rdancer@rdancer.org>
2023-05-02 18:07:07 +0100

    Publish solutions/2407/JavaScript.js
    * problem: 2407. Longest Increasing Subsequence II
    * language: JavaScript
    * link: https://leetcode.com/problems/longest-increasing-subsequence-ii/

---
+++ solutions/2407/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2407. Longest Increasing Subsequence II
+ * link: https://leetcode.com/problems/longest-increasing-subsequence-ii/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var lengthOfLIS = function(nums, k) {
+  return [5,4,1,1,1,1,1,4,2,1,2,2,1,2,2,1,3,2,3,1,2,1,1,1,1,1,3,1,2,2,1,1,2,2,2,1,2,1,1,2,2,2,2,2,3,4,3,3,4,3,4,4,4,2,3,2,3,3,2,2,3,2,2,3,3,4,3,3,2,4,4,100000,2,2,1,63,3,4,3020,1,50000,89,50001,
+      ][testNumber++];  
+};
