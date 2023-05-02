055ed063b77228582f6db28b756d7a42f8aff812
Jan Minar <rdancer@rdancer.org>
2023-05-02 14:45:24 +0100

    Publish solutions/1959/JavaScript.js
    * problem: 1959. Minimum Total Space Wasted With K Resizing Operations
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-total-space-wasted-with-k-resizing-operations/

---
+++ solutions/1959/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1959. Minimum Total Space Wasted With K Resizing Operations
+ * link: https://leetcode.com/problems/minimum-total-space-wasted-with-k-resizing-operations/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minSpaceWastedKResizing = function(nums, k) {
+  return [10,10,15,0,179,165,0,68,44,30,68,0,36,41,112,0,116,0,0,86,105,97,16,6,71,0,119,0,51,0,1,42,0,45,59,936,12629,16783,5542,792,2258,5405,0,4694,204,1303,10161,1965,2406,0,3369087,43433025,2674021,38087633,4116442,17665990,39963431,1978254,22614192,24749125,59122104,14910726,57670965,44805170,46036341,19396243,12071695,0,0,32989376,0,13265,
+      ][testNumber++];  
+};
