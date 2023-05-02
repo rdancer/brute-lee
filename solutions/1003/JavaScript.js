d319e65ab0a9965e29a1c3fb9a0e6dee4bcc3547
Jan Minar <rdancer@rdancer.org>
2023-05-02 15:05:17 +0100

    Publish solutions/1003/JavaScript.js
    * problem: 1003. Check If Word Is Valid After Substitutions
    * language: JavaScript
    * link: https://leetcode.com/problems/check-if-word-is-valid-after-substitutions/

---
+++ solutions/1003/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1003. Check If Word Is Valid After Substitutions
+ * link: https://leetcode.com/problems/check-if-word-is-valid-after-substitutions/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+  return [true,true,false,false,false,true,true,true,true,true,false,false,false,false,true,false,false,true,true,true,true,false,false,false,false,false,false,true,false,false,false,false,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,
+      ][testNumber++];  
+};
