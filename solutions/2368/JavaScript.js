ec459917b19e6407754f4d57141b8eeae19a0551
Jan Minar <rdancer@rdancer.org>
2023-05-02 17:49:04 +0100

    Publish solutions/2368/JavaScript.js
    * problem: 2368. Reachable Nodes With Restrictions
    * language: JavaScript
    * link: https://leetcode.com/problems/reachable-nodes-with-restrictions/

---
+++ solutions/2368/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 2368. Reachable Nodes With Restrictions
+ * link: https://leetcode.com/problems/reachable-nodes-with-restrictions/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number[]} restricted
+ * @return {number}
+ */
+var reachableNodes = function(n, edges, restricted) {
+  return [4,3,1,1,2,1,1,1,1,1,1,1,1,1,4,1,1,1,8,1,1,4,2,1,3,1,8,2,11,4,3,1,10,10,1,1,5,2,1,3,8,1,1,1,18,1,78,10,10,37,6,20,1,42,3,6,1,18,1,99999,1,50000,
+      ][testNumber++];  
+};
