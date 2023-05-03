e451b7066ddcee64357674806a6b38d41b57d958
Jan Minar <rdancer@rdancer.org>
2023-05-03 02:30:49 +0100

    Publish solutions/2270/JavaScript.js
    * problem: 2270. Number of Ways to Split Array
    * language: JavaScript
    * link: https://leetcode.com/problems/number-of-ways-to-split-array/

---
+++ solutions/2270/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2270. Number of Ways to Split Array
+ * link: https://leetcode.com/problems/number-of-ways-to-split-array/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var waysToSplitArray = function(nums) {
+  return [2,2,0,1,1,0,1,1,1,0,0,1,1,3,7,5,2,1,1,5,1,4,0,1,0,0,2,1,0,1,4,2,6,0,5,4,1,1,4,7,7,5,6,7,0,3,5,9,13,10,10,10,13,11,10,6,12,5,11,5,13,4,10,8,20,14,16,18,17,4,17,2,13,18,9,26,18,11,4,23,42,26,83,65,141,164,227,179,1336,938,3952,379,11294,61833,50000,50000,99999,49999,99999,70710,
+      ][testNumber++];  
+};
