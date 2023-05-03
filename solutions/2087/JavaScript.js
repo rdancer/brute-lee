e55968ed0c84767044b553ee19776f4b4231ea53
Jan Minar <rdancer@rdancer.org>
2023-05-03 10:15:30 +0100

    Publish solutions/2087/JavaScript.js
    * problem: 2087. Minimum Cost Homecoming of a Robot in a Grid
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-cost-homecoming-of-a-robot-in-a-grid/

---
+++ solutions/2087/JavaScript.js
@@ -0,0 +1,18 @@
+/*
+ * language: JavaScript
+ * problem: 2087. Minimum Cost Homecoming of a Robot in a Grid
+ * link: https://leetcode.com/problems/minimum-cost-homecoming-of-a-robot-in-a-grid/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} startPos
+ * @param {number[]} homePos
+ * @param {number[]} rowCosts
+ * @param {number[]} colCosts
+ * @return {number}
+ */
+var minCost = function(startPos, homePos, rowCosts, colCosts) {
+  return [18,0,9,14,30,26,62,8,15,26,61,25,16,40,236,45,8606,5681,9670,8385,7038,7391,9743,9182,7700,8722,134,9706,9969,9548,8448,0,5698,5986,10247,1062,1233,3151,3120,13594,16473,2674,14166,9041,451,17651,1007,9496,6270,7189,16431,7327,409,9476,45282,33355,2197,25903,18389,199413,111133,73366,195801,198196,246560,348467,270576,0,1999980000,
+      ][testNumber++];  
+};
