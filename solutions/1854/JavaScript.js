4d1d5be147f86b8133ec09d50d6b8ea25d952831
Jan Minar <rdancer@rdancer.org>
2023-05-03 09:05:13 +0100

    Publish solutions/1854/JavaScript.js
    * problem: 1854. Maximum Population Year
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-population-year/

---
+++ solutions/1854/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1854. Maximum Population Year
+ * link: https://leetcode.com/problems/maximum-population-year/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} logs
+ * @return {number}
+ */
+var maximumPopulation = function(logs) {
+  return [1993,1960,2000,2022,2005,1994,2025,2032,2011,2018,1961,1970,1983,1991,2004,1998,1982,1996,1972,1988,2028,2006,1995,2041,2039,2010,2026,2004,1997,2008,2021,2008,1994,2023,2008,2025,1991,2000,2008,1993,2027,2008,2000,1950,2000,1950,1982,1971,2049,2049,1950,1998,
+      ][testNumber++];  
+};
