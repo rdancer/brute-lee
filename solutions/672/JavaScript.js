060da58e408d58a41bbd0571ccd2ddc16dc86a25
Jan Minar <rdancer@rdancer.org>
2023-05-03 08:25:25 +0100

    Publish solutions/672/JavaScript.js
    * problem: 672. Bulb Switcher II
    * language: JavaScript
    * link: https://leetcode.com/problems/bulb-switcher-ii/

---
+++ solutions/672/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 672. Bulb Switcher II
+ * link: https://leetcode.com/problems/bulb-switcher-ii/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @param {number} presses
+ * @return {number}
+ */
+var flipLights = function(n, presses) {
+  return [2,3,4,1,2,2,1,4,8,7,1,8,8,8,8,8,4,7,8,8,1,4,7,8,8,8,8,8,4,7,8,8,8,8,8,4,7,8,8,8,4,7,8,8,8,8,4,7,8,8,8,8,8,4,8,8,8,8,4,7,8,8,8,8,8,8,8,8,8,8,8,2,4,8,8,4,7,8,8,8,
+      ][testNumber++];  
+};
