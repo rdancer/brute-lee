d5b319cea0a3cf168af6049e9a74f97112ce69fa
Jan Minar <rdancer@rdancer.org>
2023-05-03 09:35:46 +0100

    Publish solutions/1013/JavaScript.js
    * problem: 1013. Partition Array Into Three Parts With Equal Sum
    * language: JavaScript
    * link: https://leetcode.com/problems/partition-array-into-three-parts-with-equal-sum/

---
+++ solutions/1013/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1013. Partition Array Into Three Parts With Equal Sum
+ * link: https://leetcode.com/problems/partition-array-into-three-parts-with-equal-sum/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var canThreePartsEqualSum = function(arr) {
+  return [true,false,true,false,true,false,false,false,true,false,true,true,false,false,true,false,false,false,true,true,true,true,true,true,true,false,false,true,false,false,false,true,false,true,false,false,false,true,false,false,false,true,false,false,false,true,false,false,false,false,false,true,false,true,true,false,true,true,false,true,true,true,true,false,false,true,true,true,true,true,false,false,
+      ][testNumber++];  
+};
