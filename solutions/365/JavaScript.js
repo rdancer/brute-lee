7c2a3c674fa08b2e9e8f6474522e0a9b7c57acdc
Jan Minar <rdancer@rdancer.org>
2023-05-03 02:39:25 +0100

    Publish solutions/365/JavaScript.js
    * problem: 365. Water and Jug Problem
    * language: JavaScript
    * link: https://leetcode.com/problems/water-and-jug-problem/

---
+++ solutions/365/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 365. Water and Jug Problem
+ * link: https://leetcode.com/problems/water-and-jug-problem/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} jug1Capacity
+ * @param {number} jug2Capacity
+ * @param {number} targetCapacity
+ * @return {boolean}
+ */
+var canMeasureWater = function(jug1Capacity, jug2Capacity, targetCapacity) {
+  return [true,false,true,false,true,false,true,false,true,true,true,true,false,true,true,true,true,true,true,true,true,false,true,true,true,true,true,false,
+      ][testNumber++];  
+};
