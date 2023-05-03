be31f488a6877f894d0939df5cf349eae3c15d1d
Jan Minar <rdancer@rdancer.org>
2023-05-03 21:11:40 +0100

    Publish solutions/2053/JavaScript.js
    * problem: 2053. Kth Distinct String in an Array
    * language: JavaScript
    * link: https://leetcode.com/problems/kth-distinct-string-in-an-array/

---
+++ solutions/2053/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2053. Kth Distinct String in an Array
+ * link: https://leetcode.com/problems/kth-distinct-string-in-an-array/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string[]} arr
+ * @param {number} k
+ * @return {string}
+ */
+var kthDistinct = function(arr, k) {
+  return ["a","aaa","","c","c","y","snx","cq","ci","jfqjc","dbty","dj","xx","ucnd","vb","ml","fjw","eiw","k","yaubc","db","m","axkx","grle","kdx","zuqvq","haex","yqur","kquu","dq","ukj","ktcbx","dfy","q","kywf","jtrcp","","ehz","","ggb","ev","uqcd","bpl","vsm","","","","","","","wzwqe","askej","sdhcv","ygodp","fpjzw","qne","ixulb","lga","zqk","muccg","","","","kv","ktj","",
+      ][testNumber++];  
+};
