baafe965ccea3a729f022a6a063ef7a71fc1c349
Jan Minar <rdancer@rdancer.org>
2023-05-04 02:35:20 +0100

    Publish solutions/793/JavaScript.js
    * problem: 793. Preimage Size of Factorial Zeroes Function
    * language: JavaScript
    * link: https://leetcode.com/problems/preimage-size-of-factorial-zeroes-function/

---
+++ solutions/793/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 793. Preimage Size of Factorial Zeroes Function
+ * link: https://leetcode.com/problems/preimage-size-of-factorial-zeroes-function/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} k
+ * @return {number}
+ */
+var preimageSizeFZF = function(k) {
+  return [5,0,5,0,0,0,0,0,5,5,5,5,5,0,0,0,0,0,5,5,5,5,5,0,0,0,0,0,0,0,0,0,0,5,5,5,5,5,5,5,5,5,5,5,
+      ][testNumber++];  
+};
