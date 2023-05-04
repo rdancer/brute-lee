74e57e08d6149ee900fd6d33b88f12e7a85fb3ce
Jan Minar <rdancer@rdancer.org>
2023-05-04 02:45:54 +0100

    Publish solutions/2451/JavaScript.js
    * problem: 2451. Odd String Difference
    * language: JavaScript
    * link: https://leetcode.com/problems/odd-string-difference/

---
+++ solutions/2451/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2451. Odd String Difference
+ * link: https://leetcode.com/problems/odd-string-difference/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string[]} words
+ * @return {string}
+ */
+var oddString = function(words) {
+  return ["abc","bob","aba","abb","bbaaa","aabbb","bbbbbaaaaa","abbbabbbba","aaaabbbbbbaaabaaaabb","baaabaaaaaabbabbbbab","kjk","jkn","knjjm","lklnj","jnkkjmjjmn","mkmkjklmmj","mjjjlnkllkkklnklkmmk","mmnlmmmkmnlnmlmkjjnk","ddd","ddd","eewdw","wdeee","eewweedddd","ddwewweewe","dddeewdwddwdddewdwew","ddddeweeeeewddwddwee","sku","igu","yqyyo","fuorn","jalalxvqlv","aslmvdzcyo","tgkbvlkwnwebhjmsgfbl","laamgesdaztrncvmsxij","ab","za","abcdefghijklmnopqrst","abd","alm",
+      ][testNumber++];  
+};
