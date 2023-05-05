d966a1409cd1e3b5102b4ffb86aaef5852502438
Jan Minar <rdancer@rdancer.org>
2023-05-05 03:16:26 +0100

    Publish solutions/621/JavaScript.js
    * problem: 621. Task Scheduler
    * language: JavaScript
    * link: https://leetcode.com/problems/task-scheduler/

---
+++ solutions/621/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 621. Task Scheduler
+ * link: https://leetcode.com/problems/task-scheduler/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {character[]} tasks
+ * @param {number} n
+ * @return {number}
+ */
+var leastInterval = function(tasks, n) {
+  return [8,6,16,10,104,4,2,4,5,3,4,12,5,31,6,1,1,2,5,12,7,18,19,52,4256,1000,9793,6841,9594,1000,1000,10000,29821,66177,17987,66341,84161,76286,62761,36051,52651,10000,29821,66177,17987,66341,84161,76286,62761,36051,52651,10000,29821,66177,17987,66341,84161,76286,62761,36051,52651,100,1345,404,1153,523,911,1401,936,1140,404,
+      ][testNumber++];  
+};
