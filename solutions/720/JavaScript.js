fec299d5637154ba5748fc4421de9e72756a860c
Jan Minar <rdancer@rdancer.org>
2023-05-05 15:45:15 +0100

    Publish solutions/720/JavaScript.js
    * problem: 720. Longest Word in Dictionary
    * language: JavaScript
    * link: https://leetcode.com/problems/longest-word-in-dictionary/

---
+++ solutions/720/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 720. Longest Word in Dictionary
+ * link: https://leetcode.com/problems/longest-word-in-dictionary/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string[]} words
+ * @return {string}
+ */
+var longestWord = function(words) {
+  return ["world","apple","breakfast","latte","elephant","english","science","yodn","oiddm","eyj","jyhm","yyd","lel","ksed","jtgt","sgsn","ht","cypkw","otif","e","pax","utzhm","dhttn","gwo","ojo","vsw","tswux","ahxm","lemdqgqwumegvfexckxgiadwlc","lemdqgqwumegvfexckxgiadwlc","lemdqgqwumegvfexckxgiadwlc","lemdqgqwumegvfexckxgiadwlc","lemdqgqwumegvfexckxgiadwlc","dmmmcqvnt","xidlqihdd","kbbnapsip","qahgaauae","kbjdftpbd","jqmoepewqzbjzrcpofcmzeavyryoaa","cxkhcgbrleqmcdgafmukhijsctpsaa","hcztxcjepihnjuluvhquznasgbeuaa","vhzgjjwcjbdqebhpvccdgtiwftofaa","tgjrbffdokudfdunnbfyylfandlhaa","dgdxhzulhkvwzxnkzyzdmrcunumbaa","dcptsrnntcnxpyqlmpqfxqjlgsfdaa","fqljnfsocaugvhjmfdnyhyvykqadaa","ykbwpjdnfhtzpqvbdfrkpqkcsvzwaa","mmkvrvacgfpmchtslvcxtagkjcxmaa","unnmgrejgrdhyhtlnotakcfrumgcaa","ibhqjticranpkskjxxqiqejmziazaa","vojufuhnydfkfcgljzaptiyzgxfgaa","hmgtpyxwwayalsllxnebvefcrbxeaa","evpefhctjraxzxcytzcwyvneuvrjaa","klhpzrpebhteezmcznpcqqbnjqrbaa","dlywrnrvpeklabdrxferbdldbwvvaa","enstlljknxfhhzwlerfrgyvmcgseaa","hdzqaegfecyoamvtovxtdzkvjmqwaa","xtejfagzkewanazxiklpnsbjlulsaa","",
+      ][testNumber++];  
+};
