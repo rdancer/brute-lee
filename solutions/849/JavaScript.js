29267ba9d4b06f54cb70febc840db7a5f7697694
Jan Minar <rdancer@rdancer.org>
2023-05-05 15:31:00 +0100

    Publish solutions/849/JavaScript.js
    * problem: 849. Maximize Distance to Closest Person
    * language: JavaScript
    * link: https://leetcode.com/problems/maximize-distance-to-closest-person/

---
+++ solutions/849/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 849. Maximize Distance to Closest Person
+ * link: https://leetcode.com/problems/maximize-distance-to-closest-person/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} seats
+ * @return {number}
+ */
+var maxDistToClosest = function(seats) {
+  return [2,3,1,2,1,1,1,1,1,1,1,1,2,4,1,4,2,5,1,1,1,1,2,4,1,4,2,8,1,5,2,2,4,1,8,5,2,7,1,4,3,1,2,3,8,1,3,2,6,1,3,2,11,1,3,1,9,1,1,3,4,1,2,2,4,1,3,3,8,2,3,5,7,3,5,7,15,4,5,9,21,
+      ][testNumber++];  
+};
