dc7e0d9cabbe3a038b2f99e50f125b6cb83bc77f
Jan Minar <rdancer@rdancer.org>
2023-05-06 04:31:53 +0100

    Publish solutions/2366/JavaScript.js
    * problem: 2366. Minimum Replacements to Sort the Array
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-replacements-to-sort-the-array/

---
+++ solutions/2366/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2366. Minimum Replacements to Sort the Array
+ * link: https://leetcode.com/problems/minimum-replacements-to-sort-the-array/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumReplacement = function(nums) {
+  return [2,0,47,73,6,10,20,0,0,0,1,17748,16041,16493,16702,21989,18348,21249,18596,18422,16462,14332,22694,18003,21467,6721,20178,0,0,33058904,41363439,34797017,31844967,24474625,49469854,26183720,50746164,0,99998999900001,9760982177543,49889483726243,0,999999999,4999,5039242799,1512902187,0,
+      ][testNumber++];  
+};
