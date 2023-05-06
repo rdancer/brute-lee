9472fb8d2ce3d56869df1375fd1dcab5a5e90008
Jan Minar <rdancer@rdancer.org>
2023-05-06 17:42:47 +0100

    Publish solutions/2088/JavaScript.js
    * problem: 2088. Count Fertile Pyramids in a Land
    * language: JavaScript
    * link: https://leetcode.com/problems/count-fertile-pyramids-in-a-land/

---
+++ solutions/2088/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2088. Count Fertile Pyramids in a Land
+ * link: https://leetcode.com/problems/count-fertile-pyramids-in-a-land/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var countPyramids = function(grid) {
+  return [2,2,13,0,0,0,0,0,0,0,1,1,0,13,12,77,4,10,2821,0,698,207,26,317,20,26,102,98,0,70982,78776,65091,41146,46062,54520,41543,57451,25897,90611,46551,56035,104427,48500,48290,0,0,9233400,4816651,13047956,
+      ][testNumber++];  
+};
