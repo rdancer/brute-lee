c5c19d3b0b1cd95e3a705459754083d076cf93bf
Jan Minar <rdancer@rdancer.org>
2023-05-06 19:24:32 +0100

    Publish solutions/1662/JavaScript.js
    * problem: 1662. Check If Two String Arrays are Equivalent
    * language: JavaScript
    * link: https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/

---
+++ solutions/1662/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1662. Check If Two String Arrays are Equivalent
+ * link: https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string[]} word1
+ * @param {string[]} word2
+ * @return {boolean}
+ */
+var arrayStringsAreEqual = function(word1, word2) {
+  return [true,false,true,true,false,true,true,false,true,false,true,true,false,false,false,false,false,false,true,true,true,true,true,true,false,false,false,true,true,true,false,false,true,false,true,true,true,true,true,false,true,true,false,true,true,true,false,false,false,false,false,true,true,true,true,true,false,false,false,false,true,false,true,false,true,false,true,false,false,true,false,false,false,true,true,true,true,true,true,true,false,true,true,false,true,false,false,true,false,false,false,true,true,false,false,false,true,false,false,false,false,true,false,true,false,false,false,false,false,true,
+      ][testNumber++];  
+};
