20e443be2978ba20e933e782cd4d2c0288ba7abd
Jan Minar <rdancer@rdancer.org>
2023-05-08 19:25:55 +0100

    Publish solutions/2449/JavaScript.js
    * problem: 2449. Minimum Number of Operations to Make Arrays Similar
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-number-of-operations-to-make-arrays-similar/

---
+++ solutions/2449/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2449. Minimum Number of Operations to Make Arrays Similar
+ * link: https://leetcode.com/problems/minimum-number-of-operations-to-make-arrays-similar/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @param {number[]} target
+ * @return {number}
+ */
+var makeSimilar = function(nums, target) {
+  return [2,1,0,645,483,1149,942,712,309,446,941,558,268,1446,800,793,1329,929,1086,1038,2676,1395,819,24748295,12635422,9580035,9712657,20708083,32904921,58374306,1,2500000000,
+      ][testNumber++];  
+};
