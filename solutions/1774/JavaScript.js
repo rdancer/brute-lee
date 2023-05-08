60c57e2741119a7ee1cf63e318715d0adc7d3789
Jan Minar <rdancer@rdancer.org>
2023-05-08 16:32:06 +0100

    Publish solutions/1774/JavaScript.js
    * problem: 1774. Closest Dessert Cost
    * language: JavaScript
    * link: https://leetcode.com/problems/closest-dessert-cost/

---
+++ solutions/1774/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 1774. Closest Dessert Cost
+ * link: https://leetcode.com/problems/closest-dessert-cost/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} baseCosts
+ * @param {number[]} toppingCosts
+ * @param {number} target
+ * @return {number}
+ */
+var closestCost = function(baseCosts, toppingCosts, target) {
+  return [10,17,8,10,9,9,4,9,3,1,9,13,9,7,7,9,6,8,2,3,9,93,72,5,2,8,5,7,9,10309,7064,6,7372,4,9684,8,8,1794,4182,1871,3426,7302,6471,6345,3705,5685,1711,5370,2459,4183,5470,5711,1766,6898,9788,2877,9449,9459,334,4978,6968,1056,4556,3822,2613,8421,6506,4005,5171,9332,1232,9079,3259,5506,5003,5013,5069,7468,5003,744,5009,6053,5017,8,10,10246,8,120,4,
+      ][testNumber++];  
+};
