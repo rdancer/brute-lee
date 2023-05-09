4e21bab02c58293c202022474c1cbaaae3113e33
Jan Minar <rdancer@rdancer.org>
2023-05-09 18:54:11 +0100

    Publish solutions/1733/JavaScript.js
    * problem: 1733. Minimum Number of People to Teach
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-number-of-people-to-teach/

---
+++ solutions/1733/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 1733. Minimum Number of People to Teach
+ * link: https://leetcode.com/problems/minimum-number-of-people-to-teach/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @param {number[][]} languages
+ * @param {number[][]} friendships
+ * @return {number}
+ */
+var minimumTeachings = function(n, languages, friendships) {
+  return [1,2,0,4,4,0,0,3,2,1,0,4,5,1,2,1,1,8,2,0,4,1,2,6,8,8,4,10,7,0,4,4,9,8,8,8,7,4,7,2,8,4,7,8,13,7,5,13,10,8,11,4,14,1,3,8,12,5,4,0,1,3,1,1,11,7,8,37,2,0,2,12,1,8,2,1,1,9,0,2,12,15,1,9,11,4,24,40,18,2,23,27,6,4,4,1,8,1,17,1,
+      ][testNumber++];  
+};
