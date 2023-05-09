b359beb7ab6f9f9b1e5348edfb8694e0c4b6a646
Jan Minar <rdancer@rdancer.org>
2023-05-09 02:52:48 +0100

    Publish solutions/795/JavaScript.js
    * problem: 795. Number of Subarrays with Bounded Maximum
    * language: JavaScript
    * link: https://leetcode.com/problems/number-of-subarrays-with-bounded-maximum/

---
+++ solutions/795/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 795. Number of Subarrays with Bounded Maximum
+ * link: https://leetcode.com/problems/number-of-subarrays-with-bounded-maximum/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+var numSubarrayBoundedMax = function(nums, left, right) {
+  return [3,7,2,22,0,5,24,15,19,278,72,146,344,543,132,145,96,64,3091,564,4149,2858,294,11798,6073,2107,5198,91,7967,15944,14073,1891,1640,4930,97,3944,432,6743,341930,366637,374317,341565,385827,1250025000,1250025000,2123857725,
+      ][testNumber++];  
+};
