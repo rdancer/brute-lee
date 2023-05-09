16d3b85f9799347e7036427917ee88e6815801b9
Jan Minar <rdancer@rdancer.org>
2023-05-09 03:55:59 +0100

    Publish solutions/1461/JavaScript.js
    * problem: 1461. Check If a String Contains All Binary Codes of Size K
    * language: JavaScript
    * link: https://leetcode.com/problems/check-if-a-string-contains-all-binary-codes-of-size-k/

---
+++ solutions/1461/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1461. Check If a String Contains All Binary Codes of Size K
+ * link: https://leetcode.com/problems/check-if-a-string-contains-all-binary-codes-of-size-k/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {boolean}
+ */
+var hasAllCodes = function(s, k) {
+  return [true,true,false,true,false,true,true,false,true,false,true,false,false,false,true,false,true,false,false,false,false,false,true,true,true,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,false,true,false,false,true,true,false,false,true,true,false,true,false,true,true,false,false,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,false,false,false,false,false,false,false,true,true,false,true,false,false,true,false,false,false,false,false,false,false,false,false,false,true,true,true,true,false,false,true,true,true,true,true,true,false,true,true,true,true,true,
+      ][testNumber++];  
+};
