e11ef4b9d0a0c24ab5b4a320a960e23f29ff9cdc
Jan Minar <rdancer@rdancer.org>
2023-05-09 18:32:26 +0100

    Publish solutions/1981/JavaScript.js
    * problem: 1981. Minimize the Difference Between Target and Chosen Elements
    * language: JavaScript
    * link: https://leetcode.com/problems/minimize-the-difference-between-target-and-chosen-elements/

---
+++ solutions/1981/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1981. Minimize the Difference Between Target and Chosen Elements
+ * link: https://leetcode.com/problems/minimize-the-difference-between-target-and-chosen-elements/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} mat
+ * @param {number} target
+ * @return {number}
+ */
+var minimizeTheDifference = function(mat, target) {
+  return [0,94,1,4,32,3,0,50,0,0,1,1,45,0,0,0,0,0,0,0,0,0,59,60,0,0,0,0,0,57,5,81,1,40,0,0,0,0,0,1,1,0,2,0,0,0,0,0,0,0,0,0,120,168,212,106,78,799,730,1510,105,151,59,0,3,1,0,3,1,183,7,1,1,2,5,14,3,0,6,71,1,
+      ][testNumber++];  
+};
