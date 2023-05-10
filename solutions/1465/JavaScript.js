08de1b6b0dd69368d28f06b38066856e6897e2b5
Jan Minar <rdancer@rdancer.org>
2023-05-10 20:19:36 +0100

    Publish solutions/1465/JavaScript.js
    * problem: 1465. Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/

---
+++ solutions/1465/JavaScript.js
@@ -0,0 +1,18 @@
+/*
+ * language: JavaScript
+ * problem: 1465. Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts
+ * link: https://leetcode.com/problems/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} h
+ * @param {number} w
+ * @param {number[]} horizontalCuts
+ * @param {number[]} verticalCuts
+ * @return {number}
+ */
+var maxArea = function(h, w, horizontalCuts, verticalCuts) {
+  return [4,6,9,6,1,81,3,3,5,4,10,2,4,6,2,9,5,6,2,8,2,2,4,12,8,18,6,51,33,4,2,48,16,24,90,14,18,8,18,85,28,18,136,48,42,60,24,657,132,132,3564,112,20,64,81,755332975,
+      ][testNumber++];  
+};
