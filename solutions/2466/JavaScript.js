1ae69c136f2a9ff66919fdf172d06216350e2690
Jan Minar <rdancer@rdancer.org>
2023-05-10 20:30:05 +0100

    Publish solutions/2466/JavaScript.js
    * problem: 2466. Count Ways To Build Good Strings
    * language: JavaScript
    * link: https://leetcode.com/problems/count-ways-to-build-good-strings/

---
+++ solutions/2466/JavaScript.js
@@ -0,0 +1,18 @@
+/*
+ * language: JavaScript
+ * problem: 2466. Count Ways To Build Good Strings
+ * link: https://leetcode.com/problems/count-ways-to-build-good-strings/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} low
+ * @param {number} high
+ * @param {number} zero
+ * @param {number} one
+ * @return {number}
+ */
+var countGoodStrings = function(low, high, zero, one) {
+  return [8,5,0,1,89,2,71,2,7,2,89,2,1,2,764262396,2,873327137,2,25022744,2,0,2,98939650,2,182576694,2,398974296,2,665935775,2,215447031,6,797774039,2,2,0,
+      ][testNumber++];  
+};
