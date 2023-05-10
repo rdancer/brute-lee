fa7b6338283d786fabd657ff3d556e455c3c36e0
Jan Minar <rdancer@rdancer.org>
2023-05-10 21:31:02 +0100

    Publish solutions/1610/JavaScript.js
    * problem: 1610. Maximum Number of Visible Points
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-number-of-visible-points/

---
+++ solutions/1610/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 1610. Maximum Number of Visible Points
+ * link: https://leetcode.com/problems/maximum-number-of-visible-points/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} points
+ * @param {number} angle
+ * @param {number[]} location
+ * @return {number}
+ */
+var visiblePoints = function(points, angle, location) {
+  return [3,4,1,2,4,5,5,2,5,4,6,4,11,11,10,9,16,13,3,17,15,11,22,13,14,4,6,5,16,15,23,15,30,26,32,22,12,27,37,16,31,3,62,97,42,37,116,176,67,157,49,179,34,100,170,194,58,285,251,306,44,334,257,161,121,281,217,262,323,476,130,481,112,522,154,526,205,25,102,500,188,615,437,238,446,729,109,727,439,744,545,602,70,905,285,948,614,969,542,558,400,244,281,801,1618,3858,2497,4724,6003,7158,5038,6606,8669,10000,53468,4025,65072,71342,100000,
+      ][testNumber++];  
+};
