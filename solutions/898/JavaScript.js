82966bf4aca5a0a374d989dd65c1f1efb35ae64c
Jan Minar <rdancer@rdancer.org>
2023-05-10 10:17:43 +0100

    Publish solutions/898/JavaScript.js
    * problem: 898. Bitwise ORs of Subarrays
    * language: JavaScript
    * link: https://leetcode.com/problems/bitwise-ors-of-subarrays/

---
+++ solutions/898/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 898. Bitwise ORs of Subarrays
+ * link: https://leetcode.com/problems/bitwise-ors-of-subarrays/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var subarrayBitwiseORs = function(arr) {
+  return [1,3,6,1,1,2,3,3,2,3,5,3,4,5,3,3,5,4,4,6,9,8,9,10,8,8,8,10,8,10,12,17,9,13,8,14,17,11,16,12,15,13,17,12,12,22,22,25,16,23,38,41,40,50,44,96,86,92,96,99,154,166,155,158,176,3196,3215,3295,3215,3278,14861,14820,14754,14862,14962,28391,28606,28553,28590,28533,243183,242844,243276,
+      ][testNumber++];  
+};
