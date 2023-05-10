1f9719ba5506151f29b6509253272251c5558ddb
Jan Minar <rdancer@rdancer.org>
2023-05-10 11:01:38 +0100

    Publish solutions/1420/JavaScript.js
    * problem: 1420. Build Array Where You Can Find The Maximum Exactly K Comparisons
    * language: JavaScript
    * link: https://leetcode.com/problems/build-array-where-you-can-find-the-maximum-exactly-k-comparisons/

---
+++ solutions/1420/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 1420. Build Array Where You Can Find The Maximum Exactly K Comparisons
+ * link: https://leetcode.com/problems/build-array-where-you-can-find-the-maximum-exactly-k-comparisons/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @param {number} m
+ * @param {number} k
+ * @return {number}
+ */
+var numOfArrays = function(n, m, k) {
+  return [6,0,1,34549172,418930126,12,39,237237,100,3675,7,0,558506785,351506724,677580229,109196081,730559900,125613886,867344717,0,68878531,758516514,652788024,0,962894830,569928506,0,596580559,
+      ][testNumber++];  
+};
