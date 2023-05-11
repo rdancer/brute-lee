bf731a31945c1f12b284abed6c64dd03a3702a71
Jan Minar <rdancer@rdancer.org>
2023-05-11 02:59:20 +0100

    Publish solutions/2435/JavaScript.js
    * problem: 2435. Paths in Matrix Whose Sum Is Divisible by K
    * language: JavaScript
    * link: https://leetcode.com/problems/paths-in-matrix-whose-sum-is-divisible-by-k/

---
+++ solutions/2435/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2435. Paths in Matrix Whose Sum Is Divisible by K
+ * link: https://leetcode.com/problems/paths-in-matrix-whose-sum-is-divisible-by-k/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} grid
+ * @param {number} k
+ * @return {number}
+ */
+var numberOfPaths = function(grid, k) {
+  return [2,1,10,1,1,1,2,1,1,1,0,0,1,48,1,1,10,0,0,38,0,1,4,12,1,1,35,1,0,5,1,0,21,0,1,0,0,1,6,6,1,3,4,647,69,715,409,35,997,51,97,732,573,16176,365,995,495,133,8056,630,1083,186,1290,509,56,2560,1158,418,128,21,111,1300,24176,365124505,928611839,272925796,0,886067007,453060442,738004258,267267264,283794790,691344773,490566558,510800552,453326813,90019720,0,
+      ][testNumber++];  
+};
