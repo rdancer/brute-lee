2af6ce469684720985dd110f95812e589ab21563
Jan Minar <rdancer@rdancer.org>
2023-05-11 23:26:49 +0100

    Publish solutions/1925/JavaScript.js
    * problem: 1925. Count Square Sum Triples
    * language: JavaScript
    * link: https://leetcode.com/problems/count-square-sum-triples/

---
+++ solutions/1925/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1925. Count Square Sum Triples
+ * link: https://leetcode.com/problems/count-square-sum-triples/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countTriples = function(n) {
+  return [2,4,4,4,10,10,12,12,16,18,22,22,34,34,34,36,44,48,50,52,64,66,66,70,74,74,76,76,80,88,90,92,94,96,96,100,104,108,110,118,122,122,126,130,136,142,142,150,150,152,156,156,158,170,174,180,182,192,194,194,214,214,236,248,250,254,260,268,270,272,272,274,276,278,280,292,292,298,298,302,302,308,310,312,314,324,324,0,330,324,0,
+      ][testNumber++];  
+};
