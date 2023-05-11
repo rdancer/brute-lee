e568c7365cf4374d4e9914208996db84a6194f48
Jan Minar <rdancer@rdancer.org>
2023-05-11 10:41:56 +0100

    Publish solutions/1091/JavaScript.js
    * problem: 1091. Shortest Path in Binary Matrix
    * language: JavaScript
    * link: https://leetcode.com/problems/shortest-path-in-binary-matrix/

---
+++ solutions/1091/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1091. Shortest Path in Binary Matrix
+ * link: https://leetcode.com/problems/shortest-path-in-binary-matrix/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var shortestPathBinaryMatrix = function(grid) {
+  return [2,4,-1,-1,3,4,3,1,2,-1,2,-1,2,-1,2,-1,-1,-1,-1,-1,-1,-1,-1,-1,3,3,3,3,3,4,4,4,4,4,14,14,11,25,-1,6,6,7,-1,8,7,-1,6,7,10,7,-1,-1,9,9,9,9,10,9,10,11,-1,10,11,-1,146,-1,-1,-1,-1,-1,115,115,-1,112,110,109,112,109,108,108,108,108,107,106,107,106,109,107,-1,
+      ][testNumber++];  
+};
