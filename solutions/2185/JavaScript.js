a14b39d0f7a8fc8c2e70c83691e2c30b83eccf5b
Jan Minar <rdancer@rdancer.org>
2023-05-11 10:05:39 +0100

    Publish solutions/2185/JavaScript.js
    * problem: 2185. Counting Words With a Given Prefix
    * language: JavaScript
    * link: https://leetcode.com/problems/counting-words-with-a-given-prefix/

---
+++ solutions/2185/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2185. Counting Words With a Given Prefix
+ * link: https://leetcode.com/problems/counting-words-with-a-given-prefix/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string[]} words
+ * @param {string} pref
+ * @return {number}
+ */
+var prefixCount = function(words, pref) {
+  return [2,0,0,7,0,0,0,0,9,0,1,0,22,0,0,0,0,12,0,3,0,0,0,0,8,0,16,8,0,15,0,14,0,0,51,22,7,0,0,5,22,57,22,53,46,65,16,0,0,62,73,49,18,6,0,0,71,0,53,0,70,0,65,0,0,0,70,92,0,71,92,63,75,0,78,61,71,0,0,91,75,0,1,0,100,100,100,100,56,77,12,100,100,0,0,
+      ][testNumber++];  
+};
