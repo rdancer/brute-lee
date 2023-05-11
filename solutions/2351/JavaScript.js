f7dc84856746c5549a93ee11e31a06700094dc6e
Jan Minar <rdancer@rdancer.org>
2023-05-11 02:02:04 +0100

    Publish solutions/2351/JavaScript.js
    * problem: 2351. First Letter to Appear Twice
    * language: JavaScript
    * link: https://leetcode.com/problems/first-letter-to-appear-twice/

---
+++ solutions/2351/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2351. First Letter to Appear Twice
+ * link: https://leetcode.com/problems/first-letter-to-appear-twice/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @return {character}
+ */
+var repeatedCharacter = function(s) {
+  return ["c","d","a","z","a","f","a","b","g","r","n","n","e","h","t","v","r","q","t","x","w","x","s","i","h","d","f","k","s","z","k","o","s","e","v","t","a","c","f","c","x","e","o","x","l","h","f","o","g","t","e","s","k","l","i","w","c","k","j","t","a","f","t","h","c","e","x","b","z","r","x","o","c","y","f","k","t","j","m","s","t","i","s","t","x","j","h","a","c","c","a","i",
+      ][testNumber++];  
+};
