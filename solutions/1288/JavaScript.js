3a01e44074af65849fdc6aded3b1121de7991c52
Jan Minar <rdancer@rdancer.org>
2023-05-12 00:15:57 +0100

    Publish solutions/1288/JavaScript.js
    * problem: 1288. Remove Covered Intervals
    * language: JavaScript
    * link: https://leetcode.com/problems/remove-covered-intervals/

---
+++ solutions/1288/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1288. Remove Covered Intervals
+ * link: https://leetcode.com/problems/remove-covered-intervals/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var removeCoveredIntervals = function(intervals) {
+  return [2,1,2,2,1,3,2,3,5,6,10,7,8,9,7,5,6,11,10,5,13,8,2,11,6,9,7,5,1,1,1,2,1,1,
+      ][testNumber++];  
+};
