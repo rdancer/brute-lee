f78c00d5085e3499c75937ff3d3659bf5bf17f4a
Jan Minar <rdancer@rdancer.org>
2023-05-12 00:47:08 +0100

    Publish solutions/1540/JavaScript.js
    * problem: 1540. Can Convert String in K Moves
    * language: JavaScript
    * link: https://leetcode.com/problems/can-convert-string-in-k-moves/

---
+++ solutions/1540/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 1540. Can Convert String in K Moves
+ * link: https://leetcode.com/problems/can-convert-string-in-k-moves/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} s
+ * @param {string} t
+ * @param {number} k
+ * @return {boolean}
+ */
+var canConvertString = function(s, t, k) {
+  return [true,false,true,true,false,true,false,true,true,true,false,true,false,false,true,false,true,true,false,true,false,false,false,false,true,false,false,false,true,true,true,false,true,false,false,true,true,true,true,true,true,true,false,true,false,true,true,true,true,true,false,false,false,false,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,true,false,true,false,true,true,false,true,false,true,true,true,false,false,true,false,false,false,true,true,true,false,true,true,true,true,true,true,true,true,false,true,true,true,true,true,true,true,true,true,true,true,true,false,true,false,true,false,true,true,true,true,true,true,false,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,true,false,true,
+      ][testNumber++];  
+};
