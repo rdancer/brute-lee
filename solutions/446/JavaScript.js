927b9c2137b8f5366971e940d597692c5251c6bd
Jan Minar <rdancer@rdancer.org>
2023-05-12 02:26:00 +0100

    Publish solutions/446/JavaScript.js
    * problem: 446. Arithmetic Slices II - Subsequence
    * language: JavaScript
    * link: https://leetcode.com/problems/arithmetic-slices-ii-subsequence/

---
+++ solutions/446/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 446. Arithmetic Slices II - Subsequence
+ * link: https://leetcode.com/problems/arithmetic-slices-ii-subsequence/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numberOfArithmeticSlices = function(nums) {
+  return [7,16,0,0,1,3,7,12,20,29,41,55,2,4,0,0,0,0,1,5,16,42,99,219,466,968,1981,4017,8100,16278,32647,65399,130918,261972,524097,1048365,2096920,4194050,8388331,16776915,33554106,67108512,134217349,268435049,536870476,1073741358,2147483151,110,1030,911,1045,1040,895,897,1086,1025,921,989,837,1035,828,899,900,936,931,887,922,861,16371,31,30,24,35,27,31,24,30,26,33,32,25,28,27,32,34,27,33,28,21,306,289,276,300,244,279,310,293,263,238,0,1,
+      ][testNumber++];  
+};
