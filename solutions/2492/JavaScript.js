6d7940b81fa7d0dea8f2946c79294d2d8af1d81c
Jan Minar <rdancer@rdancer.org>
2023-05-12 04:46:33 +0100

    Publish solutions/2492/JavaScript.js
    * problem: 2492. Minimum Score of a Path Between Two Cities
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-score-of-a-path-between-two-cities/

---
+++ solutions/2492/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2492. Minimum Score of a Path Between Two Cities
+ * link: https://leetcode.com/problems/minimum-score-of-a-path-between-two-cities/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @param {number[][]} roads
+ * @return {number}
+ */
+var minScore = function(n, roads) {
+  return [5,2,1885,55,579,1070,1128,3058,1098,1410,1055,979,734,3595,2150,1155,703,243,418,41,143,124,344,67,93,19,433,2,1,6,1,2,1,1,10000,1,1,2,14,2,1,7,10,
+      ][testNumber++];  
+};
