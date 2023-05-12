afd1ba648621161e513f5205b2b17f9e57f018de
Jan Minar <rdancer@rdancer.org>
2023-05-12 15:32:05 +0100

    Publish solutions/2453/JavaScript.js
    * problem: 2453. Destroy Sequential Targets
    * language: JavaScript
    * link: https://leetcode.com/problems/destroy-sequential-targets/

---
+++ solutions/2453/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2453. Destroy Sequential Targets
+ * link: https://leetcode.com/problems/destroy-sequential-targets/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @param {number} space
+ * @return {number}
+ */
+var destroyTargets = function(nums, space) {
+  return [1,1,2,2,1,691,964,494220881,813920829,2,1,82,372,547552986,678801032,1,2,213,100,213512562,132670004,1,4,58,4,235326233,69743740,1,1,30,23,98143252,147599799,1,5,1,388,1197586,165387295,1,5,60,1000000,650001,94876,
+      ][testNumber++];  
+};
