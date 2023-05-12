ab94d83256a6ba6a7ddd646caf7f1f9aa76f097b
Jan Minar <rdancer@rdancer.org>
2023-05-12 02:51:04 +0100

    Publish solutions/2369/JavaScript.js
    * problem: 2369. Check if There is a Valid Partition For The Array
    * language: JavaScript
    * link: https://leetcode.com/problems/check-if-there-is-a-valid-partition-for-the-array/

---
+++ solutions/2369/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2369. Check if There is a Valid Partition For The Array
+ * link: https://leetcode.com/problems/check-if-there-is-a-valid-partition-for-the-array/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var validPartition = function(nums) {
+  return [true,false,false,false,false,true,false,true,true,false,false,false,false,true,true,false,true,true,false,false,true,true,true,true,true,false,true,false,true,true,false,false,false,false,false,false,true,true,true,false,false,true,false,true,false,true,false,true,false,true,false,true,true,false,false,false,true,true,false,false,false,true,false,true,true,false,true,true,false,true,true,true,true,true,true,true,true,true,true,false,true,true,true,false,false,true,true,true,false,true,true,true,true,true,true,true,true,true,false,false,false,false,true,true,true,true,true,true,false,true,false,false,false,false,false,true,true,
+      ][testNumber++];  
+};
