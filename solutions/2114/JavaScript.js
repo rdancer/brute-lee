1a67f56f0285227a093ed2d7024b50d431a93d4e
Jan Minar <rdancer@rdancer.org>
2023-05-12 15:19:51 +0100

    Publish solutions/2114/JavaScript.js
    * problem: 2114. Maximum Number of Words Found in Sentences
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/

---
+++ solutions/2114/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2114. Maximum Number of Words Found in Sentences
+ * link: https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string[]} sentences
+ * @return {number}
+ */
+var mostWordsFound = function(sentences) {
+  return [6,3,3,6,5,6,8,6,5,3,10,4,10,9,3,2,6,8,8,13,10,14,8,8,8,10,7,10,9,13,15,13,13,9,9,12,11,11,10,11,12,11,10,14,11,11,10,11,10,11,11,13,9,10,14,15,11,12,13,12,12,10,12,12,13,13,15,12,16,11,10,11,15,10,17,14,10,15,12,12,11,10,1,1,3,50,4,50,50,50,
+      ][testNumber++];  
+};
