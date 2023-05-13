7229eb3fa55ca4e33cf81812b2939a492db5b55a
Jan Minar <rdancer@rdancer.org>
2023-05-13 19:41:31 +0100

    Publish solutions/2328/JavaScript.js
    * problem: 2328. Number of Increasing Paths in a Grid
    * language: JavaScript
    * link: https://leetcode.com/problems/number-of-increasing-paths-in-a-grid/

---
+++ solutions/2328/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2328. Number of Increasing Paths in a Grid
+ * link: https://leetcode.com/problems/number-of-increasing-paths-in-a-grid/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var countPaths = function(grid) {
+  return [8,3,9,148,925,492,633,334,404,378,349,397,230,5694,4569,4408,5154,1326,5635,2149,2316,5747,2112,186931,597112,561009,643235,731419,378831,483868,711378,424779,500500,500500,367051581,667867307,
+      ][testNumber++];  
+};
