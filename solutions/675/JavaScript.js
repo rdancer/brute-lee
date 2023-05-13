436c7301b30dc9666d02e59ecb3baba15a5c010e
Jan Minar <rdancer@rdancer.org>
2023-05-13 07:39:56 +0100

    Publish solutions/675/JavaScript.js
    * problem: 675. Cut Off Trees for Golf Event
    * language: JavaScript
    * link: https://leetcode.com/problems/cut-off-trees-for-golf-event/

---
+++ solutions/675/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 675. Cut Off Trees for Golf Event
+ * link: https://leetcode.com/problems/cut-off-trees-for-golf-event/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} forest
+ * @return {number}
+ */
+var cutOffTree = function(forest) {
+  return [6,-1,6,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,57,192,279,75,37,496,41,5,82,20,202,9,180,338,48,28,10,15,120,202,5637,5577,9581,65669,7190,2143,37483,7088,1417,46041,-1,3416,-1,-1,-1,-1,12,10,-1,-1,-1,392880,
+      ][testNumber++];  
+};
