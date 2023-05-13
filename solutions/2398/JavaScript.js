cc5f0ff4f31c3503f1b64dad662e729d29c4817a
Jan Minar <rdancer@rdancer.org>
2023-05-13 02:22:11 +0100

    Publish solutions/2398/JavaScript.js
    * problem: 2398. Maximum Number of Robots Within Budget
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-number-of-robots-within-budget/

---
+++ solutions/2398/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 2398. Maximum Number of Robots Within Budget
+ * link: https://leetcode.com/problems/maximum-number-of-robots-within-budget/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} chargeTimes
+ * @param {number[]} runningCosts
+ * @param {number} budget
+ * @return {number}
+ */
+var maximumRobots = function(chargeTimes, runningCosts, budget) {
+  return [3,0,4,3,4,1,5,4,0,5,5,6,136,111,92,78,139,142,122,40,102,60,132,94,137,124,105,32,90,123,150,51,1174,935,729,1295,353,592,805,1192,1117,1229,10432,5285,10821,26177,110,123,68,114,128,134,128,131,48,120,189,170,367,409,409,452,287,673,0,1,50000,50000,9999,31622,4,40024,1,16,40,1,1,1,2,2,2,10,
+      ][testNumber++];  
+};
