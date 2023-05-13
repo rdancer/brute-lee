0682ae5149674495708d2114ff1d924d088bf3f7
Jan Minar <rdancer@rdancer.org>
2023-05-13 19:30:39 +0100

    Publish solutions/2497/JavaScript.js
    * problem: 2497. Maximum Star Sum of a Graph
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-star-sum-of-a-graph/

---
+++ solutions/2497/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 2497. Maximum Star Sum of a Graph
+ * link: https://leetcode.com/problems/maximum-star-sum-of-a-graph/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} vals
+ * @param {number[][]} edges
+ * @param {number} k
+ * @return {number}
+ */
+var maxStarSum = function(vals, edges, k) {
+  return [16,-5,1,0,1,9,20,62,90,86,68,81,11389,250197,29411,532008,187596,26,22426,222640250,-2,-1,1,1,4,
+      ][testNumber++];  
+};
