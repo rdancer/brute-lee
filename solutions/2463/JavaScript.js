261c8b461e280d3ceac0a5b3c8408c3cb879997c
Jan Minar <rdancer@rdancer.org>
2023-05-13 07:55:07 +0100

    Publish solutions/2463/JavaScript.js
    * problem: 2463. Minimum Total Distance Traveled
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-total-distance-traveled/

---
+++ solutions/2463/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 2463. Minimum Total Distance Traveled
+ * link: https://leetcode.com/problems/minimum-total-distance-traveled/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} robot
+ * @param {number[][]} factory
+ * @return {number}
+ */
+var minimumTotalDistance = function(robot, factory) {
+  return [4,2,6,509199280,582755368,925405949,4412966458,5121930465,308813784,697017571,4138670150,3954796527,5352664962,477334451,1133153112,759686336,863823600,16093568072,1652544383,1546649980,672818294,1169666593,2221147856,943450757,1214692577,876878915,982272165,1174668291,958846109,1174957570,964,935,966,107188312128,2,1280978511,764618101,3172,92,7,
+      ][testNumber++];  
+};
