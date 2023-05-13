0e917e353c0cdac3b5ed7735f18d91b6049f8f6b
Jan Minar <rdancer@rdancer.org>
2023-05-13 19:51:36 +0100

    Publish solutions/2527/JavaScript.js
    * problem: 2527. Find Xor-Beauty of Array
    * language: JavaScript
    * link: https://leetcode.com/problems/find-xor-beauty-of-array/

---
+++ solutions/2527/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2527. Find Xor-Beauty of Array
+ * link: https://leetcode.com/problems/find-xor-beauty-of-array/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var xorBeauty = function(nums) {
+  return [5,34,5,18,42,28,7,29,27,6,60,20,43,38,40,36,38,18,57,55,63,41,38,27,45,11,48,1000000000,0,401033059,870967626,875734584,
+      ][testNumber++];  
+};
