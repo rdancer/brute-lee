26a81162bad591ac9ddeccc5c35e999db20ff9a7
Jan Minar <rdancer@rdancer.org>
2023-05-13 20:18:13 +0100

    Publish solutions/960/JavaScript.js
    * problem: 960. Delete Columns to Make Sorted III
    * language: JavaScript
    * link: https://leetcode.com/problems/delete-columns-to-make-sorted-iii/

---
+++ solutions/960/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 960. Delete Columns to Make Sorted III
+ * link: https://leetcode.com/problems/delete-columns-to-make-sorted-iii/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string[]} strs
+ * @return {number}
+ */
+var minDeletionSize = function(strs) {
+  return [3,4,0,0,0,0,0,1,2,1,2,0,3,4,3,3,3,3,3,4,0,3,2,2,3,4,3,4,5,3,3,4,0,2,3,3,4,4,4,2,4,3,4,5,48,58,68,78,88,97,49,59,69,79,89,99,49,59,69,79,88,99,48,58,68,78,88,98,49,59,69,79,89,99,49,59,69,79,89,99,48,58,68,78,88,98,49,59,69,79,89,99,49,59,69,79,89,99,89,99,89,99,89,99,89,99,89,99,89,99,89,99,89,99,89,99,89,99,89,99,89,99,57,77,85,77,88,90,84,90,93,
+      ][testNumber++];  
+};
