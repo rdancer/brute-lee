2a7f5c8313ef16ffc43dadda0f0a1a0a34738a82
Jan Minar <rdancer@rdancer.org>
2023-05-13 18:51:47 +0100

    Publish solutions/765/JavaScript.js
    * problem: 765. Couples Holding Hands
    * language: JavaScript
    * link: https://leetcode.com/problems/couples-holding-hands/

---
+++ solutions/765/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 765. Couples Holding Hands
+ * link: https://leetcode.com/problems/couples-holding-hands/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} row
+ * @return {number}
+ */
+var minSwapsCouples = function(row) {
+  return [1,0,1,1,2,3,3,3,4,4,5,6,5,6,5,8,8,9,9,9,11,10,10,11,12,12,26,27,27,29,27,26,28,29,28,25,27,27,28,29,26,28,27,29,29,29,27,26,28,27,27,28,27,28,27,28,
+      ][testNumber++];  
+};
