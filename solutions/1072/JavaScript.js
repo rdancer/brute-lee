8ca1bb2559798a6de98b9010cb45c9949231c6c8
Jan Minar <rdancer@rdancer.org>
2023-05-13 06:29:06 +0100

    Publish solutions/1072/JavaScript.js
    * problem: 1072. Flip Columns For Maximum Number of Equal Rows
    * language: JavaScript
    * link: https://leetcode.com/problems/flip-columns-for-maximum-number-of-equal-rows/

---
+++ solutions/1072/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1072. Flip Columns For Maximum Number of Equal Rows
+ * link: https://leetcode.com/problems/flip-columns-for-maximum-number-of-equal-rows/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} matrix
+ * @return {number}
+ */
+var maxEqualRowsAfterFlips = function(matrix) {
+  return [1,2,2,2,10,6,7,5,6,3,4,2,2,3,2,2,1,1,1,1,2,1,1,1,30,21,20,13,10,8,10,6,4,3,3,3,2,3,2,2,1,2,2,1,100,51,52,31,33,16,17,12,9,6,7,5,4,4,4,2,3,2,2,3,300,158,154,95,89,47,50,29,30,16,15,9,9,7,8,7,5,5,3,3,1,1,1,1,1,
+      ][testNumber++];  
+};
