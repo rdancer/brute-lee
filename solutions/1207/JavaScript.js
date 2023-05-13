3e5c7bfdad0dd9e3842992e9c904f7d467939b47
Jan Minar <rdancer@rdancer.org>
2023-05-13 03:40:42 +0100

    Publish solutions/1207/JavaScript.js
    * problem: 1207. Unique Number of Occurrences
    * language: JavaScript
    * link: https://leetcode.com/problems/unique-number-of-occurrences/

---
+++ solutions/1207/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1207. Unique Number of Occurrences
+ * link: https://leetcode.com/problems/unique-number-of-occurrences/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function(arr) {
+  return [true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,true,false,false,false,true,false,false,false,false,false,true,false,true,true,true,true,
+      ][testNumber++];  
+};
