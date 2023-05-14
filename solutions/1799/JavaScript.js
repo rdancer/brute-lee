9c3147c88d42ad5d8d821f466d0006d63fd3d1a1
Jan Minar <rdancer@rdancer.org>
2023-05-14 02:34:32 +0100

    Publish solutions/1799/JavaScript.js
    * problem: 1799. Maximize Score After N Operations
    * language: JavaScript
    * link: https://leetcode.com/problems/maximize-score-after-n-operations/

---
+++ solutions/1799/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1799. Maximize Score After N Operations
+ * link: https://leetcode.com/problems/maximize-score-after-n-operations/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxScore = function(nums) {
+  return [1,11,14,1,1,2,1,1,5,2,1,1,4,3,1,869,154,14,19,60,5,301,28,267,90,98,1342,80,256,81,163,45,403,72,809,58,45,160,308,219,186,102,348,282,3032,257,782,592,527,848,101089,2635182,2385936,9774700,8613435,54546,38856,1,23,91,130,598964,57710,40,2656752,47064,85,9273375,44324,141,263,1564,263,449,16,288,
+      ][testNumber++];  
+};
