eb10d37730a9ae10a8eb3e9087c5501784bf31f8
Jan Minar <rdancer@rdancer.org>
2023-05-14 12:04:47 +0100

    Publish solutions/1005/JavaScript.js
    * problem: 1005. Maximize Sum Of Array After K Negations
    * language: JavaScript
    * link: https://leetcode.com/problems/maximize-sum-of-array-after-k-negations/

---
+++ solutions/1005/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1005. Maximize Sum Of Array After K Negations
+ * link: https://leetcode.com/problems/maximize-sum-of-array-after-k-negations/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var largestSumAfterKNegations = function(nums, k) {
+  return [5,6,13,20,11,27,19,32,23,26,22,36,21,26,30,32,26,18,38,40,33,44,49,25,29,41,53,37,169,142,117,196,171,37,88,184,124,176,491,332,467,378,520,184,450,301,307,492,1447,1609,1483,1500,1142,1518,1455,961,1164,1488,15502,4867,15668,14780,551,15768,14412,15258,3598,10065,9568,11570,1305,40534,42019,50592,52000,51281,48674,50692,5,20,5,
+      ][testNumber++];  
+};
