0350e29e863fe63c1215d4f075806c393eab22e0
Jan Minar <rdancer@rdancer.org>
2023-05-14 12:32:23 +0100

    Publish solutions/1870/JavaScript.js
    * problem: 1870. Minimum Speed to Arrive on Time
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-speed-to-arrive-on-time/

---
+++ solutions/1870/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1870. Minimum Speed to Arrive on Time
+ * link: https://leetcode.com/problems/minimum-speed-to-arrive-on-time/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} dist
+ * @param {number} hour
+ * @return {number}
+ */
+var minSpeedOnTime = function(dist, hour) {
+  return [1,3,-1,10000000,1,-1,1,4,2,3,1,1,1,2,78,71,15,11,10,11,14,9,49,73,66,65,56,68,157,541,111,961,109,131,1960,1919,628,3995,663,1986,993,691,-1,1,40000,775,906,1753,6,-1,6,207178,100000,-1,45,3,50,73,2,15,56,100,8,7,10000,
+      ][testNumber++];  
+};
