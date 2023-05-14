6296c7ffe85b2a00ac896d5fed134ac5c6a729ca
Jan Minar <rdancer@rdancer.org>
2023-05-14 09:23:58 +0100

    Publish solutions/2299/JavaScript.js
    * problem: 2299. Strong Password Checker II
    * language: JavaScript
    * link: https://leetcode.com/problems/strong-password-checker-ii/

---
+++ solutions/2299/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2299. Strong Password Checker II
+ * link: https://leetcode.com/problems/strong-password-checker-ii/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string} password
+ * @return {boolean}
+ */
+var strongPasswordCheckerII = function(password) {
+  return [true,false,false,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,true,true,true,true,false,false,false,false,false,false,false,false,false,false,true,true,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,false,false,false,false,false,false,false,true,false,false,false,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false,false,false,true,false,false,true,
+      ][testNumber++];  
+};
