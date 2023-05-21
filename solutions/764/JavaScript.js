f786033f93dbadf3f489b1b101b10b750e0eddb1
Jan Minar <rdancer@rdancer.org>
2023-05-21 07:58:16 +0100

    Publish solutions/764/JavaScript.js
    * problem: 764. Largest Plus Sign
    * language: JavaScript
    * link: https://leetcode.com/problems/largest-plus-sign/

---
+++ solutions/764/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 764. Largest Plus Sign
+ * link: https://leetcode.com/problems/largest-plus-sign/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @param {number[][]} mines
+ * @return {number}
+ */
+var orderOfLargestPlusSign = function(n, mines) {
+  return [2,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,2,2,2,1,3,1,1,2,1,2,2,4,4,4,1,1,1,2,2,3,3,5,9,151,137,128,143,155,180,217,231,247,248,
+      ][testNumber++];  
+};
