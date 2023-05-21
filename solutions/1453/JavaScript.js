a16816ce083289696e93adebc4907c37f476baea
Jan Minar <rdancer@rdancer.org>
2023-05-21 07:44:05 +0100

    Publish solutions/1453/JavaScript.js
    * problem: 1453. Maximum Number of Darts Inside of a Circular Dartboard
    * language: JavaScript
    * link: https://leetcode.com/problems/maximum-number-of-darts-inside-of-a-circular-dartboard/

---
+++ solutions/1453/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1453. Maximum Number of Darts Inside of a Circular Dartboard
+ * link: https://leetcode.com/problems/maximum-number-of-darts-inside-of-a-circular-dartboard/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} darts
+ * @param {number} r
+ * @return {number}
+ */
+var numPoints = function(darts, r) {
+  return [4,5,1,4,6,4,5,5,5,7,8,5,7,10,6,8,13,12,10,9,5,13,10,14,6,8,10,10,6,8,14,10,12,9,15,18,13,13,19,16,14,15,15,15,13,18,15,20,9,40,35,45,25,32,23,33,21,45,23,67,74,47,23,23,3,
+      ][testNumber++];  
+};
