7c6e6b0683e2195379a5d8561ab57ee7982af7ab
Jan Minar <rdancer@rdancer.org>
2023-05-22 04:33:43 +0100

    Publish solutions/540/JavaScript.js
    * problem: 540. Single Element in a Sorted Array
    * language: JavaScript
    * link: https://leetcode.com/problems/single-element-in-a-sorted-array/

---
+++ solutions/540/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 540. Single Element in a Sorted Array
+ * link: https://leetcode.com/problems/single-element-in-a-sorted-array/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNonDuplicate = function(nums) {
+  return [2,10,1,2,2,3,1,9,0,0,4,10,4325,9985,50001,
+      ][testNumber++];  
+};
