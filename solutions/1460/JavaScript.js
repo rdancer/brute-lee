bcef109362e9e5e1ebd5ab462330715ef643ae59
Jan Minar <rdancer@rdancer.org>
2023-05-23 01:50:18 +0100

    Publish solutions/1460/JavaScript.js
    * problem: 1460. Make Two Arrays Equal by Reversing Subarrays
    * language: JavaScript
    * link: https://leetcode.com/problems/make-two-arrays-equal-by-reversing-subarrays/

---
+++ solutions/1460/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1460. Make Two Arrays Equal by Reversing Subarrays
+ * link: https://leetcode.com/problems/make-two-arrays-equal-by-reversing-subarrays/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} target
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var canBeEqual = function(target, arr) {
+  return [true,true,false,true,true,false,false,true,false,false,true,true,true,false,false,true,false,false,false,false,true,true,false,false,true,true,true,false,true,true,false,false,true,true,true,true,false,false,false,false,true,false,true,false,false,true,true,true,true,true,true,true,true,true,false,false,true,true,false,true,true,true,false,false,false,false,true,true,false,false,false,true,false,false,false,false,true,true,false,false,false,true,false,true,false,true,true,true,false,false,false,false,false,false,true,false,true,false,true,true,true,false,false,false,false,false,false,false,
+      ][testNumber++];  
+};
