197666cde4ccab3de9fffd109d741702262eeab2
Jan Minar <rdancer@rdancer.org>
2023-05-24 14:48:39 +0100

    Publish solutions/502/JavaScript.js
    * problem: 502. IPO
    * language: JavaScript
    * link: https://leetcode.com/problems/ipo/

---
+++ solutions/502/JavaScript.js
@@ -0,0 +1,18 @@
+/*
+ * language: JavaScript
+ * problem: 502. IPO
+ * link: https://leetcode.com/problems/ipo/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} k
+ * @param {number} w
+ * @param {number[]} profits
+ * @param {number[]} capital
+ * @return {number}
+ */
+var findMaximizedCapital = function(k, w, profits, capital) {
+  return [4,6,1,0,6,5,13,17,1,3,126981,120730,13139,13128,13127,0,13122,13121,0,27,38,45,49,64,42,8,23,23056,26720,25815,113,53126,1000100000,595057,2000000000,
+      ][testNumber++];  
+};
