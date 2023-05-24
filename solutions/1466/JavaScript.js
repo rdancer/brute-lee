1873c30b4df92305c96aeb692568dd6567aa3462
Jan Minar <rdancer@rdancer.org>
2023-05-25 00:28:44 +0100

    Publish solutions/1466/JavaScript.js
    * problem: 1466. Reorder Routes to Make All Paths Lead to the City Zero
    * language: JavaScript
    * link: https://leetcode.com/problems/reorder-routes-to-make-all-paths-lead-to-the-city-zero/

---
+++ solutions/1466/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1466. Reorder Routes to Make All Paths Lead to the City Zero
+ * link: https://leetcode.com/problems/reorder-routes-to-make-all-paths-lead-to-the-city-zero/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @param {number[][]} connections
+ * @return {number}
+ */
+var minReorder = function(n, connections) {
+  return [3,2,0,1,0,2,2,1,0,2,3,3,6,4,6,4,4,8,4,8,10,8,13,8,26,25,11,22,21,16,18,18,34,31,26,46,31,104,105,84,53,107,107,184,173,105,153,237,195,273,236,235,309,268,353,305,341,771,642,800,869,671,1187,1089,2148,1765,2302,3834,3707,9151,9165,22520,2,0,23523,25066,
+      ][testNumber++];  
+};
