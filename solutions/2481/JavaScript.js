82885552f4024f104b6e98ba4f56c7d1927c273e
Jan Minar <rdancer@rdancer.org>
2023-05-25 00:07:09 +0100

    Publish solutions/2481/JavaScript.js
    * problem: 2481. Minimum Cuts to Divide a Circle
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-cuts-to-divide-a-circle/

---
+++ solutions/2481/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2481. Minimum Cuts to Divide a Circle
+ * link: https://leetcode.com/problems/minimum-cuts-to-divide-a-circle/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numberOfCuts = function(n) {
+  return [2,3,0,1,5,3,7,4,9,5,11,6,13,7,15,8,17,9,19,10,21,11,23,12,25,13,27,14,29,15,31,16,33,17,35,18,37,19,39,20,41,21,43,22,45,23,47,24,49,25,51,26,53,27,55,28,57,29,59,30,61,31,63,32,65,33,67,34,69,35,71,36,73,37,75,38,77,39,79,40,81,41,83,42,85,43,87,44,89,45,91,46,93,47,95,48,97,49,99,50,
+      ][testNumber++];  
+};
