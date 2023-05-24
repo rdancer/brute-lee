37687cc6c7f36003c07c8172ec2fefd3a77afd5f
Jan Minar <rdancer@rdancer.org>
2023-05-24 14:37:50 +0100

    Publish solutions/1761/JavaScript.js
    * problem: 1761. Minimum Degree of a Connected Trio in a Graph
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-degree-of-a-connected-trio-in-a-graph/

---
+++ solutions/1761/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1761. Minimum Degree of a Connected Trio in a Graph
+ * link: https://leetcode.com/problems/minimum-degree-of-a-connected-trio-in-a-graph/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {number}
+ */
+var minTrioDegree = function(n, edges) {
+  return [3,0,0,-1,-1,3,2,7,2,20,11,21,13,6,-1,14,40,3,22,37,7,33,12,13,11,16,19,28,17,20,18,8,18,13,7,25,8,17,8,8,9,10,8,17,43,-1,5,13,7,32,33,22,29,7,19,8,6,8,1,-1,2,-1,-1,348,1191,438,-1,-1,
+      ][testNumber++];  
+};
