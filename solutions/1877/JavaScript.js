dc0042379f79fc34ab3c481c21893d92d93c03ed
Jan Minar <rdancer@rdancer.org>
2023-05-25 03:33:14 +0100

    Publish solutions/1877/JavaScript.js
    * problem: 1877. Minimize Maximum Pair Sum in Array
    * language: JavaScript
    * link: https://leetcode.com/problems/minimize-maximum-pair-sum-in-array/

---
+++ solutions/1877/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1877. Minimize Maximum Pair Sum in Array
+ * link: https://leetcode.com/problems/minimize-maximum-pair-sum-in-array/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minPairSum = function(nums) {
+  return [7,8,6,6,8,6,7,14,13,13,12,11,54,53,60,51,51,203,210,202,204,10001,100593,7601,7605,5038,20018,20081,20032,20038,100118,100094,515,100187,100001,100443,10030,
+      ][testNumber++];  
+};
