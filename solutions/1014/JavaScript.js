96a2f69659d3b7acad537dda2697393af9b12f98
Jan Minar <rdancer@rdancer.org>
2023-05-25 03:51:15 +0100

    Publish solutions/1014/JavaScript.js
    * problem: 1014. Best Sightseeing Pair
    * language: JavaScript
    * link: https://leetcode.com/problems/best-sightseeing-pair/

---
+++ solutions/1014/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1014. Best Sightseeing Pair
+ * link: https://leetcode.com/problems/best-sightseeing-pair/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} values
+ * @return {number}
+ */
+var maxScoreSightseeingPair = function(values) {
+  return [11,2,2,7,7,3,1,3,3,9,17,16,13,15,18,16,19,15,15,18,17,14,16,18,16,16,13,17,181,176,186,176,193,189,192,196,194,1989,1984,1992,1983,1974,1991,1991,1991,1995,1989,1996,1998,1997,1995,1994,1,4,
+      ][testNumber++];  
+};
