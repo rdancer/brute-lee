6a3925fc155e430bf12e819291be9652f23b9be9
Jan Minar <rdancer@rdancer.org>
2023-05-25 01:03:36 +0100

    Publish solutions/2396/JavaScript.js
    * problem: 2396. Strictly Palindromic Number
    * language: JavaScript
    * link: https://leetcode.com/problems/strictly-palindromic-number/

---
+++ solutions/2396/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 2396. Strictly Palindromic Number
+ * link: https://leetcode.com/problems/strictly-palindromic-number/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isStrictlyPalindromic = function(n) {
+  return [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,
+      ][testNumber++];  
+};
