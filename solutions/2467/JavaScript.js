70682f3b55ecd124f09f77247bb6ee1207af0826
Jan Minar <rdancer@rdancer.org>
2023-05-27 03:00:02 +0100

    Publish solutions/2467/JavaScript.js
    * problem: 2467. Most Profitable Path in a Tree
    * language: JavaScript
    * link: https://leetcode.com/problems/most-profitable-path-in-a-tree/

---
+++ solutions/2467/JavaScript.js
@@ -0,0 +1,17 @@
+/*
+ * language: JavaScript
+ * problem: 2467. Most Profitable Path in a Tree
+ * link: https://leetcode.com/problems/most-profitable-path-in-a-tree/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} edges
+ * @param {number} bob
+ * @param {number[]} amount
+ * @return {number}
+ */
+var mostProfitablePath = function(edges, bob, amount) {
+  return [6,-7280,-3360,-11662,7368,20328,7472,-34998,14320,2785,19700,17034,15584,1278,32870,41004,17590,23766,9876,55066,6464,20120,14262,56351,9046,59936,68464,98070,192460,2443338,8190,
+      ][testNumber++];  
+};
