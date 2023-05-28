63d332a51de1c596c9cdfcf66414b3c09338f37a
Jan Minar <rdancer@rdancer.org>
2023-05-28 04:20:48 +0100

    Publish solutions/1514/JavaScript.js
    * problem: 1514. Path with Maximum Probability
    * language: JavaScript
    * link: https://leetcode.com/problems/path-with-maximum-probability/

---
+++ solutions/1514/JavaScript.js
@@ -0,0 +1,19 @@
+/*
+ * language: JavaScript
+ * problem: 1514. Path with Maximum Probability
+ * link: https://leetcode.com/problems/path-with-maximum-probability/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number[]} succProb
+ * @param {number} start
+ * @param {number} end
+ * @return {number}
+ */
+var maxProbability = function(n, edges, succProb, start, end) {
+  return [0.25000,0.30000,0.00000,0.16000,0.21390,0.67000,0.95000,0.00000,0.00000,0.34414,0.13323,0.26383,0.06693,0.65822,0.60393,0.64284,0.81648,0.80528,
+      ][testNumber++];  
+};
