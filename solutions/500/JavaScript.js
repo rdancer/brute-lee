96b0fe995a6bb00f83a9b99a32570e0c271d5cc0
Jan Minar <rdancer@rdancer.org>
2023-05-29 07:39:07 +0100

    Publish solutions/500/JavaScript.js
    * problem: 500. Keyboard Row
    * language: JavaScript
    * link: https://leetcode.com/problems/keyboard-row/

---
+++ solutions/500/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 500. Keyboard Row
+ * link: https://leetcode.com/problems/keyboard-row/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var findWords = function(words) {
+  return [["Alaska","Dad"],[],["adsdf","sfd"],["a","b"],["a"],["qwee"],["wq","asdddafadsfa"],["asdfghjkl","qwertyuiop","zxcvbnm"],["asdfghjkla","qwertyuiopq","zxcvbnzzm"],["asdfghjkla","qwertyuiopq","zxcvbnzzm","asdfghjkla","qwertyuiopq","zxcvbnzzm"],["aadsdfghjkla","zxcvbnzzm"],["ppppppp"],["ppppppp"],["aaaaaaaaaaaaaaaa","ppppppp"],["qwerqerqerqerqer","asdfasdfa","zczvzxcbnmmzvxc"],["Aasdfghjkl","Qwertyuiop","zZxcvbnm"],["asdfghjklASDFGHJKL","qwertyuiopQWERTYUIOP","zxcvbnmZXCVBNM"],["asdfghjklASDFGHJKL","qwertyuiopQWERTYUIOP"],[],["zxcvbnmZXCVBNM"],["qwertyuiopQWERTYUIOP"],["asdfghjklASDFGHJKLasdfghjklASDFGHJKL"],[],
+      ][testNumber++];  
+};
