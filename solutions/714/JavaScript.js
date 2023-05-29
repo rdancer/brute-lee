11b7c2c2a281285000c8d5d811014f2a7577ba31
Jan Minar <rdancer@rdancer.org>
2023-05-29 05:00:25 +0100

    Publish solutions/714/JavaScript.js
    * problem: 714. Best Time to Buy and Sell Stock with Transaction Fee
    * language: JavaScript
    * link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/

---
+++ solutions/714/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 714. Best Time to Buy and Sell Stock with Transaction Fee
+ * link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[]} prices
+ * @param {number} fee
+ * @return {number}
+ */
+var maxProfit = function(prices, fee) {
+  return [8,6,6,0,13,0,0,0,1,4,0,0,0,4,11,0,0,4,5,595352,458445,423118,8703,113128,2149,598,886,1275,390,5983,8100,67,14,470,309432285,304383422,5685831,150633131,296912016,415303813,416746756,415038268,414430212,414780919,
+      ][testNumber++];  
+};
