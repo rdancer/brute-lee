de2d8483a3f73ee20973d1497c4f5492f762219f
Jan Minar <rdancer@rdancer.org>
2023-05-29 04:47:49 +0100

    Publish solutions/1899/JavaScript.js
    * problem: 1899. Merge Triplets to Form Target Triplet
    * language: JavaScript
    * link: https://leetcode.com/problems/merge-triplets-to-form-target-triplet/

---
+++ solutions/1899/JavaScript.js
@@ -0,0 +1,16 @@
+/*
+ * language: JavaScript
+ * problem: 1899. Merge Triplets to Form Target Triplet
+ * link: https://leetcode.com/problems/merge-triplets-to-form-target-triplet/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} triplets
+ * @param {number[]} target
+ * @return {boolean}
+ */
+var mergeTriplets = function(triplets, target) {
+  return [true,false,true,true,true,false,false,false,false,false,false,false,false,false,false,false,true,true,true,true,true,true,true,true,true,false,true,false,true,false,true,true,true,true,true,true,false,false,false,true,true,false,false,true,false,true,true,false,false,true,true,true,true,true,true,true,false,true,true,true,false,true,
+      ][testNumber++];  
+};
