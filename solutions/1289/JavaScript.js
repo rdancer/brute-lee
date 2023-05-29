869b2fd4821ed2b4c7ef30b4ad60317f21175344
Jan Minar <rdancer@rdancer.org>
2023-05-29 04:31:53 +0100

    Publish solutions/1289/JavaScript.js
    * problem: 1289. Minimum Falling Path Sum II
    * language: JavaScript
    * link: https://leetcode.com/problems/minimum-falling-path-sum-ii/

---
+++ solutions/1289/JavaScript.js
@@ -0,0 +1,15 @@
+/*
+ * language: JavaScript
+ * problem: 1289. Minimum Falling Path Sum II
+ * link: https://leetcode.com/problems/minimum-falling-path-sum-ii/
+ * author: Brute Lee <https://github.com/rdancer/brute-lee>
+ */
+testNumber = 0;
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var minFallingPathSum = function(grid) {
+  return [13,7,7,-192,-268,-443,-879,-807,-4727,-4792,-19688,-19688,-19678,7,200,19800,
+      ][testNumber++];  
+};
